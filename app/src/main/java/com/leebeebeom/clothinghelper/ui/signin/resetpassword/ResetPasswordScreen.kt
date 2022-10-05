753dac2197928e2d548e9a70e90b0d95b679a0e9
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-05 10:32:30 +0900

    SignInBox -> SignInColumn 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/resetpassword/ResetPasswordScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/resetpassword/ResetPasswordScreen.kt
@@ -1,12 +1,9 @@
 package com.leebeebeom.clothinghelper.ui.signin.resetpassword
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -17,31 +14,22 @@ import com.leebeebeom.clothinghelper.ui.FirebaseButton
 import com.leebeebeom.clothinghelper.ui.MaxWidthTextField
 import com.leebeebeom.clothinghelper.ui.SimpleHeightSpacer
 import com.leebeebeom.clothinghelper.ui.SimpleToast
-import com.leebeebeom.clothinghelper.ui.signin.signin.SignInBox
+import com.leebeebeom.clothinghelper.ui.signin.signin.SignInColumn
 
 @Composable
 fun ResetPasswordScreen(
     navController: NavController,
     viewModel: ResetPasswordViewModel = viewModel()
 ) {
-    SignInBox(viewModel) {
-        Box(
-            modifier = Modifier
-                .padding(start = 4.dp, top = 344.dp)
-                .align(Alignment.TopStart)
-        ) {
-            Text(
-                text = stringResource(id = R.string.reset_password_text),
-                style = MaterialTheme.typography.body2
-            )
-        }
-        Column {
-            MaxWidthTextField(attr = viewModel.emailTextFieldAttr)
-        }
-        Box(modifier = Modifier.align(Alignment.BottomCenter)) {
-            FirebaseButton(R.string.check, viewModel)
-            SimpleHeightSpacer(dp = 380)
-        }
+    SignInColumn(viewModel) {
+        Text(
+            text = stringResource(id = R.string.reset_password_text),
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier.padding(start = 4.dp)
+        )
+        SimpleHeightSpacer(dp = 4)
+        MaxWidthTextField(attr = viewModel.emailTextFieldAttr)
+        FirebaseButton(R.string.check, viewModel)
     }
     if (viewModel.isFirebaseTaskSuccessful) {
         SimpleToast(resId = R.string.email_send_complete)

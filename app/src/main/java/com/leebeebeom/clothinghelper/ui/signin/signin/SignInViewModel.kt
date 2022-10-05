df7c84a45b4992e0474a04f2ac38f36a1c79db85
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-05 10:35:22 +0900

    로직 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInViewModel.kt
@@ -1,19 +1,17 @@
 package com.leebeebeom.clothinghelper.ui.signin.signin
 
-import com.leebeebeom.clothinghelper.data.OutlinedTextFieldAttr
-import com.leebeebeom.clothinghelper.ui.signin.CHFirebase
+import com.leebeebeom.clothinghelper.data.TextFieldAttr
+import com.leebeebeom.clothinghelper.data.TextFieldManager
+import com.leebeebeom.clothinghelper.ui.signin.FirebaseExecution
 import com.leebeebeom.clothinghelper.ui.signin.CHGoogleSignIn
 import com.leebeebeom.clothinghelper.ui.signin.SignInBaseViewModel
 
 class SignInViewModel : SignInBaseViewModel(), CHGoogleSignIn {
-    override val emailTextFieldAttr = OutlinedTextFieldAttr.email()
-    val passwordTextFieldAttr = OutlinedTextFieldAttr.password()
+    override val emailTextFieldAttr = TextFieldAttr.signInEmail(textFieldManager)
+    override val passwordTextFieldAttr = TextFieldAttr.signInPassword(textFieldManager)
 
-    override val isTextFieldEmpty get() = emailTextFieldAttr.isEmpty || passwordTextFieldAttr.isEmpty
-    override val isErrorEnable get() = emailTextFieldAttr.isErrorEnabled || passwordTextFieldAttr.isErrorEnabled
-
-    override fun firebaseTask(chFirebase: CHFirebase) =
-        chFirebase.signInWithEmailAndPassword(
+    override fun firebaseTask(firebaseExecution: FirebaseExecution) =
+        firebaseExecution.signInWithEmailAndPassword(
             emailTextFieldAttr.text,
             passwordTextFieldAttr.text
         )

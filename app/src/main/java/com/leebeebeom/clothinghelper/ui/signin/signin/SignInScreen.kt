b708ccfd3f396d838c3fa05b05d8abddb3d17451
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-05 10:37:16 +0900

    스크린 업데이트

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInScreen.kt
@@ -2,10 +2,10 @@ package com.leebeebeom.clothinghelper.ui.signin.signin
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,7 +14,6 @@ import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.*
 import com.leebeebeom.clothinghelper.ui.signin.SignInBaseViewModel
@@ -24,39 +23,31 @@ import com.leebeebeom.clothinghelper.ui.theme.DisabledDeep
 @Composable
 fun SignInScreen(navController: NavController, viewModel: SignInViewModel = viewModel()) {
     SignInColumn(viewModel) {
-        SimpleHeightSpacer(dp = 100)
         MaxWidthTextField(attr = viewModel.emailTextFieldAttr)
         MaxWidthTextField(attr = viewModel.passwordTextFieldAttr)
-        SimpleHeightSpacer(dp = 8)
-        Box(modifier = Modifier.fillMaxWidth()) {
-            Text(
-                text = AnnotatedString(stringResource(R.string.forget_password)),
-                style = MaterialTheme.typography.caption,
-                modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .clickable {
-                        navigate(
-                            navController,
-                            SignInNavigationRoute.RESET_PASSWORD
-                        )
-                    }
-            )
-        }
-        SimpleHeightSpacer(dp = 12)
+        ForgotPasswordText(navController)
         FirebaseButton(R.string.login, viewModel)
         OrDivider()
         GoogleSignInBtn(viewModel)
     }
+    SignUpText(navController)
+    if (viewModel.isFirebaseTaskSuccessful) {
+        SimpleToast(resId = R.string.login_complete)
+        FinishActivity()
+    }
+}
+
+@Composable
+private fun SignUpText(navController: NavController) {
     Box(
         modifier = Modifier
-            .fillMaxHeight()
+            .fillMaxSize()
+            .padding(bottom = 32.dp),
+        contentAlignment = Alignment.BottomCenter
     ) {
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 32.dp)
-                .align(Alignment.BottomCenter),
-            horizontalArrangement = Arrangement.Center
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
         ) {
             Text(
                 text = stringResource(R.string.dont_have_an_account),
@@ -72,9 +63,24 @@ fun SignInScreen(navController: NavController, viewModel: SignInViewModel = view
             )
         }
     }
-    if (viewModel.isFirebaseTaskSuccessful) {
-        SimpleToast(resId = R.string.login_complete)
-        FinishActivity()
+}
+
+@Composable
+private fun ForgotPasswordText(navController: NavController) {
+    SimpleHeightSpacer(dp = 8)
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = stringResource(R.string.forget_password),
+            style = MaterialTheme.typography.caption,
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .clickable {
+                    navigate(
+                        navController,
+                        SignInNavigationRoute.RESET_PASSWORD
+                    )
+                }
+        )
     }
 }
 
@@ -108,3 +114,6 @@ fun OrDivider() {
         Weight1Divider(dividerModifier)
     }
 }
+
+@Composable
+fun Weight1Divider(modifier: Modifier = Modifier) = Divider(modifier = modifier)

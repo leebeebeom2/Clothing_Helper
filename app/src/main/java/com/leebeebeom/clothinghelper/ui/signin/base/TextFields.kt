5b7b7b1c0b47f8bede2b76380324a0260a2b3b1c
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-14 03:28:21 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/base/TextFields.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/base/TextFields.kt
@@ -1,49 +1,39 @@
 package com.leebeebeom.clothinghelper.ui.signin.base
 
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.MaxWidthTextField
 import com.leebeebeom.clothinghelper.ui.base.TextFieldUIState
 import com.leebeebeom.clothinghelper.ui.signin.VisibleIcon
 
 @Composable
-fun PasswordConfirmTextField(
-    passwordState: TextFieldUIState,
-    onPasswordChange: (String) -> Unit,
-    visualTransformationToggle: (Boolean) -> Unit
-) {
-    MaxWidthTextField(
-        textFieldUIState = passwordState,
-        onValueChange = onPasswordChange,
-        labelResId = R.string.password_confirm,
-        trailingIcon = { VisibleIcon(visualTransformationToggle = visualTransformationToggle) }
-    )
-}
-
-@Composable
 fun PasswordTextField(
     passwordState: TextFieldUIState,
     onPasswordChange: (String) -> Unit,
-    visualTransformationToggle: (Boolean) -> Unit
+    visualTransformationToggle: (Boolean) -> Unit,
+    @StringRes label: Int = R.string.password
 ) {
-    MaxWidthTextField(
-        textFieldUIState = passwordState,
+    MaxWidthTextField(textFieldState = passwordState,
         onValueChange = onPasswordChange,
-        labelResId = R.string.password,
-        trailingIcon = { VisibleIcon(visualTransformationToggle = visualTransformationToggle) }
-    )
+        label = label,
+        trailingIcon = { VisibleIcon(visualTransformationToggle = visualTransformationToggle) })
 }
 
 @Composable
 fun EmailTextField(
     emailState: TextFieldUIState,
     onEmailChange: (String) -> Unit,
+    showKeyboardEnabled: Boolean = false
 ) {
     MaxWidthTextField(
-        textFieldUIState = emailState,
+        modifier = Modifier,
+        textFieldState = emailState,
         onValueChange = onEmailChange,
-        labelResId = R.string.email,
-        placeHolderResId = R.string.email_place_holder,
+        label = R.string.email,
+        placeHolder = R.string.email_place_holder,
+        showKeyboardEnabled = showKeyboardEnabled
     )
 }
 
@@ -53,8 +43,8 @@ fun NameTextField(
     onNameChange: (String) -> Unit,
 ) {
     MaxWidthTextField(
-        textFieldUIState = nameState,
+        textFieldState = nameState,
         onValueChange = onNameChange,
-        labelResId = R.string.name,
+        label = R.string.name,
     )
 }

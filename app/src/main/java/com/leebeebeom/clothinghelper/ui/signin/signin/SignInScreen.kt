3834fd8f788300883c0b490e554dad2c7062162a
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-14 02:57:50 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInScreen.kt
@@ -1,21 +1,23 @@
 package com.leebeebeom.clothinghelper.ui.signin.signin
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.*
-import com.leebeebeom.clothinghelper.ui.signin.*
+import com.leebeebeom.clothinghelper.ui.ClickableText
+import com.leebeebeom.clothinghelper.ui.FinishActivityOnBackPressed
+import com.leebeebeom.clothinghelper.ui.MaxWidthButton
+import com.leebeebeom.clothinghelper.ui.SimpleHeightSpacer
+import com.leebeebeom.clothinghelper.ui.signin.GoogleSignInButton
+import com.leebeebeom.clothinghelper.ui.signin.OrDivider
 import com.leebeebeom.clothinghelper.ui.signin.base.EmailTextField
 import com.leebeebeom.clothinghelper.ui.signin.base.PasswordTextField
 import com.leebeebeom.clothinghelper.ui.signin.base.SignInBaseRoot
@@ -30,8 +32,8 @@ fun SignInScreen(
 
     SignInBaseRoot(
         isLoading = signInState.isLoading,
-        toastTextId = signInState.toastTextId,
-        toastShown = viewModel::toastShown
+        toastText = signInState.toastText,
+        toastShown = viewModel.toastShown
     ) {
         EmailTextField(
             emailState = signInState.emailState,
@@ -43,26 +45,26 @@ fun SignInScreen(
             onPasswordChange = viewModel.onPasswordChange,
             visualTransformationToggle = viewModel.passwordVisualTransformationToggle
         )
+        SimpleHeightSpacer(dp = 4)
         ForgotPasswordText(onForgotPasswordClick = onForgotPasswordClick)
-        SimpleHeightSpacer(dp = 8)
+        SimpleHeightSpacer(dp = 4)
         MaxWidthButton(
-            text = stringResource(id = R.string.login),
-            enabled = signInState.loginButtonEnable,
-            onClick = { viewModel.signInWithEmailAndPassword() }
+            text = R.string.login,
+            enabled = signInState.loginButtonenabled,
+            onClick = viewModel::signInWithEmailAndPassword
         )
 
-        SimpleHeightSpacer(dp = 12)
+        SimpleHeightSpacer(dp = 8)
         OrDivider()
-        SimpleHeightSpacer(dp = 12)
-        // TODO 나중에 테스트 필요
+        SimpleHeightSpacer(dp = 8)
+        // 프리뷰 시 주석 처리
         GoogleSignInButton(
-            gso = viewModel.getGso(stringResource(id = R.string.default_web_client_id)),
             googleSignIn = viewModel::googleSignIn,
-            googleSignInClick = viewModel::googleSignInLauncherLaunch
+            googleSignInClick = viewModel::googleSignInLauncherLaunch,
+            enabled = signInState.googleButtonEnabled
         )
     }
     SignUpText(onEmailSignUpClick)
-
     FinishActivityOnBackPressed()
 }
 
@@ -80,14 +82,16 @@ private fun SignUpText(onEmailSignUpClick: () -> Unit) {
         ) {
             Text(
                 text = stringResource(R.string.dont_have_an_account),
-                style = MaterialTheme.typography.body2
-            )
-            SimpleWidthSpacer(dp = 6)
-            Text(
-                modifier = Modifier.clickable(onClick = onEmailSignUpClick),
-                text = stringResource(id = R.string.sign_up_with_email),
                 style = MaterialTheme.typography.body2,
-                color = Color(0xFF35C2C1),
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .padding(start = 4.dp)
+            )
+            ClickableText(
+                text = R.string.sign_up_with_email,
+                style = MaterialTheme.typography.body2.copy(
+                    color = Color(0xFF35C2C1), fontWeight = FontWeight.Bold
+                ), onClick = onEmailSignUpClick
             )
         }
     }
@@ -96,14 +100,11 @@ private fun SignUpText(onEmailSignUpClick: () -> Unit) {
 @Composable
 private fun ForgotPasswordText(onForgotPasswordClick: () -> Unit) {
     Box(modifier = Modifier.fillMaxWidth()) {
-        Text(
-            text = stringResource(R.string.forget_password),
+        ClickableText(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            text = R.string.forget_password,
             style = MaterialTheme.typography.caption,
-            modifier = Modifier
-                .align(Alignment.CenterEnd)
-                .clip(RoundedCornerShape(12.dp))
-                .clickable(onClick = onForgotPasswordClick)
-                .padding(4.dp)
+            onClick = onForgotPasswordClick
         )
     }
 }

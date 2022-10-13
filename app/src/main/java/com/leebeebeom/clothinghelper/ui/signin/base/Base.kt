becf404647fd72da9b4a5935d0c8d03c2bbe16d0
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-14 03:28:43 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/base/Base.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/base/Base.kt
@@ -1,49 +1,59 @@
 package com.leebeebeom.clothinghelper.ui.signin.base
 
 import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.leebeebeom.clothinghelper.ui.CenterCircularProgressIndicator
 import com.leebeebeom.clothinghelper.ui.SimpleToast
-import com.leebeebeom.clothinghelper.ui.ThemeRoot
 import com.leebeebeom.clothinghelper.ui.signin.SignInViewModel
+import com.leebeebeom.clothinghelper.ui.theme.ClothingHelperTheme
 
 @Composable
 fun SignInBaseRoot(
     isLoading: Boolean,
-    toastTextId: Int?,
+    @StringRes toastText: Int?,
     toastShown: () -> Unit,
     content: @Composable ColumnScope.() -> Unit
 ) {
+    val focusManager = LocalFocusManager.current
+    val interactionSource = remember { MutableInteractionSource() }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 20.dp),
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 20.dp)
+        .clickable(
+            interactionSource = interactionSource,
+            indication = null,
+        ) { focusManager.clearFocus() }
+        .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center,
-        content = content
-    )
+        content = content)
 
-    toastTextId?.let {
-        SimpleToast(resId = it, shownToast = toastShown)
+    toastText?.let {
+        SimpleToast(text = it, shownToast = toastShown)
     }
-
     if (isLoading) CenterCircularProgressIndicator()
 }
 
 @Composable
 fun SignInActivityRoot(
-    viewModel: SignInViewModel = viewModel(),
-    content: @Composable (PaddingValues) -> Unit
+    viewModel: SignInViewModel = viewModel(), content: @Composable (PaddingValues) -> Unit
 ) {
     if (viewModel.isLogin) (LocalContext.current as ComponentActivity).finish()
 
-    ThemeRoot {
+    ClothingHelperTheme {
         Scaffold(content = content)
     }
 }

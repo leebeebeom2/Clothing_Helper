76a4ce44aeb2faa3224d75d5406ff64e18ebaa88
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-14 03:28:01 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/SignInComponents.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/SignInComponents.kt
@@ -5,38 +5,36 @@ import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.*
-import com.leebeebeom.clothinghelper.ui.theme.Disabled
-import com.leebeebeom.clothinghelper.ui.theme.DisabledDeep
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.leebeebeom.clothinghelper.ui.ClickableIcon
+import com.leebeebeom.clothinghelper.ui.MaxWidthButton
+import com.leebeebeom.clothinghelper.ui.googleLogo
 
 @Composable
 fun VisibleIcon(visualTransformationToggle: (Boolean) -> Unit) {
     var isVisible by rememberSaveable { mutableStateOf(false) }
 
-    IconButton(onClick = {
+    ClickableIcon(
+        drawable = if (isVisible) R.drawable.ic_visibility_off else R.drawable.ic_visibility,
+        contentDescription = "visible icon"
+    ) {
         visualTransformationToggle(isVisible)
         isVisible = !isVisible
-    }) {
-        if (isVisible) SimpleIcon(
-            drawableId = R.drawable.ic_eye_close,
-            contentDescription = "invisible icon"
-        )
-        else SimpleIcon(drawableId = R.drawable.ic_eye_open, contentDescription = "visible icon")
     }
 }
 
@@ -44,58 +42,48 @@ fun VisibleIcon(visualTransformationToggle: (Boolean) -> Unit) {
 fun GoogleSignInButton(
     googleSignInClick: (Intent, ManagedActivityResultLauncher<Intent, *>) -> Unit,
     googleSignIn: (ActivityResult) -> Unit,
-    gso: GoogleSignInOptions
+    enabled: Boolean
 ) {
-    val googleSignInIntent = GoogleSignIn.getClient(LocalContext.current, gso).signInIntent
+    val googleSignInIntent = GoogleSignIn.getClient(LocalContext.current, getGso()).signInIntent
 
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult(),
         onResult = googleSignIn
     )
 
-    var isClicked by rememberSaveable { mutableStateOf(false) }
-    val coroutineScope = rememberCoroutineScope()
-
-    fun clickedToggle() {
-        coroutineScope.launch {
-            isClicked = true
-            delay(3000)
-            isClicked = false
-        }
-    }
-
     MaxWidthButton(
-        text = stringResource(id = R.string.starts_with_google_email),
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.White,
-            disabledBackgroundColor = Disabled
-        ),
-        textColor = Color.Black,
+        text = R.string.starts_with_google_email,
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.surface),
         icon = googleLogo,
-        enabled = !isClicked,
-        onClick = {
-            clickedToggle()
-            googleSignInClick(googleSignInIntent, launcher)
-        })
+        enabled = enabled,
+        onClick = { googleSignInClick(googleSignInIntent, launcher) })
 }
+
+@Composable
+private fun getGso() =
+    GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(stringResource(id = R.string.default_web_client_id))
+        .requestEmail()
+        .build()
+
 
 @Composable
 fun OrDivider() {
     Row(verticalAlignment = Alignment.CenterVertically) {
         val weightModifier = Modifier.weight(1f)
 
-        Weight1Divider(weightModifier)
+        WeightDivider(weightModifier)
         Text(
             text = stringResource(id = R.string.or),
             modifier = Modifier
                 .padding(horizontal = 14.dp)
                 .align(Alignment.CenterVertically),
             style = MaterialTheme.typography.body2,
-            color = DisabledDeep
+            color = LocalContentColor.current.copy(ContentAlpha.medium)
         )
-        Weight1Divider(weightModifier)
+        WeightDivider(weightModifier)
     }
 }
 
 @Composable
-fun Weight1Divider(modifier: Modifier = Modifier) = Divider(modifier = modifier)
+private fun WeightDivider(modifier: Modifier = Modifier) = Divider(modifier = modifier)

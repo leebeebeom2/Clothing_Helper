0c4ad37e65613c925a35cc8f9eae6f03305ff381
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-14 01:55:19 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/GoogleSignInImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/GoogleSignInImpl.kt
@@ -1,20 +1,32 @@
 package com.leebeebeom.clothinghelper.ui.signin
 
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.result.ActivityResult
+import androidx.annotation.StringRes
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.leebeebeom.clothinghelper.R
 
+
 interface GoogleSignInImpl {
-    fun loadingOn()
-    fun loadingOff()
-    fun showToast(resId: Int)
+    /**
+     * loadingOff
+     * googleButtonEnabled true
+     * showToast
+     */
+    fun googleSignInTaskFinished(@StringRes toastText: Int)
+
+    /**
+     * loadingOn
+     * googleButtonEnabled false
+     */
+    fun googleSignInTaskStart()
 
     /*
     런쳐 런치 -> 구글 로그인 액티비티 띄운 후 결과 반환
@@ -24,42 +36,49 @@ interface GoogleSignInImpl {
     gso는 구글 로그인 인텐트를 얻기 위해 필요
     구글 로그인 인텐트는 런쳐를 런치하기 위해 필요
      */
-    fun getGso(webClientId: String) =
-        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(webClientId)
-            .requestEmail()
-            .build()
+    fun googleSignIn(activityResult: ActivityResult) {
+        when (activityResult.resultCode) {
+            RESULT_CANCELED -> {
+                googleSignInTaskFinished(R.string.canceled)
+            }
+            RESULT_OK -> {
+                signInWithCredential(activityResult)
+            }
+            else -> {
+                googleSignInTaskFinished(R.string.unknown_error)
+            }
+        }
+    }
+
+    private fun signInWithCredential(activityResult: ActivityResult) {
+        getGoogleCredential(activityResult.data)?.let { credential ->
+
+            FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) googleSignInTaskFinished(R.string.google_login_complete)
+                    else googleSignInTaskFinished(R.string.google_sign_in_failed)
+                }
+
+        }
+    }
+
+    private fun getGoogleCredential(resultData: Intent?): AuthCredential? {
+        return if (resultData == null) {
+            googleSignInTaskFinished(R.string.google_sign_in_failed)
+            null
+        } else {
+            val account =
+                GoogleSignIn.getSignedInAccountFromIntent(resultData)
+                    .getResult(ApiException::class.java)
+            return GoogleAuthProvider.getCredential(account.idToken, null)
+        }
+    }
 
     fun googleSignInLauncherLaunch(
         googleSingInIntent: Intent,
         launcher: ManagedActivityResultLauncher<Intent, *>
     ) {
-        loadingOn()
-        showToast(R.string.wait_please)
+        googleSignInTaskStart()
         launcher.launch(googleSingInIntent)
-    }
-
-    fun googleSignIn(activityResult: ActivityResult) {
-        val googleCredential = getGoogleCredential(activityResult.data)
-
-        googleCredential?.let { credential ->
-            FirebaseAuth.getInstance().signInWithCredential(credential)
-                .addOnCompleteListener {
-                    if (it.isSuccessful) showToast(R.string.google_login_complete)
-                    else showToast(R.string.google_sign_in_failed)
-                    loadingOff()
-                }
-        }
-    }
-
-    private fun getGoogleCredential(data: Intent?): AuthCredential? {
-        return if (data == null) {
-            showToast(R.string.google_sign_in_failed)
-            null
-        } else {
-            val account =
-                GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException::class.java)
-            return GoogleAuthProvider.getCredential(account.idToken, null)
-        }
     }
 }

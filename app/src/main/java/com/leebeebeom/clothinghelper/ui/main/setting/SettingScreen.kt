6b89819021b7fe13e387cde7756d7faad39986dd
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-14 00:47:29 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/main/setting/SettingScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/main/setting/SettingScreen.kt
@@ -8,8 +8,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.leebeebeom.clothinghelper.R
@@ -27,14 +25,8 @@ fun SettingScreen(viewModel: SettingViewModel = viewModel()) {
 private fun SignOutButton(onSignOutClick: OnClick) {
     MaxWidthButton(
         modifier = Modifier.padding(horizontal = 20.dp, vertical = 20.dp),
-        text = stringResource(id = R.string.sign_out),
+        text = R.string.sign_out,
         colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error),
         onClick = onSignOutClick
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SignOutButtonPreview() {
-    SettingScreen()
 }

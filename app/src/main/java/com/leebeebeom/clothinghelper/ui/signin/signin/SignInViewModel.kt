090371014bf00e8d88439fe61ad44c571f9b62ad
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-17 03:59:27 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInViewModel.kt
@@ -4,92 +4,76 @@ import androidx.annotation.StringRes
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.input.ImeAction
 import com.google.firebase.auth.FirebaseAuth
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.base.TextFieldError
-import com.leebeebeom.clothinghelper.ui.base.TextFieldUIState
 import com.leebeebeom.clothinghelper.ui.signin.FirebaseErrorCode
 import com.leebeebeom.clothinghelper.ui.signin.GoogleSignInImpl
 import com.leebeebeom.clothinghelper.ui.signin.SignInBaseViewModel
 
 class SignInViewModel : SignInBaseViewModel(), GoogleSignInImpl {
-    var signInState by mutableStateOf(SignInUIState())
+    var viewModelState by mutableStateOf(SignInViewModelState())
         private set
 
-    fun signInWithEmailAndPassword() {
+    fun signInWithEmailAndPassword(
+        email: String,
+        password: String,
+        emailErrorEnabled: (Int) -> Unit,
+        passwordErrorEnabled: (Int) -> Unit,
+    ) {
         loadingOn()
 
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(
-            signInState.emailState.text,
-            signInState.passwordState.text
-        ).addOnCompleteListener {
-            if (it.isSuccessful) showToast(R.string.login_complete)
-            else fireBaseErrorCheck(it.exception)
-            loadingOff()
-        }
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) showToast(R.string.login_complete)
+                else fireBaseErrorCheck(it.exception, emailErrorEnabled, passwordErrorEnabled)
+                loadingOff()
+            }
     }
 
-    override fun showToast(@StringRes toastText: Int) {
-        signInState = signInState.showToast(toastText)
-    }
-
-    override fun setError(errorCode: String) {
+    override fun setError(
+        errorCode: String,
+        emailErrorEnabled: (Int) -> Unit,
+        passwordErrorEnabled: (Int) -> Unit
+    ) {
         when (errorCode) {
             FirebaseErrorCode.ERROR_INVALID_EMAIL ->
-                signInState.emailState.error = TextFieldError.ERROR_INVALID_EMAIL
+                emailErrorEnabled(R.string.error_invalid_email)
             FirebaseErrorCode.ERROR_USER_NOT_FOUND ->
-                signInState.emailState.error = TextFieldError.ERROR_USER_NOT_FOUND
+                emailErrorEnabled(R.string.error_user_not_found)
             FirebaseErrorCode.ERROR_WRONG_PASSWORD ->
-                signInState.passwordState.error = TextFieldError.ERROR_WRONG_PASSWORD
+                passwordErrorEnabled(R.string.error_wrong_password)
             else -> showToast(R.string.unknown_error)
         }
     }
 
-    val toastShown = { signInState = signInState.toastShown() }
+    override fun showToast(@StringRes toastText: Int) {
+        viewModelState.toastText = toastText
+    }
+
+    val toastShown = { viewModelState.toastText = null }
 
     private fun loadingOff() {
-        signInState = signInState.loadingOff()
+        viewModelState.isLoading = false
     }
 
     private fun loadingOn() {
-        signInState = signInState.loadingOn()
+        viewModelState.isLoading = true
     }
 
     override fun googleSignInTaskFinished(@StringRes toastText: Int) {
         showToast(toastText)
         loadingOff()
-        setGoogleButtonEnabled(true)
+        viewModelState.googleButtonEnabled = true
     }
 
     override fun googleSignInTaskStart() {
         loadingOn()
-        setGoogleButtonEnabled(false)
-    }
-
-    private fun setGoogleButtonEnabled(enabled: Boolean) {
-        signInState = signInState.setGoogleButtonEnabled(enabled)
+        viewModelState.googleButtonEnabled = false
     }
 }
 
-data class SignInUIState(
-    val emailState: TextFieldUIState = TextFieldUIState.emailState(ImeAction.Next),
-    val passwordState: TextFieldUIState = TextFieldUIState.passwordState(),
-    val isLoading: Boolean = false,
-    @StringRes val toastText: Int? = null,
-    val googleButtonEnabled: Boolean = true
-) {
-    val loginButtonEnabled
-        get() = !emailState.isBlank && !emailState.isError
-                && !passwordState.isBlank && !passwordState.isError
-
-    fun loadingOn() = copy(isLoading = true)
-
-    fun loadingOff() = copy(isLoading = false)
-
-    fun showToast(@StringRes toastText: Int) = copy(toastText = toastText)
-
-    fun toastShown() = copy(toastText = null)
-
-    fun setGoogleButtonEnabled(enabled: Boolean) = copy(googleButtonEnabled = enabled)
+class SignInViewModelState {
+    var isLoading: Boolean by mutableStateOf(false)
+    var toastText: Int? by mutableStateOf(null)
+    var googleButtonEnabled: Boolean by mutableStateOf(true)
 }

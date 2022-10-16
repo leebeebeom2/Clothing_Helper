1eb7848f8a068ad5497aafb5338f383def47d708
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-17 05:18:13 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/SignInActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.leebeebeom.clothinghelper.ui.signin.base.SignInActivityRoot
 import com.leebeebeom.clothinghelper.ui.signin.resetpassword.ResetPasswordScreen
 import com.leebeebeom.clothinghelper.ui.signin.signin.SignInScreen
 import com.leebeebeom.clothinghelper.ui.signin.signup.SignUpScreen
+import com.leebeebeom.clothinghelper.ui.theme.ClothingHelperTheme
 
 class SignInActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,29 +33,26 @@ object SignInDestinations {
 fun SignInNavHost() {
     val navController = rememberNavController()
 
-    fun signNavigate(destination: String) {
-        navController.navigate(destination) {
-            popUpTo(SignInDestinations.SIGN_IN)
+    ClothingHelperTheme {
+        Scaffold {
+            NavHost(
+                navController = navController,
+                startDestination = SignInDestinations.SIGN_IN,
+                modifier = Modifier.padding(it)
+            ) {
+                composable(SignInDestinations.SIGN_IN) {
+                    SignInScreen(
+                        onForgotPasswordClick = { navController.signNavigate(SignInDestinations.RESET_PASSWORD) },
+                        onEmailSignUpClick = { navController.signNavigate(SignInDestinations.SIGN_UP) }
+                    )
+                }
+                composable(SignInDestinations.SIGN_UP) { SignUpScreen() }
+                composable(SignInDestinations.RESET_PASSWORD) {
+                    ResetPasswordScreen()
+                }
+            }
         }
-    }
 
-    SignInActivityRoot {
-        NavHost(
-            navController = navController,
-            startDestination = SignInDestinations.SIGN_IN,
-            modifier = Modifier.padding(it)
-        ) {
-            composable(SignInDestinations.SIGN_IN) {
-                SignInScreen(
-                    onForgotPasswordClick = { signNavigate(SignInDestinations.RESET_PASSWORD) },
-                    onEmailSignUpClick = { signNavigate(SignInDestinations.SIGN_UP) }
-                )
-            }
-            composable(SignInDestinations.SIGN_UP) { SignUpScreen() }
-            composable(SignInDestinations.RESET_PASSWORD) {
-                ResetPasswordScreen()
-            }
-        }
     }
 }
 
@@ -63,3 +62,9 @@ object FirebaseErrorCode {
     const val ERROR_EMAIL_ALREADY_IN_USE = "ERROR_EMAIL_ALREADY_IN_USE"
     const val ERROR_WRONG_PASSWORD = "ERROR_WRONG_PASSWORD"
 }
+
+fun NavController.signNavigate(destination: String) =
+    navigate(destination) {
+        launchSingleTop = true
+        popUpTo(SignInDestinations.SIGN_IN)
+    }

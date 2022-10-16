8585315c11890cc7dc5cfa4d7e925df7f69653da
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-17 05:59:11 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/main/subcategory/SubCategoryViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/main/subcategory/SubCategoryViewModel.kt
@@ -6,39 +6,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
-import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.base.TextFieldError
-import com.leebeebeom.clothinghelper.ui.base.TextFieldUIState
 
 class SubCategoryViewModel : ViewModel() {
-    var subCategoryUIState by mutableStateOf(SubCategoryUIState())
+    var viewModelState by mutableStateOf(SubCategoryViewModelState())
         private set
-
-    val showAddCategoryDialog =
-        { subCategoryUIState = subCategoryUIState.copy(showDialog = true) }
-
-    val onDismissAddCategoryDialog = {
-        subCategoryUIState.categoryName.text = ""
-        subCategoryUIState = subCategoryUIState.copy(showDialog = false)
-    }
-
-    val onNewCategoryNameChange = { newCategoryName: String ->
-        if (subCategoryUIState.subCategories.contains(newCategoryName))
-            subCategoryUIState.categoryName.error = TextFieldError.ERROR_SAME_CATEGORY_NAME
-    }
 }
 
-data class SubCategoryUIState(
-    val showDialog: Boolean = false,
+data class SubCategoryViewModelState(
     val subCategories: SnapshotStateList<String> = getInitialSubCategories(),
-    val categoryName: TextFieldUIState = TextFieldUIState(
-        label = R.string.category,
-        placeHolder = R.string.category_place_holder
-    )
 ) {
-    val positiveButtonEnabled get() = !categoryName.isBlank && !categoryName.isError
-
-    fun addNewCategory() = subCategories.add(categoryName.text)
+    fun addNewCategory(categoryName: String) = subCategories.add(categoryName)
 }
 
 // TODO 삭제

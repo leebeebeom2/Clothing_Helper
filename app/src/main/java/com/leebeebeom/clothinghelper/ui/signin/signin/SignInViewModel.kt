90fdfef1d0c718ccde3d814dd15ac2331163d4f5
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-17 04:26:21 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInViewModel.kt
@@ -3,16 +3,15 @@ package com.leebeebeom.clothinghelper.ui.signin.signin
 import androidx.annotation.StringRes
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import com.google.firebase.auth.FirebaseAuth
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.signin.FirebaseErrorCode
 import com.leebeebeom.clothinghelper.ui.signin.GoogleSignInImpl
 import com.leebeebeom.clothinghelper.ui.signin.SignInBaseViewModel
-import com.leebeebeom.clothinghelper.ui.signin.base.BaseViewModelState
+import com.leebeebeom.clothinghelper.ui.signin.base.GoogleViewModelState
 
 class SignInViewModel : SignInBaseViewModel(), GoogleSignInImpl {
-    val viewModelState by mutableStateOf(SignInViewModelState())
+    val viewModelState by mutableStateOf(GoogleViewModelState())
 
     fun signInWithEmailAndPassword(
         email: String,
@@ -31,17 +30,12 @@ class SignInViewModel : SignInBaseViewModel(), GoogleSignInImpl {
     }
 
     override fun setError(
-        errorCode: String,
-        emailErrorEnabled: (Int) -> Unit,
-        passwordErrorEnabled: (Int) -> Unit
+        errorCode: String, emailErrorEnabled: (Int) -> Unit, passwordErrorEnabled: (Int) -> Unit
     ) {
         when (errorCode) {
-            FirebaseErrorCode.ERROR_INVALID_EMAIL ->
-                emailErrorEnabled(R.string.error_invalid_email)
-            FirebaseErrorCode.ERROR_USER_NOT_FOUND ->
-                emailErrorEnabled(R.string.error_user_not_found)
-            FirebaseErrorCode.ERROR_WRONG_PASSWORD ->
-                passwordErrorEnabled(R.string.error_wrong_password)
+            FirebaseErrorCode.ERROR_INVALID_EMAIL -> emailErrorEnabled(R.string.error_invalid_email)
+            FirebaseErrorCode.ERROR_USER_NOT_FOUND -> emailErrorEnabled(R.string.error_user_not_found)
+            FirebaseErrorCode.ERROR_WRONG_PASSWORD -> passwordErrorEnabled(R.string.error_wrong_password)
             else -> showToast(R.string.unknown_error)
         }
     }
@@ -57,18 +51,5 @@ class SignInViewModel : SignInBaseViewModel(), GoogleSignInImpl {
     override fun googleSignInTaskStart() {
         viewModelState.loadingOn()
         viewModelState.googleButtonDisEnable()
-    }
-}
-
-class SignInViewModelState : BaseViewModelState() {
-    var googleButtonEnabled: Boolean by mutableStateOf(true)
-        private set
-
-    fun googleButtonEnable() {
-        googleButtonEnabled = true
-    }
-
-    fun googleButtonDisEnable() {
-        googleButtonEnabled = false
     }
 }

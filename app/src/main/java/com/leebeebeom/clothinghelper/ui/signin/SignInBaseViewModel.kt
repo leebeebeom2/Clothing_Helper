60a6a90937b7787b5ab77bec337a41857bfe2cd8
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-17 05:19:05 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/SignInBaseViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/SignInBaseViewModel.kt
@@ -7,12 +7,20 @@ import com.leebeebeom.clothinghelper.R
 
 abstract class SignInBaseViewModel : ViewModel() {
     abstract fun showToast(@StringRes toastText: Int)
-    abstract fun setError(errorCode: String)
+    abstract fun setError(
+        errorCode: String,
+        emailErrorEnabled: (Int) -> Unit,
+        passwordErrorEnabled: (Int) -> Unit
+    )
 
-    protected fun fireBaseErrorCheck(exception: Exception?) {
+    protected fun fireBaseErrorCheck(
+        exception: Exception?,
+        setEmailError: (Int) -> Unit,
+        setPasswordError: (Int) -> Unit
+    ) {
         val firebaseException = (exception as? FirebaseAuthException)
 
         if (firebaseException == null) showToast(R.string.unknown_error)
-        else setError(firebaseException.errorCode)
+        else setError(firebaseException.errorCode, setEmailError, setPasswordError)
     }
 }

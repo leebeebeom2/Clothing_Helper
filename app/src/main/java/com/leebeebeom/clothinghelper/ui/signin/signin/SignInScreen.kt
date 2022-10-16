caa8979a50650f6f9919264d88b3c12f828b9729
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-17 04:00:09 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/signin/SignInScreen.kt
@@ -1,25 +1,31 @@
 package com.leebeebeom.clothinghelper.ui.signin.signin
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.FinishActivityOnBackPressed
 import com.leebeebeom.clothinghelper.ui.MaxWidthButton
 import com.leebeebeom.clothinghelper.ui.SimpleHeightSpacer
-import com.leebeebeom.clothinghelper.ui.base.MaxWidthTextField
 import com.leebeebeom.clothinghelper.ui.signin.GoogleSignInButton
 import com.leebeebeom.clothinghelper.ui.signin.OrDivider
-import com.leebeebeom.clothinghelper.ui.signin.VisibleIcon
+import com.leebeebeom.clothinghelper.ui.signin.base.EmailTextField
+import com.leebeebeom.clothinghelper.ui.signin.base.PasswordTextField
+import com.leebeebeom.clothinghelper.ui.signin.base.PasswordUIState
 import com.leebeebeom.clothinghelper.ui.signin.base.SignInBaseRoot
 
 @Composable
@@ -28,27 +34,37 @@ fun SignInScreen(
     onEmailSignUpClick: () -> Unit,
     viewModel: SignInViewModel = viewModel()
 ) {
-    val signInState = viewModel.signInState
+    val state = rememberSignInScreenUIState()
+    val signInState = viewModel.viewModelState
 
     SignInBaseRoot(
         isLoading = signInState.isLoading,
         toastText = signInState.toastText,
         toastShown = viewModel.toastShown
     ) {
-        MaxWidthTextField(
-            state = signInState.emailState,
+        EmailTextField(
+            email = state.email,
+            error = state.emailError,
+            onEmailChange = state::onEmailChange,
+            imeAction = ImeAction.Next
         )
 
-        MaxWidthTextField(
-            state = signInState.passwordState,
-            trailingIcon = { VisibleIcon(signInState.passwordState) }
+        PasswordTextField(
+            password = state.password,
+            onPasswordChange = state::onPasswordChange,
+            error = state.passwordError,
+            imeAction = ImeAction.Done
         )
 
         ForgotPasswordText(onForgotPasswordClick = onForgotPasswordClick)
         MaxWidthButton(
             text = R.string.login,
-            enabled = signInState.loginButtonEnabled,
-            onClick = viewModel::signInWithEmailAndPassword
+            enabled = state.signInButtonEnabled,
+            onClick = {
+                viewModel.signInWithEmailAndPassword(state.email, state.password,
+                    emailErrorEnabled = { state.emailErrorEnabled(it) },
+                    passwordErrorEnabled = { state.passwordErrorEnabled(it) })
+            }
         )
 
         SimpleHeightSpacer(dp = 8)
@@ -108,3 +124,42 @@ private fun ForgotPasswordText(onForgotPasswordClick: () -> Unit) {
         }
     }
 }
+
+class SignInScreenUIState(
+    initialEmail: String = "",
+    @StringRes initialEmailError: Int? = null,
+    initialPassword: String = "",
+    @StringRes initialPasswordError: Int? = null
+) : PasswordUIState(initialEmail, initialEmailError, initialPassword, initialPasswordError) {
+
+    val signInButtonEnabled
+        get() = email.isNotBlank() && emailError == null &&
+                password.isNotBlank() && passwordError == null
+
+    companion object {
+        val Saver: Saver<SignInScreenUIState, *> = listSaver(
+            save = {
+                listOf(
+                    it.email,
+                    it.emailError,
+                    it.password,
+                    it.passwordError
+                )
+            },
+            restore = {
+                SignInScreenUIState(
+                    it[0] as String,
+                    it[1] as? Int,
+                    it[2] as String,
+                    it[3] as? Int
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun rememberSignInScreenUIState() =
+    rememberSaveable(saver = SignInScreenUIState.Saver) {
+        SignInScreenUIState()
+    }

672d8b9be5603a6ad83745bf27fb59a9dd73f218
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-17 04:15:17 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/resetpassword/ResetPasswordScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/resetpassword/ResetPasswordScreen.kt
@@ -1,50 +1,86 @@
 package com.leebeebeom.clothinghelper.ui.signin.resetpassword
 
 import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.MaxWidthButton
 import com.leebeebeom.clothinghelper.ui.SimpleHeightSpacer
-import com.leebeebeom.clothinghelper.ui.base.MaxWidthTextField
+import com.leebeebeom.clothinghelper.ui.base.EmailUIState
+import com.leebeebeom.clothinghelper.ui.signin.base.EmailTextField
 import com.leebeebeom.clothinghelper.ui.signin.base.SignInBaseRoot
 
 @Composable
 fun ResetPasswordScreen(viewModel: ResetPasswordViewModel = viewModel()) {
-    val state = viewModel.state
+    val state = rememberResetScreenUIState()
+    val viewModelState = viewModel.viewModelState
 
-    if (state.goBack) {
+    if (viewModelState.goBack) {
         (LocalContext.current as ComponentActivity).onBackPressedDispatcher.onBackPressed()
-        viewModel.wentBack()
+        viewModelState.wentBack()
     }
 
     SignInBaseRoot(
-        isLoading = state.isLoading,
-        toastText = state.toastText,
-        toastShown = viewModel.toastShown
+        isLoading = viewModelState.isLoading,
+        toastText = viewModelState.toastText,
+        toastShown = viewModelState.toastShown
     ) {
+
         Text(
             text = stringResource(id = R.string.reset_password_text),
             style = MaterialTheme.typography.body2,
             modifier = Modifier.padding(start = 4.dp, bottom = 4.dp)
         )
 
-        MaxWidthTextField(
-            state = state.emailState,
-            showKeyboardEnabled = true
+        EmailTextField(
+            email = state.email,
+            onEmailChange = state::onEmailChange,
+            showKeyboardEnable = true,
+            error = state.emailError,
+            imeAction = ImeAction.Done
         )
         SimpleHeightSpacer(dp = 12)
         MaxWidthButton(
             text = R.string.check,
             enabled = state.submitButtonEnabled,
-            onClick = viewModel::sendResetPasswordEmail
+            onClick = {
+                viewModel.sendResetPasswordEmail(
+                    email = state.email,
+                    emailErrorEnabled = { state.emailErrorEnabled(it) })
+            }
         )
     }
 }
+
+class ResetPasswordScreenUIState(
+    initialEmail: String = "",
+    @StringRes initialEmailError: Int? = null
+) : EmailUIState(initialEmail, initialEmailError) {
+
+    val submitButtonEnabled get() = email.isNotBlank() && emailError == null
+
+    companion object {
+        val Saver: Saver<ResetPasswordScreenUIState, *> = listSaver(
+            save = { listOf(it.email, it.emailError) },
+            restore = { ResetPasswordScreenUIState(it[0] as String, it[1] as? Int) }
+        )
+    }
+}
+
+@Composable
+fun rememberResetScreenUIState() =
+    rememberSaveable(saver = ResetPasswordScreenUIState.Saver) {
+        ResetPasswordScreenUIState()
+    }
--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/resetpassword/ResetPasswordViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/resetpassword/ResetPasswordViewModel.kt
@@ -6,81 +6,47 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import com.google.firebase.auth.FirebaseAuth
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.base.TextFieldError
-import com.leebeebeom.clothinghelper.ui.base.TextFieldUIState
 import com.leebeebeom.clothinghelper.ui.signin.FirebaseErrorCode
 import com.leebeebeom.clothinghelper.ui.signin.SignInBaseViewModel
+import com.leebeebeom.clothinghelper.ui.signin.base.BaseViewModelState
 
 class ResetPasswordViewModel : SignInBaseViewModel() {
-    var state by mutableStateOf(ResetPasswordUIState())
-        private set
+    val viewModelState by mutableStateOf(ResetPasswordViewModelState())
 
-    fun sendResetPasswordEmail() {
-        loadingOn()
+    fun sendResetPasswordEmail(email: String, emailErrorEnabled: (Int) -> Unit) {
+        viewModelState.loadingOn()
 
-        FirebaseAuth.getInstance()
-            .sendPasswordResetEmail(state.emailState.text)
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    showToast(R.string.email_send_complete)
-                    goBack()
-                } else fireBaseErrorCheck(it.exception)
-                loadingOff()
-            }
-    }
-
-    private fun loadingOn() {
-        state = state.loadingOn()
-    }
-
-    private fun loadingOff() {
-        state = state.loadingOff()
-    }
-
-    override fun showToast(@StringRes toastText: Int) {
-        state = state.showToast(toastText)
-    }
-
-    override fun setError(errorCode: String) {
-        when (errorCode) {
-            FirebaseErrorCode.ERROR_INVALID_EMAIL -> state.emailState.error =
-                TextFieldError.ERROR_INVALID_EMAIL
-            FirebaseErrorCode.ERROR_USER_NOT_FOUND -> state.emailState.error =
-                TextFieldError.ERROR_USER_NOT_FOUND
-            else -> showToast(R.string.unknown_error)
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener {
+            if (it.isSuccessful) {
+                showToast(R.string.email_send_complete)
+                viewModelState.goBack()
+            } else fireBaseErrorCheck(it.exception, emailErrorEnabled) {}
+            viewModelState.loadingOff()
         }
     }
 
-    val toastShown = {
-        state = state.toastShown()
-    }
+    override fun showToast(@StringRes toastText: Int) = viewModelState.showToast(toastText)
 
-    private fun goBack() {
-        state = state.goBack()
-    }
-
-    fun wentBack() {
-        state = state.wentBack()
+    override fun setError(
+        errorCode: String, emailErrorEnabled: (Int) -> Unit, passwordErrorEnabled: (Int) -> Unit
+    ) {
+        when (errorCode) {
+            FirebaseErrorCode.ERROR_INVALID_EMAIL -> emailErrorEnabled(R.string.error_invalid_email)
+            FirebaseErrorCode.ERROR_USER_NOT_FOUND -> emailErrorEnabled(R.string.error_user_not_found)
+            else -> showToast(R.string.unknown_error)
+        }
     }
 }
 
-data class ResetPasswordUIState(
-    val isLoading: Boolean = false,
-    @StringRes val toastText: Int? = null,
-    val emailState: TextFieldUIState = TextFieldUIState.emailState(),
-    val goBack: Boolean = false
-) {
-    val submitButtonEnabled get() = !emailState.isBlank && !emailState.isError
+class ResetPasswordViewModelState : BaseViewModelState() {
+    var goBack: Boolean by mutableStateOf(false)
+        private set
 
-    fun loadingOn() = copy(isLoading = true)
+    fun goBack() {
+        goBack = true
+    }
 
-    fun loadingOff() = copy(isLoading = false)
-
-    fun showToast(@StringRes toastText: Int) = copy(toastText = toastText)
-
-    fun toastShown() = copy(toastText = null)
-
-    fun goBack() = copy(goBack = true)
-
-    fun wentBack() = copy(goBack = false)
+    fun wentBack() {
+        goBack = false
+    }
 }

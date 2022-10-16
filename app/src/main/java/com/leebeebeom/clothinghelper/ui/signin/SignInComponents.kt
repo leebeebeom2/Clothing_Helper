f4e0f1bc75db8564bfe4ca9c338a08992dd162b1
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-17 05:19:53 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/SignInComponents.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/SignInComponents.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -19,13 +23,17 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.MaxWidthButton
 import com.leebeebeom.clothinghelper.ui.SimpleIcon
-import com.leebeebeom.clothinghelper.ui.base.TextFieldUIState
 import com.leebeebeom.clothinghelper.ui.googleLogo
 
 @Composable
-fun VisibleIcon(state: TextFieldUIState) {
-    IconButton(onClick = state.visualTransformationToggle) {
-        SimpleIcon(drawable = if (state.isVisible) R.drawable.ic_visibility_off else R.drawable.ic_visibility)
+fun VisibleIcon(onIconClick: () -> Unit) {
+    var isVisible by rememberSaveable { mutableStateOf(false) }
+
+    IconButton(onClick = {
+        onIconClick()
+        isVisible = !isVisible
+    }) {
+        SimpleIcon(drawable = if (isVisible) R.drawable.ic_visibility_off else R.drawable.ic_visibility)
     }
 }
 
@@ -38,12 +46,10 @@ fun GoogleSignInButton(
     val googleSignInIntent = GoogleSignIn.getClient(LocalContext.current, getGso()).signInIntent
 
     val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.StartActivityForResult(),
-        onResult = googleSignIn
+        contract = ActivityResultContracts.StartActivityForResult(), onResult = googleSignIn
     )
 
-    MaxWidthButton(
-        text = R.string.starts_with_google_email,
+    MaxWidthButton(text = R.string.starts_with_google_email,
         colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.surface),
         icon = googleLogo,
         enabled = enabled,
@@ -51,11 +57,8 @@ fun GoogleSignInButton(
 }
 
 @Composable
-private fun getGso() =
-    GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestIdToken(stringResource(id = R.string.default_web_client_id))
-        .requestEmail()
-        .build()
+private fun getGso() = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+    .requestIdToken(stringResource(id = R.string.default_web_client_id)).requestEmail().build()
 
 
 @Composable

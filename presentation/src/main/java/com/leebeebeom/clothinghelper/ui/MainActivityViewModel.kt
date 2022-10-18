1c3bbaaed140ffc2ffeb37a70ec9475683cedf23
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-19 02:17:38 +0900

    리팩토링

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/MainActivityViewModel.kt
@@ -0,0 +1,52 @@
+package com.leebeebeom.clothinghelper.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.leebeebeom.clothinghelper.domain.usecase.user.UserInfoUserCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    private val userInfoUserCase: UserInfoUserCase
+) : ViewModel() {
+
+    var viewModelState = MainViewModelState(
+        userInfoUserCase.isLogin.value,
+        userInfoUserCase.name.value,
+        userInfoUserCase.email.value
+    )
+
+    init {
+        viewModelScope.launch {
+            userInfoUserCase.isLogin.collect(viewModelState::isLogin)
+        }
+        viewModelScope.launch {
+            userInfoUserCase.name.collect(viewModelState::updateName)
+        }
+    }
+}
+
+class MainViewModelState(
+    initialLoginState: Boolean,
+    initialName: String,
+    initialEmail: String,
+) {
+    var isLogin by mutableStateOf(initialLoginState)
+        private set
+    var name by mutableStateOf(initialName)
+        private set
+    val email by mutableStateOf(initialEmail)
+
+    fun isLogin(isLogin: Boolean) {
+        this.isLogin = isLogin
+    }
+
+    fun updateName(name: String) {
+        this.name = name
+    }
+}

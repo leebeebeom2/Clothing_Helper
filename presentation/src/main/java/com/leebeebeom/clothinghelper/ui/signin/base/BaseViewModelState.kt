dae4701a7568d01b4299f0de5613f8d7dcaf1696
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-18 23:01:44 +0900

    리팩토링

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/signin/base/BaseViewModelState.kt
@@ -0,0 +1,44 @@
+package com.leebeebeom.clothinghelper.ui.signin.base
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.leebeebeom.clothinghelper.ui.base.BaseViewModelState
+
+open class BaseSignInViewModelState : BaseViewModelState() {
+    var emailError: Int? by mutableStateOf(null)
+        private set
+
+    fun emailErrorOn(@StringRes error: Int) {
+        emailError = error
+    }
+
+    fun emailErrorOff() {
+        emailError = null
+    }
+}
+
+open class BaseSignInUpViewModelState : BaseSignInViewModelState() {
+    var passwordError: Int? by mutableStateOf(null)
+        private set
+
+    fun passwordErrorOn(@StringRes error: Int) {
+        passwordError = error
+    }
+
+    fun passwordErrorOff() {
+        passwordError = null
+    }
+
+    var googleButtonEnabled by mutableStateOf(true)
+        private set
+
+    fun googleButtonEnable() {
+        googleButtonEnabled = true
+    }
+
+    fun googleButtonDisable() {
+        googleButtonEnabled = false
+    }
+}

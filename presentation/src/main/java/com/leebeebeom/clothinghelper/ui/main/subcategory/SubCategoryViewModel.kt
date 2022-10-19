bc9314adf5c6aa2cc830575b2eff488e151bf96f
leebeebeom <boole92@naver.com>
2022-10-20 02:32:08 +0900

    addSubCategory 함수 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/subcategory/SubCategoryViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/subcategory/SubCategoryViewModel.kt
@@ -3,17 +3,26 @@ package com.leebeebeom.clothinghelper.ui.main.subcategory
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
+import com.leebeebeom.clothinghelper.data.model.SubCategoryParent
+import com.leebeebeom.clothinghelper.domain.usecase.subcategory.AddSubCategoryUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SubCategoryViewModel : ViewModel() {
+@HiltViewModel
+class SubCategoryViewModel @Inject constructor(
+    private val addSubCategoryUseCase: AddSubCategoryUseCase
+) : ViewModel() {
     var viewModelState = SubCategoryViewModelState()
         private set
+
+    fun addSubCategory(name: String) {
+        addSubCategoryUseCase(SubCategoryParent.Top, name) // TODO 패런트 로직 구현
+    }
 }
 
 data class SubCategoryViewModelState(
     val subCategories: SnapshotStateList<String> = getInitialSubCategories(),
-) {
-    fun addNewCategory(categoryName: String) = subCategories.add(categoryName)
-}
+)
 
 // TODO 삭제
 fun getInitialSubCategories() =

72a6658b894585d14144838fcd98cb8a0642079d
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2022-10-19 23:55:12 +0900

    모델 생성

--- presentation/src/main/java/com/leebeebeom/clothinghelper/data/datasource/SubCategoryRemoteDataSource.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/data/datasource/SubCategoryRemoteDataSource.kt
@@ -2,7 +2,7 @@ package com.leebeebeom.clothinghelper.data.datasource
 
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import com.leebeebeom.clothinghelper.data.model.MainCategory
+import com.leebeebeom.clothinghelper.data.model.SubCategoryParent
 import com.leebeebeom.clothinghelper.data.model.SubCategory
 import com.leebeebeom.clothinghelper.data.model.User
 import com.leebeebeom.clothinghelper.domain.usecase.user.UserInfoUserCase
@@ -21,34 +21,34 @@ class SubCategoryRemoteDataSource(private val userInfoUserCase: UserInfoUserCase
         var timeStamp = System.currentTimeMillis()
 
         return listOf(
-            SubCategory(MainCategory.Top, timeStamp++, "반팔"),
-            SubCategory(MainCategory.Top, timeStamp++, "긴팔"),
-            SubCategory(MainCategory.Top, timeStamp++, "셔츠"),
-            SubCategory(MainCategory.Top, timeStamp++, "반팔 셔츠"),
-            SubCategory(MainCategory.Top, timeStamp++, "니트"),
-            SubCategory(MainCategory.Top, timeStamp++, "반팔 니트"),
-            SubCategory(MainCategory.Top, timeStamp++, "니트 베스트"),
-            SubCategory(MainCategory.Bottom, timeStamp++, "데님"),
-            SubCategory(MainCategory.Bottom, timeStamp++, "반바지"),
-            SubCategory(MainCategory.Bottom, timeStamp++, "슬랙스"),
-            SubCategory(MainCategory.Bottom, timeStamp++, "스웻"),
-            SubCategory(MainCategory.Bottom, timeStamp++, "나일론"),
-            SubCategory(MainCategory.Bottom, timeStamp++, "치노"),
-            SubCategory(MainCategory.OUTER, timeStamp++, "코트"),
-            SubCategory(MainCategory.OUTER, timeStamp++, "자켓"),
-            SubCategory(MainCategory.OUTER, timeStamp++, "바람막이"),
-            SubCategory(MainCategory.OUTER, timeStamp++, "항공점퍼"),
-            SubCategory(MainCategory.OUTER, timeStamp++, "블루종"),
-            SubCategory(MainCategory.OUTER, timeStamp++, "점퍼"),
-            SubCategory(MainCategory.OUTER, timeStamp++, "야상"),
-            SubCategory(MainCategory.ETC, timeStamp++, "신발"),
-            SubCategory(MainCategory.ETC, timeStamp++, "목걸이"),
-            SubCategory(MainCategory.ETC, timeStamp++, "팔찌"),
-            SubCategory(MainCategory.ETC, timeStamp++, "귀걸이"),
-            SubCategory(MainCategory.ETC, timeStamp++, "볼캡"),
-            SubCategory(MainCategory.ETC, timeStamp++, "비니"),
-            SubCategory(MainCategory.ETC, timeStamp++, "머플러"),
-            SubCategory(MainCategory.ETC, timeStamp, "장갑")
+            SubCategory(SubCategoryParent.Top, timeStamp++, "반팔"),
+            SubCategory(SubCategoryParent.Top, timeStamp++, "긴팔"),
+            SubCategory(SubCategoryParent.Top, timeStamp++, "셔츠"),
+            SubCategory(SubCategoryParent.Top, timeStamp++, "반팔 셔츠"),
+            SubCategory(SubCategoryParent.Top, timeStamp++, "니트"),
+            SubCategory(SubCategoryParent.Top, timeStamp++, "반팔 니트"),
+            SubCategory(SubCategoryParent.Top, timeStamp++, "니트 베스트"),
+            SubCategory(SubCategoryParent.Bottom, timeStamp++, "데님"),
+            SubCategory(SubCategoryParent.Bottom, timeStamp++, "반바지"),
+            SubCategory(SubCategoryParent.Bottom, timeStamp++, "슬랙스"),
+            SubCategory(SubCategoryParent.Bottom, timeStamp++, "스웻"),
+            SubCategory(SubCategoryParent.Bottom, timeStamp++, "나일론"),
+            SubCategory(SubCategoryParent.Bottom, timeStamp++, "치노"),
+            SubCategory(SubCategoryParent.OUTER, timeStamp++, "코트"),
+            SubCategory(SubCategoryParent.OUTER, timeStamp++, "자켓"),
+            SubCategory(SubCategoryParent.OUTER, timeStamp++, "바람막이"),
+            SubCategory(SubCategoryParent.OUTER, timeStamp++, "항공점퍼"),
+            SubCategory(SubCategoryParent.OUTER, timeStamp++, "블루종"),
+            SubCategory(SubCategoryParent.OUTER, timeStamp++, "점퍼"),
+            SubCategory(SubCategoryParent.OUTER, timeStamp++, "야상"),
+            SubCategory(SubCategoryParent.ETC, timeStamp++, "신발"),
+            SubCategory(SubCategoryParent.ETC, timeStamp++, "목걸이"),
+            SubCategory(SubCategoryParent.ETC, timeStamp++, "팔찌"),
+            SubCategory(SubCategoryParent.ETC, timeStamp++, "귀걸이"),
+            SubCategory(SubCategoryParent.ETC, timeStamp++, "볼캡"),
+            SubCategory(SubCategoryParent.ETC, timeStamp++, "비니"),
+            SubCategory(SubCategoryParent.ETC, timeStamp++, "머플러"),
+            SubCategory(SubCategoryParent.ETC, timeStamp, "장갑")
         )
     }
 }

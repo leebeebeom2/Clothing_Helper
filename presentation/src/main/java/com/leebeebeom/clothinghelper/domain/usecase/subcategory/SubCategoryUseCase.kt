442d319fdc58f466e6a2723ee2752116e0e88fac
leebeebeom <boole92@naver.com>
2022-10-20 03:08:00 +0900

    AddSubCategoryUseCase, GetAllSubCategoryUseCase 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/SubCategoryUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.leebeebeom.clothinghelper.domain.usecase.subcategory
-
-import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
-
-class WriteInitialSubCategoryUseCase(private val subCategoryRepository: SubCategoryRepository) {
-    operator fun invoke() = subCategoryRepository.writeInitialSubCategory()
-}

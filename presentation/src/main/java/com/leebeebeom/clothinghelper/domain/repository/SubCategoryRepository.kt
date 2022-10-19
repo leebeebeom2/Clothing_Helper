785ccd4a3c8e806ecb5a606caf474a235584af01
leebeebeom <boole92@naver.com>
2022-10-20 02:31:35 +0900

    addSubCategory 함수 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
@@ -1,5 +1,8 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
+import com.leebeebeom.clothinghelper.data.model.SubCategoryParent
+
 interface SubCategoryRepository {
     fun writeInitialSubCategory()
+    fun addSubCategory(subCategoryParent:SubCategoryParent, name:String)
 }

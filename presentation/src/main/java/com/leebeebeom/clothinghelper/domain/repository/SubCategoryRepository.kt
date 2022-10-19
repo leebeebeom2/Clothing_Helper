cd0aede4d5807259c6910da826974afd8ff55b01
leebeebeom <boole92@naver.com>
2022-10-20 03:17:13 +0900

    subCategory 리스트 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
@@ -5,7 +5,12 @@ import com.leebeebeom.clothinghelper.data.model.SubCategoryParent
 import kotlinx.coroutines.flow.StateFlow
 
 interface SubCategoryRepository {
+    var allSubCategories: StateFlow<List<SubCategory>>
+    var topSubCategories: StateFlow<List<SubCategory>>
+    var bottomSubCategories: StateFlow<List<SubCategory>>
+    var outerSubCategories: StateFlow<List<SubCategory>>
+    var etcSubCategories: StateFlow<List<SubCategory>>
+
     fun writeInitialSubCategory()
-    fun addSubCategory(subCategoryParent:SubCategoryParent, name:String)
-    fun getAllSubCategory():StateFlow<List<SubCategory>>
+    fun addSubCategory(subCategoryParent: SubCategoryParent, name: String)
 }

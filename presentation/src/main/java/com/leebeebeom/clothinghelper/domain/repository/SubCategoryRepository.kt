b77e0c49cc3943ea7748e99299c48ab872a9c911
leebeebeom <boole92@naver.com>
2022-10-20 03:08:07 +0900

    getAllSubCategory

--- presentation/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
@@ -1,8 +1,11 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
+import com.leebeebeom.clothinghelper.data.model.SubCategory
 import com.leebeebeom.clothinghelper.data.model.SubCategoryParent
+import kotlinx.coroutines.flow.StateFlow
 
 interface SubCategoryRepository {
     fun writeInitialSubCategory()
     fun addSubCategory(subCategoryParent:SubCategoryParent, name:String)
+    fun getAllSubCategory():StateFlow<List<SubCategory>>
 }

dd5a7a62f6f3ae3f90072f474429292c9334837a
leebeebeom <boole92@naver.com>
2022-10-20 03:16:37 +0900

    allSubCategories 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/base/MainScreenRootViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/base/MainScreenRootViewModel.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.leebeebeom.clothinghelper.data.model.SubCategory
+import com.leebeebeom.clothinghelper.domain.usecase.subcategory.GetAllSubCategoryUseCase
 import com.leebeebeom.clothinghelper.domain.usecase.user.UserInfoUserCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,14 +14,21 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainScreenRootViewModel @Inject constructor(
-    private val userInfoUserCase: UserInfoUserCase
+    private val userInfoUserCase: UserInfoUserCase,
+    private val getAllSubCategoryUseCase: GetAllSubCategoryUseCase
 ) : ViewModel() {
     val viewModelState =
         MainNavHostViewModelState(userInfoUserCase.name.value, userInfoUserCase.email.value)
 
+    lateinit var allSubCategory: List<SubCategory>
+        private set
+
     init {
         viewModelScope.launch {
             userInfoUserCase.name.collect { viewModelState.nameUpdate(it) }
+        }
+        viewModelScope.launch {
+            getAllSubCategoryUseCase().collect { allSubCategory = it }
         }
     }
 }

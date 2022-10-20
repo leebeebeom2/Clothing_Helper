3f83ffd5ecf647637ff1482c9fc726fd315dec7a
leebeebeom <boole92@naver.com>
2022-10-21 03:19:49 +0900

    리팩토링

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/SubCategoryRepository.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/SubCategoryRepository.kt
@@ -2,35 +2,30 @@ package com.leebeebeom.clothinghelperdomain.repository
 
 import com.leebeebeom.clothinghelperdomain.model.SubCategory
 import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
-import com.leebeebeom.clothinghelperdomain.model.User
 import kotlinx.coroutines.flow.StateFlow
 
 interface SubCategoryRepository {
-    suspend fun writeInitialSubCategory(user: User)
+    suspend fun writeInitialSubCategory()
 
     fun getTopSubCategories(
-        uid: String, onCancelled: (errorCode: Int, message: String) -> Unit
+        onCancelled: (errorCode: Int, message: String) -> Unit
     ): StateFlow<List<SubCategory>>
 
     fun getBottomSubCategories(
-        uid: String,
         onCancelled: (Int, String) -> Unit
     ): StateFlow<List<SubCategory>>
 
     fun getOuterSubCategories(
-        uid: String,
         onCancelled: (Int, String) -> Unit
     ): StateFlow<List<SubCategory>>
 
     fun getEtcSubCategories(
-        uid: String,
         onCancelled: (Int, String) -> Unit
     ): StateFlow<List<SubCategory>>
 
     suspend fun addSubCategory(
         subCategoryParent: SubCategoryParent,
         name: String,
-        uid: String,
         onSuccess: () -> Unit,
         onFailed: () -> Unit
     )

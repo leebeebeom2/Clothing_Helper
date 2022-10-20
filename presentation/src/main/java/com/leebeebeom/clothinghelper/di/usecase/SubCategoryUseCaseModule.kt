55da1640b95ca4ffe12e93540a2ec1628c55e586
leebeebeom <boole92@naver.com>
2022-10-20 13:55:24 +0900

    코드 정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/di/usecase/SubCategoryUseCaseModule.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/di/usecase/SubCategoryUseCaseModule.kt
@@ -1,9 +1,10 @@
 package com.leebeebeom.clothinghelper.di.usecase
 
-import com.leebeebeom.clothinghelper.data.repository.SubCategoryRepositoryImpl
-import com.leebeebeom.clothinghelper.domain.usecase.subcategory.AddSubCategoryUseCase
-import com.leebeebeom.clothinghelper.domain.usecase.subcategory.GetAllSubCategoryUseCase
-import com.leebeebeom.clothinghelper.domain.usecase.subcategory.WriteInitialSubCategoryUseCase
+import com.leebeebeom.clothinghelperdata.repository.SubCategoryRepositoryImpl
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.AddSubCategoryUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoriesUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.WriteInitialSubCategoryUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.user.UserInfoUserCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,8 +16,10 @@ import javax.inject.Singleton
 class SubCategoryUseCaseModule {
     @Singleton
     @Provides
-    fun provideWriteSubCategoryUseCase(subCategoryRepository: SubCategoryRepositoryImpl) =
-        WriteInitialSubCategoryUseCase(subCategoryRepository)
+    fun provideWriteSubCategoryUseCase(
+        subCategoryRepository: SubCategoryRepositoryImpl,
+        userInfoUserCase: UserInfoUserCase
+    ) = WriteInitialSubCategoryUseCase(subCategoryRepository, userInfoUserCase)
 
     @Singleton
     @Provides
@@ -25,6 +28,6 @@ class SubCategoryUseCaseModule {
 
     @Singleton
     @Provides
-    fun provideGetAllSubCategoryUseCase(subCategoryRepository: SubCategoryRepositoryImpl) =
-        GetAllSubCategoryUseCase(subCategoryRepository)
+    fun provideGetSubCategoriesUseCase(subCategoryRepository: SubCategoryRepositoryImpl) =
+        GetSubCategoriesUseCase(subCategoryRepository)
 }

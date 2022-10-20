0ec89321f2fbae2fee2c441516bb8ee8ba740999
leebeebeom <boole92@naver.com>
2022-10-20 12:51:24 +0900

    로그인 상태 로직 변경

--- presentation/src/main/java/com/leebeebeom/clothinghelper/ui/MainActivity.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/MainActivity.kt
@@ -5,11 +5,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelper.ui.main.base.MainNavHost
 import com.leebeebeom.clothinghelper.ui.signin.SignInNavHost
+import com.leebeebeom.clothinghelperdomain.usecase.user.GetLoginStateUseCase
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 const val TAG = "TAG"
 
@@ -29,4 +38,21 @@ class MainActivity : ComponentActivity() {
 fun MainActivityNavHost(viewModel: MainActivityViewModel = hiltViewModel()) {
     if (viewModel.isLogin) MainNavHost()
     else SignInNavHost()
+}
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    getLoginStateUseCase: GetLoginStateUseCase
+) : ViewModel() {
+
+    var isLogin by mutableStateOf(getLoginStateUseCase.isLogin.value)
+        private set
+
+    init {
+        viewModelScope.launch {
+            getLoginStateUseCase.isLogin.collect {
+                isLogin = it
+            }
+        }
+    }
 }

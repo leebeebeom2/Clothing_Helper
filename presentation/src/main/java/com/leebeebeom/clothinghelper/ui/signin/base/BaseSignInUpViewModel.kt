6e96dd38fbaba4f5c59a28984860669f9f579c07
leebeebeom <boole92@naver.com>
2022-10-20 13:10:09 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/ui/signin/base/BaseSignInUpViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/signin/base/BaseSignInUpViewModel.kt
@@ -1,20 +1,29 @@
 package com.leebeebeom.clothinghelper.ui.signin.base
 
-import android.app.Activity
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
 import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
-import com.google.firebase.auth.AuthResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.domain.repository.FireBaseListeners
-import com.leebeebeom.clothinghelper.domain.usecase.subcategory.WriteInitialSubCategoryUseCase
-import com.leebeebeom.clothinghelper.domain.usecase.user.GoogleSignInUseCase
 import com.leebeebeom.clothinghelper.ui.TAG
+import com.leebeebeom.clothinghelperdomain.repository.FireBaseListeners
+import com.leebeebeom.clothinghelperdomain.usecase.user.GoogleSignInUseCase
+
+object FirebaseErrorCode {
+    const val ERROR_INVALID_EMAIL = "ERROR_INVALID_EMAIL"
+    const val ERROR_USER_NOT_FOUND = "ERROR_USER_NOT_FOUND"
+    const val ERROR_EMAIL_ALREADY_IN_USE = "ERROR_EMAIL_ALREADY_IN_USE"
+    const val ERROR_WRONG_PASSWORD = "ERROR_WRONG_PASSWORD"
+}
 
 abstract class BaseSignInUpViewModel(
-    private val googleSignInUseCase: GoogleSignInUseCase,
-    private val writeInitialSubCategoryUseCase: WriteInitialSubCategoryUseCase
+    private val googleSignInUseCase: GoogleSignInUseCase
 ) : ViewModel() {
     abstract val viewModelState: BaseSignInUpViewModelState
 
@@ -24,20 +33,41 @@ abstract class BaseSignInUpViewModel(
     fun googleButtonDisable() = viewModelState.googleButtonDisable()
     fun googleButtonEnable() = viewModelState.googleButtonEnable()
 
-    fun signInWithGoogleEmail(activityResult: ActivityResult) =
-        googleSignInUseCase(activityResult, googleSignInListener)
+    fun signInWithGoogleEmail(activityResult: ActivityResult) {
+        when (activityResult.resultCode) {
+            RESULT_OK ->
+                googleSignInUseCase(getGoogleCredential(activityResult), googleSignInListener)
+            RESULT_CANCELED -> showToast(R.string.canceled)
+            else -> {
+                showToast(R.string.unknown_error)
+                Log.d(
+                    TAG,
+                    "BaseSignInUpViewModel.signInWithGoogleEmail: resultCode = ${activityResult.resultCode}"
+                )
+            }
+        }
+    }
+
+    private fun getGoogleCredential(activityResult: ActivityResult): AuthCredential? {
+        return if (activityResult.data == null) {
+            showToast(R.string.unknown_error)
+            Log.d(TAG, "BaseSignInUpViewModel.getGoogleCredential: activityResult.data = null")
+            null
+        } else {
+            val account = GoogleSignIn
+                .getSignedInAccountFromIntent(activityResult.data)
+                .getResult(ApiException::class.java)
+            GoogleAuthProvider.getCredential(account.idToken, null)
+        }
+    }
 
     private val googleSignInListener = object : FireBaseListeners.GoogleSignInListener {
-        override fun googleSignInFailed(activityResult: ActivityResult) {
-            if (activityResult.data == null) {
-                showToast(R.string.unknown_error)
-                Log.d(TAG, "googleSignInFailed: activityResult.data = null")
-            } else
-                if (activityResult.resultCode == Activity.RESULT_CANCELED) showToast(R.string.canceled)
-                else {
-                    showToast(R.string.unknown_error)
-                    Log.d(TAG, "googleSignInFailed: $activityResult")
-                }
+        override fun googleCredentialCastFailed() {
+            showToast(R.string.unknown_error)
+            Log.d(
+                TAG,
+                "BaseSignInUpViewModel.googleCredentialCastFailed: AuthCredential cast failed"
+            )
         }
 
         override fun taskStart() {
@@ -45,10 +75,7 @@ abstract class BaseSignInUpViewModel(
             googleButtonDisable()
         }
 
-        override fun taskSuccess(authResult: AuthResult?) {
-            authResult?.additionalUserInfo?.run {
-                if (isNewUser) writeInitialSubCategoryUseCase()
-            }
+        override fun taskSuccess() {
             showToast(R.string.google_login_complete)
         }
 
@@ -61,13 +88,5 @@ abstract class BaseSignInUpViewModel(
             loadingOff()
             googleButtonEnable()
         }
-
     }
-}
-
-object FirebaseErrorCode {
-    const val ERROR_INVALID_EMAIL = "ERROR_INVALID_EMAIL"
-    const val ERROR_USER_NOT_FOUND = "ERROR_USER_NOT_FOUND"
-    const val ERROR_EMAIL_ALREADY_IN_USE = "ERROR_EMAIL_ALREADY_IN_USE"
-    const val ERROR_WRONG_PASSWORD = "ERROR_WRONG_PASSWORD"
 }

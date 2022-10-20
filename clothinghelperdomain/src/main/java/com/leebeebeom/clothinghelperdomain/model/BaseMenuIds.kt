84d3e7a45a2dc5392b4a842f4b4c87e936b309b6
leebeebeom <boole92@naver.com>
2022-10-21 01:44:20 +0900

    리팩토링

---
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/BaseMenuIds.kt
@@ -0,0 +1,23 @@
+package com.leebeebeom.clothinghelperdomain.model
+
+data class EssentialMenu(
+    val id: Int,
+    val name: Int,
+    val drawable: Int,
+)
+
+data class MainCategory(
+    val id: Int,
+    val name: Int
+)
+
+object BaseMenuIds {
+    const val MAIN_SCREEN = 0
+    const val FAVORITE = 1
+    const val SEE_ALL = 2
+    const val TRASH = 3
+    const val TOP = 4
+    const val BOTTOM = 5
+    const val OUTER = 6
+    const val ETC = 7
+}

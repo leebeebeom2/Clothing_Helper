2700455c1aea777a8a7a90e27de05785bc3581e9
leebeebeom <boole92@naver.com>
2022-10-22 02:10:50 +0900

    로직 변경

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryViewModel.kt
@@ -3,24 +3,35 @@ package com.leebeebeom.clothinghelper.main.subcategory
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
+import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.AddSubCategoryUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.user.GetUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SubCategoryViewModel @Inject constructor(
+    private val getUserUseCase: GetUserUseCase,
     private val addSubCategoryUseCase: AddSubCategoryUseCase
 ) : ViewModel() {
     var viewModelState = SubCategoryViewModelState()
         private set
 
-    fun addSubCategory(name: String) =
-        viewModelScope.launch {
-            addSubCategoryUseCase(SubCategoryParent.Top, name, {}, {}) // TODO 패런트 로직 구현
-        }
+    fun addSubCategory(name: String) {
+        val uid = getUserUseCase().value?.uid!! // TODO 수정
+        addSubCategoryUseCase(
+            uid,
+            SubCategoryParent.Top,
+            name,
+            addSubCategoryListener
+        ) // TODO 패런트 로직 구현
+    }
+
+    val addSubCategoryListener = object : FirebaseListener {
+        override fun taskSuccess() {} // TODO 구현
+        override fun taskFailed(exception: Exception?) {} // TODO 구현
+    }
 }
 
 data class SubCategoryViewModelState(

8958d212bbafe476bb31d9d4d3ede4a852169331
leebeebeom <boole92@naver.com>
2022-10-22 01:50:05 +0900

    코루틴 삭제

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/user/UserUseCases.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/user/UserUseCases.kt
@@ -1,62 +1,40 @@
 package com.leebeebeom.clothinghelperdomain.usecase.user
 
-import com.leebeebeom.clothinghelperdomain.repository.FireBaseListeners
+import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener
 import com.leebeebeom.clothinghelperdomain.repository.UserRepository
-import com.leebeebeom.clothinghelperdomain.usecase.subcategory.WriteInitialSubCategoriesUseCase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class GetSignInStateUseCase(private val userRepository: UserRepository) {
-    suspend operator fun invoke() = withContext(Dispatchers.IO) { userRepository.isSignIn() }
+    operator fun invoke() = userRepository.isSignIn()
 }
 
-class GoogleSignInUseCase(
-    private val userRepository: UserRepository,
-    private val writeInitialSubCategoriesUseCase: WriteInitialSubCategoriesUseCase
-) {
-    suspend operator fun invoke(
-        googleCredential: Any?, googleSignInListener: FireBaseListeners.GoogleSignInListener
-    ): Unit = withContext(Dispatchers.IO) {
-        val isFirstUser = userRepository.googleSignIn(googleCredential, googleSignInListener)
-        if (isFirstUser) userRepository.getUser().value?.let { writeInitialSubCategoriesUseCase(it) }
-    }
+class GoogleSignInUseCase(private val userRepository: UserRepository) {
+    operator fun invoke(
+        googleCredential: Any?, googleSignInListener: FirebaseListener
+    ) = userRepository.googleSignIn(googleCredential, googleSignInListener)
 }
 
 class SignInUseCase(private val userRepository: UserRepository) {
-    suspend operator fun invoke(
-        email: String, password: String, signInListener: FireBaseListeners.SignInListener
-    ) = withContext(Dispatchers.IO) { userRepository.signIn(email, password, signInListener) }
+    operator fun invoke(
+        email: String, password: String, signInListener: FirebaseListener
+    ) = userRepository.signIn(email, password, signInListener)
 }
 
-class SignUpUseCase(
-    private val userRepository: UserRepository,
-    private val writeInitialSubCategoriesUseCase: WriteInitialSubCategoriesUseCase
-) {
-    suspend operator fun invoke(
+class SignUpUseCase(private val userRepository: UserRepository) {
+    operator fun invoke(
         email: String,
         password: String,
         name: String,
-        signUpListener: FireBaseListeners.SignUpListener,
-        updateNameListener: FireBaseListeners.UpdateNameListener
-    ): Unit? = withContext(Dispatchers.IO) {
-        userRepository.signUp(email, password, name, signUpListener, updateNameListener)
-        userRepository.getUser().value?.let { writeInitialSubCategoriesUseCase(it) }
-    }
+        signUpListener: FirebaseListener,
+        updateNameListener: FirebaseListener
+    ) = userRepository.signUp(email, password, name, signUpListener, updateNameListener)
 }
 
 class ResetPasswordUseCase(private val userRepository: UserRepository) {
-    suspend operator fun invoke(
-        email: String, resetPasswordListener: FireBaseListeners.ResetPasswordListener
-    ) = withContext(Dispatchers.IO) {
-        userRepository.resetPasswordEmail(
-            email, resetPasswordListener
-        )
-    }
+    operator fun invoke(
+        email: String, resetPasswordListener: FirebaseListener
+    ) = userRepository.resetPasswordEmail(email, resetPasswordListener)
 }
 
 class GetUserUseCase(private val userRepository: UserRepository) {
-    suspend operator fun invoke() = withContext(Dispatchers.IO) {
-        userRepository.getUser()
-    }
-
+    operator fun invoke() = userRepository.getUser()
 }

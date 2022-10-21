9b80997234b20104bd7cf2f39a746e7305c88d73
leebeebeom <boole92@naver.com>
2022-10-22 01:29:29 +0900

    리팩토링

--- clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/UserRepositoryImpl.kt
+++ clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/UserRepositoryImpl.kt
@@ -12,8 +12,7 @@ import com.leebeebeom.clothinghelperdomain.repository.UserRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class UserRepositoryImpl(val subCategoryRepositoryImpl: SubCategoryRepositoryImpl) :
-    UserRepository {
+class UserRepositoryImpl(val subCategoryRepositoryImpl: SubCategoryRepositoryImpl) : UserRepository {
     private val auth = FirebaseAuth.getInstance()
 
     private lateinit var _isSignIn: MutableStateFlow<Boolean>

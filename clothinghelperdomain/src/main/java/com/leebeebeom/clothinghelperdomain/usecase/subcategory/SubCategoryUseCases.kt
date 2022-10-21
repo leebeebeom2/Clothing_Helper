65b501d4c33f8bc6c8dee0e53e3c1e79f7d66058
leebeebeom <boole92@naver.com>
2022-10-22 01:56:14 +0900

    코루틴 삭제

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/subcategory/SubCategoryUseCases.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/subcategory/SubCategoryUseCases.kt
@@ -1,15 +1,8 @@
 package com.leebeebeom.clothinghelperdomain.usecase.subcategory
 
 import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
-import com.leebeebeom.clothinghelperdomain.model.User
+import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener
 import com.leebeebeom.clothinghelperdomain.repository.SubCategoryRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-
-class WriteInitialSubCategoriesUseCase(private val subCategoryRepository: SubCategoryRepository) {
-    suspend operator fun invoke(user: User) =
-        withContext(Dispatchers.IO) { subCategoryRepository.writeInitialSubCategory(user) }
-}
 
 class GetSubCategoriesUserCase(
     private val getTopSubcategoriesUseCase: GetTopSubcategoriesUseCase,
@@ -17,69 +10,54 @@ class GetSubCategoriesUserCase(
     private val getOuterSubcategoriesUseCase: GetOuterSubcategoriesUseCase,
     private val getEtcSubcategoriesUseCase: GetEtcSubcategoriesUseCase
 ) {
-    suspend fun getTopSubCategories(uid: String, onCancelled: (Int, String) -> Unit) =
-        withContext(Dispatchers.IO) {
-            getTopSubcategoriesUseCase.invoke(uid, onCancelled)
-        }
+    fun getTopSubCategories(uid: String, onCancelled: (Int, String) -> Unit) =
+        getTopSubcategoriesUseCase.invoke(uid, onCancelled)
 
-    suspend fun getBottomSubCategories(uid: String, onCancelled: (Int, String) -> Unit) =
-        withContext(Dispatchers.IO) {
-            getBottomSubcategoriesUseCase.invoke(uid, onCancelled)
-        }
+    fun getBottomSubCategories(uid: String, onCancelled: (Int, String) -> Unit) =
+        getBottomSubcategoriesUseCase.invoke(uid, onCancelled)
 
-    suspend fun getOuterSubCategories(uid: String, onCancelled: (Int, String) -> Unit) =
-        withContext(Dispatchers.IO) {
-            getOuterSubcategoriesUseCase.invoke(uid, onCancelled)
+    fun getOuterSubCategories(uid: String, onCancelled: (Int, String) -> Unit) =
+        getOuterSubcategoriesUseCase.invoke(uid, onCancelled)
 
-        }
 
-    suspend fun getEtcSubCategories(uid: String, onCancelled: (Int, String) -> Unit) =
-        withContext(Dispatchers.IO) {
-            getEtcSubcategoriesUseCase.invoke(uid, onCancelled)
-        }
+    fun getEtcSubCategories(uid: String, onCancelled: (Int, String) -> Unit) =
+        getEtcSubcategoriesUseCase.invoke(uid, onCancelled)
 }
 
 class GetTopSubcategoriesUseCase(private val subCategoryRepository: SubCategoryRepository) {
-    suspend operator fun invoke(uid: String, onCancelled: (Int, String) -> Unit) =
-        withContext(Dispatchers.IO)
-        { subCategoryRepository.getTopSubCategories(uid, onCancelled) }
+    operator fun invoke(uid: String, onCancelled: (Int, String) -> Unit) =
+        subCategoryRepository.getTopSubCategories(uid, onCancelled)
 }
 
 class GetBottomSubcategoriesUseCase(private val subCategoryRepository: SubCategoryRepository) {
-    suspend operator fun invoke(uid: String, onCancelled: (Int, String) -> Unit) =
-        withContext(Dispatchers.IO)
-        { subCategoryRepository.getBottomSubCategories(uid, onCancelled) }
+    operator fun invoke(uid: String, onCancelled: (Int, String) -> Unit) =
+        subCategoryRepository.getBottomSubCategories(uid, onCancelled)
 }
 
 class GetOuterSubcategoriesUseCase(private val subCategoryRepository: SubCategoryRepository) {
-    suspend operator fun invoke(uid: String, onCancelled: (Int, String) -> Unit) =
-        withContext(Dispatchers.IO)
-        { subCategoryRepository.getOuterSubCategories(uid, onCancelled) }
+    operator fun invoke(uid: String, onCancelled: (Int, String) -> Unit) =
+        subCategoryRepository.getOuterSubCategories(uid, onCancelled)
 }
 
 class GetEtcSubcategoriesUseCase(private val subCategoryRepository: SubCategoryRepository) {
-    suspend operator fun invoke(uid: String, onCancelled: (Int, String) -> Unit) =
-        withContext(Dispatchers.IO)
-        { subCategoryRepository.getEtcSubCategories(uid, onCancelled) }
+    operator fun invoke(uid: String, onCancelled: (Int, String) -> Unit) =
+        subCategoryRepository.getEtcSubCategories(uid, onCancelled)
 }
 
 
 class AddSubCategoryUseCase(
     private val subCategoryRepository: SubCategoryRepository
 ) {
-    suspend operator fun invoke(
+    operator fun invoke(
         uid: String,
         subCategoryParent: SubCategoryParent,
         name: String,
-        onSuccess: () -> Unit,
-        onFailed: () -> Unit
-    ) = withContext(Dispatchers.IO) {
+        addSubCategoryListener: FirebaseListener
+    ) =
         subCategoryRepository.addSubCategory(
             uid = uid,
             subCategoryParent = subCategoryParent,
             name = name,
-            onSuccess = onSuccess,
-            onFailed = onFailed
+            addSubCategoryListener
         )
-    }
 }

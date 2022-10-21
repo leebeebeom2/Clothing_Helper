9cda277f04fd68e2a9caa4025606ae986eda7b0c
leebeebeom <boole92@naver.com>
2022-10-22 06:11:54 +0900

    기본 인자 삭제

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/user/UserUseCases.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/user/UserUseCases.kt
@@ -1,16 +1,24 @@
 package com.leebeebeom.clothinghelperdomain.usecase.user
 
 import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener
+import com.leebeebeom.clothinghelperdomain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelperdomain.repository.UserRepository
 
 class GetSignInStateUseCase(private val userRepository: UserRepository) {
-    operator fun invoke() = userRepository.isSignIn()
+    operator fun invoke() = userRepository.isSignIn
 }
 
-class GoogleSignInUseCase(private val userRepository: UserRepository) {
+class GoogleSignInUseCase(
+    private val userRepository: UserRepository,
+    private val subCategoryRepository: SubCategoryRepository
+) {
     operator fun invoke(
         googleCredential: Any?, googleSignInListener: FirebaseListener
-    ) = userRepository.googleSignIn(googleCredential, googleSignInListener)
+    ) = userRepository.googleSignIn(
+        googleCredential,
+        googleSignInListener,
+        subCategoryRepository::writeInitialSubCategory
+    )
 }
 
 class SignInUseCase(private val userRepository: UserRepository) {
@@ -19,14 +27,24 @@ class SignInUseCase(private val userRepository: UserRepository) {
     ) = userRepository.signIn(email, password, signInListener)
 }
 
-class SignUpUseCase(private val userRepository: UserRepository) {
+class SignUpUseCase(
+    private val userRepository: UserRepository,
+    private val subCategoryRepository: SubCategoryRepository
+) {
     operator fun invoke(
         email: String,
         password: String,
         name: String,
         signUpListener: FirebaseListener,
         updateNameListener: FirebaseListener
-    ) = userRepository.signUp(email, password, name, signUpListener, updateNameListener)
+    ) = userRepository.signUp(
+        email,
+        password,
+        name,
+        signUpListener,
+        updateNameListener,
+        subCategoryRepository::writeInitialSubCategory
+    )
 }
 
 class ResetPasswordUseCase(private val userRepository: UserRepository) {
@@ -36,7 +54,7 @@ class ResetPasswordUseCase(private val userRepository: UserRepository) {
 }
 
 class GetUserUseCase(private val userRepository: UserRepository) {
-    operator fun invoke() = userRepository.getUser()
+    operator fun invoke() = userRepository.user
 }
 
 class SignOutUseCase(private val userRepository: UserRepository) {

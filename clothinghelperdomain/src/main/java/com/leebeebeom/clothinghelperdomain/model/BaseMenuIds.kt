2e747ae5c5e1b76cf317e6ab20525e9a4140c30e
leebeebeom <boole92@naver.com>
2022-10-22 14:18:46 +0900

    에센셜 메뉴 수정

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/BaseMenuIds.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/BaseMenuIds.kt
@@ -1,20 +1,13 @@
 package com.leebeebeom.clothinghelperdomain.model
 
+enum class EssentialMenus{
+    MAIN_SCREEN, FAVORITE,SEE_ALL,TRASH
+}
+
 data class EssentialMenu(
-    val id: Int,
     val name: Int,
     val drawable: Int,
+    val type: EssentialMenus
 )
 
-data class MainCategory(
-    val id: Int,
-    val name: Int,
-    val type: SubCategoryParent
-)
-
-object BaseMenuIds {
-    const val MAIN_SCREEN = 0
-    const val FAVORITE = 1
-    const val SEE_ALL = 2
-    const val TRASH = 3
-}
+data class MainCategory(val name: Int, val type: SubCategoryParent)

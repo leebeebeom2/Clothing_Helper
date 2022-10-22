c40cb80ec2d4e039bdf0d56db7750d66b199f97d
leebeebeom <boole92@naver.com>
2022-10-22 15:18:32 +0900

    서브 카테고리 스크린 구현

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryViewModel.kt
@@ -1,43 +1,84 @@
 package com.leebeebeom.clothinghelper.main.subcategory
 
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.leebeebeom.clothinghelperdomain.model.SubCategory
 import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
 import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.AddSubCategoryUseCase
-import com.leebeebeom.clothinghelperdomain.usecase.user.GetUserUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoriesUserCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SubCategoryViewModel @Inject constructor(
-    private val getUserUseCase: GetUserUseCase,
-    private val addSubCategoryUseCase: AddSubCategoryUseCase
+    private val addSubCategoryUseCase: AddSubCategoryUseCase,
+    private val getSubCategoriesUserCase: GetSubCategoriesUserCase
 ) : ViewModel() {
     var viewModelState = SubCategoryViewModelState()
         private set
 
-    fun addSubCategory(name: String) {
-        val uid = getUserUseCase().value?.uid!! // TODO 수정
-        addSubCategoryUseCase(
-            uid,
-            SubCategoryParent.Top,
-            name,
-            addSubCategoryListener
-        ) // TODO 패런트 로직 구현
+    init {
+        viewModelScope.launch {
+            getSubCategoriesUserCase.getTopSubCategories()
+                .collect(viewModelState::updateTopSubCategories)
+        }
+        viewModelScope.launch {
+            getSubCategoriesUserCase.getBottomSubCategories()
+                .collect(viewModelState::updateBottomSubCategories)
+        }
+        viewModelScope.launch {
+            getSubCategoriesUserCase.getOuterSubCategories()
+                .collect(viewModelState::updateOuterSubCategories)
+        }
+        viewModelScope.launch {
+            getSubCategoriesUserCase.getEtcSubCategories()
+                .collect(viewModelState::updateEtcSubCategories)
+        }
     }
 
-    val addSubCategoryListener = object : FirebaseListener {
-        override fun taskSuccess() {} // TODO 구현
-        override fun taskFailed(exception: Exception?) {} // TODO 구현
+    fun addSubCategory(parent: SubCategoryParent, name: String) {
+        addSubCategoryUseCase(parent, name, addSubCategoryListener)
+    }
+
+    private val addSubCategoryListener = object : FirebaseListener {
+        override fun taskSuccess() {}
+        override fun taskFailed(exception: Exception?) {} // TODO showToast 구현
     }
 }
 
-data class SubCategoryViewModelState(
-    val subCategories: SnapshotStateList<String> = getInitialSubCategories(),
-)
+class SubCategoryViewModelState {
+    private var topSubCategories by mutableStateOf(emptyList<SubCategory>())
+    private var bottomSubCategories by mutableStateOf(emptyList<SubCategory>())
+    private var outerSubCategories by mutableStateOf(emptyList<SubCategory>())
+    private var etcSubCategories by mutableStateOf(emptyList<SubCategory>())
 
-// TODO 삭제
-fun getInitialSubCategories() =
-    mutableStateListOf("반팔 티셔츠", "긴팔 티셔츠", "셔츠", "반팔 셔츠", "니트", "반팔 니트", "니트 베스트")
+    fun updateTopSubCategories(topSubCategories: List<SubCategory>) {
+        this.topSubCategories = topSubCategories
+    }
+
+    fun updateBottomSubCategories(bottomSubCategories: List<SubCategory>) {
+        this.bottomSubCategories = bottomSubCategories
+    }
+
+    fun updateOuterSubCategories(outerSubCategories: List<SubCategory>) {
+        this.outerSubCategories = outerSubCategories
+    }
+
+    fun updateEtcSubCategories(etcSubCategories: List<SubCategory>) {
+        this.etcSubCategories = etcSubCategories
+    }
+
+    fun getSubCategories(parent: SubCategoryParent): List<SubCategory> {
+        return when (parent) {
+            SubCategoryParent.TOP -> topSubCategories
+            SubCategoryParent.BOTTOM -> bottomSubCategories
+            SubCategoryParent.OUTER -> outerSubCategories
+            SubCategoryParent.ETC -> etcSubCategories
+        }
+    }
+}

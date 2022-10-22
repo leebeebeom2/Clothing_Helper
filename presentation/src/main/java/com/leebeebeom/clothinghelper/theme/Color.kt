02ddd993e68e6482beac419bdce4894a815463ec
leebeebeom <boole92@naver.com>
2022-10-22 23:52:45 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/theme/Color.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/theme/Color.kt
@@ -2,9 +2,6 @@ package com.leebeebeom.clothinghelper.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Disabled = Color(0x50888888)
 val Black = Color.Black

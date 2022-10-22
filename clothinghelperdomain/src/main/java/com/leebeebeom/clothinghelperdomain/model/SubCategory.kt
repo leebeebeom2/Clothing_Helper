94856663fc16b0509f13287c57d561e2d2a58582
leebeebeom <boole92@naver.com>
2022-10-22 14:11:07 +0900

    서브 카테고리 패런트 수장

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/SubCategory.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/SubCategory.kt
@@ -1,14 +1,11 @@
 package com.leebeebeom.clothinghelperdomain.model
 
-object SubCategoryParent {
-    const val TOP = 4
-    const val BOTTOM = 5
-    const val OUTER = 6
-    const val ETC = 7
+enum class SubCategoryParent {
+    TOP, BOTTOM, OUTER, ETC
 }
 
 data class SubCategory(
-    val parentId: Int = SubCategoryParent.TOP,
+    val parentId: SubCategoryParent = SubCategoryParent.TOP,
     val id: Long = 0,
     val name: String = ""
 )

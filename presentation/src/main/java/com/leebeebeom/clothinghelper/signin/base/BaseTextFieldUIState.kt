a2f9a5d5220f27d1b72c3ab5c0ea8386acf5c43c
leebeebeom <boole92@naver.com>
2022-10-24 21:55:02 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/BaseTextFieldUIState.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/BaseTextFieldUIState.kt
@@ -7,9 +7,9 @@ import androidx.compose.runtime.setValue
 open class EmailUIState(email: String = "") {
     var email: String by mutableStateOf(email)
 
-    fun onEmailChange(email: String, errorOff: () -> Unit) {
+    fun onEmailChange(email: String, hideEmailError: () -> Unit) {
         this.email = email
-        errorOff()
+        hideEmailError()
     }
 }
 
@@ -17,8 +17,8 @@ open class PasswordUIState(email: String = "", password: String = "") :
     EmailUIState(email) {
     var password: String by mutableStateOf(password)
 
-    open fun onPasswordChange(password: String, errorOff: () -> Unit) {
+    open fun onPasswordChange(password: String, hidePasswordError: () -> Unit) {
         this.password = password
-        errorOff()
+        hidePasswordError()
     }
 }

08700f8c5d401dfd289bdfcf83c0009e0c57b64e
leebeebeom <boole92@naver.com>
2022-10-24 21:31:21 +0900

    실패 처리

--- clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/PreferencesRepositoryImpl.kt
+++ clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/PreferencesRepositoryImpl.kt
@@ -4,18 +4,26 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import com.leebeebeom.clothinghelperdomain.repository.PreferencesRepository
 import com.leebeebeom.clothinghelperdomain.repository.UserPreferences
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 class PreferencesRepositoryImpl(private val dataStore: DataStore<Preferences>) :
     PreferencesRepository {
 
-    override val preferencesFlow: Flow<UserPreferences> = dataStore.data.map {
-        val allExpand = it[PreferenceKeys.ALL_EXPAND] ?: false
-        UserPreferences(allExpand)
-    }
+    override val preferencesFlow: Flow<UserPreferences> = dataStore.data
+        .catch {
+            if (it is IOException) emit(emptyPreferences())
+            else throw it
+        }
+        .map {
+            val allExpand = it[PreferenceKeys.ALL_EXPAND] ?: false
+            UserPreferences(allExpand)
+        }
 
     override suspend fun toggleAllExpand() {
         dataStore.edit {

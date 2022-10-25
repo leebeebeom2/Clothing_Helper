40970f9c76ea43de88c456ed5d5d00dd845d1764
leebeebeom <boole92@naver.com>
2022-10-26 03:47:25 +0900

    카테고리 컴포넌트 밖으로 이동

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/AddCategoryDialogFab.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/AddCategoryDialogFab.kt
@@ -1,9 +1,9 @@
 package com.leebeebeom.clothinghelper.main.subcategory
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,16 +11,11 @@ import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.MaxWidthTextField
-import com.leebeebeom.clothinghelper.base.SimpleHeightSpacer
-import com.leebeebeom.clothinghelper.base.SimpleIcon
+import com.leebeebeom.clothinghelper.base.*
 import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
 @Composable
@@ -38,93 +33,36 @@ fun AddCategoryDialogFab(
     }
 
     if (state.showDialog) Dialog(onDismissRequest = state::onDismissDialog) {
-        Surface(color = MaterialTheme.colors.surface, shape = RoundedCornerShape(20.dp)) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 20.dp, end = 20.dp, top = 20.dp)
-            ) {
-                Text(
-                    text = stringResource(R.string.add_category_title),
-                    style = MaterialTheme.typography.subtitle1,
-                    modifier = Modifier.padding(start = 4.dp, bottom = 12.dp)
-                )
-                DialogTextField(categoryName = state.categoryName,
-                    error = state.categoryNameError,
-                    onCategoryNameChange = { state.onCategoryNameChange(it, subCategories) })
-                DialogTextButtons(positiveButtonEnabled = state.positiveButtonEnabled,
-                    onCancelButtonClick = state::onDismissDialog,
-                    onPositiveButtonClick = {
-                        onPositiveButtonClick(state.categoryName)
-                        state.onDismissDialog()
-                    })
-                SimpleHeightSpacer(dp = 20)
-            }
-        }
-    }
-}
+        DialogRoot {
+            DialogTitle(title = R.string.add_category_title)
 
-@Composable
-private fun DialogTextButtons(
-    positiveButtonEnabled: Boolean,
-    onCancelButtonClick: () -> Unit,
-    onPositiveButtonClick: () -> Unit
-) {
-    Row {
-        val weightModifier = Modifier.weight(1f)
-        DialogTextButton(
-            modifier = weightModifier,
-            textColor = MaterialTheme.colors.error,
-            text = R.string.cancel,
-            onClick = onCancelButtonClick
-        )
-        DialogTextButton(
-            modifier = weightModifier,
-            text = R.string.check,
-            enabled = positiveButtonEnabled,
-            onClick = onPositiveButtonClick
-        )
-    }
-}
-
-@Composable
-private fun DialogTextButton(
-    modifier: Modifier,
-    @StringRes text: Int,
-    textColor: Color = Color.Unspecified,
-    enabled: Boolean = true,
-    onClick: () -> Unit
-) {
-    Box(modifier = modifier) {
-        TextButton(
-            modifier = Modifier.align(Alignment.Center),
-            onClick = onClick,
-            enabled = enabled
-        ) {
-            Text(
-                text = stringResource(id = text),
-                style = MaterialTheme.typography.subtitle1,
-                color = textColor
+            DialogTextField(categoryName = state.categoryName,
+                error = state.categoryNameError,
+                onCategoryNameChange = { state.onCategoryNameChange(it, subCategories) })
+            DialogTextButtons(
+                positiveButton = {
+                    DialogTextButton(
+                        modifier = it,
+                        text = R.string.check,
+                        enabled = state.positiveButtonEnabled,
+                        onClick = {
+                            onPositiveButtonClick(state.categoryName)
+                            state.onDismissDialog()
+                        }
+                    )
+                },
+                negativeButton = {
+                    DialogTextButton(
+                        modifier = it,
+                        textColor = MaterialTheme.colors.error,
+                        text = R.string.cancel,
+                        onClick = state::onDismissDialog
+                    )
+                }
             )
+            SimpleHeightSpacer(dp = 20)
         }
     }
-}
-
-@Composable
-private fun DialogTextField(
-    categoryName: String,
-    @StringRes error: Int?,
-    onCategoryNameChange: (String) -> Unit,
-) {
-    MaxWidthTextField(
-        label = R.string.category,
-        placeholder = R.string.category_place_holder,
-        text = categoryName,
-        onValueChange = onCategoryNameChange,
-        error = error,
-        showKeyboardEnabled = true
-    )
-    SimpleHeightSpacer(dp = 12)
 }
 
 class AddCategoryDialogUIState(

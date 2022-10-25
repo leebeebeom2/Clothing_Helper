5ca45b928c7d3ba143c497cab6f0c635872f93ef
leebeebeom <boole92@naver.com>
2022-10-26 06:02:07 +0900

    에딧 서브카테고리 네임 다이얼로그 생성

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/EditSubCategoryNameDialog.kt
@@ -0,0 +1,33 @@
+package com.leebeebeom.clothinghelper.main.subcategory
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.base.DialogRoot
+import com.leebeebeom.clothinghelper.base.DialogTextButtons
+import com.leebeebeom.clothinghelper.base.DialogTextField
+import com.leebeebeom.clothinghelper.base.DialogTitle
+
+@Composable
+fun EditSubCategoryNameDialog(
+    categoryName: String,
+    @StringRes error: Int,
+    onCategoryNameChange: (String) -> Unit,
+    onPositiveButtonClick: () -> Unit,
+    onDisMissDialog: () -> Unit,
+    positiveButtonEnabled: Boolean
+) {
+    DialogRoot {
+        DialogTitle(title = R.string.edit_category_name)
+        DialogTextField(
+            categoryName = categoryName,
+            error = error,
+            onCategoryNameChange = onCategoryNameChange
+        )
+        DialogTextButtons(
+            positiveButtonEnabled = positiveButtonEnabled,
+            onPositiveButtonClick = onPositiveButtonClick,
+            onDismissDialog = onDisMissDialog
+        )
+    }
+}

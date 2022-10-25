dfc80a5ed520f6595ab85ea09364fdf2f5c20131
leebeebeom <boole92@naver.com>
2022-10-26 06:18:30 +0900

    베이스 텍스트 필드 다이얼로그 스테이트 생성

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/AddCategoryDialogFab.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/AddCategoryDialogFab.kt
@@ -5,17 +5,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.*
+import com.leebeebeom.clothinghelper.signin.base.BaseSubCategoryTextFieldDialogUIState
 import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
 @Composable
@@ -36,12 +34,12 @@ fun AddCategoryDialogFab(
         DialogRoot {
             DialogTitle(title = R.string.add_category_title)
 
-            DialogTextField(categoryName = state.categoryName,
-                error = state.categoryNameError,
-                onCategoryNameChange = { state.onCategoryNameChange(it, subCategories) })
+            DialogTextField(categoryName = state.text,
+                error = state.error,
+                onCategoryNameChange = { state.onTextChange(it, subCategories) })
             DialogTextButtons(
                 positiveButtonEnabled = state.positiveButtonEnabled,
-                onPositiveButtonClick = { onPositiveButtonClick(state.categoryName) },
+                onPositiveButtonClick = { onPositiveButtonClick(state.text) },
                 onDismissDialog = state::onDismissDialog,
             )
             SimpleHeightSpacer(dp = 20)
@@ -53,37 +51,16 @@ class AddCategoryDialogUIState(
     categoryName: String = "",
     @StringRes categoryNameError: Int? = null,
     showDialog: Boolean = false
-) {
-    var categoryName by mutableStateOf(categoryName)
-        private set
-    var categoryNameError by mutableStateOf(categoryNameError)
-        private set
-    var showDialog by mutableStateOf(showDialog)
-        private set
-
-    fun onCategoryNameChange(newName: String, subCategories: List<SubCategory>) {
-        this.categoryName = newName
-        categoryNameError = null
-        if (subCategories.map { it.name }.contains(newName))
-            categoryNameError = R.string.error_same_category_name
-    }
-
-    val positiveButtonEnabled get() = categoryName.isNotBlank() && categoryNameError == null
+) : BaseSubCategoryTextFieldDialogUIState(categoryName, categoryNameError, showDialog) {
 
     fun showDialog() {
         showDialog = true
     }
 
-    fun onDismissDialog() {
-        showDialog = false
-        categoryName = ""
-        categoryNameError = null
-    }
-
     companion object {
         val Saver: Saver<AddCategoryDialogUIState, *> = listSaver(save = {
             listOf(
-                it.categoryName, it.categoryNameError, it.showDialog
+                it.text, it.error, it.showDialog
             )
         }, restore = {
             AddCategoryDialogUIState(it[0] as String, it[1] as? Int, it[2] as Boolean)

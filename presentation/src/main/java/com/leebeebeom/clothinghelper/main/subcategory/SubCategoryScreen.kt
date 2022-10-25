b075632879fcb501eb45cd95217c7aa1200ad513
leebeebeom <boole92@naver.com>
2022-10-26 05:42:30 +0900

    바텀앱바 밖으로 이동

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryScreen.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryScreen.kt
@@ -1,30 +1,18 @@
 package com.leebeebeom.clothinghelper.main.subcategory
 
 import androidx.activity.compose.BackHandler
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
 import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
 import androidx.compose.animation.graphics.res.animatedVectorResource
 import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
-import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.SimpleHeightSpacer
-import com.leebeebeom.clothinghelper.base.SimpleIcon
 import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
 
 /*
@@ -81,64 +69,6 @@ fun SubCategoryScreen(
     }
 
     BackHandler(enabled = viewModelState.selectMode) { viewModelState.selectModeOff() }
-}
-
-@Composable
-private fun SubCategoryBottomAppBar(
-    isSelectMode: Boolean,
-    selectedSubCategoriesSize: Int,
-    onAllSelectCheckBoxClick: () -> Unit,
-    isAllSelected: Boolean
-) {
-    AnimatedVisibility(
-        visible = isSelectMode,
-        enter = expandVertically(expandFrom = Alignment.Top, animationSpec = tween(250)),
-        exit = shrinkVertically(shrinkTowards = Alignment.Top, animationSpec = tween(250))
-    ) {
-        BottomAppBar {
-
-            IconButton(onClick = onAllSelectCheckBoxClick) {
-                CircleCheckBox(isChecked = isAllSelected, modifier = Modifier.size(20.dp))
-            }
-            Text(
-                text = "${selectedSubCategoriesSize}개 선택됨",
-                modifier = Modifier.offset((-8).dp, 1.dp)
-            )
-            Row(modifier = Modifier.weight(1f), horizontalArrangement = Arrangement.End) {
-                val scale by animateFloatAsState(targetValue = if (selectedSubCategoriesSize == 1) 1f else 0f)
-                BottomAppBarIcon(
-                    modifier = Modifier.scale(scale),
-                    onClick = { /*TODO*/ },
-                    drawable = R.drawable.ic_edit,
-                    text = R.string.change_name
-                )
-                BottomAppBarIcon(
-                    onClick = { /*TODO*/ },
-                    drawable = R.drawable.ic_delete2,
-                    text = R.string.delete
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun BottomAppBarIcon(
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit,
-    @DrawableRes drawable: Int,
-    @StringRes text: Int
-) {
-    IconButton(modifier = modifier, onClick = onClick) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            SimpleIcon(drawable = drawable)
-            SimpleHeightSpacer(dp = 4)
-            Text(
-                text = stringResource(id = text),
-                style = MaterialTheme.typography.caption
-            )
-        }
-    }
 }
 
 @OptIn(ExperimentalAnimationGraphicsApi::class)

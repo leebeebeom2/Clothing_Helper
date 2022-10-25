687a68c6c845060a627a3bea168e92c4109c24c3
leebeebeom <boole92@naver.com>
2022-10-25 21:39:11 +0900

    셀렉트 모드 구현 1

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryScreen.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryScreen.kt
@@ -1,12 +1,13 @@
 package com.leebeebeom.clothinghelper.main.subcategory
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentColor
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -20,6 +21,9 @@ fun SubCategoryScreen(
     getIsSubCategoriesLoading: (SubCategoryParent) -> Boolean
 ) {
     val viewModelState = viewModel.getViewModelState(parentName)
+
+    //TODO 테스트
+    var isSelectMode by remember { mutableStateOf(false) }
 
     Box(modifier = Modifier.fillMaxSize()) {
         if (getIsSubCategoriesLoading(viewModelState.subCategoryParent))
@@ -35,7 +39,8 @@ fun SubCategoryScreen(
             subCategories = viewModelState.getSubCategories(),
             getExpandState = viewModelState::getExpandState,
             toggleExpand = viewModelState::expandToggle,
-            onLongClick = {}
+            onLongClick = { isSelectMode = true },
+            isSelectMode = isSelectMode
         )
 
         AddCategoryDialogFab(
@@ -46,4 +51,6 @@ fun SubCategoryScreen(
             subCategories = viewModelState.getSubCategories()
         )
     }
+
+    BackHandler(enabled = isSelectMode) { isSelectMode = false }
 }

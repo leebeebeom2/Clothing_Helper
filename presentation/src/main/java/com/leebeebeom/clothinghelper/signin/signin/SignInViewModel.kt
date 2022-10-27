d5d470faacadf307d480661841ae4583fc204d07
leebeebeom <boole92@naver.com>
2022-10-28 02:18:23 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signin/SignInViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signin/SignInViewModel.kt
@@ -1,15 +1,12 @@
 package com.leebeebeom.clothinghelper.signin.signin
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import com.google.firebase.auth.FirebaseAuthException
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.TAG
-import com.leebeebeom.clothinghelper.signin.base.FirebaseErrorCode
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInViewModelState
+import com.leebeebeom.clothinghelper.signin.base.setFireBaseError
 import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener
 import com.leebeebeom.clothinghelperdomain.usecase.user.GoogleSignInUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.user.SignInUseCase
@@ -26,38 +23,21 @@ class SignInViewModel @Inject constructor(
 
     fun signInWithEmailAndPassword(email: String, password: String) {
         viewModelState.loadingOn()
-        signInUseCase(email, password, signInListener)
+        signInUseCase(email, password, signInListener) { viewModelState.loadingOff }
     }
 
     private val signInListener = object : FirebaseListener {
         override fun taskSuccess() {
             viewModelState.showToast(R.string.sign_in_complete)
-            viewModelState.loadingOff()
         }
 
         override fun taskFailed(exception: Exception?) {
-            val firebaseAuthException = exception as? FirebaseAuthException
-
-            if (firebaseAuthException == null) {
-                viewModelState.showToast(R.string.unknown_error)
-                Log.d(TAG, "SignInViewModel.taskFailed: firebaseAuthException = null")
-            } else setError(firebaseAuthException.errorCode)
-            viewModelState.loadingOff()
-        }
-    }
-
-    private fun setError(errorCode: String) {
-        when (errorCode) {
-            FirebaseErrorCode.ERROR_INVALID_EMAIL ->
-                viewModelState.showEmailError(R.string.error_invalid_email)
-            FirebaseErrorCode.ERROR_USER_NOT_FOUND ->
-                viewModelState.showEmailError(R.string.error_user_not_found)
-            FirebaseErrorCode.ERROR_WRONG_PASSWORD ->
-                viewModelState.showPasswordError(R.string.error_wrong_password)
-            else -> {
-                viewModelState.showToast(R.string.unknown_error)
-                Log.d(TAG, "SignInViewModel.setError: $errorCode")
-            }
+            setFireBaseError(
+                exception,
+                viewModelState.showEmailError,
+                viewModelState.showPasswordError,
+                viewModelState.showToast
+            )
         }
     }
 }

6604a7c140cd89f91b0704fa0fc4f9feb4daae26
leebeebeom <boole92@naver.com>
2022-10-28 01:16:04 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SignInBaseRoot.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SignInBaseRoot.kt
@@ -2,18 +2,16 @@ package com.leebeebeom.clothinghelper.signin.base
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
-import com.leebeebeom.clothinghelper.base.CenterCircularProgressIndicator
+import com.leebeebeom.clothinghelper.base.CenterDotProgressIndicator
 import com.leebeebeom.clothinghelper.base.SimpleToast
 
 @Composable
@@ -24,24 +22,19 @@ fun SignInBaseRoot(
     content: @Composable ColumnScope.() -> Unit
 ) {
     val focusManager = LocalFocusManager.current
-    val interactionSource = remember { MutableInteractionSource() }
 
     Surface(color = MaterialTheme.colors.background) {
         Column(modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 20.dp)
-            .clickable(
-                interactionSource = interactionSource,
-                indication = null,
-            ) { focusManager.clearFocus() }
+            .clickable { focusManager.clearFocus() } // TODO 인터렉션 소스 삭제 동작 확인
             .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.Center,
             content = content
         )
     }
-    toastText?.let {
-        SimpleToast(text = it, shownToast = toastShown)
-    }
 
-    if (isLoading) CenterCircularProgressIndicator()
+    toastText?.let { SimpleToast(text = it, shownToast = toastShown) }
+
+    if (isLoading) CenterDotProgressIndicator()
 }

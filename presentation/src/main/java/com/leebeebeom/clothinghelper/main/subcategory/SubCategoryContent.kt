cc47459e08b05a026c9dd72c74a956e996ad7280
leebeebeom <boole92@naver.com>
2022-10-28 06:16:50 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryContent.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryContent.kt
@@ -7,23 +7,21 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.SimpleIcon
 import com.leebeebeom.clothinghelperdomain.model.SubCategory
-import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
 
 @Composable
 fun SubCategoryContent(
-    subCategoryParent: SubCategoryParent,
+    headerText: String,
     allExpandIconClick: () -> Unit,
     allExpand: Boolean,
     subCategories: List<SubCategory>,
     onLongClick: () -> Unit,
     isSelectMode: Boolean,
-    onSelect: (SubCategory) -> Unit,
+    onSubCategoryClick: (SubCategory) -> Unit,
     selectedSubCategories: Set<SubCategory>
 ) {
     LazyColumn(
@@ -35,18 +33,18 @@ fun SubCategoryContent(
     ) {
         item {
             Header(
-                subCategoryParent = subCategoryParent,
+                headerText = headerText,
                 allExpandIconClick = allExpandIconClick,
                 allExpand = allExpand
             )
         }
-        this.items(items = subCategories,
+        items(items = subCategories,
             key = { subCategory -> subCategory.key }) {
             SubCategoryCard(
                 subCategory = it,
                 onLongClick = onLongClick,
                 isSelectMode = isSelectMode,
-                onSelect = { onSelect(it) },
+                onSubCategoryClick = onSubCategoryClick,
                 allExpand = allExpand,
                 isChecked = selectedSubCategories.contains(it)
             )
@@ -56,12 +54,11 @@ fun SubCategoryContent(
 
 @Composable
 private fun Header(
-    subCategoryParent: SubCategoryParent, allExpandIconClick: () -> Unit, allExpand: Boolean
+    headerText: String, allExpandIconClick: () -> Unit, allExpand: Boolean
 ) {
-    // Header Text
     Text(
         modifier = Modifier.padding(4.dp),
-        text = stringResource(id = getHeaderTextRes(subCategoryParent)),
+        text = headerText,
         style = MaterialTheme.typography.h2,
         fontSize = 32.sp
     )
@@ -70,16 +67,7 @@ private fun Header(
     Row(verticalAlignment = Alignment.CenterVertically) {
         Divider(modifier = Modifier.weight(1f))
         AllExpandIcon(allExpandIconClick = allExpandIconClick, allExpand)
-        SortIcon {/*TODO*/ }
-    }
-}
-
-private fun getHeaderTextRes(subCategoryParent: SubCategoryParent): Int {
-    return when (subCategoryParent) {
-        SubCategoryParent.TOP -> R.string.top
-        SubCategoryParent.BOTTOM -> R.string.bottom
-        SubCategoryParent.OUTER -> R.string.outer
-        SubCategoryParent.ETC -> R.string.etc
+        SortIcon()
     }
 }
 
@@ -96,8 +84,9 @@ private fun AllExpandIcon(allExpandIconClick: () -> Unit, allExpand: Boolean) {
 }
 
 @Composable
-private fun SortIcon(sortIconClick: () -> Unit) {
-    IconButton(onClick = sortIconClick, modifier = Modifier.size(22.dp)) {
+private fun SortIcon() {
+    // TODO 드랍다운 메뉴
+    IconButton(onClick = {}, modifier = Modifier.size(22.dp)) {
         SimpleIcon(
             drawable = R.drawable.ic_sort, tint = LocalContentColor.current.copy(0.5f)
         )

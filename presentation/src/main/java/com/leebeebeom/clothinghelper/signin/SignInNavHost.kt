df856d569b78e400b60095305c68e9d87335f71c
leebeebeom <boole92@naver.com>
2022-10-28 01:31:20 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/SignInNavHost.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/SignInNavHost.kt
@@ -1,18 +1,10 @@
 package com.leebeebeom.clothinghelper.signin
 
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
-import com.google.accompanist.navigation.animation.composable
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-import com.leebeebeom.clothinghelper.base.navigation.BaseNavHost
-import com.leebeebeom.clothinghelper.base.navigation.NavigateAnime.slideInBottom
-import com.leebeebeom.clothinghelper.base.navigation.NavigateAnime.slideInRight
-import com.leebeebeom.clothinghelper.base.navigation.NavigateAnime.slideOutBottom
-import com.leebeebeom.clothinghelper.base.navigation.NavigateAnime.slideOutRight
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.leebeebeom.clothinghelper.signin.resetpassword.ResetPasswordScreen
 import com.leebeebeom.clothinghelper.signin.signin.SignInScreen
 import com.leebeebeom.clothinghelper.signin.signup.SignUpScreen
@@ -24,38 +16,27 @@ object SignInDestinations {
     const val RESET_PASSWORD = "resetPassword"
 }
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun SignInNavHost() {
-    val navController = rememberAnimatedNavController()
+    val navController = rememberNavController()
 
     ClothingHelperTheme {
-        Scaffold {
-            BaseNavHost(
-                modifier = Modifier.padding(it),
-                navController = navController,
-                startDestination = SignInDestinations.SIGN_IN
-            ) {
-                composable(route = SignInDestinations.SIGN_IN) {
-                    SignInScreen(
-                        onForgotPasswordClick = { navController.signNavigate(SignInDestinations.RESET_PASSWORD) },
-                        onEmailSignUpClick = { navController.signNavigate(SignInDestinations.SIGN_UP) }
-                    )
-                }
-                composable(
-                    route = SignInDestinations.SIGN_UP,
-                    enterTransition = { slideInBottom },
-                    exitTransition = { slideOutBottom }
-                ) { SignUpScreen() }
-                composable(
-                    route = SignInDestinations.RESET_PASSWORD,
-                    enterTransition = { slideInRight },
-                    exitTransition = { slideOutRight }
-                ) { ResetPasswordScreen() }
+        NavHost(
+            navController = navController,
+            startDestination = SignInDestinations.SIGN_IN
+        ) {
+            composable(route = SignInDestinations.SIGN_IN) {
+                SignInScreen(
+                    onForgotPasswordClick = { navController.signNavigate(SignInDestinations.RESET_PASSWORD) },
+                    onEmailSignUpClick = { navController.signNavigate(SignInDestinations.SIGN_UP) }
+                )
             }
+            composable(route = SignInDestinations.SIGN_UP) { SignUpScreen { navController.popBackStack() } }
+            composable(route = SignInDestinations.RESET_PASSWORD) { ResetPasswordScreen { navController.popBackStack() } }
         }
     }
 }
+
 
 fun NavController.signNavigate(destination: String) =
     navigate(destination) {

d24d4e98be49954bbb8e1feb9eeeabbac414e979
leebeebeom <boole92@naver.com>
2022-10-28 01:10:07 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/theme/Theme.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/theme/Theme.kt
@@ -1,45 +1,22 @@
 package com.leebeebeom.clothinghelper.theme
 
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Black,
-    primaryVariant = Black,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
     primary = Black,
     primaryVariant = Black,
     secondary = Teal200,
     background = WhiteSmoke
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
 fun ClothingHelperTheme(
-    darkTheme: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = NanumBarunGothic,
         shapes = Shapes,
         content = content,

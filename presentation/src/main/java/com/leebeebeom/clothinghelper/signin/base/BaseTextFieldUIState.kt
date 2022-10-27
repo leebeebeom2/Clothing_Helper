d91b578c80632ff4cce4367b48013933ba26b7b0
leebeebeom <boole92@naver.com>
2022-10-28 01:50:10 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/BaseTextFieldUIState.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/BaseTextFieldUIState.kt
@@ -12,8 +12,9 @@ open class OneTextFiledState(text: String = "") {
     private val _text = mutableStateOf(text)
     val text get() = _text.value
 
-    fun onTextChange(newText: String, hideTextError: () -> Unit) =
+    val onTextChange = { newText: String, hideTextError: () -> Unit ->
         baseOnTextChange(_text, newText, hideTextError)
+    }
 }
 
 open class TwoTextFiledState(text: String = "", text2: String = "") :
@@ -21,8 +22,9 @@ open class TwoTextFiledState(text: String = "", text2: String = "") :
     private val _text2 = mutableStateOf(text2)
     val text2 get() = _text2.value
 
-    open fun onText2Change(newText2: String, hideText2Error: () -> Unit) =
+    val onText2Change = { newText2: String, hideText2Error: () -> Unit ->
         baseOnTextChange(_text2, newText2, hideText2Error)
+    }
 }
 
 open class FourTextFieldState(
@@ -37,9 +39,11 @@ open class FourTextFieldState(
     private val _text4 = mutableStateOf(text4)
     val text4 get() = _text4.value
 
-    open fun onText3Change(newText3: String, hideText3Error: () -> Unit) =
+    val onText3Change = { newText3: String, hideText3Error: () -> Unit ->
         baseOnTextChange(_text3, newText3, hideText3Error)
+    }
 
-    open fun onText4Change(newText4: String, hideText4Error: () -> Unit) =
+    val onText4Change = { newText4: String, hideText4Error: () -> Unit ->
         baseOnTextChange(_text4, newText4, hideText4Error)
+    }
 }

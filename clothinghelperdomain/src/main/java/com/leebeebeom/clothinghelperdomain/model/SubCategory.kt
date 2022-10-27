74a3483792f738a26d6801498bc67ac918a2e696
leebeebeom <boole92@naver.com>
2022-10-28 04:03:00 +0900

    SubCategoryParentIndex 이동

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/SubCategory.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/SubCategory.kt
@@ -4,6 +4,13 @@ enum class SubCategoryParent {
     TOP, BOTTOM, OUTER, ETC
 }
 
+object SubCategoryParentIndex {
+    const val TOP = 0
+    const val BOTTOM = 1
+    const val OUTER = 2
+    const val ETC = 3
+}
+
 data class SubCategory(
     val parent: SubCategoryParent = SubCategoryParent.TOP,
     val key: String = "",

1a4d1ec4c2b33ced2b504e8d8f1745c4c0004969
leebeebeom <boole92@naver.com>
2022-10-28 01:10:21 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/theme/Type.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/theme/Type.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.leebeebeom.clothinghelper.R
 
-// Set of Material typography styles to start with
 val NanumBarunGothicFontFamily = FontFamily(
     Font(R.font.nanumbarungothicultralight, FontWeight.Thin),
     Font(R.font.nanumbarungothicultralight, FontWeight.ExtraLight),
@@ -89,23 +88,3 @@ val NanumBarunGothic = Typography(
         letterSpacing = 1.5.sp
     )
 )
-
-//val Typography = Typography(
-//    body1 = TextStyle(
-//        fontFamily = FontFamily.Default,
-//        fontWeight = FontWeight.Normal,
-//        fontSize = 16.sp,
-//    )
-/* Other default text styles to override
-button = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.W500,
-    fontSize = 14.sp
-),
-caption = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
-    fontSize = 12.sp
-)
-*/
-//)

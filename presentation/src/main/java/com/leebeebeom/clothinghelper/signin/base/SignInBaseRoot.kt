1830c53ee572e81c2c5f4e07dc1d223d1158e872
leebeebeom <boole92@naver.com>
2022-10-28 01:20:45 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SignInBaseRoot.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SignInBaseRoot.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.base.CenterDotProgressIndicator
 import com.leebeebeom.clothinghelper.base.SimpleToast
+import com.leebeebeom.clothinghelper.theme.ClothingHelperTheme
 
 @Composable
 fun SignInBaseRoot(
@@ -21,14 +23,19 @@ fun SignInBaseRoot(
 ) {
     val focusManager = LocalFocusManager.current
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(horizontal = 20.dp)
-        .clickable { focusManager.clearFocus() } // TODO 인터렉션 소스 삭제 동작 확인
-        .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.Center,
-        content = content
-    )
+    ClothingHelperTheme {
+        Scaffold {
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
+                .padding(it)
+                .clickable { focusManager.clearFocus() } // TODO 인터렉션 소스 삭제 동작 확인
+                .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Center,
+                content = content
+            )
+        }
+    }
 
     toastText?.let { SimpleToast(text = it, shownToast = toastShown) }
 

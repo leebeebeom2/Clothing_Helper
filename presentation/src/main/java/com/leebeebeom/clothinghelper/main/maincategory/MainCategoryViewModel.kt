35483b84114cfa32374643e7f2052cb29ae53d7c
leebeebeom <boole92@naver.com>
2022-10-28 04:10:56 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryViewModel.kt
@@ -1,8 +1,6 @@
 package com.leebeebeom.clothinghelper.main.maincategory
 
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
@@ -13,63 +11,29 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainCategoryViewModel @Inject constructor(
-    private val loadAndGetSubCategoriesUseCase: LoadAndGetSubCategoriesUseCase
+    loadAndGetSubCategoriesUseCase: LoadAndGetSubCategoriesUseCase
 ) : ViewModel() {
 
     val viewModelState = MainCategoryViewModelState()
 
     init {
-        viewModelScope.launch {
-            loadAndGetSubCategoriesUseCase.topSubCategories.collect {
-                viewModelState.topCategoriesSizeUpdate(it.size)
-            }
-        }
-        viewModelScope.launch {
-            loadAndGetSubCategoriesUseCase.bottomSubCategories.collect {
-                viewModelState.bottomCategoriesSizeUpdate(it.size)
-            }
-        }
-        viewModelScope.launch {
-            loadAndGetSubCategoriesUseCase.outerSubCategories.collect {
-                viewModelState.outerCategoriesSizeUpdate(it.size)
-            }
-        }
-        viewModelScope.launch {
-            loadAndGetSubCategoriesUseCase.etcSubCategories.collect {
-                viewModelState.etcCategoriesSizeUpdate(it.size)
+        loadAndGetSubCategoriesUseCase.allSubCategories.forEachIndexed { i, subCategories ->
+            viewModelScope.launch {
+                subCategories.collect {
+                    viewModelState.categoriesSizeUpdate(i, it.size)
+                }
             }
         }
     }
 }
 
 class MainCategoryViewModelState {
-    private var topCategoriesSize by mutableStateOf(0)
-    private var bottomCategoriesSize by mutableStateOf(0)
-    private var outerCategoriesSize by mutableStateOf(0)
-    private var etcCategoriesSize by mutableStateOf(0)
+    private val allSubCategoriesSize = MutableList(4) { mutableStateOf(0) }
 
-    fun topCategoriesSizeUpdate(size: Int) {
-        topCategoriesSize = size
+    fun categoriesSizeUpdate(index: Int, size: Int) {
+        allSubCategoriesSize[index].value = size
     }
 
-    fun bottomCategoriesSizeUpdate(size: Int) {
-        bottomCategoriesSize = size
-    }
-
-    fun outerCategoriesSizeUpdate(size: Int) {
-        outerCategoriesSize = size
-    }
-
-    fun etcCategoriesSizeUpdate(size: Int) {
-        etcCategoriesSize = size
-    }
-
-    fun getSubCategoriesSize(subCategoryParent: SubCategoryParent): Int {
-        return when (subCategoryParent) {
-            SubCategoryParent.TOP -> topCategoriesSize
-            SubCategoryParent.BOTTOM -> bottomCategoriesSize
-            SubCategoryParent.OUTER -> outerCategoriesSize
-            SubCategoryParent.ETC -> etcCategoriesSize
-        }
-    }
+    fun getSubCategoriesSize(subCategoryParent: SubCategoryParent): Int =
+        allSubCategoriesSize[subCategoryParent.ordinal].value
 }

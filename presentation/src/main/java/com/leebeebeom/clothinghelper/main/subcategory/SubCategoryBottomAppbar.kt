b343ccdca6ee7b6479500445f9fc77bfbe0621a4
leebeebeom <boole92@naver.com>
2022-10-28 08:55:30 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryBottomAppbar.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryBottomAppbar.kt
@@ -2,64 +2,81 @@ package com.leebeebeom.clothinghelper.main.subcategory
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.SimpleHeightSpacer
 import com.leebeebeom.clothinghelper.base.SimpleIcon
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun SubCategoryBottomAppBar(
     isSelectMode: Boolean,
     selectedSubCategoriesSize: Int,
+    subCategoriesSize: Int,
     onAllSelectCheckBoxClick: () -> Unit,
-    isAllSelected: Boolean,
     onEditSubCategoryNameClick: () -> Unit
 ) {
+    val duration = 250
+
     AnimatedVisibility(
         visible = isSelectMode,
-        enter = expandVertically(expandFrom = Alignment.Top, animationSpec = tween(250)),
-        exit = shrinkVertically(shrinkTowards = Alignment.Top, animationSpec = tween(250))
+        enter = expandVertically(
+            expandFrom = Alignment.Top,
+            animationSpec = tween(durationMillis = duration)
+        ),
+        exit = shrinkVertically(
+            shrinkTowards = Alignment.Top,
+            animationSpec = tween(durationMillis = duration)
+        )
     ) {
         BottomAppBar {
-
             IconButton(onClick = onAllSelectCheckBoxClick) {
-                CircleCheckBox(isChecked = isAllSelected, modifier = Modifier.size(20.dp))
+                CircleCheckBox(
+                    isChecked = selectedSubCategoriesSize == subCategoriesSize,
+                    modifier = Modifier.size(20.dp)
+                )
             }
             Text(
-                text = "${selectedSubCategoriesSize}개 선택됨",
+                text = stringResource(
+                    id = R.string.selected,
+                    formatArgs = arrayOf(selectedSubCategoriesSize)
+                ),
                 modifier = Modifier.offset((-8).dp, 1.dp)
             )
             Row(modifier = Modifier.weight(1f), horizontalArrangement = Arrangement.End) {
-                val editIconScale by animateFloatAsState(targetValue = if (selectedSubCategoriesSize == 1) 1f else 0f)
-                val deleteIconScale by animateFloatAsState(targetValue = if (selectedSubCategoriesSize > 0) 1f else 0f)
-                BottomAppBarIcon(
-                    modifier = Modifier.scale(editIconScale),
-                    onClick = onEditSubCategoryNameClick,
-                    drawable = R.drawable.ic_edit,
-                    text = R.string.change_name
-                )
-                BottomAppBarIcon(
-                    modifier = Modifier.scale(deleteIconScale),
-                    onClick = { /*TODO*/ },
-                    drawable = R.drawable.ic_delete2,
-                    text = R.string.delete
-                )
+                AnimatedVisibility( // TODO 확인
+                    visible = selectedSubCategoriesSize == 1,
+                    enter = scaleIn(),
+                    exit = scaleOut()
+                ) {
+                    BottomAppBarIcon(
+                        onClick = onEditSubCategoryNameClick,
+                        drawable = R.drawable.ic_edit,
+                        text = R.string.change_name
+                    )
+                }
+                AnimatedVisibility( // TODO 확인
+                    visible = selectedSubCategoriesSize > 0,
+                    enter = scaleIn(),
+                    exit = scaleOut()
+                ) {
+                    BottomAppBarIcon(
+                        onClick = { /*TODO*/ },
+                        drawable = R.drawable.ic_delete2,
+                        text = R.string.delete
+                    )
+                }
             }
         }
     }

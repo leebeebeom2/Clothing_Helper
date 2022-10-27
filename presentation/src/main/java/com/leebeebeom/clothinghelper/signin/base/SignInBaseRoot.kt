9be5d5fae130492e1ae0a656c73f8d1d9b9c8c96
leebeebeom <boole92@naver.com>
2022-10-28 01:17:56 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SignInBaseRoot.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SignInBaseRoot.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
@@ -23,16 +21,14 @@ fun SignInBaseRoot(
 ) {
     val focusManager = LocalFocusManager.current
 
-    Surface(color = MaterialTheme.colors.background) {
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 20.dp)
-            .clickable { focusManager.clearFocus() } // TODO 인터렉션 소스 삭제 동작 확인
-            .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.Center,
-            content = content
-        )
-    }
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 20.dp)
+        .clickable { focusManager.clearFocus() } // TODO 인터렉션 소스 삭제 동작 확인
+        .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        content = content
+    )
 
     toastText?.let { SimpleToast(text = it, shownToast = toastShown) }
 

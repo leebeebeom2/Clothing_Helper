e4480711fa699024b05f24d356a31a252efc2acb
leebeebeom <boole92@naver.com>
2022-10-28 09:46:46 +0900

    EditSubCategoryNameDialog 분리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/EditSubCategoryNameDialog.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/EditSubCategoryNameDialog.kt
@@ -1,36 +1,71 @@
 package com.leebeebeom.clothinghelper.main.subcategory
 
-import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.DialogRoot
-import com.leebeebeom.clothinghelper.base.DialogTextButtons
-import com.leebeebeom.clothinghelper.base.DialogTextField
-import com.leebeebeom.clothinghelper.base.DialogTitle
+import com.leebeebeom.clothinghelper.main.base.SubCategoryTextFieldDialog
+import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
 @Composable
 fun EditSubCategoryNameDialog(
-    categoryName: String,
-    @StringRes error: Int?,
-    onCategoryNameChange: (String) -> Unit,
-    onPositiveButtonClick: () -> Unit,
-    onDismissDialog: () -> Unit,
-    positiveButtonEnabled: Boolean
+    initialCategoryName: String,
+    showDialog: Boolean,
+    subCategories: List<SubCategory>,
+    onPositiveButtonClick: (String) -> Unit
 ) {
     // TODO 커서 포지션(텍스트 셀렉트),  처음 다이얼로그 보일 시 확인 버튼 disable(변경되어야 enable)
-    DialogRoot(onDismissDialog = onDismissDialog) {
-        DialogTitle(text = R.string.edit_category_name)
-        DialogTextField(
-            text = categoryName,
-            error = error,
-            onTextChange = onCategoryNameChange,
-            label = R.string.category,
-            placeHolder = R.string.category_place_holder
-        )
-        DialogTextButtons(
-            positiveButtonEnabled = positiveButtonEnabled,
-            onPositiveButtonClick = onPositiveButtonClick,
-            onDismissDialog = onDismissDialog
+    if (showDialog){
+        val state = rememberEditSubCategoryNameDialogUIState(initialCategoryName)
+
+        SubCategoryTextFieldDialog(
+            categoryName = state.categoryName,
+            error = state.error,
+            onDismissDialog = state.onDismissDialog,
+            title = R.string.edit_category_name,
+            onCategoryNameChange = {
+                state.onCategoryNameChange(
+                    newName = it,
+                    subCategories = subCategories
+                )
+            },
+            positiveButtonEnabled = state.positiveButtonEnabled,
+            onPositiveButtonClick = { onPositiveButtonClick(state.categoryName) }
         )
     }
 }
+
+class EditSubCategoryNameDialogUIState(
+    initialCategoryName: String = "",
+    error: Int? = null,
+    showDialog: Boolean = false // 미사용
+) : BaseSubCategoryTextFieldDialogUIState(initialCategoryName, error, showDialog) {
+
+    override val text: MutableState<String> = mutableStateOf(initialCategoryName)
+
+    companion object {
+        val Saver: Saver<EditSubCategoryNameDialogUIState, *> = listSaver(
+            save = {
+                listOf(
+                    it.categoryName,
+                    it.error,
+                    it.showDialog
+                )
+            },
+            restore = {
+                EditSubCategoryNameDialogUIState(
+                    it[0] as String, it[1] as? Int, it[2] as Boolean
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun rememberEditSubCategoryNameDialogUIState(initialCategoryName: String) =
+    rememberSaveable(saver = EditSubCategoryNameDialogUIState.Saver) {
+        EditSubCategoryNameDialogUIState(initialCategoryName)
+    }

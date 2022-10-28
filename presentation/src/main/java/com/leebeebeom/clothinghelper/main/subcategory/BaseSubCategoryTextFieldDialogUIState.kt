606ec77921f64ff722086376ad07ee2cac820250
leebeebeom <boole92@naver.com>
2022-10-28 09:18:46 +0900

    리팩토링

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/BaseSubCategoryTextFieldDialogUIState.kt
@@ -0,0 +1,37 @@
+package com.leebeebeom.clothinghelper.main.subcategory
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.signin.base.OneTextFiledState
+import com.leebeebeom.clothinghelperdomain.model.SubCategory
+
+open class BaseSubCategoryTextFieldDialogUIState(
+    text: String = "", error: Int? = null, showDialog: Boolean = false
+) : OneTextFiledState(text) {
+    val categoryName get() = text.value
+
+    var error by mutableStateOf(error)
+        protected set
+    var showDialog by mutableStateOf(showDialog)
+        protected set
+
+    private val hideCategoryNameError = { this.error = null }
+
+    private val categoryNameInit = { this.text.value = "" }
+
+    fun onCategoryNameChange(newName: String, subCategories: List<SubCategory>) {
+        super.onTextChange(newName, hideCategoryNameError)
+        if (subCategories.map { it.name }.contains(newName)) error =
+            R.string.error_same_category_name
+    }
+
+    val positiveButtonEnabled get() = categoryName.isNotBlank() && error == null
+
+    fun onDismissDialog() {
+        showDialog = false
+        categoryNameInit()
+        error = null
+    }
+}

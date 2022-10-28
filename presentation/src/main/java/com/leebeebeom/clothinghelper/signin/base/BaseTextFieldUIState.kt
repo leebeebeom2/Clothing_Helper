0046259d30f12de54733746ebac3e760ec08c5c6
leebeebeom <boole92@naver.com>
2022-10-29 00:56:46 +0900

    TextFieldValue로 교체

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/BaseTextFieldUIState.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/BaseTextFieldUIState.kt
@@ -2,25 +2,30 @@ package com.leebeebeom.clothinghelper.signin.base
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.text.input.TextFieldValue
 
-private fun baseOnTextChange(_text: MutableState<String>, text: String, hideTextError: () -> Unit) {
+private fun baseOnTextChange(
+    _text: MutableState<TextFieldValue>,
+    text: TextFieldValue,
+    hideTextError: () -> Unit
+) {
     _text.value = text
     hideTextError()
 }
 
 open class OneTextFiledState(text: String = "") {
-    protected open val text = mutableStateOf(text)
+    protected open val text = mutableStateOf(TextFieldValue(text))
 
-    protected val onTextChange = { newText: String, hideTextError: () -> Unit ->
+    protected val onTextChange = { newText: TextFieldValue, hideTextError: () -> Unit ->
         baseOnTextChange(this.text, newText, hideTextError)
     }
 }
 
 open class TwoTextFiledState(text: String = "", text2: String = "") :
     OneTextFiledState(text) {
-    protected val text2 = mutableStateOf(text2)
+    protected val text2 = mutableStateOf(TextFieldValue(text2))
 
-    protected val onText2Change = { newText2: String, hideText2Error: () -> Unit ->
+    protected val onText2Change = { newText2: TextFieldValue, hideText2Error: () -> Unit ->
         baseOnTextChange(this.text2, newText2, hideText2Error)
     }
 }
@@ -31,15 +36,15 @@ open class FourTextFieldState(
     text3: String = "",
     text4: String = ""
 ) : TwoTextFiledState(text, text2) {
-    protected val text3 = mutableStateOf(text3)
+    protected val text3 = mutableStateOf(TextFieldValue(text3))
 
-    protected val text4 = mutableStateOf(text4)
+    protected val text4 = mutableStateOf(TextFieldValue(text4))
 
-    protected val onText3Change = { newText3: String, hideText3Error: () -> Unit ->
+    protected val onText3Change = { newText3: TextFieldValue, hideText3Error: () -> Unit ->
         baseOnTextChange(this.text3, newText3, hideText3Error)
     }
 
-    protected val onText4Change = { newText4: String, hideText4Error: () -> Unit ->
+    protected val onText4Change = { newText4: TextFieldValue, hideText4Error: () -> Unit ->
         baseOnTextChange(this.text4, newText4, hideText4Error)
     }
 }

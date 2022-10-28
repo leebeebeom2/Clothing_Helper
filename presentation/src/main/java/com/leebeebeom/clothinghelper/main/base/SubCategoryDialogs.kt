a16792efdabd565b098161f4903492943151eeb8
leebeebeom <boole92@naver.com>
2022-10-28 09:26:40 +0900

    생성

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/SubCategoryDialogs.kt
@@ -0,0 +1,36 @@
+package com.leebeebeom.clothinghelper.main.base
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.base.DialogRoot
+import com.leebeebeom.clothinghelper.base.DialogTextButtons
+import com.leebeebeom.clothinghelper.base.DialogTextField
+import com.leebeebeom.clothinghelper.base.DialogTitle
+
+@Composable
+fun SubCategoryTextFieldDialog(
+    onDismissDialog: () -> Unit,
+    @StringRes title: Int,
+    categoryName: String = "",
+    error: Int? = null,
+    onTextChange: (String) -> Unit,
+    positiveButtonEnabled: Boolean,
+    onPositiveButtonClick: () -> Unit
+) {
+    DialogRoot(onDismissDialog = onDismissDialog) {
+        DialogTitle(text = title)
+        DialogTextField(
+            label = R.string.category,
+            placeHolder = R.string.category_place_holder,
+            text = categoryName,
+            error = error,
+            onTextChange = onTextChange
+        )
+        DialogTextButtons(
+            positiveButtonEnabled = positiveButtonEnabled,
+            onPositiveButtonClick = onPositiveButtonClick,
+            onDismissDialog = onDismissDialog
+        )
+    }
+}

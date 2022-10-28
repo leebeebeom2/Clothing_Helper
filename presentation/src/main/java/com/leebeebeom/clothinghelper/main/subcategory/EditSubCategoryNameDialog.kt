640aec5bf31710af027158a453530e136d584a33
leebeebeom <boole92@naver.com>
2022-10-29 02:38:06 +0900

    onFocusChanged 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/EditSubCategoryNameDialog.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/EditSubCategoryNameDialog.kt
@@ -32,7 +32,8 @@ fun EditSubCategoryNameDialog(
             )
         },
         positiveButtonEnabled = state.positiveButtonEnabled,
-        onPositiveButtonClick = { onPositiveButtonClick(state.categoryName.text) }
+        onPositiveButtonClick = { onPositiveButtonClick(state.categoryName.text) },
+        onFocusChanged = {} // TODO 구현
     )
 }
 

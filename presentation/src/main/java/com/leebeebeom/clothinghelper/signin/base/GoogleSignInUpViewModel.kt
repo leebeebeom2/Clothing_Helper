cb1e3378f6b3ba5254582abb2d8b6b69820b1672
leebeebeom <boole92@naver.com>
2022-10-31 01:05:05 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/GoogleSignInUpViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/GoogleSignInUpViewModel.kt
@@ -11,29 +11,29 @@ import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.GoogleAuthProvider
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.TAG
-import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener2
+import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.usecase.user.GoogleSignInUseCase
 
 abstract class GoogleSignInUpViewModel(private val googleSignInUseCase: GoogleSignInUseCase) :
     ViewModel() {
     abstract val viewModelState: GoogleSignInViewModelState
 
-    // call from UI
-    val taskStart = {
-        viewModelState.loadingOn()
-        viewModelState.setGoogleButtonDisable()
-    }
-
     fun signInWithGoogleEmail(activityResult: ActivityResult) {
         when (activityResult.resultCode) {
             RESULT_OK -> googleSignInUseCase(
-                credential = getGoogleCredential(activityResult),
-                listener = listener
-            )
+                credential = getGoogleCredential(activityResult)
+            ) {
+                if (it is FirebaseResult.Success)
+                    viewModelState.showToast(R.string.google_sign_in_complete)
+                else {
+                    viewModelState.showToast(R.string.unknown_error)
+                    viewModelState.updateGoogleButtonEnabled(true)
+                    Log.d(TAG, "taskFailed: ${(it as FirebaseResult.Fail).exception}")
+                }
+            }
             RESULT_CANCELED -> {
                 viewModelState.showToast(R.string.canceled)
-                listener.taskFailed(null)
-                listener.taskFinish()
+                viewModelState.updateGoogleButtonEnabled(false)
             }
             else -> {
                 viewModelState.showToast(R.string.unknown_error)
@@ -41,8 +41,7 @@ abstract class GoogleSignInUpViewModel(private val googleSignInUseCase: GoogleSi
                     TAG,
                     "GoogleSignInUpViewModel.signInWithGoogleEmail: resultCode = ${activityResult.resultCode}"
                 )
-                listener.taskFailed(null)
-                listener.taskFinish()
+                viewModelState.updateGoogleButtonEnabled(false)
             }
         }
     }
@@ -57,22 +56,6 @@ abstract class GoogleSignInUpViewModel(private val googleSignInUseCase: GoogleSi
                 .getSignedInAccountFromIntent(activityResult.data)
                 .getResult(ApiException::class.java)
             GoogleAuthProvider.getCredential(account.idToken, null)
-        }
-    }
-
-    private val listener = object : FirebaseListener2 {
-        override fun taskSuccess() {
-            viewModelState.showToast(R.string.google_sign_in_complete)
-        }
-
-        override fun taskFailed(exception: Exception?) {
-            viewModelState.showToast(R.string.unknown_error)
-            viewModelState.setGoogleButtonEnable()
-            Log.d(TAG, "taskFailed: $exception")
-        }
-
-        override fun taskFinish() {
-            viewModelState.loadingOff()
         }
     }
 }

a1ff324f0b5dfaf204ae668099bf54d47ff3d8b5
leebeebeom <boole92@naver.com>
2022-10-31 03:57:57 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signup/SignUpViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signup/SignUpViewModel.kt
@@ -1,12 +1,17 @@
 package com.leebeebeom.clothinghelper.signin.signup
 
 import android.util.Log
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.TAG
+import com.leebeebeom.clothinghelper.base.MaxWidthTextFieldState
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInViewModelState
 import com.leebeebeom.clothinghelper.signin.base.setFireBaseError
-import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener2
+import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.usecase.user.GoogleSignInUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.user.SignUpUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,38 +21,79 @@ import javax.inject.Inject
 class SignUpViewModel @Inject constructor(
     private val signUpUseCase: SignUpUseCase, googleSignInUseCase: GoogleSignInUseCase
 ) : GoogleSignInUpViewModel(googleSignInUseCase) {
-    override val viewModelState = GoogleSignInViewModelState()
+    override val viewModelState = SignUpViewModelState()
 
-    fun signUpWithEmailAndPassword(email: String, password: String, name: String) {
-        viewModelState.loadingOn()
+    fun signUpWithEmailAndPassword() {
         signUpUseCase(
-            email = email,
-            password = password,
-            name = name,
-            signUpListener = signUpListener,
-            updateNameListener = updateNameListener
-        )
-    }
-
-    private val signUpListener = object : FirebaseListener2 {
-        override fun taskSuccess() = viewModelState.showToast(R.string.sign_up_complete)
-        override fun taskFailed(exception: Exception?) = setFireBaseError(
-            exception = exception,
-            showEmailError = viewModelState.showEmailError,
-            showPasswordError = {},
-            showToast = viewModelState::showToast
-        )
-
-        override fun taskFinish() = viewModelState.loadingOff()
-    }
-
-    private val updateNameListener = object : FirebaseListener2 {
-        override fun taskFailed(exception: Exception?) {
-            viewModelState.showToast(R.string.name_update_failed)
-            Log.d(TAG, "taskFailed: $exception")
+            email = viewModelState.email,
+            password = viewModelState.password,
+            name = viewModelState.name,
+            onSignUpDone = {
+                when (it) {
+                    is FirebaseResult.Success -> viewModelState.showToast(R.string.sign_up_complete)
+                    is FirebaseResult.Fail -> {
+                        setFireBaseError(
+                            exception = it.exception,
+                            updateEmailError = viewModelState.emailState::updateError,
+                            updatePasswordError = {},
+                            showToast = viewModelState::showToast
+                        )
+                    }
+                }
+            }
+        ) {
+            if (it is FirebaseResult.Fail) {
+                viewModelState.showToast(R.string.name_update_failed)
+                Log.d(TAG, "taskFailed: $it.exception")
+            }
         }
+    }
+}
 
-        override fun taskFinish() {}
-        override fun taskSuccess() {}
+class SignUpViewModelState : GoogleSignInViewModelState() {
+    val emailState = MaxWidthTextFieldState.email(ImeAction.Next)
+    val nameState = MaxWidthTextFieldState(
+        label = R.string.name, keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+    )
+    val passwordState = SignUpPasswordState()
+    val passwordConfirmState = SignUpPasswordConfirmState(passwordState = passwordState)
+
+    val email get() = emailState.textFiled.text
+    val name get() = nameState.textFiled.text
+    val password get() = passwordState.textFiled.text
+    val passwordConfirm get() = passwordConfirmState.textFiled.text
+
+    val submitButtonEnable
+        get() = email.isNotBlank() && name.isNotBlank() && password.isNotBlank() && passwordConfirm.isNotBlank() &&
+                !emailState.isError && !passwordState.isError && !passwordConfirmState.isError
+}
+
+class SignUpPasswordState :
+    MaxWidthTextFieldState(
+        label = R.string.password,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Next
+        )
+    ) {
+
+    override fun onValueChange(newText: TextFieldValue) {
+        super.onValueChange(newText)
+        if (newText.text.isNotBlank() && newText.text.length < 6) updateError(R.string.error_weak_password)
+    }
+}
+
+class SignUpPasswordConfirmState(private val passwordState: SignUpPasswordState) :
+    MaxWidthTextFieldState(
+        label = R.string.password_confirm,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Done
+        )
+    ) {
+    override fun onValueChange(newText: TextFieldValue) {
+        super.onValueChange(newText)
+        if (newText.text.isNotBlank() && newText.text != passwordState.textFiled.text)
+            updateError(R.string.error_password_confirm_not_same)
     }
 }

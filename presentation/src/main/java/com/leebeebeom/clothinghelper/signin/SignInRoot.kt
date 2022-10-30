eb9d528e2d6593d4da4906655f229c5d6f622d37
leebeebeom <boole92@naver.com>
2022-10-31 04:44:34 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/SignInRoot.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/SignInRoot.kt
@@ -3,7 +3,6 @@ package com.leebeebeom.clothinghelper.signin
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -33,7 +32,6 @@ fun SignInRoot(
     ClothingHelperTheme {
         Scaffold(
             modifier = Modifier
-                .fillMaxSize()
                 .padding(horizontal = 20.dp)
                 .clickable(
                     interactionSource = interactionSource, indication = null

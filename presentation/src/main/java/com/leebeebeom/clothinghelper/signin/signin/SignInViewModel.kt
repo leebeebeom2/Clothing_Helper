eb41e99c4b8da4ce2737d88acdfb172f81ccd105
leebeebeom <boole92@naver.com>
2022-10-31 02:50:53 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signin/SignInViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signin/SignInViewModel.kt
@@ -1,13 +1,12 @@
 package com.leebeebeom.clothinghelper.signin.signin
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.ImeAction
 import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.base.MaxWidthTextFieldState
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInViewModelState
 import com.leebeebeom.clothinghelper.signin.base.setFireBaseError
-import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener2
+import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.usecase.user.GoogleSignInUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.user.SignInUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -21,30 +20,25 @@ class SignInViewModel @Inject constructor(
 
     override val viewModelState = SignInViewModelState()
 
-    fun signInWithEmailAndPassword(email: String, password: String) {
-        viewModelState.loadingOn()
-        signInUseCase(email, password, signInListener)
-    }
-
-    private val signInListener = object : FirebaseListener2 {
-        override fun taskSuccess() = viewModelState.showToast(R.string.sign_in_complete)
-
-        override fun taskFailed(exception: Exception?) =
-            setFireBaseError(
-                exception = exception,
-                showEmailError = viewModelState.showEmailError,
-                showPasswordError = viewModelState.showPasswordError,
-                showToast = viewModelState::showToast
-            )
-
-        override fun taskFinish() = viewModelState.loadingOff()
+    fun signInWithEmailAndPassword() {
+        signInUseCase(viewModelState.email, viewModelState.password) {
+            when (it) {
+                is FirebaseResult.Success -> viewModelState.showToast(R.string.sign_in_complete)
+                is FirebaseResult.Fail -> setFireBaseError(
+                    exception = it.exception,
+                    updateEmailError = viewModelState.emailState::updateError,
+                    updatePasswordError = viewModelState.passwordState::updateError,
+                    showToast = viewModelState::showToast
+                )
+            }
+        }
     }
 }
 
 class SignInViewModelState : GoogleSignInViewModelState() {
-    var passwordError: Int? by mutableStateOf(null)
-        private set
+    val emailState = MaxWidthTextFieldState.email(ImeAction.Next)
+    val passwordState = MaxWidthTextFieldState.password(ImeAction.Done)
 
-    val showPasswordError = { error: Int -> passwordError = error }
-    val hidePasswordError = { passwordError = null }
+    val email get() = emailState.textFiled.text
+    val password get() = passwordState.textFiled.text
 }

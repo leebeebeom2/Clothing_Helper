94bd9b2eb5abadedad5e1c6bd7e5f60dee5e3c65
leebeebeom <boole92@naver.com>
2022-10-31 04:05:56 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/resetpassword/ResetPasswordViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/resetpassword/ResetPasswordViewModel.kt
@@ -3,12 +3,13 @@ package com.leebeebeom.clothinghelper.signin.resetpassword
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.ImeAction
 import androidx.lifecycle.ViewModel
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.signin.base.EmailViewModelState
-import com.leebeebeom.clothinghelper.signin.base.TaskSuccessViewModelState
+import com.leebeebeom.clothinghelper.base.BaseViewModelState
+import com.leebeebeom.clothinghelper.base.MaxWidthTextFieldState
 import com.leebeebeom.clothinghelper.signin.base.setFireBaseError
-import com.leebeebeom.clothinghelperdomain.repository.FirebaseListener
+import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.usecase.user.ResetPasswordUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -19,27 +20,36 @@ class ResetPasswordViewModel @Inject constructor(
 ) : ViewModel() {
     val viewModelState = ResetPasswordViewModelState()
 
-    fun sendResetPasswordEmail(email: String) {
-        viewModelState.loadingOn()
-        resetPasswordUseCase(email, resetPasswordListener) { viewModelState.loadingOff() }
-    }
-
-    private val resetPasswordListener = object : FirebaseListener {
-        override fun taskSuccess() {
-            viewModelState.showToast(R.string.email_send_complete)
-            viewModelState.taskSuccess()
+    fun sendResetPasswordEmail() {
+        resetPasswordUseCase(viewModelState.email) {
+            when (it) {
+                is FirebaseResult.Success -> {
+                    viewModelState.showToast(R.string.email_send_complete)
+                    viewModelState.updateTaskSuccess(true)
+                }
+                is FirebaseResult.Fail -> {
+                    setFireBaseError(
+                        exception = it.exception,
+                        updateEmailError = viewModelState.emailState::updateError,
+                        updatePasswordError = {},
+                        showToast = viewModelState::showToast
+                    )
+                }
+            }
         }
-
-        override fun taskFailed(exception: Exception?) =
-            setFireBaseError(
-                exception = exception,
-                showEmailError = viewModelState.showEmailError,
-                showPasswordError = {},
-                showToast = viewModelState::showToast
-            )
     }
 }
 
-class ResetPasswordViewModelState : EmailViewModelState(), TaskSuccessViewModelState {
-    override var taskSuccess by mutableStateOf(false)
+class ResetPasswordViewModelState : BaseViewModelState() {
+    val emailState = MaxWidthTextFieldState.email(imeAction = ImeAction.Done)
+    val email get() = emailState.textFiled.text
+
+    val submitButtonEnabled get() = email.isNotEmpty() && !emailState.isError
+
+    var isTaskSuccess by mutableStateOf(false)
+        private set
+
+    fun updateTaskSuccess(isSuccess: Boolean) {
+        isTaskSuccess = isSuccess
+    }
 }

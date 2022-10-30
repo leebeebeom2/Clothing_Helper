391ab8c3871fb081706076dcb2ee910c3339cc0e
leebeebeom <boole92@naver.com>
2022-10-31 02:47:48 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SetFireBaseError.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SetFireBaseError.kt
@@ -14,18 +14,18 @@ object FirebaseErrorCode {
 
 fun setFireBaseError(
     exception: Exception?,
-    showEmailError: (error: Int) -> Unit,
-    showPasswordError: (error: Int) -> Unit,
+    updateEmailError: (error: Int) -> Unit,
+    updatePasswordError: (error: Int) -> Unit,
     showToast: (text: Int) -> Unit
 ) {
     val firebaseAuthException = exception as? FirebaseAuthException
 
     if (firebaseAuthException == null) showToast(R.string.unknown_error)
     else when (firebaseAuthException.errorCode) {
-        FirebaseErrorCode.ERROR_INVALID_EMAIL -> showEmailError(R.string.error_invalid_email)
-        FirebaseErrorCode.ERROR_EMAIL_ALREADY_IN_USE -> showEmailError(R.string.error_email_already_in_use)
-        FirebaseErrorCode.ERROR_USER_NOT_FOUND -> showEmailError(R.string.error_user_not_found)
-        FirebaseErrorCode.ERROR_WRONG_PASSWORD -> showPasswordError(R.string.error_wrong_password)
+        FirebaseErrorCode.ERROR_INVALID_EMAIL -> updateEmailError(R.string.error_invalid_email)
+        FirebaseErrorCode.ERROR_EMAIL_ALREADY_IN_USE -> updateEmailError(R.string.error_email_already_in_use)
+        FirebaseErrorCode.ERROR_USER_NOT_FOUND -> updateEmailError(R.string.error_user_not_found)
+        FirebaseErrorCode.ERROR_WRONG_PASSWORD -> updatePasswordError(R.string.error_wrong_password)
         else -> {
             showToast(R.string.unknown_error)
             Log.d(TAG, "setError: ${firebaseAuthException.errorCode}")

4282b161a1b9ee3676e1aed7b988a61432f7b82a
leebeebeom <boole92@naver.com>
2022-11-01 01:47:07 +0900

    구조 변경

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryViewModel.kt
@@ -4,23 +4,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
-import com.leebeebeom.clothinghelperdomain.usecase.subcategory.LoadAndGetSubCategoriesUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoriesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainCategoryViewModel @Inject constructor(
-    loadAndGetSubCategoriesUseCase: LoadAndGetSubCategoriesUseCase
-) : ViewModel() {
+class MainCategoryViewModel @Inject constructor(private val getSubCategoriesUseCase: GetSubCategoriesUseCase) :
+    ViewModel() {
 
     val viewModelState = MainCategoryViewModelState()
 
     init {
-        loadAndGetSubCategoriesUseCase.allSubCategories.forEachIndexed { i, subCategoriesFlow ->
-            viewModelScope.launch {
-                subCategoriesFlow.collect {
-                    viewModelState.categoriesSizeUpdate(i, it.size)
+        viewModelScope.launch {
+            getSubCategoriesUseCase(scope = this).forEachIndexed { i, subCategoriesFlow ->
+                viewModelScope.launch {
+                    subCategoriesFlow.collect { viewModelState.categoriesSizeUpdate(i, it.size) }
                 }
             }
         }

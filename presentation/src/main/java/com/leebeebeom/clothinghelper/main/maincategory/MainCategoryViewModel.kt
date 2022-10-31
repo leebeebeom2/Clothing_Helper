86a19e7c367a5622d181e10127f65bdbaf57b47f
leebeebeom <boole92@naver.com>
2022-11-01 01:51:43 +0900

    구조 변경

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryViewModel.kt
@@ -1,21 +1,31 @@
 package com.leebeebeom.clothinghelper.main.maincategory
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoriesUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoryLoadingStateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainCategoryViewModel @Inject constructor(private val getSubCategoriesUseCase: GetSubCategoriesUseCase) :
+class MainCategoryViewModel @Inject constructor(
+    private val getSubCategoriesUseCase: GetSubCategoriesUseCase,
+    private val getSubCategoryLoadingStateUseCase: GetSubCategoryLoadingStateUseCase
+) :
     ViewModel() {
 
     val viewModelState = MainCategoryViewModelState()
 
     init {
+        viewModelScope.launch {
+            getSubCategoryLoadingStateUseCase.isLoading.collect(viewModelState::updateIsLoading)
+        }
+
         viewModelScope.launch {
             getSubCategoriesUseCase(scope = this).forEachIndexed { i, subCategoriesFlow ->
                 viewModelScope.launch {
@@ -27,6 +37,13 @@ class MainCategoryViewModel @Inject constructor(private val getSubCategoriesUseC
 }
 
 class MainCategoryViewModelState {
+    var isLoading by mutableStateOf(false)
+        private set
+
+    fun updateIsLoading(isLoading: Boolean) {
+        this.isLoading = isLoading
+    }
+
     private val allSubCategoriesSize = List(4) { mutableStateOf(0) }
 
     fun categoriesSizeUpdate(index: Int, size: Int) {

6d0e13cdbc0dcc4e157e0c8e576c1389d869260f
leebeebeom <boole92@naver.com>
2022-11-01 01:05:42 +0900

    구조 변경

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/MainScreenRootViewModel.kt
@@ -0,0 +1,46 @@
+package com.leebeebeom.clothinghelper.main.root
+
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.TAG
+import com.leebeebeom.clothinghelper.main.base.BaseSubCategoriesViewModel
+import com.leebeebeom.clothinghelper.main.base.BaseSubCategoriesViewModelState
+import com.leebeebeom.clothinghelperdomain.usecase.signin.GetUserUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoriesUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoryLoadingStateUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.LoadSubCategoriesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainScreenRootViewModel @Inject constructor(
+    getSubCategoryLoadingStateUseCase: GetSubCategoryLoadingStateUseCase,
+    getSubCategoriesUseCase: GetSubCategoriesUseCase,
+    getUserUseCase: GetUserUseCase,
+    private val loadSubCategoriesUseCase: LoadSubCategoriesUseCase
+) : BaseSubCategoriesViewModel(
+    getUserUseCase = getUserUseCase,
+    getSubCategoryLoadingStateUseCase = getSubCategoryLoadingStateUseCase,
+    getSubCategoriesUseCase = getSubCategoriesUseCase
+) {
+    override val viewModelState = MainRootViewModelState()
+
+    init {
+        viewModelScope.launch {
+            loadSubCategoriesUseCase(viewModelState::onLoadFailed)
+        }
+
+        collectSubCategories()
+    }
+}
+
+class MainRootViewModelState : BaseSubCategoriesViewModelState() {
+    fun onLoadFailed(exception: Exception?) {
+        showToast(R.string.sub_categories_load_failed)
+        Log.d(
+            TAG, "MainScreenRootViewModel.subCategoryLoadFailed: errorCode = $exception"
+        )
+    }
+}

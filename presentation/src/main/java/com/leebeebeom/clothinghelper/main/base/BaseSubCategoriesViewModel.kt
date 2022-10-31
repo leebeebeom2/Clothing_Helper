8592e33f3702de745e7a8f73c58d66b426f994b0
leebeebeom <boole92@naver.com>
2022-11-01 00:47:05 +0900

    알고리즘 변경

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/BaseSubCategoriesViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/BaseSubCategoriesViewModel.kt
@@ -1,35 +1,53 @@
 package com.leebeebeom.clothinghelper.main.base
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelper.base.BaseViewModelState
 import com.leebeebeom.clothinghelperdomain.model.SubCategory
 import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
-import com.leebeebeom.clothinghelperdomain.usecase.subcategory.LoadAndGetSubCategoriesUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoriesUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoryLoadingStateUseCase
 import kotlinx.coroutines.launch
 
 abstract class BaseSubCategoriesViewModel(
-    private val loadAndGetSubCategoriesUseCase: LoadAndGetSubCategoriesUseCase
+    private val getSubCategoryLoadingStateUseCase: GetSubCategoryLoadingStateUseCase,
+    private val getSubCategoriesUseCase: GetSubCategoriesUseCase
 ) : ViewModel() {
     abstract val viewModelState: BaseSubCategoriesViewModelState
 
+    init {
+        viewModelScope.launch {
+            getSubCategoryLoadingStateUseCase.isLoading.collect(viewModelState::updateLoading)
+        }
+    }
+
     protected fun collectSubCategories() =
-        loadAndGetSubCategoriesUseCase.allSubCategories.forEachIndexed { i, subCategoriesFlow ->
-            viewModelScope.launch {
-                subCategoriesFlow.collect { viewModelState.updateSubCategories(i, it) }
+        viewModelScope.launch {
+            getSubCategoriesUseCase(scope = this).forEachIndexed { i, subCategoriesFlow ->
+                viewModelScope.launch {
+                    subCategoriesFlow.collect { viewModelState.updateSubCategories(i, it) }
+                }
             }
         }
 }
 
 open class BaseSubCategoriesViewModelState : BaseViewModelState() {
+    var isLoading by mutableStateOf(false)
+        private set
+
+    fun updateLoading(isLoading: Boolean) {
+        this.isLoading = isLoading
+    }
+
     private val allSubCategories = List(4) { mutableStateOf(emptyList<SubCategory>()) }
 
     fun updateSubCategories(index: Int, subCategories: List<SubCategory>) {
         allSubCategories[index].value = subCategories
     }
 
-    val getSubCategories =
-        { subCategoryParent: SubCategoryParent -> allSubCategories[subCategoryParent.ordinal].value }
-
+    fun getSubCategories(subCategoryParent: SubCategoryParent) =
+        allSubCategories[subCategoryParent.ordinal].value
 }

b0157dd68098f561ed3f6f0c2cbbcd030d996303
leebeebeom <boole92@naver.com>
2022-11-01 02:42:22 +0900

    구조 변경

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/PreferencesUseCases.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/PreferencesUseCases.kt
@@ -1,11 +1,23 @@
 package com.leebeebeom.clothinghelperdomain.usecase.preferences
 
+import com.leebeebeom.clothinghelperdomain.repository.SortOrder
 import com.leebeebeom.clothinghelperdomain.repository.SubCategoryPreferencesRepository
+import com.leebeebeom.clothinghelperdomain.repository.SubCategorySort
 
 class ToggleAllExpandUseCase(private val subCategoryPreferencesRepository: SubCategoryPreferencesRepository) {
     suspend operator fun invoke() = subCategoryPreferencesRepository.toggleAllExpand()
 }
 
-class GetSubCategoryPreferencesUseCase(private val subCategoryPreferencesRepository: SubCategoryPreferencesRepository) {
+class GetSubCategorySortPreferencesUseCase(private val subCategoryPreferencesRepository: SubCategoryPreferencesRepository) {
     operator fun invoke() = subCategoryPreferencesRepository.subCategorySort
+}
+
+class ChangeSortUseCase(private val subCategoryPreferencesRepository: SubCategoryPreferencesRepository) {
+    suspend operator fun invoke(subCategorySort: SubCategorySort) =
+        subCategoryPreferencesRepository.changeSort(subCategorySort)
+}
+
+class ChangeOrderUseCate(private val subCategoryPreferencesRepository: SubCategoryPreferencesRepository) {
+    suspend operator fun invoke(order: SortOrder) =
+        subCategoryPreferencesRepository.changeOrder(order)
 }

c0e6121ea63d766d04a305ff28141f6c754a83bd
leebeebeom <boole92@naver.com>
2022-11-01 00:36:29 +0900

    구조 변경

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/PreferencesUseCases.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/PreferencesUseCases.kt
@@ -1,14 +1,11 @@
 package com.leebeebeom.clothinghelperdomain.usecase.preferences
 
 import com.leebeebeom.clothinghelperdomain.repository.SubCategoryPreferencesRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.stateIn
 
 class ToggleAllExpandUseCase(private val subCategoryPreferencesRepository: SubCategoryPreferencesRepository) {
     suspend operator fun invoke() = subCategoryPreferencesRepository.toggleAllExpand()
 }
 
 class GetSubCategoryPreferencesUseCase(private val subCategoryPreferencesRepository: SubCategoryPreferencesRepository) {
-    suspend operator fun invoke(scope: CoroutineScope) =
-        subCategoryPreferencesRepository.subCategoryPreferences.stateIn(scope)
+    operator fun invoke() = subCategoryPreferencesRepository.subCategorySort
 }

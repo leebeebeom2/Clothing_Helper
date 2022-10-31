9e74f841a8287695e3ff6c22a1b3958771c41d7f
leebeebeom <boole92@naver.com>
2022-11-01 02:43:59 +0900

    구조 변경

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/PreferencesUseCases.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/PreferencesUseCases.kt
@@ -4,6 +4,10 @@ import com.leebeebeom.clothinghelperdomain.repository.SortOrder
 import com.leebeebeom.clothinghelperdomain.repository.SubCategoryPreferencesRepository
 import com.leebeebeom.clothinghelperdomain.repository.SubCategorySort
 
+class GetSubCategoryAllExpandUseCase(subCategoryPreferencesRepository: SubCategoryPreferencesRepository) {
+    val isAllExpand = subCategoryPreferencesRepository.subCategoryAllExpand
+}
+
 class ToggleAllExpandUseCase(private val subCategoryPreferencesRepository: SubCategoryPreferencesRepository) {
     suspend operator fun invoke() = subCategoryPreferencesRepository.toggleAllExpand()
 }

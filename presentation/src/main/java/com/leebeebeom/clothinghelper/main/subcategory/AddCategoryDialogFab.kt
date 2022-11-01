7d4e23950b56c955fd3bbc4ea497e869e56ea294
leebeebeom <boole92@naver.com>
2022-11-01 15:51:54 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/AddCategoryDialogFab.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/AddCategoryDialogFab.kt
@@ -5,11 +5,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.SimpleIcon
@@ -29,47 +29,33 @@ fun AddCategoryDialogFab(
         modifier = modifier.size(48.dp),
         onClick = state::showDialog,
         backgroundColor = MaterialTheme.colors.primary,
-    ) {
-        SimpleIcon(drawable = R.drawable.ic_add)
-    }
+    ) { SimpleIcon(drawable = R.drawable.ic_add) }
 
-    if (state.showDialog)
-        SubCategoryTextFieldDialog(
-            onDismissDialog = state.onDismissDialog,
-            categoryName = state.categoryName,
-            error = state.error,
-            title = R.string.add_category,
-            onCategoryNameChange = { state.onCategoryNameChange(it, subCategories) },
-            positiveButtonEnabled = state.positiveButtonEnabled,
-            onPositiveButtonClick = {
-                onPositiveButtonClick(
-                    state.categoryName.text,
-                    subCategoryParent
-                )
-            },
-            onFocusChanged = state.onFocusChanged
+
+    if (state.showDialog) SubCategoryTextFieldDialog(state = state, onPositiveButtonClick = {
+        onPositiveButtonClick(
+            state.textFiled.text, subCategoryParent
         )
+    })
+
+    LaunchedEffect(key1 = state.textFiled, key2 = subCategories) {
+        if (subCategories.map { it.name }
+                .contains(state.textFiled.text)) state.updateError(R.string.error_same_category_name)
+    }
 }
 
 class AddCategoryDialogUIState(
-    categoryName: String = "",
-    @StringRes categoryNameError: Int? = null,
-    showDialog: Boolean = false
-) : BaseSubCategoryTextFieldDialogUIState(categoryName, categoryNameError, showDialog) {
-
-    fun showDialog() {
-        showDialog = true
-    }
-
-    val onFocusChanged = { focusState: FocusState -> onText1FocusChanged(focusState) }
-
+    categoryName: String = "", showDialog: Boolean = false, @StringRes error: Int? = null
+) : BaseSubCategoryTextFieldDialogState(
+    text = categoryName, showDialog = showDialog, title = R.string.add_category, error = error
+) {
     companion object {
         val Saver: Saver<AddCategoryDialogUIState, *> = listSaver(save = {
             listOf(
-                it.categoryName.text, it.error, it.showDialog
+                it.textFiled.text, it.error, it.showDialog
             )
         }, restore = {
-            AddCategoryDialogUIState(it[0] as String, it[1] as? Int, it[2] as Boolean)
+            AddCategoryDialogUIState(it[0] as String, it[1] as Boolean)
         })
     }
 }

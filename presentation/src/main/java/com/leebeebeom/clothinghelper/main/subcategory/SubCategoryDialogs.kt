937749035f385365b0671a3ca88112a735376bea
leebeebeom <boole92@naver.com>
2022-11-01 15:57:16 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialogs.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialogs.kt
@@ -1,14 +1,18 @@
 package com.leebeebeom.clothinghelper.main.subcategory
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.DialogRoot
 import com.leebeebeom.clothinghelper.base.DialogTextButtons
 import com.leebeebeom.clothinghelper.base.DialogTextField
 import com.leebeebeom.clothinghelper.base.DialogTitle
+import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
 @Composable
 fun SubCategoryTextFieldDialog(
     state: BaseSubCategoryTextFieldDialogState,
+    subCategories: List<SubCategory>,
     onPositiveButtonClick: () -> Unit,
 ) {
     DialogRoot(onDismissDialog = state::onDismissDialog) {
@@ -19,5 +23,10 @@ fun SubCategoryTextFieldDialog(
             onPositiveButtonClick = onPositiveButtonClick,
             onDismissDialog = state::onDismissDialog
         )
+    }
+
+    LaunchedEffect(key1 = state.textFiled, key2 = subCategories) {
+        if (subCategories.map { it.name }
+                .contains(state.textFiled.text)) state.updateError(R.string.error_same_category_name)
     }
 }

fb2a86fa6c90343320d76bba1c7ff7d9c30fc5e5
leebeebeom <boole92@naver.com>
2022-11-01 10:08:09 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialogs.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialogs.kt
@@ -1,10 +1,6 @@
 package com.leebeebeom.clothinghelper.main.subcategory
 
-import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.focus.FocusState
-import androidx.compose.ui.text.input.TextFieldValue
-import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.DialogRoot
 import com.leebeebeom.clothinghelper.base.DialogTextButtons
 import com.leebeebeom.clothinghelper.base.DialogTextField
@@ -12,29 +8,16 @@ import com.leebeebeom.clothinghelper.base.DialogTitle
 
 @Composable
 fun SubCategoryTextFieldDialog(
-    onDismissDialog: () -> Unit,
-    @StringRes title: Int,
-    categoryName: TextFieldValue,
-    error: Int?,
-    onCategoryNameChange: (TextFieldValue) -> Unit,
-    positiveButtonEnabled: Boolean,
+    state: BaseSubCategoryTextFieldDialogState,
     onPositiveButtonClick: () -> Unit,
-    onFocusChanged: (FocusState) -> Unit
 ) {
-    DialogRoot(onDismissDialog = onDismissDialog) {
-        DialogTitle(text = title)
-        DialogTextField(
-            label = R.string.category,
-            placeHolder = R.string.category_place_holder,
-            text = categoryName,
-            error = error,
-            onTextChange = onCategoryNameChange,
-            onFocusChanged = onFocusChanged
-        )
+    DialogRoot(onDismissDialog = state::onDismissDialog) {
+        DialogTitle(text = state.title)
+        DialogTextField(state)
         DialogTextButtons(
-            positiveButtonEnabled = positiveButtonEnabled,
+            positiveButtonEnabled = state.positiveButtonEnabled,
             onPositiveButtonClick = onPositiveButtonClick,
-            onDismissDialog = onDismissDialog
+            onDismissDialog = state::onDismissDialog
         )
     }
 }

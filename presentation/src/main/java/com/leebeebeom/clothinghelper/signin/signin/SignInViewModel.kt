fe83bc167220658b2b02c254b6b261b53bf39011
leebeebeom <boole92@naver.com>
2022-11-02 12:27:41 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signin/SignInViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signin/SignInViewModel.kt
@@ -1,7 +1,7 @@
 package com.leebeebeom.clothinghelper.signin.signin
 
+import androidx.annotation.StringRes
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.MaxWidthTextFieldUIState
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInViewModelState
 import com.leebeebeom.clothinghelper.signin.base.setFireBaseError
@@ -9,6 +9,9 @@ import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.usecase.signin.GoogleSignInUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.signin.SignInUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,29 +19,39 @@ class SignInViewModel @Inject constructor(
     private val signInUseCase: SignInUseCase, googleSignInUseCase: GoogleSignInUseCase
 ) : GoogleSignInUpViewModel(googleSignInUseCase) {
 
-    override val uiState = SignInViewModelState()
+    private val _uiState = MutableStateFlow(SignInUIState())
+    val uiState get() = _uiState.asStateFlow()
 
-    fun signInWithEmailAndPassword() {
-        signInUseCase(uiState.email, uiState.password) {
+    fun signInWithEmailAndPassword(email: String, password: String) {
+        signInUseCase(email, password) {
             when (it) {
-                is FirebaseResult.Success -> uiState.showToast(R.string.sign_in_complete)
+                is FirebaseResult.Success -> showToast(R.string.sign_in_complete)
                 is FirebaseResult.Fail -> setFireBaseError(
                     exception = it.exception,
-                    updateEmailError = uiState.emailState::updateError,
-                    updatePasswordError = uiState.passwordState::updateError,
-                    showToast = uiState::showToast
+                    updateEmailError = ::updateEmailError,
+                    updatePasswordError = ::updatePasswordError,
+                    showToast = ::showToast
                 )
             }
         }
     }
+
+    fun updateEmailError(@StringRes error: Int?) = _uiState.update { it.copy(emailError = error) }
+
+    fun updatePasswordError(@StringRes error: Int?) =
+        _uiState.update { it.copy(passwordError = error) }
+
+    override fun updateGoogleButtonEnabled(enabled: Boolean) =
+        _uiState.update { it.copy(googleButtonEnabled = enabled) }
+
+    override fun showToast(toastText: Int?) = _uiState.update { it.copy(toastText = toastText) }
+
+    override fun toastShown() = _uiState.update { it.copy(toastText = null) }
 }
 
-class SignInViewModelState : GoogleSignInViewModelState() {
-    val emailState = MaxWidthTextFieldUIState()
-    val passwordState = MaxWidthTextFieldUIState()
-
-    val email get() = emailState.textFiled.text.trim()
-    val password get() = passwordState.textFiled.text.trim()
-
-    val signInButtonEnabled get() = email.isNotBlank() && password.isNotBlank() && !emailState.isError && !passwordState.isError
-}
+data class SignInUIState(
+    override val toastText: Int? = null,
+    override val googleButtonEnabled: Boolean = false,
+    @StringRes val emailError: Int? = null,
+    @StringRes val passwordError: Int? = null
+) : GoogleSignInViewModelState()

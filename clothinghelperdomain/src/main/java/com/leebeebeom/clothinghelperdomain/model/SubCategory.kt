c8c177d8371176c56e4a8354339f165c663a518f
leebeebeom <boole92@naver.com>
2022-11-03 02:28:16 +0900

    Parcelize 추가

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/SubCategory.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/SubCategory.kt
@@ -1,12 +1,16 @@
 package com.leebeebeom.clothinghelperdomain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 enum class SubCategoryParent {
     TOP, BOTTOM, OUTER, ETC
 }
 
+@Parcelize
 data class SubCategory(
     val parent: SubCategoryParent = SubCategoryParent.TOP,
     val key: String = "",
     val name: String = "",
     val createDate: Long = 0
-)
+) : Parcelable

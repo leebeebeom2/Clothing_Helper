d36d850e578f75c6f9a70e8beedab05c354246b0
leebeebeom <boole92@naver.com>
2022-11-02 12:56:08 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/base/MaxWidthButton.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/base/MaxWidthButton.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.leebeebeom.clothinghelper.R
 
 @Composable
 fun MaxWidthButton(
@@ -57,3 +58,8 @@ fun rememberMaxWidthButtonState(
     @StringRes text: Int,
     colors: ButtonColors = ButtonDefaults.buttonColors()
 ) = remember { MaxWidthButtonState(text, colors) }
+
+@Composable
+fun rememberGoogleButtonState() = rememberMaxWidthButtonState(
+    text = R.string.starts_with_google_email,
+    colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.surface))

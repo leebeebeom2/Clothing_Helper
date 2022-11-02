0a6fe161af2db04557e4dfa152e41fdb33fb8280
leebeebeom <boole92@naver.com>
2022-11-02 12:24:09 +0900

    확장함수 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/util/ExtensionFuntions.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/util/ExtensionFuntions.kt
@@ -1,7 +1,13 @@
 package com.leebeebeom.clothinghelper.util
 
+import kotlinx.coroutines.flow.MutableStateFlow
+
 inline fun <T> Collection<T>.taskAndReturn(crossinline task: (MutableList<T>) -> Unit): Set<T> {
     val temp = toMutableList()
     task(temp)
     return temp.toSet()
+}
+
+fun <T> MutableStateFlow<T>.update(task: (T) -> T) {
+    this.value = task(value)
 }

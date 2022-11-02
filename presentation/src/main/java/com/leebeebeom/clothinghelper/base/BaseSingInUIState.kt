3b4901cd6f5bd4f0b23770bb0ca684317829ba17
leebeebeom <boole92@naver.com>
2022-11-02 12:18:54 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/base/BaseSingInUIState.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/base/BaseSingInUIState.kt
@@ -1,5 +1,13 @@
 package com.leebeebeom.clothinghelper.base
 
 import androidx.annotation.StringRes
+import androidx.lifecycle.ViewModel
 
-abstract class BaseUIState(@StringRes val toastText: Int? = null)
+abstract class BaseViewModel : ViewModel() {
+    abstract fun showToast(@StringRes toastText: Int?)
+    abstract fun toastShown()
+}
+
+abstract class BaseUIState {
+    abstract val toastText: Int?
+}

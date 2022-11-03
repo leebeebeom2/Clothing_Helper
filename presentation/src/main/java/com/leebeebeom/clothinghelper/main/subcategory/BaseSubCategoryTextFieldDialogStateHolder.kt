9fd4af0a9a65f4897747043f21ab41abdc0a8ea6
leebeebeom <boole92@naver.com>
2022-11-04 01:50:03 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/BaseSubCategoryTextFieldDialogStateHolder.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/BaseSubCategoryTextFieldDialogStateHolder.kt
@@ -2,7 +2,9 @@ package com.leebeebeom.clothinghelper.main.subcategory
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.text.input.TextFieldValue
+import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.MaxWidthTextFieldStateHolder
+import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
 abstract class BaseSubCategoryTextFieldDialogStateHolder {
     abstract val titleRes: Int
@@ -22,5 +24,11 @@ abstract class BaseSubCategoryTextFieldDialogStateHolder {
     fun onDismissDialog() {
         showDialogState.value = false
         textFiledStateHolder.onValueChange(TextFieldValue("")) { errorState.value = null }
+    }
+
+    open fun updateError(newName: String, subCategories: List<SubCategory>) {
+        errorState.value = null
+        if (subCategories.map { it.name }.contains(newName))
+            errorState.value = R.string.error_same_category_name
     }
 }

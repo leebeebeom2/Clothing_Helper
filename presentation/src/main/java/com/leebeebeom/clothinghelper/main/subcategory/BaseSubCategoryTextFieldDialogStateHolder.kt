cd34259045a1e84d40a89ed30a2782656240e3d1
leebeebeom <boole92@naver.com>
2022-11-04 00:29:31 +0900

    위치 변경

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/BaseSubCategoryTextFieldDialogStateHolder.kt
@@ -0,0 +1,26 @@
+package com.leebeebeom.clothinghelper.main.subcategory
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.text.input.TextFieldValue
+import com.leebeebeom.clothinghelper.base.MaxWidthTextFieldStateHolder
+
+abstract class BaseSubCategoryTextFieldDialogStateHolder {
+    abstract val titleRes: Int
+    abstract val textFiledStateHolder: MaxWidthTextFieldStateHolder
+    protected abstract var errorState: MutableState<Int?>
+    val error get() = errorState.value
+
+    protected abstract var showDialogState: MutableState<Boolean>
+    val showDialog get() = showDialogState.value
+
+    open val positiveButtonEnabled get() = textFiledStateHolder.textState.isNotBlank() && error == null
+
+    open fun showDialog() {
+        showDialogState.value = true
+    }
+
+    fun onDismissDialog() {
+        showDialogState.value = false
+        textFiledStateHolder.onValueChange(TextFieldValue("")){errorState.value = null}
+    }
+}

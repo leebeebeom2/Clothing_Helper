aab94fac260c0502f6ac7c4528a5cf98d53869b5
leebeebeom <boole92@naver.com>
2022-11-04 03:53:16 +0900

    버그 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/DrawerContents.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/DrawerContents.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -55,7 +54,7 @@ fun DrawerContents(
             }
 
             items(drawerContentsState.mainCategories, key = { it.type.name }) {
-                val drawerMainCategoryState by rememberDrawerMainCategoryState(
+                val drawerMainCategoryState = rememberDrawerMainCategoryState(
                     mainCategory = it, drawerContentsState = drawerContentsState
                 )
                 DrawerMainCategory(

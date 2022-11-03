4b9939462339201f5c5c95c2841271a4f8e055f8
leebeebeom <boole92@naver.com>
2022-11-04 03:27:17 +0900

    코드 정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signup/SignUpViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signup/SignUpViewModel.kt
@@ -4,8 +4,8 @@ import android.util.Log
 import androidx.annotation.StringRes
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.TAG
-import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUIState
+import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
 import com.leebeebeom.clothinghelper.signin.base.setFireBaseError
 import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.usecase.signin.GoogleSignInUseCase
@@ -25,24 +25,19 @@ class SignUpViewModel @Inject constructor(
     val uiState get() = _uiState.asStateFlow()
 
     fun signUpWithEmailAndPassword(email: String, name: String, password: String) {
-        signUpUseCase(
-            email = email,
-            password = password,
-            name = name,
-            onSignUpDone = {
-                when (it) {
-                    is FirebaseResult.Success -> showToast(R.string.sign_up_complete)
-                    is FirebaseResult.Fail -> {
-                        setFireBaseError(
-                            exception = it.exception,
-                            updateEmailError = ::updateEmailError,
-                            updatePasswordError = {},
-                            showToast = ::showToast
-                        )
-                    }
+        signUpUseCase(email = email, password = password, name = name, onSignUpDone = {
+            when (it) {
+                is FirebaseResult.Success -> showToast(R.string.sign_up_complete)
+                is FirebaseResult.Fail -> {
+                    setFireBaseError(
+                        exception = it.exception,
+                        updateEmailError = ::updateEmailError,
+                        updatePasswordError = {},
+                        showToast = ::showToast
+                    )
                 }
             }
-        ) {
+        }) {
             if (it is FirebaseResult.Fail) {
                 showToast(R.string.name_update_failed)
                 Log.d(TAG, "taskFailed: $it.exception")
@@ -53,17 +48,13 @@ class SignUpViewModel @Inject constructor(
     override fun updateGoogleButtonEnabled(enabled: Boolean) =
         _uiState.update { it.copy(googleButtonEnabled = enabled) }
 
-    override fun showToast(toastText: Int?) =
-        _uiState.update { it.copy(toastText = toastText) }
+    override fun showToast(toastText: Int?) = _uiState.update { it.copy(toastText = toastText) }
 
-    override fun toastShown() =
-        _uiState.update { it.copy(toastText = null) }
+    override fun toastShown() = _uiState.update { it.copy(toastText = null) }
 
-    override fun updateEmailError(error: Int?) =
-        _uiState.update { it.copy(emailError = error) }
+    override fun updateEmailError(error: Int?) = _uiState.update { it.copy(emailError = error) }
 
-    fun updatePasswordError(error: Int?) =
-        _uiState.update { it.copy(passwordError = error) }
+    fun updatePasswordError(error: Int?) = _uiState.update { it.copy(passwordError = error) }
 
     fun updatePasswordConfirmError(error: Int?) =
         _uiState.update { it.copy(passwordConfirmError = error) }

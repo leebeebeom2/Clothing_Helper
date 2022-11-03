4f6d8b61e816e1b99ecd7bf37d515a4ed522aa8c
leebeebeom <boole92@naver.com>
2022-11-04 00:16:32 +0900

    위치 변경

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryCard.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryCard.kt
@@ -1,4 +1,4 @@
-package com.leebeebeom.clothinghelper.main.subcategory
+package com.leebeebeom.clothinghelper.main.subcategory.content
 
 import androidx.annotation.StringRes
 import androidx.compose.animation.*

7d02cf3b3c30be1f14b9b160ee35a885ecb1987a
leebeebeom <boole92@naver.com>
2022-11-05 00:11:01 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/AddSubcategoryDialogFab.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/AddSubcategoryDialogFab.kt
@@ -5,27 +5,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.MaxWidthTextFieldStateHolder
 import com.leebeebeom.clothinghelper.base.SimpleIcon
-import com.leebeebeom.clothinghelper.base.rememberSubCategoryDialogTextFieldStateHolder
-import com.leebeebeom.clothinghelper.main.subcategory.BaseSubCategoryTextFieldDialogState
 import com.leebeebeom.clothinghelper.main.subcategory.SubCategoryTextFieldDialog
 import com.leebeebeom.clothinghelperdomain.model.SubCategory
-import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
 
 @Composable
 fun BoxScope.AddSubcategoryDialogFab(
-    onPositiveButtonClick: (String, SubCategoryParent) -> Unit,
-    subCategoriesState: List<SubCategory>,
-    subCategoryParent: SubCategoryParent
+    onPositiveButtonClick: (String) -> Unit,
+    subCategories: List<SubCategory>,
 ) {
-    val state = rememberAddCategoryDialogState(subCategoriesState = subCategoriesState)
+    val state = rememberAddCategoryDialogState(subCategories = subCategories)
 
     FloatingActionButton(
         modifier = Modifier
@@ -37,30 +38,74 @@ fun BoxScope.AddSubcategoryDialogFab(
     ) { SimpleIcon(drawable = R.drawable.ic_add) }
 
     if (state.showDialog)
-        SubCategoryTextFieldDialog(state = state) {
-            onPositiveButtonClick(state.textFiledStateHolder.textState.trim(), subCategoryParent)
-        }
+        SubCategoryTextFieldDialog(
+            titleRes = R.string.add_category,
+            positiveButtonEnabled = state.positiveButtonEnabled,
+            onValueChange = state::onValueChange,
+            onFocusChanged = state::onFocusChange,
+            onDismiss = state::onDismiss,
+            onPositiveButtonClick = { onPositiveButtonClick(state.text) }
+        )
 }
 
-class AddCategoryDialogState(
-    override val titleRes: Int = R.string.add_category,
-    override val textFiledStateHolder: MaxWidthTextFieldStateHolder,
-    override var showDialogState: MutableState<Boolean>,
-    override val errorState: MutableState<Int?>,
-    override val subCategoriesState: List<SubCategory>
-) : BaseSubCategoryTextFieldDialogState()
+data class AddCategoryDialogState(
+    private val textState: MutableState<String>,
+    private val textFieldValueState: MutableState<TextFieldValue>,
+    private val showDialogState: MutableState<Boolean>,
+    private val errorState: MutableState<Int?>,
+    val subCategories: List<SubCategory>
+) {
+    val showDialog get() = showDialogState.value
+    val error get() = errorState.value
+    val text get() = textState.value
+
+    val positiveButtonEnabled get() = text.isNotBlank() && error == null
+
+    fun showDialog() {
+        showDialogState.value = true
+    }
+
+    fun onDismiss() {
+        showDialogState.value = false
+        textState.value = ""
+        textFieldValueState.value = TextFieldValue("")
+        errorState.value = null
+    }
+
+    fun onValueChange(newTextFiled: TextFieldValue) {
+        if (text != newTextFiled.text) errorState.value = null
+        textState.value = newTextFiled.text
+        textFieldValueState.value = newTextFiled
+        if (subCategories.map { it.name }.contains(newTextFiled.text))
+            errorState.value = R.string.error_same_category_name
+    }
+
+    fun onFocusChange(newFocusState: FocusState) {
+        if (newFocusState.hasFocus)
+            textFieldValueState.value =
+                textFieldValueState.value.copy(selection = TextRange(textFieldValueState.value.text.length))
+    }
+}
 
 @Composable
 fun rememberAddCategoryDialogState(
-    textFiledStateHolder: MaxWidthTextFieldStateHolder = rememberSubCategoryDialogTextFieldStateHolder(),
+    textState: MutableState<String> = rememberSaveable { mutableStateOf("") },
+    textFieldValueState: MutableState<TextFieldValue> = remember {
+        mutableStateOf(
+            TextFieldValue(
+                textState.value
+            )
+        )
+    },
     errorState: MutableState<Int?> = rememberSaveable { mutableStateOf(null) },
     showDialogState: MutableState<Boolean> = rememberSaveable { mutableStateOf(false) },
-    subCategoriesState: List<SubCategory>
-) = remember(subCategoriesState) {
+    subCategories: List<SubCategory>
+) = remember(subCategories) {
     AddCategoryDialogState(
-        textFiledStateHolder = textFiledStateHolder,
+        textState = textState,
+        textFieldValueState = textFieldValueState,
         errorState = errorState,
         showDialogState = showDialogState,
-        subCategoriesState = subCategoriesState
+        subCategories = subCategories
     )
 }

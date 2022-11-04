cdba15ab109c2cab919202d308ad07f902af4c8b
leebeebeom <boole92@naver.com>
2022-11-04 10:00:15 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/base/Dialog.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/base/Dialog.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
@@ -47,12 +48,13 @@ fun DialogTextField(
     textFieldStateHolder: MaxWidthTextFieldState,
     error: Int?,
     onValueChange: (TextFieldValue) -> Unit,
+    onFocusChange: (FocusState) -> Unit
 ) {
     MaxWidthTextField(
         state = textFieldStateHolder,
         error = error,
         onValueChange = onValueChange,
-        onFocusChanged = textFieldStateHolder.onFocusChanged
+        onFocusChanged = onFocusChange
     )
     SimpleHeightSpacer(dp = 12)
 }

d48f0f48323a5de372a2442d396e2a00d932a400
leebeebeom <boole92@naver.com>
2022-11-05 00:56:53 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialogs.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialogs.kt
@@ -2,12 +2,14 @@ package com.leebeebeom.clothinghelper.main.subcategory
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.text.input.TextFieldValue
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.*
+import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -44,5 +46,24 @@ fun SubCategoryTextFieldDialog(
             onPositiveButtonClick = onPositiveButtonClick,
             onDismiss = onDismiss
         )
+    }
+}
+
+abstract class BaseSubCategoryTextFieldDialogState {
+    protected abstract val textState: MutableState<String>
+    protected abstract val textFieldValueMutableState: MutableState<TextFieldValue>
+    protected abstract val errorState: MutableState<Int?>
+    protected abstract val subCategories: List<SubCategory>
+
+    val text get() = textState.value
+    val textFieldValueState: State<TextFieldValue> get() = textFieldValueMutableState
+    val error get() = errorState.value
+
+    fun onValueChange(newTextFiled: TextFieldValue) {
+        if (textState.value != newTextFiled.text) errorState.value = null
+        textState.value = newTextFiled.text
+        textFieldValueMutableState.value = newTextFiled
+        if (subCategories.map { it.name }.contains(newTextFiled.text))
+            errorState.value = R.string.error_same_category_name
     }
 }

8cb2179ec4c1baf2c291615212f7ddeaedfd1e3e
leebeebeom <boole92@naver.com>
2022-11-04 11:52:59 +0900

    RequestFocus 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/base/ClearFocus.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/base/ClearFocus.kt
@@ -1,9 +1,16 @@
 package com.leebeebeom.clothinghelper.base
 
 import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
 
 interface ClearFocus {
-    val focusManager:FocusManager
+    val focusManager: FocusManager
 
     fun clearFocus() = focusManager.clearFocus()
+}
+
+interface RequestFocus {
+    val focusRequester: FocusRequester
+
+    fun requestFocus() = focusRequester.requestFocus()
 }

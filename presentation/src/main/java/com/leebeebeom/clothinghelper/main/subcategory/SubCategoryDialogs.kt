79af4f99eb2a5daa38207fc90cd9a45fa0115827
leebeebeom <boole92@naver.com>
2022-11-05 00:13:49 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialogs.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialogs.kt
@@ -1,57 +1,48 @@
 package com.leebeebeom.clothinghelper.main.subcategory
 
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.text.input.TextFieldValue
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.*
-import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SubCategoryTextFieldDialog(
-    state: BaseSubCategoryTextFieldDialogState,
+    @StringRes titleRes: Int,
+    error: Int?,
+    textFieldValueState: State<TextFieldValue>,
+    positiveButtonEnabled: Boolean,
+    onValueChange: (TextFieldValue) -> Unit,
+    onFocusChanged: (FocusState) -> Unit,
     onPositiveButtonClick: () -> Unit,
+    onDismiss: () -> Unit
 ) {
-    DialogRoot(onDismissDialog = state::onDismissDialog) {
-        DialogTitle(text = state.titleRes)
+
+    val maxWidthTextFieldState =
+        rememberMaxWidthTextFiledState(
+            textFieldValueState = textFieldValueState,
+            label = R.string.category,
+            showKeyboardEnabled = true,
+            placeholder = R.string.category_place_holder
+        )
+
+    DialogRoot(onDismiss = onDismiss) {
+        DialogTitle(text = titleRes)
+
         DialogTextField(
-            textFieldStateHolder = state.textFiledStateHolder,
-            error = state.error,
-            updateError = state::updateError
+            state = maxWidthTextFieldState.value,
+            error = error,
+            onValueChange = onValueChange,
+            onFocusChanged = onFocusChanged
         )
         DialogTextButtons(
-            positiveButtonEnabled = state.positiveButtonEnabled,
+            positiveButtonEnabled = positiveButtonEnabled,
             onPositiveButtonClick = onPositiveButtonClick,
-            onDismissDialog = state::onDismissDialog
+            onDismiss = onDismiss
         )
-    }
-}
-
-abstract class BaseSubCategoryTextFieldDialogState {
-    abstract val titleRes: Int
-    abstract val textFiledStateHolder: MaxWidthTextFieldStateHolder
-    protected abstract val errorState: MutableState<Int?>
-    val error get() = errorState.value
-
-    protected abstract var showDialogState: MutableState<Boolean>
-    val showDialog get() = showDialogState.value
-
-    protected abstract val subCategoriesState: List<SubCategory>
-
-    open val positiveButtonEnabled get() = textFiledStateHolder.textState.isNotBlank() && error == null
-
-    open fun showDialog() {
-        showDialogState.value = true
-    }
-
-    fun onDismissDialog() {
-        showDialogState.value = false
-        textFiledStateHolder.onValueChange(TextFieldValue("")) { errorState.value = null }
-    }
-
-    open fun updateError(newName: String) {
-        errorState.value = null
-        if (subCategoriesState.map { it.name }.contains(newName.trim()))
-            errorState.value = R.string.error_same_category_name
     }
 }

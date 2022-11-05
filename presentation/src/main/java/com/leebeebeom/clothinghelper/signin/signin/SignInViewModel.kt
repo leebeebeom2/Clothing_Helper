6b2f1cc074aae8ae31ce41ab42771048cae6b7bb
leebeebeom <boole92@naver.com>
2022-11-06 02:57:57 +0900

    로그인 로직 suspend로 변경

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signin/SignInViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signin/SignInViewModel.kt
@@ -1,9 +1,10 @@
 package com.leebeebeom.clothinghelper.signin.signin
 
 import androidx.annotation.StringRes
+import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUIState
+import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
 import com.leebeebeom.clothinghelper.signin.base.setFireBaseError
 import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.usecase.signin.GoogleSignInUseCase
@@ -12,6 +13,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,19 +24,20 @@ class SignInViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(SignInUIState())
     val uiState get() = _uiState.asStateFlow()
 
-    fun signInWithEmailAndPassword(email: String, password: String) {
-        signInUseCase(email, password) {
-            when (it) {
+    fun signInWithEmailAndPassword(email: String, password: String) =
+        viewModelScope.launch {
+
+            when (val result = signInUseCase(email, password)) {
                 is FirebaseResult.Success -> showToast(R.string.sign_in_complete)
                 is FirebaseResult.Fail -> setFireBaseError(
-                    exception = it.exception,
+                    exception = result.exception,
                     updateEmailError = ::updateEmailError,
                     updatePasswordError = ::updatePasswordError,
                     showToast = ::showToast
                 )
+                else -> showToast(R.string.unknown_error)
             }
         }
-    }
 
     fun updatePasswordError(@StringRes error: Int?) =
         _uiState.update { it.copy(passwordError = error) }

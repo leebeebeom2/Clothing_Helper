22c2dc7d1ad08b0ea4c39310419d7f2e5c25bb6b
leebeebeom <boole92@naver.com>
2022-11-05 11:57:05 +0900

    코드 정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryContent.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryContent.kt
@@ -66,7 +66,12 @@ fun SubCategoryContent(
         }
 
         AddSubcategoryDialogFab(
-            onPositiveButtonClick = { onAddCategoryPositiveButtonClick(it.trim(), state.value.parent) },
+            onPositiveButtonClick = {
+                onAddCategoryPositiveButtonClick(
+                    it.trim(),
+                    state.value.parent
+                )
+            },
             subCategories = state.value.subCategories,
         )
     }

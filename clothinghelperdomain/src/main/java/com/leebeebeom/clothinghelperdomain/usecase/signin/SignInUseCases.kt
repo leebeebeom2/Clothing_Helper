f7a3ab5bab032f5d9680707c0b17f7ccd43010cb
leebeebeom <boole92@naver.com>
2022-11-06 05:08:40 +0900

    로직 변경

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/signin/SignInUseCases.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/signin/SignInUseCases.kt
@@ -1,10 +1,10 @@
 package com.leebeebeom.clothinghelperdomain.usecase.signin
 
+import com.leebeebeom.clothinghelperdomain.model.AuthResult
 import com.leebeebeom.clothinghelperdomain.model.SignIn
 import com.leebeebeom.clothinghelperdomain.model.SignUp
-import com.leebeebeom.clothinghelperdomain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelperdomain.repository.UserRepository
-import com.leebeebeom.clothinghelperdomain.repository.onDone
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.PushInitialSubCategoriesUseCase
 
 class GetSignInLoadingStateUseCase(private val userRepository: UserRepository) {
     operator fun invoke() = userRepository.isLoading
@@ -16,45 +16,34 @@ class GetSignInStateUseCase(private val userRepository: UserRepository) {
 
 class GoogleSignInUseCase(
     private val userRepository: UserRepository,
-    private val subCategoryRepository: SubCategoryRepository
+    private val pushInitialSubCategoriesUseCase: PushInitialSubCategoriesUseCase
 ) {
-    operator fun invoke(
-        credential: Any?, onDone: onDone
-    ) = userRepository.googleSignIn(
-        credential = credential,
-        onDone = onDone,
-        pushInitialSubCategories = subCategoryRepository::pushInitialSubCategories
-    )
+    suspend operator fun invoke(credential: Any?): AuthResult {
+        val authResult = userRepository.googleSignIn(credential = credential)
+        pushInitialSubCategoriesUseCase.push(authResult) // 실패 성공 상관 없이 진행
+        return authResult
+    }
 }
 
 class SignInUseCase(private val userRepository: UserRepository) {
-    operator fun invoke(
-        email: String, password: String, onDone: onDone
-    ) = userRepository.signIn(signIn = SignIn(email = email, password = password), onDone = onDone)
+    suspend operator fun invoke(email: String, password: String) =
+        userRepository.signIn(signIn = SignIn(email = email, password = password))
 }
 
 class SignUpUseCase(
     private val userRepository: UserRepository,
-    private val subCategoryRepository: SubCategoryRepository
+    private val pushInitialSubCategoriesUseCase: PushInitialSubCategoriesUseCase
 ) {
-    operator fun invoke(
-        email: String,
-        password: String,
-        name: String,
-        onSignUpDone: onDone,
-        onNameUpdateDone: onDone
-    ) = userRepository.signUp(
-        signUp = SignUp(email = email, password = password, name = name),
-        onSignUpDone = onSignUpDone,
-        onNameUpdateDone = onNameUpdateDone,
-        pushInitialSubCategories = subCategoryRepository::pushInitialSubCategories,
-    )
+    suspend operator fun invoke(email: String, password: String, name: String): AuthResult {
+        val authResult =
+            userRepository.signUp(signUp = SignUp(email = email, password = password, name = name))
+        pushInitialSubCategoriesUseCase.push(authResult) // 실패 성공 상관 없이 진행
+        return authResult
+    }
 }
 
 class ResetPasswordUseCase(private val userRepository: UserRepository) {
-    operator fun invoke(
-        email: String, onDone: onDone
-    ) = userRepository.resetPasswordEmail(email = email, onDone = onDone)
+    suspend operator fun invoke(email: String) = userRepository.resetPasswordEmail(email = email)
 }
 
 class GetUserUseCase(private val userRepository: UserRepository) {
@@ -62,5 +51,9 @@ class GetUserUseCase(private val userRepository: UserRepository) {
 }
 
 class SignOutUseCase(private val userRepository: UserRepository) {
-    operator fun invoke() = userRepository.signOut()
+    suspend operator fun invoke() = userRepository.signOut()
+}
+
+suspend fun PushInitialSubCategoriesUseCase.push(authResult: AuthResult) {
+    if (authResult is AuthResult.Success && authResult.isNewer) invoke(authResult.user.uid)
 }

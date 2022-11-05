b6627bb54089f40aee7159ca09bcb0930032f2a0
leebeebeom <boole92@naver.com>
2022-11-06 05:08:09 +0900

    로직 변경

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/FirebaseResult.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/FirebaseResult.kt
@@ -4,3 +4,13 @@ sealed class FirebaseResult {
     object Success : FirebaseResult()
     data class Fail(val exception: Exception?) : FirebaseResult()
 }
+
+sealed class AuthResult {
+    data class Success(val user: User, val isNewer: Boolean) : AuthResult()
+    data class Fail(val exception: Exception?) : AuthResult()
+}
+
+sealed class SubCategoryPushResult {
+    data class Success(val subCategory: SubCategory) : SubCategoryPushResult()
+    data class Fail(val exception: Exception?) : SubCategoryPushResult()
+}

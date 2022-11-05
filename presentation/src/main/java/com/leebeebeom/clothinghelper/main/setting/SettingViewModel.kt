b27313b7c45ef9448003aa31bbb881d074ade927
leebeebeom <boole92@naver.com>
2022-11-06 04:19:02 +0900

    signOut suspend 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/setting/SettingViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/setting/SettingViewModel.kt
@@ -2,13 +2,13 @@ package com.leebeebeom.clothinghelper.main.setting
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.leebeebeom.clothinghelper.di.usecase.SignInUseCaseModule_SignOutUseCaseFactory.signOutUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.signin.SignOutUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingViewModel @Inject constructor(private val signOutUseCase: SignOutUseCase) : ViewModel() {
+class SettingViewModel @Inject constructor(private val signOutUseCase: SignOutUseCase) :
+    ViewModel() {
     fun signOut() = viewModelScope.launch { signOutUseCase() }
 }

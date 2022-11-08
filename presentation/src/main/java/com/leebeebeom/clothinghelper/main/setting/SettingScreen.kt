9f5054d5181febc377cd43a5748e7fb35ec4fb69
leebeebeom <boole92@naver.com>
2022-11-09 03:46:36 +0900

    사인아웃 로직 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/setting/SettingScreen.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/setting/SettingScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -14,12 +13,10 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.MaxWidthButton
 import com.leebeebeom.clothinghelper.base.rememberMaxWidthButtonState
-import kotlinx.coroutines.launch
 
 @Composable
 fun SettingScreen(
     viewModel: SettingViewModel = hiltViewModel(),
-    onSignOutButtonClick: suspend () -> Unit,
     drawerCloseBackHandler: @Composable () -> Unit
 ) {
     drawerCloseBackHandler()
@@ -31,23 +28,19 @@ fun SettingScreen(
             .padding(bottom = 20.dp),
         contentAlignment = Alignment.BottomCenter
     ) {
-        val coroutineScope = rememberCoroutineScope()
 
-        SignOutButton(onSignOutClick = {
-            viewModel.signOut()
-            coroutineScope.launch { onSignOutButtonClick() }
-        })
+        SignOutButton(onClick = viewModel::signOut)
     }
 }
 
 @Composable
-private fun SignOutButton(onSignOutClick: () -> Unit) {
+private fun SignOutButton(onClick: () -> Unit) {
     MaxWidthButton(
         state = rememberMaxWidthButtonState(
             text = R.string.sign_out,
             enabled = true,
             colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error)
         ),
-        onClick = onSignOutClick
+        onClick = onClick
     )
 }

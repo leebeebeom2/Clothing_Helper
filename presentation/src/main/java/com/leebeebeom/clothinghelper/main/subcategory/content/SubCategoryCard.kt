ad3e523afe966fea07402249dbb317de50d03c01
leebeebeom <boole92@naver.com>
2022-11-09 13:22:01 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryCard.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryCard.kt
@@ -2,21 +2,17 @@ package com.leebeebeom.clothinghelper.main.subcategory.content
 
 import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Transition
+import androidx.compose.animation.core.animateDp
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -32,39 +28,43 @@ import com.leebeebeom.clothinghelper.main.base.isExpandStateWithIsAllExpand
 import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
 // TODO 선택 모드 애니메이션 렉
+// Expand 트랜지션
 
 @Composable
 fun SubCategoryCard(
-    state: SubCategoryCardState, onClick: () -> Unit
+    subCategory: () -> SubCategory,
+    isAllExpand: () -> Boolean,
+    isChecked: () -> Boolean,
+    selectModeTransition: Transition<Boolean>,
+    onClick: () -> Unit
 ) {
-    val isExpandState = isExpandStateWithIsAllExpand(isAllExpand = state.isAllExpand)
+    var isExpand by isExpandStateWithIsAllExpand(isAllExpand = isAllExpand)
 
     Card(elevation = 2.dp, shape = RoundedCornerShape(12.dp)) {
         Column(
-            modifier = Modifier.clip(RoundedCornerShape(12.dp)).clickable(onClick = onClick)
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp))
+                .clickable(onClick = onClick)
         ) {
-            val subCategoryCardTitleState =
-                rememberSubCategoryCardTitleState(subCategoryCardState = state)
             SubCategoryCardTitle(
-                state = subCategoryCardTitleState,
-                isExpanded = isExpandState.value,
-                onExpandIconClick = { isExpandState.value = !isExpandState.value },
-                onCheckBoxClick = onClick
+                isExpanded = { isExpand },
+                onExpandIconClick = { isExpand = !isExpand },
+                onCheckBoxClick = onClick,
+                subCategory = subCategory,
+                selectModeTransition = selectModeTransition,
+                isChecked = isChecked
             )
-
-            AnimatedVisibility(
-                visible = isExpandState.value,
-                enter = Anime.SubCategoryCard.expandIn,
-                exit = Anime.SubCategoryCard.shrinkOut
-            ) { SubCategoryInfo() }
+            SubCategoryInfo { isExpand }
         }
     }
 }
 
 @Composable
 private fun SubCategoryCardTitle(
-    state: SubCategoryCardTitleState,
-    isExpanded: Boolean,
+    subCategory: () -> SubCategory,
+    selectModeTransition: Transition<Boolean>,
+    isChecked: () -> Boolean,
+    isExpanded: () -> Boolean,
     onExpandIconClick: () -> Unit,
     onCheckBoxClick: () -> Unit
 ) {
@@ -80,17 +80,13 @@ private fun SubCategoryCardTitle(
                 modifier = Modifier.weight(1f), verticalAlignment = Alignment.CenterVertically
             ) {
                 TitleCircleCheckBox(
-                    isSelectMode = state.isSelectMode,
-                    isChecked = state.isChecked,
+                    selectModeTransition = selectModeTransition,
+                    isChecked = isChecked,
                     onClick = onCheckBoxClick
                 )
-                Spacer(
-                    modifier = Modifier
-                        .animateContentSize(tween(Anime.CircleCheckBox.duration))
-                        .width(if (state.isSelectMode) 2.dp else 12.dp)
-                        .background(Color.Transparent)
-                )
-                Title(name = state.title)
+                val width by selectModeTransition.animateDp(label = "width") { if (it) 2.dp else 12.dp }
+                Spacer(modifier = Modifier.width(width))
+                Name(subCategory = subCategory)
                 SimpleWidthSpacer(dp = 4)
                 TotalCount(isExpanded = isExpanded)
             }
@@ -103,25 +99,10 @@ private fun SubCategoryCardTitle(
     }
 }
 
-data class SubCategoryCardTitleState(
-    val title: String, val isSelectMode: Boolean, val isChecked: Boolean
-)
-
 @Composable
-fun rememberSubCategoryCardTitleState(
-    subCategoryCardState: SubCategoryCardState
-) = remember(subCategoryCardState) {
-    SubCategoryCardTitleState(
-        title = subCategoryCardState.subCategory.name,
-        isSelectMode = subCategoryCardState.isSelectMode,
-        isChecked = subCategoryCardState.isChecked
-    )
-}
-
-@Composable
-private fun TotalCount(isExpanded: Boolean) {
+private fun TotalCount(isExpanded: () -> Boolean) =
     AnimatedVisibility(
-        visible = !isExpanded,
+        visible = !isExpanded(),
         enter = Anime.SubCategoryCard.fadeIn,
         exit = Anime.SubCategoryCard.fadeOut
     ) {
@@ -131,25 +112,26 @@ private fun TotalCount(isExpanded: Boolean) {
             ), maxLines = 1, overflow = TextOverflow.Ellipsis
         )
     }
-}
 
 @Composable
-private fun Title(name: String) {
+private fun Name(subCategory: () -> SubCategory) =
     Text(
         modifier = Modifier.widthIn(max = 300.dp),
-        text = name,
+        text = subCategory().name,
         style = MaterialTheme.typography.subtitle1,
         maxLines = 1,
         overflow = TextOverflow.Ellipsis,
     )
-}
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-private fun RowScope.TitleCircleCheckBox(
-    isSelectMode: Boolean, isChecked: Boolean, onClick: () -> Unit
-) {
-    AnimatedVisibility(
-        visible = isSelectMode,
+private fun TitleCircleCheckBox(
+    selectModeTransition: Transition<Boolean>,
+    isChecked: () -> Boolean,
+    onClick: () -> Unit
+) =
+    selectModeTransition.AnimatedVisibility(
+        visible = { it },
         enter = Anime.CircleCheckBox.expandIn,
         exit = Anime.CircleCheckBox.shrinkOut
     ) {
@@ -161,23 +143,29 @@ private fun RowScope.TitleCircleCheckBox(
             onClick = onClick
         )
     }
-}
 
 @Composable
-private fun SubCategoryInfo() {
-    Surface(color = MaterialTheme.colors.background) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        ) {
-            SubCategoryInfoText( // TODO
-                infoTitle = R.string.average_size, info = R.string.top_info
-            )
+private fun SubCategoryInfo(isExpand: () -> Boolean) {
+    AnimatedVisibility(
+        visible = isExpand(),
+        enter = Anime.SubCategoryCard.expandIn,
+        exit = Anime.SubCategoryCard.shrinkOut
+    ) {
 
-            SubCategoryInfoText(
-                infoTitle = R.string.most_have_size, info = R.string.top_info
-            )
+        Surface(color = MaterialTheme.colors.background) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                SubCategoryInfoText( // TODO
+                    infoTitle = R.string.average_size, info = R.string.top_info
+                )
+
+                SubCategoryInfoText(
+                    infoTitle = R.string.most_have_size, info = R.string.top_info
+                )
+            }
         }
     }
 }
@@ -198,27 +186,5 @@ private fun RowScope.SubCategoryInfoText(@StringRes infoTitle: Int, @StringRes i
             SimpleHeightSpacer(dp = 2)
             Text(text = stringResource(id = info))
         }
-    }
-}
-
-data class SubCategoryCardState(
-    val subCategory: SubCategory,
-    val isSelectMode: Boolean,
-    val isAllExpand: Boolean,
-    val isChecked: Boolean,
-)
-
-@Composable
-fun rememberSubCategoryCardState(
-    subCategory: SubCategory,
-    subCategoryContentState: State<SubCategoryContentState>,
-) = remember(subCategory) {
-    derivedStateOf {
-        SubCategoryCardState(
-            subCategory = subCategory,
-            isSelectMode = subCategoryContentState.value.isSelectMode,
-            isAllExpand = subCategoryContentState.value.isAllExpand,
-            isChecked = subCategoryContentState.value.selectedSubCategories.contains(subCategory)
-        )
     }
 }

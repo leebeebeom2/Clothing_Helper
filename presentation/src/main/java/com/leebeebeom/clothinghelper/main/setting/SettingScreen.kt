e349d14052f7783f7d3ba7b8dda25135f7ebd6aa
leebeebeom <boole92@naver.com>
2022-11-09 12:24:06 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/setting/SettingScreen.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/setting/SettingScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.MaxWidthButton
-import com.leebeebeom.clothinghelper.base.rememberMaxWidthButtonState
 
 @Composable
 fun SettingScreen(
@@ -27,19 +26,15 @@ fun SettingScreen(
             .padding(horizontal = 20.dp)
             .padding(bottom = 20.dp),
         contentAlignment = Alignment.BottomCenter
-    ) {
-        SignOutButton(onClick = viewModel::signOut)
-    }
+    ) { SignOutButton(onClick = viewModel::signOut) }
 }
 
 @Composable
 private fun SignOutButton(onClick: () -> Unit) {
     MaxWidthButton(
-        state = rememberMaxWidthButtonState(
-            text = R.string.sign_out,
-            enabled = true,
-            colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error)
-        ),
+        text = R.string.sign_out,
+        enabled = { true },
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error),
         onClick = onClick
     )
 }

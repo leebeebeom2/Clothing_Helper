8cd434331a5b7f247099d98747771172fe59381a
leebeebeom <boole92@naver.com>
2022-11-09 23:52:15 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialog.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryDialog.kt
@@ -7,62 +7,43 @@ import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.text.input.TextFieldValue
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.*
-import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SubCategoryTextFieldDialog(
-    @StringRes titleRes: Int,
-    error: Int?,
-    textFieldValue: TextFieldValue,
-    positiveButtonEnabled: Boolean,
+    @StringRes title: Int,
+    @StringRes error: () -> Int?,
+    textFieldValue: () -> TextFieldValue,
+    positiveButtonEnabled: () -> Boolean,
+    showDialog: () -> Boolean,
     onValueChange: (TextFieldValue) -> Unit,
     onFocusChanged: (FocusState) -> Unit,
     onPositiveButtonClick: () -> Unit,
     onDismiss: () -> Unit
 ) {
-    DialogRoot(onDismiss = onDismiss) {
-        DialogTitle(text = titleRes)
+    if (showDialog())
+        DialogRoot(onDismiss = onDismiss) {
+            DialogTitle(text = title)
 
-        val maxWidthTextFieldState = rememberMaxWidthTextFiledState(
-            textFieldValue = textFieldValue,
-            label = R.string.category,
-            showKeyboardEnabled = true,
-            placeholder = R.string.category_place_holder
-        )
+            val textFieldState = rememberMaxWidthTextFiledState(
+                label = R.string.category,
+                showKeyboardEnabled = true,
+                placeholder = R.string.category_place_holder
+            )
 
-        MaxWidthTextField(
-            state = maxWidthTextFieldState,
-            error = error,
-            onValueChange = onValueChange,
-            onFocusChanged = onFocusChanged
-        )
-        SimpleHeightSpacer(dp = 12)
+            MaxWidthTextField(
+                textFieldValue = textFieldValue,
+                state = textFieldState,
+                error = error,
+                onValueChange = onValueChange,
+                onFocusChanged = onFocusChanged
+            )
+            SimpleHeightSpacer(dp = 12)
 
-        DialogTextButtons(
-            positiveButtonEnabled = positiveButtonEnabled,
-            onPositiveButtonClick = onPositiveButtonClick,
-            onDismiss = onDismiss
-        )
-    }
-}
-
-abstract class BaseSubCategoryTextFieldDialogState {
-    protected abstract val textState: MutableState<String>
-    protected abstract val textFieldValueState: MutableState<TextFieldValue>
-    protected abstract val errorState: MutableState<Int?>
-    protected abstract val subCategories: List<SubCategory>
-
-    val text get() = textState.value
-    val textFieldValue: TextFieldValue get() = textFieldValueState.value
-    val error get() = errorState.value
-    open val positiveButtonEnabled get() = text.isNotBlank() && error == null
-
-    open fun onValueChange(newTextFiled: TextFieldValue) {
-        if (textState.value != newTextFiled.text) errorState.value = null
-        textState.value = newTextFiled.text
-        textFieldValueState.value = newTextFiled
-        if (subCategories.map { it.name }.contains(newTextFiled.text)) errorState.value =
-            R.string.error_same_category_name
-    }
+            DialogTextButtons(
+                positiveButtonEnabled = positiveButtonEnabled,
+                onPositiveButtonClick = onPositiveButtonClick,
+                onDismiss = onDismiss
+            )
+        }
 }

109ca5aa8593a738cef88311bedce3281a209e51
leebeebeom <boole92@naver.com>
2022-11-09 12:14:40 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryViewModel.kt
@@ -2,15 +2,10 @@ package com.leebeebeom.clothinghelper.main.maincategory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.leebeebeom.clothinghelper.main.base.BaseSubCategoryUIState
-import com.leebeebeom.clothinghelperdomain.model.SubCategory
-import com.leebeebeom.clothinghelperdomain.model.User
+import com.leebeebeom.clothinghelper.main.base.BaseMainUIState
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoriesUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetSubCategoryLoadingStateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,26 +15,15 @@ class MainCategoryViewModel @Inject constructor(
     private val getSubCategoryLoadingStateUseCase: GetSubCategoryLoadingStateUseCase
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(MainCategoryUIState())
-    val uiState get() = _uiState.asStateFlow()
+    val uiStates = BaseMainUIState()
 
     init {
         viewModelScope.launch {
-            combine(
-                getSubCategoryLoadingStateUseCase(),
-                getSubCategoriesUseCase()
-            ) { isLoading, allSubCategories ->
-                MainCategoryUIState(isLoading = isLoading, allSubCategories = allSubCategories)
-            }.collect {
-                _uiState.value = it
-            }
+            getSubCategoryLoadingStateUseCase().collect(uiStates::updateIsLoading)
+        }
+
+        viewModelScope.launch {
+            getSubCategoriesUseCase().collect(uiStates::updateAllSubCategories)
         }
     }
 }
-
-data class MainCategoryUIState(
-    override val toastText: Int? = null,
-    override val user: User? = null,
-    override val allSubCategories: List<List<SubCategory>> = List(4) { emptyList() },
-    override val isLoading: Boolean = false
-) : BaseSubCategoryUIState()

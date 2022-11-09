3d90eda7c7896e061338ab864ecef763f9590a13
leebeebeom <boole92@naver.com>
2022-11-09 12:44:15 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryBottomAppbar.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryBottomAppbar.kt
@@ -3,14 +3,13 @@ package com.leebeebeom.clothinghelper.main.subcategory.content
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Transition
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -22,24 +21,29 @@ import com.leebeebeom.clothinghelper.base.CircleCheckBox
 import com.leebeebeom.clothinghelper.base.CustomIconButton
 import com.leebeebeom.clothinghelper.base.SimpleHeightSpacer
 import com.leebeebeom.clothinghelper.base.SimpleWidthSpacer
-import com.leebeebeom.clothinghelper.main.subcategory.SubCategoryStates
-import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun SubCategoryBottomAppBar(
-    state: State<SubCategoryBottomAppbarState>,
+    selectedSubCategoriesSize: () -> Int,
+    subCategoriesSize: () -> Int,
+    selectModeTransition: () -> Transition<Boolean>,
     onAllSelectCheckBoxClick: () -> Unit,
     onEditSubCategoryNameClick: () -> Unit
 ) {
-    AnimatedVisibility(
-        visible = state.value.isSelectMode,
+    val isAllSelected by remember { derivedStateOf { selectedSubCategoriesSize() == subCategoriesSize() } }
+    val showEditName by remember { derivedStateOf { selectedSubCategoriesSize() == 1 } }
+    val showDelete by remember { derivedStateOf { selectedSubCategoriesSize() > 0 } }
+
+    selectModeTransition().AnimatedVisibility(
+        visible = { it },
         enter = expandIn,
         exit = shrinkOut
     ) {
         BottomAppBar {
             SimpleWidthSpacer(dp = 4)
             CircleCheckBox(
-                isChecked = state.value.isAllSelected,
+                isChecked = { isAllSelected },
                 modifier = Modifier.size(22.dp),
                 onClick = onAllSelectCheckBoxClick
             )
@@ -47,24 +51,22 @@ fun SubCategoryBottomAppBar(
             Text(
                 text = stringResource(
                     id = R.string.count_selected,
-                    formatArgs = arrayOf(state.value.selectedSubCategoriesSize)
+                    formatArgs = arrayOf(selectedSubCategoriesSize())
                 ), modifier = Modifier.offset((-8).dp, 1.dp)
             )
             Row(modifier = Modifier.weight(1f), horizontalArrangement = Arrangement.End) {
-
-                if (state.value.showEditName)
-                    BottomAppBarIcon(
-                        onClick = onEditSubCategoryNameClick,
-                        drawable = R.drawable.ic_edit,
-                        text = R.string.change_name
-                    )
-
-                if (state.value.showDeleted)
-                    BottomAppBarIcon(
-                        onClick = { /*TODO*/ },
-                        drawable = R.drawable.ic_delete2,
-                        text = R.string.delete
-                    )
+                BottomAppBarIcon(
+                    visible = { showEditName },
+                    onClick = onEditSubCategoryNameClick,
+                    drawable = R.drawable.ic_edit,
+                    text = R.string.change_name
+                )
+                BottomAppBarIcon(
+                    visible = { showDelete },
+                    onClick = { /*TODO*/ },
+                    drawable = R.drawable.ic_delete2,
+                    text = R.string.delete
+                )
             }
             SimpleWidthSpacer(dp = 4)
         }
@@ -73,39 +75,17 @@ fun SubCategoryBottomAppBar(
 
 @Composable
 fun BottomAppBarIcon(
+    visible: () -> Boolean,
     onClick: () -> Unit,
     @DrawableRes drawable: Int,
     @StringRes text: Int
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        CustomIconButton(onClick = onClick, drawable = drawable)
-        SimpleHeightSpacer(dp = 4)
-        Text(text = stringResource(id = text), style = MaterialTheme.typography.caption)
-    }
-}
-
-data class SubCategoryBottomAppbarState(
-    val isSelectMode: Boolean,
-    val selectedSubCategoriesSize: Int,
-    val subCategoriesSize: Int,
-) {
-    val isAllSelected get() = selectedSubCategoriesSize == subCategoriesSize
-    val showEditName get() = selectedSubCategoriesSize == 1
-    val showDeleted get() = selectedSubCategoriesSize > 0
-}
-
-@Composable
-fun rememberSubCategoryBottomAppbarState(
-    subCategoryStates: SubCategoryStates,
-    subCategoriesState: State<List<SubCategory>>
-) = remember {
-    derivedStateOf {
-        SubCategoryBottomAppbarState(
-            isSelectMode = subCategoryStates.isSelectMode,
-            selectedSubCategoriesSize = subCategoryStates.selectedSubCategoriesSize,
-            subCategoriesSize = subCategoriesState.value.size
-        )
-    }
+    if (visible())
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CustomIconButton(onClick = onClick, drawable = drawable)
+            SimpleHeightSpacer(dp = 4)
+            Text(text = stringResource(id = text), style = MaterialTheme.typography.caption)
+        }
 }

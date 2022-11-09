9e6abde6be2e49df56ff8ce3eea73dff7c63bf7c
leebeebeom <boole92@naver.com>
2022-11-09 10:20:27 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/base/MaxWidthButton.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/base/MaxWidthButton.kt
@@ -6,16 +6,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.leebeebeom.clothinghelper.R
 
 @Composable
 fun MaxWidthButton(

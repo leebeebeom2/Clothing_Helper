e2d6c26982de381052b159d5adbd0af34d718392
leebeebeom <boole92@naver.com>
2022-11-09 11:43:00 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/base/BaseUIState.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/base/BaseUIState.kt
@@ -1,5 +1,22 @@
 package com.leebeebeom.clothinghelper.base
 
-abstract class BaseUIState {
-    abstract val toastText: Int?
+import androidx.annotation.StringRes
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+
+open class BaseUIState {
+    @StringRes
+    private val _toastText: MutableState<Int?> = mutableStateOf(null)
+
+    val toastText by derivedStateOf { _toastText.value }
+
+    fun showToast(@StringRes text: Int?) {
+        _toastText.value = text
+    }
+
+    fun toastShown() {
+        _toastText.value = null
+    }
 }

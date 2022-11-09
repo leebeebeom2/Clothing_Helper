6ba7a9dc6c062ac67581c761c2f505e53c3549b9
leebeebeom <boole92@naver.com>
2022-11-09 23:31:50 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/Components.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/Components.kt
@@ -8,10 +8,7 @@ import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.LocalContentColor
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -45,10 +42,10 @@ fun AllExpandIcon(
 @Composable
 fun isExpandStateWithIsAllExpand(isAllExpand: () -> Boolean): MutableState<Boolean> {
     val isExpandState = rememberSaveable { mutableStateOf(isAllExpand()) }
-    val rememberedIsAllExpandState = rememberSaveable { mutableStateOf(isAllExpand()) }
-    if (isAllExpand() != rememberedIsAllExpandState.value) {
+    var rememberedIsAllExpandState by rememberSaveable { mutableStateOf(isAllExpand()) }
+    if (isAllExpand() != rememberedIsAllExpandState) {
         isExpandState.value = isAllExpand()
-        rememberedIsAllExpandState.value = isAllExpand()
+        rememberedIsAllExpandState = isAllExpand()
     }
     return isExpandState
 }

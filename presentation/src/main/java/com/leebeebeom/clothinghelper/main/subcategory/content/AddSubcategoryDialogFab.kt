54202fe0df12bfb9df07b7085ef5418cd8c9c88a
leebeebeom <boole92@naver.com>
2022-11-09 23:52:03 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/AddSubcategoryDialogFab.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/AddSubcategoryDialogFab.kt
@@ -1,15 +1,12 @@
 package com.leebeebeom.clothinghelper.main.subcategory.content
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
@@ -18,16 +15,14 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.SimpleIcon
-import com.leebeebeom.clothinghelper.main.subcategory.BaseSubCategoryTextFieldDialogState
 import com.leebeebeom.clothinghelper.main.subcategory.SubCategoryTextFieldDialog
-import com.leebeebeom.clothinghelperdomain.model.SubCategory
 
 @Composable
 fun BoxScope.AddSubcategoryDialogFab(
     onPositiveButtonClick: (String) -> Unit,
-    subCategories: List<SubCategory>,
+    isExistsName: (String) -> Boolean,
 ) {
-    val state = rememberAddCategoryDialogState(subCategories = subCategories)
+    val state = remember { AddCategoryDialogState() }
 
     FloatingActionButton(
         modifier = Modifier
@@ -38,65 +33,60 @@ fun BoxScope.AddSubcategoryDialogFab(
         backgroundColor = MaterialTheme.colors.primary,
     ) { SimpleIcon(drawable = R.drawable.ic_add) }
 
-    if (state.showDialog)
-        SubCategoryTextFieldDialog(
-            titleRes = R.string.add_category,
-            error = state.error,
-            textFieldValue = state.textFieldValue,
-            positiveButtonEnabled = state.positiveButtonEnabled,
-            onValueChange = state::onValueChange,
-            onFocusChanged = state::onFocusChange,
-            onDismiss = state::onDismiss,
-            onPositiveButtonClick = { onPositiveButtonClick(state.text) }
-        )
+    SubCategoryTextFieldDialog(
+        title = R.string.add_category,
+        error = { state.error },
+        textFieldValue = { state.textFieldValue },
+        positiveButtonEnabled = { state.positiveButtonEnabled },
+        showDialog = { state.showDialog },
+        onValueChange = {
+            state.onValueChange(it)
+            if (isExistsName(it.text)) state.updateError(R.string.error_same_category_name)
+        },
+        onFocusChanged = state::onFocusChange,
+        onDismiss = state::onDismiss,
+        onPositiveButtonClick = { onPositiveButtonClick(state.text) }
+    )
 }
 
-data class AddCategoryDialogState(
-    override val textState: MutableState<String>,
-    override val textFieldValueState: MutableState<TextFieldValue>,
-    private val showDialogState: MutableState<Boolean>,
-    override val errorState: MutableState<Int?>,
-    override val subCategories: List<SubCategory>
-) : BaseSubCategoryTextFieldDialogState() {
-    val showDialog get() = showDialogState.value
+class AddCategoryDialogState(initialText: String = "") {
+    var text: String = initialText
+        private set
+
+    var textFieldValue by mutableStateOf(TextFieldValue(text))
+        private set
+
+    var error: Int? by mutableStateOf(null)
+        private set
+
+    var showDialog by mutableStateOf(false)
+        private set
 
     fun showDialog() {
-        showDialogState.value = true
+        showDialog = true
+    }
+
+    fun updateError(@StringRes error: Int) {
+        this.error = error
+    }
+
+    val positiveButtonEnabled by derivedStateOf { text.isNotBlank() && error == null }
+
+    fun onValueChange(newTextFiled: TextFieldValue) {
+        if (text != newTextFiled.text) error = null
+        text = newTextFiled.text
+        textFieldValue = newTextFiled
     }
 
     fun onDismiss() {
-        showDialogState.value = false
-        textState.value = ""
-        textFieldValueState.value = TextFieldValue("")
-        errorState.value = null
+        showDialog = false
+        text = ""
+        textFieldValue = TextFieldValue("")
+        error = null
     }
 
     fun onFocusChange(newFocusState: FocusState) {
         if (newFocusState.hasFocus)
-            textFieldValueState.value =
-                textFieldValueState.value.copy(
-                    selection = TextRange(
-                        textFieldValueState.value.text.length
-                    )
-                )
+            textFieldValue = textFieldValue.copy(selection = TextRange(text.length))
     }
-}
-
-@Composable
-fun rememberAddCategoryDialogState(
-    textState: MutableState<String> = rememberSaveable { mutableStateOf("") },
-    textFieldValueState: MutableState<TextFieldValue> = remember {
-        mutableStateOf(TextFieldValue(textState.value))
-    },
-    errorState: MutableState<Int?> = rememberSaveable { mutableStateOf(null) },
-    showDialogState: MutableState<Boolean> = rememberSaveable { mutableStateOf(false) },
-    subCategories: List<SubCategory>
-) = remember(subCategories) {
-    AddCategoryDialogState(
-        textState = textState,
-        textFieldValueState = textFieldValueState,
-        errorState = errorState,
-        showDialogState = showDialogState,
-        subCategories = subCategories
-    )
 }

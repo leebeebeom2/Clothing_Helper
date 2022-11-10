8fee8f0bc8e0d893e0c3d13d8ce9d125e152555f
leebeebeom <boole92@naver.com>
2022-11-11 00:22:59 +0900

    코드 정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/util/ExtensionFuntions.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/util/ExtensionFuntions.kt
@@ -2,10 +2,10 @@ package com.leebeebeom.clothinghelper.util
 
 import androidx.navigation.NavHostController
 
-inline fun <T> Collection<T>.taskAndReturn(crossinline task: (MutableList<T>) -> Unit): Set<T> {
-    val temp = toMutableList()
+inline fun <T> Collection<T>.taskAndReturnSet(crossinline task: (MutableSet<T>) -> Unit): Set<T> {
+    val temp = toMutableSet()
     task(temp)
-    return temp.toSet()
+    return temp
 }
 
 fun NavHostController.navigateSingleTop(route: String) = navigate(route) {

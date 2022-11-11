deae50947d946e8b2edf01a7bf076fef6110be66
leebeebeom <boole92@naver.com>
2022-11-12 00:55:11 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/util/ExtensionFuntions.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/util/ExtensionFuntions.kt
@@ -1,11 +1,13 @@
 package com.leebeebeom.clothinghelper.util
 
 import androidx.navigation.NavHostController
+import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.collections.immutable.toImmutableSet
 
-inline fun <T> Collection<T>.taskAndReturnSet(crossinline task: (MutableSet<T>) -> Unit): Set<T> {
+inline fun <T> Collection<T>.taskAndReturnSet(crossinline task: (MutableSet<T>) -> Unit): ImmutableSet<T> {
     val temp = toMutableSet()
     task(temp)
-    return temp
+    return temp.toImmutableSet()
 }
 
 fun NavHostController.navigateSingleTop(route: String) = navigate(route) {

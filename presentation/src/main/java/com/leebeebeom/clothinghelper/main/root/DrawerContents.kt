2a69e089dbd7559df21491eac5e34ce9bf256203
leebeebeom <boole92@naver.com>
2022-11-13 23:12:27 +0900

    드로어 로우 롱클릭 구현

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/DrawerContents.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/DrawerContents.kt
@@ -1,6 +1,10 @@
 package com.leebeebeom.clothinghelper.main.root
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,7 +15,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
@@ -115,14 +123,35 @@ private fun EssentialMenu(
 fun DrawerContentRow(
     modifier: Modifier,
     onClick: () -> Unit,
+    onLongClick: () -> Unit = {},
+    onLongPress: (Offset) -> Unit = {},
     content: @Composable RowScope.() -> Unit
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val haptic = LocalHapticFeedback.current
+
     Row(
         modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 8.dp)
             .clip(RoundedCornerShape(12.dp))
-            .clickable(onClick = onClick)
+            .indication(interactionSource = interactionSource, LocalIndication.current)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onPress = {
+                        val press = PressInteraction.Press(it)
+                        interactionSource.emit(press)
+                        tryAwaitRelease()
+                        interactionSource.emit(PressInteraction.Release(press))
+                    },
+                    onTap = { onClick() },
+                    onLongPress = {
+                        onLongPress(it)
+                        onLongClick()
+                        haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                    },
+                )
+            }
             .padding(start = 4.dp),
         verticalAlignment = Alignment.CenterVertically,
         content = content

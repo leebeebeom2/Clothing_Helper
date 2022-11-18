d07967ddeac1c813828dc78a857f3042f4d6d99d
leebeebeom <boole92@naver.com>
2022-11-19 03:07:13 +0900

    정렬 EDIT 추가

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/subcategory/GetAllSubCategoriesUseCase.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/subcategory/GetAllSubCategoriesUseCase.kt
@@ -34,6 +34,8 @@ private fun getSortSubCategories(
         sort == Sort.NAME && order == Order.DESCENDING -> subCategories.sortedByDescending { it.name }
         sort == Sort.CREATE && order == Order.ASCENDING -> subCategories.sortedBy { it.createDate }
         sort == Sort.CREATE && order == Order.DESCENDING -> subCategories.sortedByDescending { it.createDate }
+        sort == Sort.EDIT && order == Order.ASCENDING -> subCategories.sortedBy { it.editDate }
+        sort == Sort.EDIT && order == Order.DESCENDING -> subCategories.sortedByDescending { it.editDate }
         else -> subCategories
     }
 }

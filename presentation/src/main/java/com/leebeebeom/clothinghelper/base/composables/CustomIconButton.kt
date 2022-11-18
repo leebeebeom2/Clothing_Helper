34ce7b8619e2bbaa68431f4eee9dfd57958b185c
leebeebeom <boole92@naver.com>
2022-11-19 03:33:06 +0900

    정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/base/composables/CustomIconButton.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/base/composables/CustomIconButton.kt
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.LocalContentAlpha
@@ -14,6 +15,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,6 +23,7 @@ fun CustomIconButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
     @DrawableRes drawable: Int,
+    size:Dp = Dp.Unspecified,
     tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current),
     contentDescription: String? = null
 ) {
@@ -29,6 +32,7 @@ fun CustomIconButton(
         onClick = onClick,
         painter = painterResource(id = drawable),
         tint = tint,
+        size = size,
         contentDescription = contentDescription
     )
 }
@@ -38,11 +42,12 @@ fun CustomIconButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
     painter: Painter,
+    size: Dp = Dp.Unspecified,
     tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current),
     contentDescription: String? = null
 ) {
     Box(
-        modifier = Modifier
+        modifier = modifier
             .clip(CircleShape)
             .clickable(onClick = onClick)
             .padding(2.dp)
@@ -50,7 +55,7 @@ fun CustomIconButton(
         Icon(
             painter = painter,
             contentDescription = contentDescription,
-            modifier = modifier,
+            modifier = Modifier.size(size),
             tint = tint
         )
     }

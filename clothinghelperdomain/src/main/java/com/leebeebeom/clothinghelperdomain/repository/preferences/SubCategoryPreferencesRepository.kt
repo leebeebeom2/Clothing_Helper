5a9ab9a834911454ece45fbb0aedbaacbd2aef2b
leebeebeom <boole92@naver.com>
2022-11-19 05:57:37 +0900

    추상화

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/preferences/SubCategoryPreferencesRepository.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/preferences/SubCategoryPreferencesRepository.kt
@@ -1,8 +1,3 @@
 package com.leebeebeom.clothinghelperdomain.repository.preferences
 
-import com.leebeebeom.clothinghelperdomain.model.Order
-import com.leebeebeom.clothinghelperdomain.model.Sort
-import com.leebeebeom.clothinghelperdomain.model.SortPreferences
-import kotlinx.coroutines.flow.Flow
-
 interface SubCategoryPreferencesRepository : SortPreferenceRepository

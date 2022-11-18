e300e384a6a7ac74f6c41488d8e1114ae5dfe5c0
leebeebeom <boole92@naver.com>
2022-11-19 07:16:31 +0900

    분리

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/base/AddAndEditContainer.kt
@@ -0,0 +1,22 @@
+package com.leebeebeom.clothinghelper.base
+
+import androidx.annotation.StringRes
+import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
+import kotlinx.coroutines.TimeoutCancellationException
+
+interface AddAndEditContainer {
+    fun showToast(@StringRes text: Int)
+    val uid: String?
+
+    fun showToastWhenFail(
+        result: FirebaseResult,
+        @StringRes networkFail: Int,
+        @StringRes fail: Int
+    ) {
+        if (result is FirebaseResult.Fail)
+            when (result.exception) {
+                is TimeoutCancellationException -> showToast(networkFail)
+                else -> showToast(fail)
+            }
+    }
+}

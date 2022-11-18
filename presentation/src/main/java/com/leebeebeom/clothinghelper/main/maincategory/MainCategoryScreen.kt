054024414c1067a7a9ad09e066d1795283a08df4
leebeebeom <boole92@naver.com>
2022-11-19 00:05:52 +0900

    정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryScreen.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/maincategory/MainCategoryScreen.kt
@@ -3,24 +3,15 @@ package com.leebeebeom.clothinghelper.main.maincategory
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.composables.DotProgressIndicator
-import com.leebeebeom.clothinghelper.base.composables.SimpleIcon
-import com.leebeebeom.clothinghelper.base.composables.SingleLineText
 import com.leebeebeom.clothinghelper.main.base.BaseMainUIState
-import com.leebeebeom.clothinghelper.main.root.model.MainCategory
 import com.leebeebeom.clothinghelper.main.root.model.getMainCategories
 import com.leebeebeom.clothinghelperdomain.model.container.SubCategoryParent
 

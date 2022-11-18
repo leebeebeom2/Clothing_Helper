185bb73f5307a4540f381af3c825577f6d8bcbff
leebeebeom <boole92@naver.com>
2022-11-19 02:50:29 +0900

    분리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/header/SubCategoryHeader.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/header/SubCategoryHeader.kt
@@ -2,7 +2,6 @@ package com.leebeebeom.clothinghelper.main.subcategory.content.header
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Divider
 import androidx.compose.material.LocalContentColor
@@ -13,33 +12,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.CustomIconButton
-import com.leebeebeom.clothinghelper.base.SingleLineText
-import com.leebeebeom.clothinghelper.main.base.AllExpandIcon
+import com.leebeebeom.clothinghelper.base.composables.CustomIconButton
+import com.leebeebeom.clothinghelper.base.composables.SingleLineText
 import com.leebeebeom.clothinghelper.util.getHeaderStringRes
-import com.leebeebeom.clothinghelperdomain.model.SubCategoryParent
-import com.leebeebeom.clothinghelperdomain.repository.SortOrder
-import com.leebeebeom.clothinghelperdomain.repository.SubCategorySort
-import com.leebeebeom.clothinghelperdomain.repository.SubCategorySortPreferences
+import com.leebeebeom.clothinghelperdomain.model.Order
+import com.leebeebeom.clothinghelperdomain.model.container.SubCategoryParent
+import com.leebeebeom.clothinghelperdomain.model.Sort
+import com.leebeebeom.clothinghelperdomain.model.SortPreferences
 
 @Composable
 fun SubCategoryHeader(
     parent: SubCategoryParent,
-    isAllExpanded: () -> Boolean,
-    sort: () -> SubCategorySortPreferences,
-    allExpandIconClick: () -> Unit,
-    onSortClick: (SubCategorySort) -> Unit,
-    onOrderClick: (SortOrder) -> Unit
+    sort: () -> SortPreferences,
+    onSortClick: (Sort) -> Unit,
+    onOrderClick: (Order) -> Unit
 ) {
-    HeaderText(parent = parent)
+    SingleLineText(
+        text = getHeaderStringRes(parent),
+        style = MaterialTheme.typography.h2.copy(fontSize = 32.sp)
+    )
 
     Row(verticalAlignment = Alignment.CenterVertically) {
         Divider(modifier = Modifier.weight(1f))
 
-        AllExpandIcon(
-            allExpandIconClick = allExpandIconClick,
-            isAllExpanded = isAllExpanded
-        )
         SortIcon(
             sort = sort,
             onSortClick = onSortClick,
@@ -49,30 +44,10 @@ fun SubCategoryHeader(
 }
 
 @Composable
-fun HeaderText(parent: SubCategoryParent) {
-    SingleLineText(
-        text = getHeaderStringRes(parent),
-        style = MaterialTheme.typography.h2.copy(fontSize = 32.sp)
-    )
-}
-
-@Composable
-private fun AllExpandIcon(allExpandIconClick: () -> Unit, isAllExpanded: () -> Boolean) {
-    Box(modifier = Modifier.offset(0.dp, 0.dp)) {
-        AllExpandIcon(
-            size = 22.dp,
-            onClick = allExpandIconClick,
-            tint = LocalContentColor.current.copy(0.5f),
-            isAllExpanded = isAllExpanded
-        )
-    }
-}
-
-@Composable
 private fun SortIcon(
-    sort: () -> SubCategorySortPreferences,
-    onSortClick: (SubCategorySort) -> Unit,
-    onOrderClick: (SortOrder) -> Unit
+    sort: () -> SortPreferences,
+    onSortClick: (Sort) -> Unit,
+    onOrderClick: (Order) -> Unit
 ) {
     var showDropDownMenu by remember { mutableStateOf(false) }
 

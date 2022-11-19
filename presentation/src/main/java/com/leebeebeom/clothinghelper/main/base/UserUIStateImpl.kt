96529a407c5e5fdd7095b4ec0fb740647bf0e0b8
leebeebeom <boole92@naver.com>
2022-11-19 14:29:31 +0900

    상속 로직 변경

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/UserUIStateImpl.kt
@@ -0,0 +1,22 @@
+package com.leebeebeom.clothinghelper.main.base
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.leebeebeom.clothinghelper.map.StableUser
+import com.leebeebeom.clothinghelper.map.toStable
+import com.leebeebeom.clothinghelperdomain.model.User
+
+interface UserUIState {
+    val user: StableUser?
+    fun updateUser(user: User?)
+}
+
+class UserUIStateImpl : UserUIState {
+    override var user: StableUser? by mutableStateOf(null)
+        private set
+
+    override fun updateUser(user: User?) {
+        this.user = user?.toStable()
+    }
+}

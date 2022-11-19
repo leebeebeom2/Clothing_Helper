95b15e59390226b603a79977b941301795af325e
leebeebeom <boole92@naver.com>
2022-11-19 14:39:54 +0900

    수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/map/StableFolder.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/map/StableFolder.kt
@@ -2,6 +2,14 @@ package com.leebeebeom.clothinghelper.map
 
 import com.leebeebeom.clothinghelperdomain.model.container.Folder
 
-data class StableFolder(val parentKey: String = "", val name: String = "")
+data class StableFolder(
+    val parentKey: String = "",
+    val createDate: Long = 0,
+    val name: String = "",
+    val key: String = "",
+    val editDate: Long = 0
+)
 
-fun Folder.toStable() = StableFolder(parentKey, name)
+fun Folder.toStable() = StableFolder(parentKey, createDate, name, key, editDate)
+
+fun StableFolder.toStable() = Folder(parentKey, createDate, name, key, editDate)

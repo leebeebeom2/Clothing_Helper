db1bbfc641132ccec04ca2bfee18ea6105b93b88
leebeebeom <boole92@naver.com>
2022-11-19 22:57:45 +0900

    로드 로직 수정

---
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/LoadDataUseCase.kt
@@ -0,0 +1,27 @@
+package com.leebeebeom.clothinghelperdomain.usecase
+
+import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
+import com.leebeebeom.clothinghelperdomain.repository.FolderRepository
+import com.leebeebeom.clothinghelperdomain.repository.SubCategoryRepository
+import com.leebeebeom.clothinghelperdomain.usecase.user.GetUserUseCase
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.flow.collectLatest
+import javax.inject.Inject
+
+@ViewModelScoped
+class LoadDataUseCase @Inject constructor(
+    private val getUserUseCase: GetUserUseCase,
+    private val subCategoryRepository: SubCategoryRepository,
+    private val folderRepository: FolderRepository
+) {
+    suspend fun load(onUpdateSubCategoriesFail: (FirebaseResult) -> Unit) =
+        getUserUseCase.user.collectLatest {
+            val subCategoryResult = subCategoryRepository.loadSubCategories(it?.uid)
+            val folderResult = folderRepository.loadFolders(it?.uid)
+            when {
+                subCategoryResult is FirebaseResult.Fail ->
+                    onUpdateSubCategoriesFail(subCategoryResult)
+                folderResult is FirebaseResult.Fail -> onUpdateSubCategoriesFail(folderResult)
+            }
+        }
+}

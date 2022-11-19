dde73dd25cfeea0a81f4b327be914359dcef638d
leebeebeom <boole92@naver.com>
2022-11-20 01:45:51 +0900

    서브 카테고리 UI State 로직 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/dialogs/EditSubCategoryNameDialog.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/dialogs/EditSubCategoryNameDialog.kt
@@ -1,18 +1,22 @@
 package com.leebeebeom.clothinghelper.main.subcategory.dialogs
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.dialogs.EditDialog
 import com.leebeebeom.clothinghelper.base.dialogs.EditDialogState
 import com.leebeebeom.clothinghelper.map.StableSubCategory
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 
 @Composable
 fun EditSubCategoryNameDialog(
     showDialog: () -> Boolean,
     subCategory: () -> StableSubCategory?,
-    subCategoryNames: () -> ImmutableList<String>,
+    subCategories: () -> ImmutableList<StableSubCategory>,
     onPositiveButtonClick: (StableSubCategory) -> Unit,
     onDismiss: () -> Unit
 ) {
@@ -20,6 +24,12 @@ fun EditSubCategoryNameDialog(
         subCategory()?.let { selectedSubCategory ->
             val state = rememberSaveable(saver = EditDialogState.Saver) {
                 EditDialogState(selectedSubCategory.name)
+            }
+
+            val names by remember {
+                derivedStateOf {
+                    subCategories().map { it.name }.toImmutableList()
+                }
             }
 
             EditDialog(
@@ -30,7 +40,7 @@ fun EditSubCategoryNameDialog(
                 title = R.string.edit_category_name,
                 onDismiss = onDismiss,
                 onPositiveButtonClick = { onPositiveButtonClick(selectedSubCategory.copy(name = state.text.trim())) },
-                names = subCategoryNames,
+                names = { names },
                 existNameError = R.string.error_exist_category_name
             )
         }

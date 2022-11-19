3020ac5850e59ab89cca445a077b0d87f4fc17cc
leebeebeom <boole92@naver.com>
2022-11-19 23:55:09 +0900

    정렬 아이콘 불리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryHeader.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/SubCategoryHeader.kt
@@ -1,19 +1,10 @@
 package com.leebeebeom.clothinghelper.main.subcategory.content
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Divider
-import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.sp
-import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.composables.CustomIconButton
 import com.leebeebeom.clothinghelper.base.composables.SingleLineText
-import com.leebeebeom.clothinghelper.main.base.SortDropdownMenu
+import com.leebeebeom.clothinghelper.base.composables.SortIconWithDivider
 import com.leebeebeom.clothinghelper.util.getHeaderStringRes
 import com.leebeebeom.clothinghelperdomain.model.Order
 import com.leebeebeom.clothinghelperdomain.model.Sort
@@ -32,39 +23,5 @@ fun SubCategoryHeader(
         style = MaterialTheme.typography.h2.copy(fontSize = 32.sp)
     )
 
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Divider(modifier = Modifier.weight(1f))
-
-        SortIcon(
-            sort = sort,
-            onSortClick = onSortClick,
-            onOrderClick = onOrderClick
-        )
-    }
-}
-
-@Composable
-private fun SortIcon(
-    sort: () -> SortPreferences,
-    onSortClick: (Sort) -> Unit,
-    onOrderClick: (Order) -> Unit
-) {
-    var showDropDownMenu by remember { mutableStateOf(false) }
-
-    Box {
-        CustomIconButton(
-            onClick = { showDropDownMenu = true },
-            drawable = R.drawable.ic_sort,
-            tint = LocalContentColor.current.copy(0.5f),
-            size = 22.dp
-        )
-
-        SortDropdownMenu(
-            show = { showDropDownMenu },
-            sort = sort,
-            onSortClick = onSortClick,
-            onOrderClick = onOrderClick,
-            onDismiss = { showDropDownMenu = false }
-        )
-    }
+    SortIconWithDivider(sort, onSortClick, onOrderClick)
 }

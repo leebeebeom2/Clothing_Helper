575b8b0b7497411ad2b8202e5eedd2f00d738dbc
leebeebeom <boole92@naver.com>
2022-11-20 01:40:32 +0900

    서브 카테고리 UI State 로직 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/SubCategoryUIState.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/SubCategoryUIState.kt
@@ -14,87 +14,21 @@ import kotlinx.collections.immutable.toImmutableList
 interface SubCategoryUIState {
     val allSubCategories: List<StableSubCategory>
 
-    val topSubCategories: ImmutableList<StableSubCategory>
-    val bottomSubCategories: ImmutableList<StableSubCategory>
-    val outerSubCategories: ImmutableList<StableSubCategory>
-    val etcSubCategories: ImmutableList<StableSubCategory>
-
-    val topSubCategoriesSize: Int
-    val bottomSubCategoriesSize: Int
-    val outerSubCategoriesSize: Int
-    val etcSubCategoriesSize: Int
-
-    val topSubCategoryNames: ImmutableList<String>
-    val bottomSubCategoryNames: ImmutableList<String>
-    val outerSubCategoryNames: ImmutableList<String>
-    val etcSubCategoryNames: ImmutableList<String>
-
     fun loadAllSubCategories(allSubCategories: List<SubCategory>)
 
-    fun getSubCategories(subCategoryParent: SubCategoryParent): ImmutableList<StableSubCategory> {
-        return when (subCategoryParent) {
-            SubCategoryParent.TOP -> topSubCategories
-            SubCategoryParent.BOTTOM -> bottomSubCategories
-            SubCategoryParent.OUTER -> outerSubCategories
-            SubCategoryParent.ETC -> etcSubCategories
-        }
-    }
-
-    fun getSubCategoriesSize(subCategoryParent: SubCategoryParent): Int {
-        return when (subCategoryParent) {
-            SubCategoryParent.TOP -> topSubCategoriesSize
-            SubCategoryParent.BOTTOM -> bottomSubCategoriesSize
-            SubCategoryParent.OUTER -> outerSubCategoriesSize
-            SubCategoryParent.ETC -> etcSubCategoriesSize
-        }
-    }
-
-    fun getSubCategoryNames(subCategoryParent: SubCategoryParent): ImmutableList<String> {
-        return when (subCategoryParent) {
-            SubCategoryParent.TOP -> topSubCategoryNames
-            SubCategoryParent.BOTTOM -> bottomSubCategoryNames
-            SubCategoryParent.OUTER -> outerSubCategoryNames
-            SubCategoryParent.ETC -> etcSubCategoryNames
-        }
-    }
+    fun getSubCategories(subCategoryParent: SubCategoryParent): ImmutableList<StableSubCategory>
 }
 
 class SubCategoryUIStateImpl : SubCategoryUIState {
     override var allSubCategories by mutableStateOf(emptyList<StableSubCategory>())
         private set
 
-    override val topSubCategories by derivedStateOf {
-        allSubCategories.filter { it.parent == SubCategoryParent.TOP }.toImmutableList()
-    }
-    override val bottomSubCategories by derivedStateOf {
-        allSubCategories.filter { it.parent == SubCategoryParent.BOTTOM }.toImmutableList()
-    }
-    override val outerSubCategories by derivedStateOf {
-        allSubCategories.filter { it.parent == SubCategoryParent.OUTER }.toImmutableList()
-    }
-    override val etcSubCategories by derivedStateOf {
-        allSubCategories.filter { it.parent == SubCategoryParent.ETC }.toImmutableList()
-    }
-
-    override val topSubCategoriesSize by derivedStateOf { topSubCategories.size }
-    override val bottomSubCategoriesSize by derivedStateOf { bottomSubCategories.size }
-    override val outerSubCategoriesSize by derivedStateOf { outerSubCategories.size }
-    override val etcSubCategoriesSize by derivedStateOf { etcSubCategories.size }
-
-    override val topSubCategoryNames by derivedStateOf {
-        topSubCategories.map { it.name }.toImmutableList()
-    }
-    override val bottomSubCategoryNames by derivedStateOf {
-        bottomSubCategories.map { it.name }.toImmutableList()
-    }
-    override val outerSubCategoryNames by derivedStateOf {
-        outerSubCategories.map { it.name }.toImmutableList()
-    }
-    override val etcSubCategoryNames by derivedStateOf {
-        etcSubCategories.map { it.name }.toImmutableList()
-    }
-
     override fun loadAllSubCategories(allSubCategories: List<SubCategory>) {
         this.allSubCategories = allSubCategories.map { it.toStable() }
     }
+
+    override fun getSubCategories(subCategoryParent: SubCategoryParent): ImmutableList<StableSubCategory> =
+        derivedStateOf {
+            allSubCategories.filter { it.parent == subCategoryParent }.toImmutableList()
+        }.value
 }

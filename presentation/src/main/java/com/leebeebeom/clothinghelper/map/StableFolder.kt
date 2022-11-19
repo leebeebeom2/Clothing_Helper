0fadbb9ee23dfb2df20d0e7ddd5d4528c0e61dc1
leebeebeom <boole92@naver.com>
2022-11-20 02:56:25 +0900

    수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/map/StableFolder.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/map/StableFolder.kt
@@ -1,14 +1,15 @@
 package com.leebeebeom.clothinghelper.map
 
+import com.leebeebeom.clothinghelperdomain.model.container.BaseContainer
 import com.leebeebeom.clothinghelperdomain.model.container.Folder
 
 data class StableFolder(
     val parentKey: String = "",
-    val createDate: Long = 0,
-    val name: String = "",
-    val key: String = "",
-    val editDate: Long = 0
-)
+    override val createDate: Long = 0,
+    override val name: String = "",
+    override val key: String = "",
+    override val editDate: Long = 0
+) : BaseContainer()
 
 fun Folder.toStable() = StableFolder(parentKey, createDate, name, key, editDate)
 
--- presentation/src/main/java/com/leebeebeom/clothinghelper/map/StableSubCategory.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/map/StableSubCategory.kt
@@ -1,17 +1,32 @@
 package com.leebeebeom.clothinghelper.map
 
+import com.leebeebeom.clothinghelperdomain.model.container.BaseContainer
 import com.leebeebeom.clothinghelperdomain.model.container.SubCategory
 import com.leebeebeom.clothinghelperdomain.model.container.SubCategoryParent
 
 data class StableSubCategory(
     val parent: SubCategoryParent = SubCategoryParent.TOP,
-    val key: String = "",
-    val name: String = "",
-    val createDate: Long = 0
-)
+    override val key: String = "",
+    override val name: String = "",
+    override val createDate: Long = 0,
+    override val editDate: Long = 0
+) : BaseContainer() {
+}
 
 fun SubCategory.toStable() =
-    StableSubCategory(parent = parent, key = key, name = name, createDate = createDate)
+    StableSubCategory(
+        parent = parent,
+        key = key,
+        name = name,
+        createDate = createDate,
+        editDate = editDate
+    )
 
 fun StableSubCategory.toUnstable() =
-    SubCategory(parent = parent, key = key, name = name, createDate = createDate)
+    SubCategory(
+        parent = parent,
+        key = key,
+        name = name,
+        createDate = createDate,
+        editDate = editDate
+    )

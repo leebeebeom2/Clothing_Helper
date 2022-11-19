f3857089abc7e407cc591194645b93e84dafe8de
leebeebeom <boole92@naver.com>
2022-11-20 02:57:55 +0900

    셀렉트 모드 분리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/SubCategoryViewModel.kt
@@ -1,6 +1,5 @@
 package com.leebeebeom.clothinghelper.main.subcategory
 
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -12,20 +11,17 @@ import com.leebeebeom.clothinghelper.main.base.interfaces.*
 import com.leebeebeom.clothinghelper.main.subcategory.interfaces.AddSubCategory
 import com.leebeebeom.clothinghelper.main.subcategory.interfaces.EditSubCategoryName
 import com.leebeebeom.clothinghelper.map.StableSubCategory
-import com.leebeebeom.clothinghelper.util.taskAndReturnSet
 import com.leebeebeom.clothinghelperdomain.model.Order
 import com.leebeebeom.clothinghelperdomain.model.Sort
 import com.leebeebeom.clothinghelperdomain.model.container.SubCategoryParent
+import com.leebeebeom.clothinghelperdomain.usecase.GetDataLoadingStateUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.preferences.sort.SubCategorySortUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.AddSubCategoryUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.EditSubCategoryNameUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.GetAllSubCategoriesUseCase
-import com.leebeebeom.clothinghelperdomain.usecase.GetDataLoadingStateUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.user.GetUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.toImmutableList
-import kotlinx.collections.immutable.toImmutableSet
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -44,7 +40,7 @@ class SubCategoryViewModel @Inject constructor(
     private val uiState = SubCategoryScreenUIState()
 
     fun getUiState(parent: SubCategoryParent): SubCategoryScreenUIState {
-        uiState.setParent(parent)
+        uiState.setSubCategories(parent)
         return uiState
     }
 
@@ -82,57 +78,13 @@ class SubCategoryScreenUIState :
     LoadingUIState by LoadingUIStateImpl(),
     UserUIState by UserUIStateImpl(),
     SubCategoryUIState by SubCategoryUIStateImpl(),
-    SortUIState by SortUIStateImpl() {
-    var parent = SubCategoryParent.TOP
+    SortUIState by SortUIStateImpl(),
+    SelectMode<StableSubCategory> by SelectModeImpl() {
+
+    override var list by mutableStateOf(emptyList<StableSubCategory>().toImmutableList())
         private set
 
-    var isSelectMode by mutableStateOf(false)
-        private set
-
-    var selectedSubCategoryKeys by mutableStateOf(linkedSetOf<String>().toImmutableSet())
-        private set
-    val selectedSubCategoriesSize by derivedStateOf { selectedSubCategoryKeys.size }
-
-    val firstSelectedSubCategory by derivedStateOf {
-        selectedSubCategoryKeys.firstOrNull()
-            ?.let { key -> subCategories.firstOrNull { it.key == key } }
-    }
-
-    var subCategories by mutableStateOf(emptyList<StableSubCategory>().toImmutableList())
-        private set
-
-    val isAllSelected by derivedStateOf { selectedSubCategoryKeys.size == subCategories.size }
-    val showEditIcon by derivedStateOf { selectedSubCategoriesSize == 1 }
-    val showDeleteIcon by derivedStateOf { selectedSubCategoriesSize > 0 }
-
-    fun setParent(parent: SubCategoryParent) {
-        this.parent = parent
-        subCategories = getSubCategories(parent)
-    }
-
-    fun toggleAllSelect() {
-        selectedSubCategoryKeys =
-            if (selectedSubCategoriesSize == subCategories.size) emptySet<String>().toImmutableSet()
-            else subCategories.map { it.key }.toImmutableSet()
-
-    }
-
-    fun onSelect(key: String) {
-        selectedSubCategoryKeys =
-            if (selectedSubCategoryKeys.contains(key)) selectedSubCategoryKeys.taskAndReturnSet {
-                it.remove(key)
-            }
-            else selectedSubCategoryKeys.taskAndReturnSet { it.add(key) }
-    }
-
-    fun selectModeOn(key: String) {
-        onSelect(key)
-        isSelectMode = true
-    }
-
-    suspend fun selectModeOff() {
-        isSelectMode = false
-        delay(200)
-        selectedSubCategoryKeys = selectedSubCategoryKeys.taskAndReturnSet { it.clear() }
+    fun setSubCategories(parent: SubCategoryParent) {
+        list = getSubCategories(parent)
     }
 }

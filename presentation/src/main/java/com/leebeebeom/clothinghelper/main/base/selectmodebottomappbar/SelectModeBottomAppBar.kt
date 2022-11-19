e24ea8aecc5ac7acfdb939264c402dd55bf8cf4b
leebeebeom <boole92@naver.com>
2022-11-20 02:59:39 +0900

    수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/selectmodebottomappbar/SelectModeBottomAppBar.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/selectmodebottomappbar/SelectModeBottomAppBar.kt
@@ -17,8 +17,6 @@ import com.leebeebeom.clothinghelper.base.composables.SimpleWidthSpacer
 fun BoxScope.SelectModeBottomAppBar(
     selectedSize: () -> Int,
     isAllSelected: () -> Boolean,
-    showEditIcon: () -> Boolean,
-    showDeleteIcon: () -> Boolean,
     onAllSelectCheckBoxClick: () -> Unit,
     onEditIconClick: () -> Unit,
     isSelectMode: () -> Boolean
@@ -38,13 +36,13 @@ fun BoxScope.SelectModeBottomAppBar(
 
             Row(modifier = Modifier.weight(1f), horizontalArrangement = Arrangement.End) {
                 SelectModeBottomAppBarIcon(
-                    visible = showEditIcon,
+                    visible = { selectedSize() == 1 },
                     onClick = onEditIconClick,
                     drawable = R.drawable.ic_edit,
                     text = R.string.change_name
                 )
                 SelectModeBottomAppBarIcon(
-                    visible = showDeleteIcon,
+                    visible = { selectedSize() > 0 },
                     onClick = { /*TODO*/ },
                     drawable = R.drawable.ic_delete2,
                     text = R.string.delete

4b454805e376f5cb1d4f2d1204a9be5f7d859767
leebeebeom <boole92@naver.com>
2022-11-20 02:05:35 +0900

    분리

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/SortUIState.kt
@@ -0,0 +1,20 @@
+package com.leebeebeom.clothinghelper.main.base.interfaces
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.leebeebeom.clothinghelperdomain.model.SortPreferences
+
+interface SortUIState {
+    val sort: SortPreferences
+    fun updateSort(sort: SortPreferences)
+}
+
+class SortUIStateImpl : SortUIState {
+    override var sort by mutableStateOf(SortPreferences())
+        private set
+
+    override fun updateSort(sort: SortPreferences) {
+        this.sort = sort
+    }
+}

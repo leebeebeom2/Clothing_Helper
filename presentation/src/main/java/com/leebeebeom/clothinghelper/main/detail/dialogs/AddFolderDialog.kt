c99c388b94ce573d78d0f9944cba34eac1002fbd
leebeebeom <boole92@naver.com>
2022-11-20 02:17:42 +0900

    로직 변경

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/detail/dialogs/AddFolderDialog.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/detail/dialogs/AddFolderDialog.kt
@@ -1,24 +1,31 @@
 package com.leebeebeom.clothinghelper.main.detail.dialogs
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.dialogs.AddDialog
 import com.leebeebeom.clothinghelper.base.dialogs.AddDialogState
+import com.leebeebeom.clothinghelper.map.StableFolder
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 
 @Composable
 fun AddFolderDialog(
     state: AddDialogState,
-    folderNames: () -> ImmutableList<String>,
+    folders: () -> ImmutableList<StableFolder>,
     onPositiveButtonClick: (String) -> Unit
 ) {
+    val names by remember { derivedStateOf { folders().map { it.name }.toImmutableList() } }
+
     AddDialog(
         label = R.string.add_folder,
         placeHolder = R.string.folder_place_holder,
         title = R.string.add_folder,
         onPositiveButtonClick = onPositiveButtonClick,
         state = state,
-        names = folderNames,
+        names = { names },
         existNameError = R.string.error_exist_folder_name
     )
 }

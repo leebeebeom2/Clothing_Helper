b52cf88424dd44137ddcdaee4b264dc7441b05bc
leebeebeom <boole92@naver.com>
2022-11-20 01:40:36 +0900

    서브 카테고리 UI State 로직 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/contents/dropdownmenus/DrawerMainCategoryDropDownMenu.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/contents/dropdownmenus/DrawerMainCategoryDropDownMenu.kt
@@ -6,13 +6,14 @@ import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.DropDownMenuRoot
 import com.leebeebeom.clothinghelper.base.dialogs.AddDialogState
 import com.leebeebeom.clothinghelper.main.subcategory.dialogs.AddSubCategoryDialog
+import com.leebeebeom.clothinghelper.map.StableSubCategory
 import kotlinx.collections.immutable.ImmutableList
 
 @Composable
 fun DrawerMainCategoryDropDownMenu(
     show: () -> Boolean,
     onDismiss: () -> Unit,
-    subCategoryNames: () -> ImmutableList<String>,
+    subCategories: () -> ImmutableList<StableSubCategory>,
     onAddSubCategoryPositiveClick: (String) -> Unit
 ) {
     val state = rememberSaveable(saver = AddDialogState.Saver) { AddDialogState() }
@@ -27,7 +28,7 @@ fun DrawerMainCategoryDropDownMenu(
 
     AddSubCategoryDialog(
         state = state,
-        subCategoryNames = subCategoryNames,
+        subCategories = subCategories,
         onPositiveButtonClick = onAddSubCategoryPositiveClick
     )
 }

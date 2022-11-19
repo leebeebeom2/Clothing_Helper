499350558c56dca802ff529a5ef35f1cbcb9e186
leebeebeom <boole92@naver.com>
2022-11-19 14:42:40 +0900

    상속 로직 변경

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/LoadingUIState.kt
@@ -0,0 +1,20 @@
+package com.leebeebeom.clothinghelper.main.base
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+interface LoadingUIState {
+    val isLoading: Boolean
+    fun updateIsLoading(isLoading: Boolean)
+}
+
+class LoadingUIStateImpl : LoadingUIState {
+    override var isLoading by mutableStateOf(false)
+        private set
+
+    override fun updateIsLoading(isLoading: Boolean) {
+        this.isLoading = isLoading
+    }
+}
+

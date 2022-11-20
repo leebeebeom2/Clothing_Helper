b72766804c3b53080fd98eab23a0e21da64d64e4
leebeebeom <boole92@naver.com>
2022-11-21 08:58:46 +0900

    추상화

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/FolderRepository.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/FolderRepository.kt
@@ -1,13 +1,8 @@
 package com.leebeebeom.clothinghelperdomain.repository
 
-import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.model.container.Folder
 import kotlinx.coroutines.flow.Flow
 
-interface FolderRepository : LoadingRepository {
+interface FolderRepository : LoadingRepository, ContainerRepository<Folder> {
     val allFolders: Flow<List<Folder>>
-
-    suspend fun loadFolders(uid: String?): FirebaseResult
-    suspend fun addFolder(folder: Folder, uid: String): FirebaseResult
-    suspend fun editFolderName(newFolder: Folder, uid: String): FirebaseResult
 }

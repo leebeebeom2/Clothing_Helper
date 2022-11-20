145e42346fefa8d572a2c93902dbba671102b80b
leebeebeom <boole92@naver.com>
2022-11-21 08:58:43 +0900

    추상화

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/SubCategoryRepository.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/SubCategoryRepository.kt
@@ -1,14 +1,10 @@
 package com.leebeebeom.clothinghelperdomain.repository
 
-import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.model.container.SubCategory
 import kotlinx.coroutines.flow.Flow
 
-interface SubCategoryRepository : LoadingRepository {
+interface SubCategoryRepository : LoadingRepository, ContainerRepository<SubCategory> {
     val allSubCategories: Flow<List<SubCategory>>
 
-    suspend fun loadSubCategories(uid: String?): FirebaseResult
     suspend fun pushInitialSubCategories(uid: String)
-    suspend fun addSubCategory(subCategory: SubCategory, uid: String): FirebaseResult
-    suspend fun editSubCategoryName(newSubCategory: SubCategory, uid: String): FirebaseResult
 }

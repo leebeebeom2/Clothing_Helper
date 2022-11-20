c1df6d0a5b6e857feac469f9496175a1a3c194f2
leebeebeom <boole92@naver.com>
2022-11-20 17:06:32 +0900

    분리

---
+++ clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/base/DatabasePath.kt
@@ -0,0 +1,7 @@
+package com.leebeebeom.clothinghelperdata.repository.base
+
+object DatabasePath {
+    const val SUB_CATEGORIES = "sub categories"
+    const val USER_INFO = "user info"
+    const val FOLDERS = "folders"
+}

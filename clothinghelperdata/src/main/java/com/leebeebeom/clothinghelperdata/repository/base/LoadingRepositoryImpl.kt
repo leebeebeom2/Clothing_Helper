8a01aa895588dae45679cff39a12e09b77147086
leebeebeom <boole92@naver.com>
2022-11-21 08:26:28 +0900

    kdoc 작성

--- clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/base/LoadingRepositoryImpl.kt
+++ clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/base/LoadingRepositoryImpl.kt
@@ -7,17 +7,11 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.withContext
 
-/**
- * [loadingOn]과 [loadingOff]로 [isLoading]의 상태 변경
- */
 class LoadingRepositoryImpl(initialLoading: Boolean) : LoadingRepository {
     private val _isLoading = MutableStateFlow(initialLoading)
     override val isLoading get() = _isLoading.asStateFlow()
 
     fun loadingOn() = _isLoading.update { true }
 
-    /**
-     * [NonCancellable]로 코루틴이 취소되어도 동작 보장
-     */
     suspend fun loadingOff() = withContext(NonCancellable) { _isLoading.update { false } }
 }

a78863487310f15e2857fb9403713b6f947f60f6
leebeebeom <boole92@naver.com>
2022-11-20 17:11:29 +0900

    정리

--- clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/util/getSorted.kt
+++ clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/util/getSorted.kt
@@ -6,18 +6,18 @@ import com.leebeebeom.clothinghelperdomain.model.SortPreferences
 import com.leebeebeom.clothinghelperdomain.model.container.BaseContainer
 
 fun <T : BaseContainer> getSorted(
-    subCategories: List<T>, sortPreferences: SortPreferences
+    list: List<T>, sortPreferences: SortPreferences
 ): List<T> {
     val sort = sortPreferences.sort
     val order = sortPreferences.order
 
     return when {
-        sort == Sort.NAME && order == Order.ASCENDING -> subCategories.sortedBy { it.name }
-        sort == Sort.NAME && order == Order.DESCENDING -> subCategories.sortedByDescending { it.name }
-        sort == Sort.CREATE && order == Order.ASCENDING -> subCategories.sortedBy { it.createDate }
-        sort == Sort.CREATE && order == Order.DESCENDING -> subCategories.sortedByDescending { it.createDate }
-        sort == Sort.EDIT && order == Order.ASCENDING -> subCategories.sortedBy { it.editDate }
-        sort == Sort.EDIT && order == Order.DESCENDING -> subCategories.sortedByDescending { it.editDate }
-        else -> subCategories
+        sort == Sort.NAME && order == Order.ASCENDING -> list.sortedBy { it.name }
+        sort == Sort.NAME && order == Order.DESCENDING -> list.sortedByDescending { it.name }
+        sort == Sort.CREATE && order == Order.ASCENDING -> list.sortedBy { it.createDate }
+        sort == Sort.CREATE && order == Order.DESCENDING -> list.sortedByDescending { it.createDate }
+        sort == Sort.EDIT && order == Order.ASCENDING -> list.sortedBy { it.editDate }
+        sort == Sort.EDIT && order == Order.DESCENDING -> list.sortedByDescending { it.editDate }
+        else -> list
     }
 }

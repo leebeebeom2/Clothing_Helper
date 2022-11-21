d890744bc129b36ea0e95407d034d0b4db8d123c
leebeebeom <boole92@naver.com>
2022-11-21 09:03:35 +0900

    정리

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/ContainerRepository.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/ContainerRepository.kt
@@ -5,5 +5,5 @@ import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 interface ContainerRepository<T> {
     suspend fun load(uid: String?, type: Class<T>): FirebaseResult
     suspend fun add(t: T, uid: String): FirebaseResult
-    suspend fun edit(t: T, uid: String): FirebaseResult
+    suspend fun edit(newT: T, uid: String): FirebaseResult
 }

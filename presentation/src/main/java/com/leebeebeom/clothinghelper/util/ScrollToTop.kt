0f420474f972455c5ef44d73e10aba48051bc22b
leebeebeom <boole92@naver.com>
2022-11-21 13:06:20 +0900

    정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/util/ScrollToTop.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/util/ScrollToTop.kt
@@ -5,19 +5,15 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 
 val LazyListState.showScrollToTopButton
-    get() = run {
-        val show by derivedStateOf { firstVisibleItemScrollOffset > 0 }
-        show
-    }
+    get() = run { derivedStateOf { firstVisibleItemScrollOffset > 0 }.value }
 
 suspend fun LazyListState.scrollToTop() {
     val scrollAnimationSpec = tween<Float>(300, easing = FastOutLinearInEasing)
     val firstVisibleItemIndex = layoutInfo.visibleItemsInfo.firstOrNull()?.index
 
-    firstVisibleItemIndex?.also {
+    firstVisibleItemIndex?.let {
         if (it <= 2) animateScrollBy(-1000f, scrollAnimationSpec)
         else {
             scrollToItem(2)

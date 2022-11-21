6b94bca00bdedaa61fd9958fc0050f967cf1f740
leebeebeom <boole92@naver.com>
2022-11-21 15:32:53 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/model/EssentialMenu.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/model/EssentialMenu.kt
@@ -4,17 +4,17 @@ import com.leebeebeom.clothinghelper.R
 import kotlinx.collections.immutable.toImmutableList
 
 data class EssentialMenu(
-    val name: Int, val drawable: Int, val type: EssentialMenus
+    val name: Int, val drawable: Int, val type: EssentialMenuType
 )
 
-enum class EssentialMenus {
+enum class EssentialMenuType {
     MainScreen, Favorite, SeeAll, Trash
 }
 
 fun getEssentialMenus() =
     listOf(
-        EssentialMenu(R.string.main_screen, R.drawable.ic_home, EssentialMenus.MainScreen),
-        EssentialMenu(R.string.favorite, R.drawable.ic_star, EssentialMenus.Favorite),
-        EssentialMenu(R.string.see_all, R.drawable.ic_list, EssentialMenus.SeeAll),
-        EssentialMenu(R.string.trash, R.drawable.ic_delete, EssentialMenus.Trash)
+        EssentialMenu(R.string.main_screen, R.drawable.ic_home, EssentialMenuType.MainScreen),
+        EssentialMenu(R.string.favorite, R.drawable.ic_star, EssentialMenuType.Favorite),
+        EssentialMenu(R.string.see_all, R.drawable.ic_list, EssentialMenuType.SeeAll),
+        EssentialMenu(R.string.trash, R.drawable.ic_delete, EssentialMenuType.Trash)
     ).toImmutableList()

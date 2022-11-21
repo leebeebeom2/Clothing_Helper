619b558449dbcbdae005670dcba57f0ebb838f11
leebeebeom <boole92@naver.com>
2022-11-21 14:05:19 +0900

    정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/SortDropDownMenu.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/SortDropDownMenu.kt
@@ -5,8 +5,6 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
@@ -14,17 +12,16 @@ import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.DropDownMenuRoot
+import com.leebeebeom.clothinghelper.base.composables.DropDownMenuRoot
 import com.leebeebeom.clothinghelper.base.composables.SimpleHeightSpacer
 import com.leebeebeom.clothinghelper.base.composables.SimpleWidthSpacer
 import com.leebeebeom.clothinghelper.base.composables.SingleLineText
+import com.leebeebeom.clothinghelper.util.noRippleClickable
 import com.leebeebeom.clothinghelperdomain.model.Order
 import com.leebeebeom.clothinghelperdomain.model.Sort
 import com.leebeebeom.clothinghelperdomain.model.SortPreferences
@@ -59,42 +56,35 @@ fun SortDropdownMenu(
 
 @Composable
 private fun OrderButtons(
-    sort: () -> SortPreferences,
-    onOrderClick: (Order) -> Unit
+    sort: () -> SortPreferences, onOrderClick: (Order) -> Unit
 ) {
     Column {
         SortButton(
             text = R.string.sort_Ascending,
-            isSelected = { sort().order == Order.ASCENDING }
-        ) { onOrderClick(Order.ASCENDING) }
+            isSelected = { sort().order == Order.ASCENDING }) { onOrderClick(Order.ASCENDING) }
         SimpleHeightSpacer(dp = 8)
         SortButton(
             text = R.string.sort_Descending,
-            isSelected = { sort().order == Order.DESCENDING }
-        ) { onOrderClick(Order.DESCENDING) }
+            isSelected = { sort().order == Order.DESCENDING }) { onOrderClick(Order.DESCENDING) }
     }
 }
 
 @Composable
 private fun SortButtons(
-    sort: () -> SortPreferences,
-    onSortClick: (Sort) -> Unit
+    sort: () -> SortPreferences, onSortClick: (Sort) -> Unit
 ) {
     Column {
         SortButton(
             text = R.string.sort_name,
-            isSelected = { sort().sort == Sort.NAME }
-        ) { onSortClick(Sort.NAME) }
+            isSelected = { sort().sort == Sort.NAME }) { onSortClick(Sort.NAME) }
         SimpleHeightSpacer(dp = 8)
         SortButton(
             text = R.string.sort_create_date,
-            isSelected = { sort().sort == Sort.CREATE }
-        ) { onSortClick(Sort.CREATE) }
+            isSelected = { sort().sort == Sort.CREATE }) { onSortClick(Sort.CREATE) }
         SimpleHeightSpacer(dp = 8)
         SortButton(
             text = R.string.sort_edit_date,
-            isSelected = { sort().sort == Sort.EDIT }
-        ) { onSortClick(Sort.EDIT) }
+            isSelected = { sort().sort == Sort.EDIT }) { onSortClick(Sort.EDIT) }
     }
 }
 
@@ -113,9 +103,7 @@ private fun Header() {
 
 @Composable
 fun SortButton(
-    @StringRes text: Int,
-    isSelected: () -> Boolean,
-    onSortButtonClick: () -> Unit
+    @StringRes text: Int, isSelected: () -> Boolean, onSortButtonClick: () -> Unit
 ) {
     val strokeColor by animateColorAsState(
         targetValue = if (isSelected()) MaterialTheme.colors.primary.copy(0.8f) else Color.Transparent,
@@ -125,22 +113,14 @@ fun SortButton(
     Box(
         modifier = Modifier
             .border(
-                BorderStroke(width = 1.5.dp, color = strokeColor),
-                shape = RoundedCornerShape(20.dp)
+                BorderStroke(width = 1.5.dp, color = strokeColor), shape = RoundedCornerShape(20.dp)
             )
             .padding(horizontal = 16.dp, vertical = 12.dp)
+            .noRippleClickable(onClick = onSortButtonClick)
     ) {
         SingleLineText(
-            modifier = Modifier.noRippleClickable(onSortButtonClick),
             text = text,
             style = MaterialTheme.typography.body2,
         )
-    }
-}
-
-fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
-    clickable(indication = null,
-        interactionSource = remember { MutableInteractionSource() }) {
-        onClick()
     }
 }

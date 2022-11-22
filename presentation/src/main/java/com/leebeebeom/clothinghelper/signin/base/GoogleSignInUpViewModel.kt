c8833e3fede7aaa579bee6b102b485ca6136cc98
leebeebeom <boole92@naver.com>
2022-11-23 00:47:45 +0900

    정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/GoogleSignInUpViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/GoogleSignInUpViewModel.kt
@@ -14,9 +14,7 @@ import com.google.firebase.auth.GoogleAuthProvider
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelperdata.repository.A_NETWORK_ERROR
 import com.leebeebeom.clothinghelperdata.repository.util.TAG
-import com.leebeebeom.clothinghelperdata.repository.util.logE
 import com.leebeebeom.clothinghelperdomain.model.AuthResult
-import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.usecase.user.GoogleSignInUseCase
 import kotlinx.coroutines.launch
 
@@ -43,12 +41,12 @@ abstract class GoogleSignInUpViewModel(private val googleSignInUseCase: GoogleSi
 
     private fun googleSignIn(activityResult: ActivityResult) {
         viewModelScope.launch {
-            val result = googleSignInUseCase.googleSignIn(getGoogleCredential(activityResult)) {
-                if (it is FirebaseResult.Fail) {
-                    showToast(R.string.data_load_failed)
-                    logE("signInWithGoogleEmail", it.exception)
-                }
-            }
+            val result =
+                googleSignInUseCase.googleSignIn(
+                    credential = getGoogleCredential(activityResult),
+                    onSubCategoriesLoadFail = { showToast(R.string.data_load_failed) }
+                )
+
             when (result) {
                 is AuthResult.Success -> {
                     showToast(R.string.google_sign_in_complete)
@@ -56,10 +54,7 @@ abstract class GoogleSignInUpViewModel(private val googleSignInUseCase: GoogleSi
                 }
                 is AuthResult.Fail ->
                     if (result.errorCode == A_NETWORK_ERROR) showToast(R.string.network_error)
-                    else {
-                        logE("signInWithGoogleEmail", Exception(result.errorCode))
-                        unknownFail()
-                    }
+                    else unknownFail()
                 is AuthResult.UnknownFail -> unknownFail()
             }
         }

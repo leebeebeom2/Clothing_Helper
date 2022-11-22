b915a06db4fd6d9555a2253b564b23ffcd89a9c1
leebeebeom <boole92@naver.com>
2022-11-23 03:59:40 +0900

    정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/MainRootViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/MainRootViewModel.kt
@@ -6,12 +6,16 @@ import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.ToastUIState
 import com.leebeebeom.clothinghelper.base.ToastUIStateImpl
 import com.leebeebeom.clothinghelper.main.base.interfaces.*
+import com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.folder.AddFolder
+import com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.folder.EditFolder
 import com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.subcategory.AddSubCategory
 import com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.subcategory.EditSubCategory
+import com.leebeebeom.clothinghelper.map.StableFolder
 import com.leebeebeom.clothinghelper.map.StableSubCategory
-import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
 import com.leebeebeom.clothinghelperdomain.usecase.GetDataLoadingStateUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.LoadDataUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.folder.AddFolderUseCase
+import com.leebeebeom.clothinghelperdomain.usecase.folder.EditFolderNameUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.folder.GetAllFoldersUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.AddSubCategoryUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.subcategory.EditSubCategoryUseCase
@@ -31,8 +35,10 @@ class MainRootViewModel @Inject constructor(
     private val getUserUseCase: GetUserUseCase,
     private val getAllFoldersUseCase: GetAllFoldersUseCase,
     override val addSubCategoryUseCase: AddSubCategoryUseCase,
-    override val editSubCategoryUseCase: EditSubCategoryUseCase
-) : AddSubCategory, EditSubCategory, ViewModel() {
+    override val editSubCategoryUseCase: EditSubCategoryUseCase,
+    override val addFoldersUseCase: AddFolderUseCase,
+    override val editFolderNameUseCase: EditFolderNameUseCase
+) : AddSubCategory, EditSubCategory, AddFolder, EditFolder, ViewModel() {
 
     val uiState = MainRootUIState()
 
@@ -40,11 +46,10 @@ class MainRootViewModel @Inject constructor(
         viewModelScope.launch {
             launch {
                 loadDataUseCase.load {
-                    if (it is FirebaseResult.Fail)
-                        when (it.exception) {
-                            is TimeoutCancellationException -> showToast(R.string.network_error_for_load)
-                            else -> uiState.showToast(R.string.data_load_failed)
-                        }
+                    when (it.exception) {
+                        is TimeoutCancellationException -> showToast(R.string.network_error_for_load)
+                        else -> uiState.showToast(R.string.data_load_failed)
+                    }
                 }
             }
 
@@ -63,7 +68,19 @@ class MainRootViewModel @Inject constructor(
 
     fun editSubCategoryName(subCategory: StableSubCategory) {
         viewModelScope.launch {
-            super.baseEditSubCategoryName(subCategory)
+            super.baseEditSubCategory(subCategory)
+        }
+    }
+
+    fun addFolder(folder: StableFolder) {
+        viewModelScope.launch {
+            super.baseAddFolder(folder)
+        }
+    }
+
+    fun editFolder(newFolder:StableFolder){
+        viewModelScope.launch {
+            super.baseEditFolder(newFolder)
         }
     }
 

ddbeb01e579503ea3f30f055abb10cfe8a06a597
leebeebeom <boole92@naver.com>
2022-11-23 04:19:08 +0900

    로직 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/dialogs/AddSubCategoryDialog.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/dialogs/AddSubCategoryDialog.kt
@@ -1,21 +1,20 @@
-package com.leebeebeom.clothinghelper.main.subcategory.dialogs
+package com.leebeebeom.clothinghelper.main.base.dialogs
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.dialogs.AddDialog
-import com.leebeebeom.clothinghelper.base.dialogs.AddDialogState
 import com.leebeebeom.clothinghelper.map.StableSubCategory
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
 @Composable
 fun AddSubCategoryDialog(
-    state: AddDialogState,
     subCategories: () -> ImmutableList<StableSubCategory>,
     onPositiveButtonClick: (String) -> Unit,
+    show: () -> Boolean,
+    onDismiss: () -> Unit
 ) {
     val names by remember { derivedStateOf { subCategories().map { it.name }.toImmutableList() } }
 
@@ -23,9 +22,10 @@ fun AddSubCategoryDialog(
         label = R.string.add_category,
         placeHolder = R.string.category_place_holder,
         title = R.string.add_category,
-        state = state,
         names = { names },
         existNameError = R.string.error_exist_category_name,
-        onPositiveButtonClick = onPositiveButtonClick
+        onPositiveButtonClick = onPositiveButtonClick,
+        show = show,
+        onDismiss = onDismiss
     )
 }

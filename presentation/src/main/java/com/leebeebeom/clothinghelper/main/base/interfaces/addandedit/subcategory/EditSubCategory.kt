d6ee83c0f9fa7e04275b6b1a5eab2bba0d40e6fb
leebeebeom <boole92@naver.com>
2022-11-23 03:51:09 +0900

    정리

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/addandedit/subcategory/EditSubCategory.kt
@@ -0,0 +1,22 @@
+package com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.subcategory
+
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.BaseContainerAddAndEdit
+import com.leebeebeom.clothinghelper.map.StableSubCategory
+import com.leebeebeom.clothinghelper.map.toUnstable
+import com.leebeebeom.clothinghelperdomain.usecase.subcategory.EditSubCategoryUseCase
+
+interface EditSubCategory : BaseContainerAddAndEdit {
+    val editSubCategoryUseCase: EditSubCategoryUseCase
+
+    suspend fun baseEditSubCategoryName(newSubCategory: StableSubCategory) {
+        uid?.let {
+            val result = editSubCategoryUseCase.edit(newSubCategory.toUnstable(), it)
+            showFailToast(
+                result,
+                R.string.network_error_for_edit_sub_category,
+                R.string.edit_category_name_failed
+            )
+        }
+    }
+}

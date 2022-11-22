b4ead5936125c4d3fd248dc0337db05703b97838
leebeebeom <boole92@naver.com>
2022-11-23 00:15:32 +0900

    정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/util/dragSelect/DragSelect.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/util/dragSelect/DragSelect.kt
@@ -1,9 +1,7 @@
 package com.leebeebeom.clothinghelper.util.dragSelect
 
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.pointerInput
 
@@ -16,18 +14,16 @@ fun Modifier.dragSelect(
     onDragStart: (Boolean) -> Unit,
     onSelect: (key: String) -> Unit,
     onLongClick: (key: String) -> Unit
-): Modifier = composed {
-    val realDragSelector =
-        remember { dragSelector as? ListDragSelector ?: dragSelector as GridDragSelector }
+): Modifier {
+    val realDragSelector = dragSelector as? ListDragSelector ?: dragSelector as GridDragSelector
 
-    this.then(Modifier.pointerInput(Unit) {
+    return this.then(Modifier.pointerInput(Unit) {
         detectDragGesturesAfterLongPress(onDragStart = { offset ->
             onDragStart(true)
             interceptOutOfBoundsChildEvents = true
             realDragSelector.dragSelectStart(offset, onLongClick)
         }, onDrag = { change, _ ->
             realDragSelector.onDrag(change.position, onSelect)
-            realDragSelector.onDragEndMove(change.position, onSelect)
         }, onDragEnd = {
             dragEnd(onDragStart, realDragSelector)
         }, onDragCancel = {

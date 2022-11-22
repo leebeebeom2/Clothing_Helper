7ac57d6b6c4ffd504edc7d7a32ca5835b44fdd13
leebeebeom <boole92@naver.com>
2022-11-23 04:41:39 +0900

    AddFolder 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/contents/dropdownmenus/DrawerSubCategoryDropDownMenu.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/contents/dropdownmenus/DrawerSubCategoryDropDownMenu.kt
@@ -7,7 +7,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.main.base.composables.DropDownMenuRoot
-import com.leebeebeom.clothinghelper.main.subcategory.dialogs.EditSubCategoryDialog
+import com.leebeebeom.clothinghelper.main.base.dialogs.EditSubCategoryDialog
+import com.leebeebeom.clothinghelper.main.detail.dialogs.AddFolderDialog
+import com.leebeebeom.clothinghelper.map.StableFolder
 import com.leebeebeom.clothinghelper.map.StableSubCategory
 import kotlinx.collections.immutable.ImmutableList
 
@@ -17,23 +19,42 @@ fun DrawerSubCategoryDropDownMenu(
     onDismiss: () -> Unit,
     subCategories: () -> ImmutableList<StableSubCategory>,
     subCategory: () -> StableSubCategory,
-    onEditSubCategoryPositiveClick: (StableSubCategory) -> Unit
+    onEditSubCategoryPositiveClick: (StableSubCategory) -> Unit,
+    onAddFolderPositiveClick: (StableFolder) -> Unit,
+    folders: () -> ImmutableList<StableFolder>
 ) {
-    var showDialog by rememberSaveable { mutableStateOf(false) }
+    var showSubCategoryEditDialog by rememberSaveable { mutableStateOf(false) }
+    var showAddFolderDialog by rememberSaveable { mutableStateOf(false) }
 
     DropDownMenuRoot(show = show, onDismiss = onDismiss) {
         DrawerDropdownMenuItem(
             text = R.string.edit_category,
-            onClick = { showDialog = true },
+            onClick = { showSubCategoryEditDialog = true },
+            onDismiss = onDismiss
+        )
+
+        DrawerDropdownMenuItem(
+            text = R.string.add_folder,
+            onClick = { showSubCategoryEditDialog = true },
             onDismiss = onDismiss
         )
     }
 
     EditSubCategoryDialog(
-        show = { showDialog },
+        show = { showSubCategoryEditDialog },
         subCategory = subCategory,
         subCategories = subCategories,
         onPositiveButtonClick = onEditSubCategoryPositiveClick,
-        onDismiss = { showDialog = false }
+        onDismiss = { showSubCategoryEditDialog = false }
+    )
+
+    AddFolderDialog(folders = folders,
+        onPositiveButtonClick = {
+            onAddFolderPositiveClick(
+                StableFolder(parentKey = subCategory().key, name = it)
+            )
+        },
+        show = { showAddFolderDialog },
+        onDismiss = { showAddFolderDialog = false }
     )
 }

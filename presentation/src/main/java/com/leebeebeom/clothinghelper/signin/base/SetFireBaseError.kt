617e764b7cbaf777737c689ada61c7e9c5ec8361
leebeebeom <boole92@naver.com>
2022-11-23 00:35:15 +0900

    정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SetFireBaseError.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/SetFireBaseError.kt
@@ -5,7 +5,7 @@ import com.leebeebeom.clothinghelperdata.repository.A_NETWORK_ERROR
 import com.leebeebeom.clothinghelperdata.repository.TOO_MANY_REQUEST
 import com.leebeebeom.clothinghelperdata.repository.util.logE
 
-object FirebaseErrorCode {
+private object FirebaseAuthErrorCode {
     const val ERROR_INVALID_EMAIL = "ERROR_INVALID_EMAIL"
     const val ERROR_USER_NOT_FOUND = "ERROR_USER_NOT_FOUND"
     const val ERROR_EMAIL_ALREADY_IN_USE = "ERROR_EMAIL_ALREADY_IN_USE"
@@ -19,10 +19,10 @@ fun setFireBaseError(
     showToast: (text: Int) -> Unit
 ) {
     when (errorCode) {
-        FirebaseErrorCode.ERROR_INVALID_EMAIL -> updateEmailError(R.string.error_invalid_email)
-        FirebaseErrorCode.ERROR_EMAIL_ALREADY_IN_USE -> updateEmailError(R.string.error_email_already_in_use)
-        FirebaseErrorCode.ERROR_USER_NOT_FOUND -> updateEmailError(R.string.error_user_not_found)
-        FirebaseErrorCode.ERROR_WRONG_PASSWORD -> updatePasswordError(R.string.error_wrong_password)
+        FirebaseAuthErrorCode.ERROR_INVALID_EMAIL -> updateEmailError(R.string.error_invalid_email)
+        FirebaseAuthErrorCode.ERROR_EMAIL_ALREADY_IN_USE -> updateEmailError(R.string.error_email_already_in_use)
+        FirebaseAuthErrorCode.ERROR_USER_NOT_FOUND -> updateEmailError(R.string.error_user_not_found)
+        FirebaseAuthErrorCode.ERROR_WRONG_PASSWORD -> updatePasswordError(R.string.error_wrong_password)
         A_NETWORK_ERROR -> showToast(R.string.network_error)
         TOO_MANY_REQUEST -> showToast(R.string.too_many_requst_error)
         else -> {

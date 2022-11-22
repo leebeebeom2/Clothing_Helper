d7de4c88867dee2ce087e2b7e10a53191313ec11
leebeebeom <boole92@naver.com>
2022-11-23 04:19:00 +0900

    로직 수정

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/dialogs/EditSubCategoryDialog.kt
@@ -0,0 +1,35 @@
+package com.leebeebeom.clothinghelper.main.base.dialogs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.map.StableSubCategory
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+
+@Composable
+fun EditSubCategoryDialog(
+    show: () -> Boolean,
+    subCategory: () -> StableSubCategory?,
+    subCategories: () -> ImmutableList<StableSubCategory>,
+    onPositiveButtonClick: (String) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val names by remember {
+        derivedStateOf { subCategories().map { it.name }.toImmutableList() }
+    }
+
+    EditDialog(
+        label = R.string.edit_category,
+        placeHolder = R.string.category_place_holder,
+        title = R.string.edit_category,
+        show = show,
+        names = { names },
+        existNameError = R.string.error_exist_category_name,
+        onDismiss = onDismiss,
+        onPositiveButtonClick = onPositiveButtonClick,
+        initialName = { subCategory()?.name }
+    )
+}

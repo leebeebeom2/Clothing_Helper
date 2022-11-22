ea3d5de26d034266daf46324892a076589415b61
leebeebeom <boole92@naver.com>
2022-11-23 03:52:55 +0900

    정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/addandedit/folder/AddFolder.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/addandedit/folder/AddFolder.kt
@@ -1,18 +1,18 @@
-package com.leebeebeom.clothinghelper.main.detail.interfaces
+package com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.folder
 
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.AddAndEditContainer
+import com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.BaseContainerAddAndEdit
 import com.leebeebeom.clothinghelper.map.StableFolder
 import com.leebeebeom.clothinghelper.map.toStable
 import com.leebeebeom.clothinghelperdomain.usecase.folder.AddFolderUseCase
 
-interface AddFolder : AddAndEditContainer {
+interface AddFolder : BaseContainerAddAndEdit {
     val addFoldersUseCase: AddFolderUseCase
 
     suspend fun baseAddFolder(folder: StableFolder) {
         uid?.let {
             val result = addFoldersUseCase.add(folder.toStable(), it)
-            showToastWhenFail(
+            showFailToast(
                 result,
                 R.string.network_error_for_add_folder,
                 R.string.add_folder_failed
--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/addandedit/folder/EditFolderName.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/addandedit/folder/EditFolderName.kt
@@ -1,18 +1,18 @@
-package com.leebeebeom.clothinghelper.main.detail.interfaces
+package com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.folder
 
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.AddAndEditContainer
+import com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.BaseContainerAddAndEdit
 import com.leebeebeom.clothinghelper.map.StableFolder
 import com.leebeebeom.clothinghelper.map.toStable
 import com.leebeebeom.clothinghelperdomain.usecase.folder.EditFolderNameUseCase
 
-interface EditFolderName : AddAndEditContainer {
+interface EditFolderName : BaseContainerAddAndEdit {
     val editFolderNameUseCase: EditFolderNameUseCase
 
     suspend fun baseEditFolderName(folder: StableFolder) {
         uid?.let {
             val result = editFolderNameUseCase.edit(folder.toStable(), it)
-            showToastWhenFail(
+            showFailToast(
                 result,
                 R.string.network_error_for_edit_folder,
                 R.string.edit_folder_name_failed

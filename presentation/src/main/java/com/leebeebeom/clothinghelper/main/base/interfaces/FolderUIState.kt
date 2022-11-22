a39f3cb7026d0520ac79937bf51b720259f15d4c
leebeebeom <boole92@naver.com>
2022-11-23 03:16:22 +0900

    getFolder 로직 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/FolderUIState.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/FolderUIState.kt
@@ -1,9 +1,6 @@
 package com.leebeebeom.clothinghelper.main.base.interfaces
 
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import com.leebeebeom.clothinghelper.map.StableFolder
 import com.leebeebeom.clothinghelper.map.toStable
 import com.leebeebeom.clothinghelperdomain.model.container.Folder
@@ -21,10 +18,16 @@ class FolderUIStateImpl : FolderUIState {
     override var allFolders by mutableStateOf(emptyList<StableFolder>())
         private set
 
-    override fun getFolders(parentKey: String): ImmutableList<StableFolder> =
-        derivedStateOf {
-            allFolders.filter { it.parentKey == parentKey }.toImmutableList()
-        }.value
+    private val folderMap = hashMapOf<String, State<ImmutableList<StableFolder>>>()
+
+    override fun getFolders(parentKey: String): ImmutableList<StableFolder> {
+        if (folderMap[parentKey] == null) {
+            folderMap[parentKey] = derivedStateOf {
+                allFolders.filter { it.parentKey == parentKey }.toImmutableList()
+            }
+        }
+        return folderMap[parentKey]?.value!!
+    }
 
     override fun loadAllFolders(folders: List<Folder>) {
         allFolders = folders.map { it.toStable() }

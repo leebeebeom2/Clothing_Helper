71e74534b6ef66ae335996978b3b85a48f813ee5
leebeebeom <boole92@naver.com>
2022-11-23 01:55:48 +0900

    폰트 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/composables/OrDivider.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/composables/OrDivider.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.composables.SingleLineText
@@ -24,6 +25,7 @@ fun OrDivider() {
             modifier = Modifier.padding(horizontal = 14.dp),
             style = MaterialTheme.typography.body2.copy(
                 color = LocalContentColor.current.copy(ContentAlpha.disabled),
+                fontWeight = FontWeight.Bold
             )
         )
         Divider(modifier = modifier)

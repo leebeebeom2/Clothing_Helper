9ba9cba5c4892bf12efdfbe74ae9702af8f6589c
leebeebeom <boole92@naver.com>
2022-11-25 01:44:06 +0900

    parent 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/map/StableFolder.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/map/StableFolder.kt
@@ -2,15 +2,17 @@ package com.leebeebeom.clothinghelper.map
 
 import com.leebeebeom.clothinghelperdomain.model.container.BaseContainer
 import com.leebeebeom.clothinghelperdomain.model.container.Folder
+import com.leebeebeom.clothinghelperdomain.model.container.SubCategoryParent
 
 data class StableFolder(
     val parentKey: String = "",
     override val createDate: Long = 0,
     override val name: String = "",
     override val key: String = "",
-    override val editDate: Long = 0
+    override val editDate: Long = 0,
+    override val parent: SubCategoryParent = SubCategoryParent.TOP
 ) : BaseContainer()
 
-fun Folder.toStable() = StableFolder(parentKey, createDate, name, key, editDate)
+fun Folder.toStable() = StableFolder(parentKey, createDate, name, key, editDate, parent)
 
-fun StableFolder.toStable() = Folder(parentKey, createDate, name, key, editDate)
+fun StableFolder.toStable() = Folder(parentKey, createDate, name, key, editDate, parent)

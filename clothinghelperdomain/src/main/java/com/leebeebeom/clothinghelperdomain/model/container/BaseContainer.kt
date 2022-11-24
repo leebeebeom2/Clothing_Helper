ede22e07d79ddc1108745b4b3a7130a5e5c68731
leebeebeom <boole92@naver.com>
2022-11-25 01:42:55 +0900

    parent 추가

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/container/BaseContainer.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/model/container/BaseContainer.kt
@@ -1,6 +1,7 @@
 package com.leebeebeom.clothinghelperdomain.model.container
 
 abstract class BaseContainer {
+    abstract val parent: SubCategoryParent
     abstract val name: String
     abstract val key: String
     abstract val createDate: Long

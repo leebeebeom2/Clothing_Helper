a716bfc2fa863d6d2bdb0e109f4ea63dda364b23
leebeebeom <boole92@naver.com>
2022-11-25 00:00:59 +0900

    성능 최적화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/composables/ScrollToTopFab.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/composables/ScrollToTopFab.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.FloatingActionButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,13 +30,15 @@ fun BoxScope.ScrollToTopFab(show: () -> Boolean, toTop: suspend () -> Unit) {
         enter = Anime.ScrollToTopFab.scaleIn,
         exit = Anime.ScrollToTopFab.scaleOut
     ) {
-        FloatingActionButton(
-            onClick = { coroutineScope.launch { toTop() } },
-            backgroundColor = MaterialTheme.colors.surface,
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-                .size(36.dp),
-            elevation = FloatingActionButtonDefaults.elevation(4.dp, 10.dp, 6.dp, 6.dp)
-        ) { SimpleIcon(drawable = R.drawable.ic_expand_less) }
+        key("scrollToTopFab") {
+            FloatingActionButton(
+                onClick = { coroutineScope.launch { toTop() } },
+                backgroundColor = MaterialTheme.colors.surface,
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .size(36.dp),
+                elevation = FloatingActionButtonDefaults.elevation(4.dp, 10.dp, 6.dp, 6.dp)
+            ) { SimpleIcon(drawable = R.drawable.ic_expand_less) }
+        }
     }
 }

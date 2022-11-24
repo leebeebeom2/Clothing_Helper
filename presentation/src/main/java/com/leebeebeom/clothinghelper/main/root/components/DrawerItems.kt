f0d19ecbefc94047bd8844e11310b641d30ffbc0
leebeebeom <boole92@naver.com>
2022-11-25 01:50:11 +0900

    background 분기 구현

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/components/DrawerItems.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/components/DrawerItems.kt
@@ -2,10 +2,10 @@ package com.leebeebeom.clothinghelper.main.root.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
+import androidx.compose.ui.graphics.Color
 import com.leebeebeom.clothinghelper.base.Anime
 import com.leebeebeom.clothinghelperdomain.model.container.BaseContainer
 import kotlinx.collections.immutable.ImmutableList
@@ -14,15 +14,16 @@ import kotlinx.collections.immutable.ImmutableList
 fun <T : BaseContainer> DrawerItems(
     show: () -> Boolean,
     items: () -> ImmutableList<T>,
-    item: @Composable (T) -> Unit
+    backGround: Color,
+    item: @Composable (T) -> Unit,
 ) {
     AnimatedVisibility(
         visible = show(),
         enter = Anime.DrawerList.listExpand,
         exit = Anime.DrawerList.listShrink
     ) {
-        key("subCategories") {
-            Surface(color = MaterialTheme.colors.primary) {
+        key("drawerItems") {
+            Surface(color = backGround) {
                 Column { items().forEach { key(it.key) { item(it) } } }
             }
         }

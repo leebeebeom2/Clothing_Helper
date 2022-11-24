e4d703620bd2d862e8ec039cac42858cd57b0ac0
leebeebeom <boole92@naver.com>
2022-11-25 04:05:47 +0900

    폴더 드랍다운 메뉴 생성

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/contents/dropdownmenus/DrawerFolderDropDownMenu.kt
@@ -0,0 +1,50 @@
+package com.leebeebeom.clothinghelper.main.root.contents.dropdownmenus
+
+import androidx.compose.runtime.*
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.main.base.composables.DropDownMenuRoot
+import com.leebeebeom.clothinghelper.main.base.dialogs.AddFolderDialog
+import com.leebeebeom.clothinghelper.main.base.dialogs.EditFolderDialog
+import com.leebeebeom.clothinghelper.map.StableFolder
+import kotlinx.collections.immutable.ImmutableList
+
+@Composable
+fun DrawerFolderDropDownMenu(
+    show: () -> Boolean, onDismiss: () -> Unit,
+    folders: () -> ImmutableList<StableFolder>,
+    onAddFolderPositiveClick: (String) -> Unit,
+    onEditFolderPositiveClick: (String) -> Unit,
+    folder: () -> StableFolder
+) {
+    var showAddFolderDialog by remember { mutableStateOf(false) }
+    var showEditFolderDialog by remember { mutableStateOf(false) }
+
+    DropDownMenuRoot(show = show, onDismiss = onDismiss) {
+        DrawerDropdownMenuItem(
+            text = R.string.add_folder,
+            onClick = { showAddFolderDialog = true },
+            onDismiss = onDismiss
+        )
+
+        DrawerDropdownMenuItem(
+            text = R.string.edit_folder,
+            onClick = { showEditFolderDialog = true },
+            onDismiss = onDismiss
+        )
+    }
+
+    AddFolderDialog(
+        folders = folders,
+        onPositiveButtonClick = onAddFolderPositiveClick,
+        show = { showAddFolderDialog },
+        onDismiss = { showAddFolderDialog = false }
+    )
+
+    EditFolderDialog(
+        folders = folders,
+        show = { showEditFolderDialog },
+        onDismiss = { showEditFolderDialog = false },
+        onPositiveButtonClick = onEditFolderPositiveClick,
+        folder = folder
+    )
+}

6f9859d09edddcd945cb8eaf9fd824839503f89a
leebeebeom <boole92@naver.com>
2022-11-25 02:20:44 +0900

    로직 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/dialogs/EditFolderDialog.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/dialogs/EditFolderDialog.kt
@@ -1,23 +1,29 @@
 package com.leebeebeom.clothinghelper.main.base.dialogs
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.map.StableFolder
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 
 @Composable
 fun EditFolderDialog(
-    folderNames: () -> ImmutableList<String>,
+    folders: () -> ImmutableList<StableFolder>,
     show: () -> Boolean,
     onDismiss: () -> Unit,
     onPositiveButtonClick: (String) -> Unit,
     folder: () -> StableFolder
 ) {
+    val names by remember { derivedStateOf { folders().map { it.name }.toImmutableList() } }
+
     EditDialog(
         label = R.string.folder,
         placeHolder = R.string.folder_place_holder,
         title = R.string.edit_folder,
-        names = folderNames,
+        names = { names },
         existNameError = R.string.error_exist_folder_name,
         onDismiss = onDismiss,
         onPositiveButtonClick = onPositiveButtonClick,

f58f68ae7607525ab3e4157354d26fa2b17ae116
leebeebeom <boole92@naver.com>
2022-11-25 00:09:23 +0900

    분리

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/components/DrawerExpandIcon.kt
@@ -0,0 +1,36 @@
+package com.leebeebeom.clothinghelper.main.root.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.leebeebeom.clothinghelper.base.composables.DotProgressIndicator
+import com.leebeebeom.clothinghelper.main.base.composables.ExpandIcon
+import kotlinx.collections.immutable.ImmutableList
+
+@Composable
+fun DrawerExpandIcon(
+    isLoading: () -> Boolean,
+    isExpanded: () -> Boolean,
+    onClick: () -> Unit,
+    items: () -> ImmutableList<*>
+) {
+    val show by remember { derivedStateOf { items().size > 0 } }
+
+    if (isLoading())
+        DotProgressIndicator(
+            modifier = Modifier.padding(end = 4.dp),
+            size = 4.dp,
+            color = MaterialTheme.colors.surface.copy(ContentAlpha.disabled)
+        )
+    else if (show)
+        ExpandIcon(
+            isExpanded = isExpanded,
+            onClick = onClick
+        )
+}

d5e4751fc09ff43a8f827f586248a8c96a8ff766
leebeebeom <boole92@naver.com>
2022-11-26 01:10:52 +0900

    레이아웃 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/title/SubCategoryName.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/title/SubCategoryName.kt
@@ -1,16 +1,12 @@
 package com.leebeebeom.clothinghelper.main.subcategory.content.subcategorycard.title
 
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.base.composables.SingleLineText
 
 @Composable
 fun SubCategoryName(name: () -> String) {
     SingleLineText(
-        modifier = Modifier.widthIn(max = 300.dp),
         text = name(),
         style = MaterialTheme.typography.subtitle1
     )

62ff78f329d1da0d6f603dd57006aa2da1a5337e
leebeebeom <boole92@naver.com>
2022-11-26 02:07:46 +0900

    레이아웃 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/title/SubCategoryCardTitle.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/title/SubCategoryCardTitle.kt
@@ -1,15 +1,12 @@
 package com.leebeebeom.clothinghelper.main.subcategory.content.subcategorycard.title
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.leebeebeom.clothinghelper.base.composables.SimpleWidthSpacer
+import com.leebeebeom.clothinghelper.base.composables.SimpleHeightSpacer
 import com.leebeebeom.clothinghelper.main.base.composables.ExpandIcon
 import com.leebeebeom.clothinghelper.map.StableFolder
 import com.leebeebeom.clothinghelper.map.StableSubCategory
@@ -29,27 +26,24 @@ fun SubCategoryCardTitle(
     Surface(elevation = 4.dp) {
         Row(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(vertical = 12.dp)
-                .padding(end = 4.dp),
-            verticalAlignment = Alignment.CenterVertically,
+                .padding(top = 8.dp, bottom = 8.dp, end = 0.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier.weight(1f), verticalAlignment = Alignment.CenterVertically
-            ) {
-                SubCategoryCircleCheckBox(
-                    isSelectMode = isSelectMode,
-                    subCategory = subCategory,
-                    onClick = onCheckBoxClick,
-                    selectedCategoryKeys = selectedCategoryKeys
-                )
-                SubCategoryTitleAnimateSpacer(isSelectMode = isSelectMode)
-                SubCategoryName(name = { subCategory().name })
-                SimpleWidthSpacer(dp = 4)
+            SubCategoryCircleCheckBox(
+                isSelectMode = isSelectMode,
+                subCategory = subCategory,
+                onClick = onCheckBoxClick,
+                selectedCategoryKeys = selectedCategoryKeys
+            )
+            SubCategoryTitleAnimateSpacer(isSelectMode = isSelectMode)
+            Column(modifier = Modifier.weight(1f)) {
+                SubCategoryName { subCategory().name }
+                SimpleHeightSpacer(dp = 4)
+                SubCategoryTitleTotalCount(folders)
             }
-            SubCategoryTitleTotalCount(folders)
             ExpandIcon(
-                modifier = Modifier.size(24.dp),
+                modifier = Modifier.size(30.dp),
                 isExpanded = isExpanded,
                 onClick = onExpandIconClick
             )

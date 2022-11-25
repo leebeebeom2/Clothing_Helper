6eda7c0172b4688a3dde34a3e9fba28284779a20
leebeebeom <boole92@naver.com>
2022-11-26 02:18:03 +0900

    사이즈 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/title/SubCategoryCircleCheckBox.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/title/SubCategoryCircleCheckBox.kt
@@ -29,7 +29,7 @@ fun SubCategoryCircleCheckBox(
                 isChecked = { isChecked },
                 modifier = Modifier.padding(start = 4.dp),
                 onClick = onClick,
-                size = 18.dp
+                size = 20.dp
             )
         }
     }

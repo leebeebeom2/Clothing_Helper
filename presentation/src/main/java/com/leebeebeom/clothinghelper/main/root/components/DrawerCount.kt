9daf21b3f3402138ee4ff6d8770bcc26ba2b86a1
leebeebeom <boole92@naver.com>
2022-11-26 02:40:55 +0900

    생성

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/components/DrawerCount.kt
@@ -0,0 +1,27 @@
+package com.leebeebeom.clothinghelper.main.root.components
+
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.base.composables.SingleLineText
+import kotlinx.collections.immutable.ImmutableList
+
+@Composable
+fun DrawerCount(
+    items: () -> ImmutableList<*>
+) {
+    SingleLineText(
+        text = stringResource(
+            id = R.string.folders_charts,
+            formatArgs = arrayOf(items().size, 0)
+        ),
+        style = MaterialTheme.typography.caption.copy(
+            LocalContentColor.current.copy(
+                ContentAlpha.disabled
+            )
+        )
+    )
+}

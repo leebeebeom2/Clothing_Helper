e902c5ce3e64b699c8a12332f3e710581dfbbea1
leebeebeom <boole92@naver.com>
2022-11-26 02:07:35 +0900

    레이아웃 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/title/SubCategoryTitleTotalCount.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/title/SubCategoryTitleTotalCount.kt
@@ -4,6 +4,8 @@ import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.composables.SingleLineText
 import com.leebeebeom.clothinghelper.map.StableFolder
 import kotlinx.collections.immutable.ImmutableList
@@ -11,7 +13,11 @@ import kotlinx.collections.immutable.ImmutableList
 @Composable
 fun SubCategoryTitleTotalCount(folders: () -> ImmutableList<StableFolder>) {
     SingleLineText(
-        text = "(${folders().size})", style = MaterialTheme.typography.caption.copy(
+        text = stringResource(
+            id = R.string.folders_charts,
+            formatArgs = arrayOf(folders().size, 0)
+        ),
+        style = MaterialTheme.typography.caption.copy(
             color = LocalContentColor.current.copy(ContentAlpha.medium)
         )
     )

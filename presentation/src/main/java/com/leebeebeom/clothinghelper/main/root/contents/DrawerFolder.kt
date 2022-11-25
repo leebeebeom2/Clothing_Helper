1c991aec9c2cdab102773d111eac9b46e04ba0ac
leebeebeom <boole92@naver.com>
2022-11-26 02:47:19 +0900

    레이아웃 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/contents/DrawerFolder.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/root/contents/DrawerFolder.kt
@@ -1,5 +1,6 @@
 package com.leebeebeom.clothinghelper.main.root.contents
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,9 +17,9 @@ import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.base.composables.SimpleIcon
 import com.leebeebeom.clothinghelper.base.composables.SingleLineText
 import com.leebeebeom.clothinghelper.main.root.components.DrawerContentRow
+import com.leebeebeom.clothinghelper.main.root.components.DrawerCount
 import com.leebeebeom.clothinghelper.main.root.components.DrawerExpandIcon
 import com.leebeebeom.clothinghelper.main.root.components.DrawerItems
-import com.leebeebeom.clothinghelper.main.root.components.DrawerTotalCount
 import com.leebeebeom.clothinghelper.main.root.contents.dropdownmenus.DrawerFolderDropDownMenu
 import com.leebeebeom.clothinghelper.map.StableFolder
 import com.leebeebeom.clothinghelper.theme.DarkGray
@@ -43,23 +44,28 @@ fun DrawerFolder(
             .padding(start = startPadding),
         onClick = onClick,
         onLongClick = { showDropDownMenu = true }) {
-        SimpleIcon(drawable = R.drawable.ic_folder, modifier = Modifier.size(24.dp))
 
-        SingleLineText(
-            modifier = Modifier.padding(start = 4.dp),
-            text = folder().name,
-            style = MaterialTheme.typography.subtitle2
-        )
-        DrawerFolderDropDownMenu(
-            show = { showDropDownMenu },
-            onDismiss = { showDropDownMenu = false },
-            folders = { folders(folder().key) },
-            onAddFolderPositiveClick = onAddFolderPositiveClick,
-            onEditFolderPositiveClick = onEditFolderPositiveClick,
-            folder = folder
-        )
-        DrawerTotalCount(items = { folders(folder().key) }, isLoading = { false })
+        SimpleIcon(drawable = R.drawable.ic_folder, modifier = Modifier.size(25.dp))
 
+        Column(
+            Modifier
+                .padding(start = 8.dp)
+                .padding(vertical = 8.dp)
+                .weight(1f)) {
+            SingleLineText(
+                text = folder().name,
+                style = MaterialTheme.typography.subtitle2
+            )
+            DrawerFolderDropDownMenu(
+                show = { showDropDownMenu },
+                onDismiss = { showDropDownMenu = false },
+                folders = { folders(folder().key) },
+                onAddFolderPositiveClick = onAddFolderPositiveClick,
+                onEditFolderPositiveClick = onEditFolderPositiveClick,
+                folder = folder
+            )
+            DrawerCount { folders(folder().key) }
+        }
         DrawerExpandIcon(
             isLoading = { false },
             isExpanded = { isExpanded },

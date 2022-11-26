4d50a3e75f4ee81a4ab9a9333c54d836e49fd4f2
leebeebeom <boole92@naver.com>
2022-11-27 02:01:12 +0900

    LazyGridState 추가

--- presentation/src/main/java/com/leebeebeom/clothinghelper/util/ScrollToTop.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/util/ScrollToTop.kt
@@ -1,12 +1,26 @@
 package com.leebeebeom.clothinghelper.util
 
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.runtime.derivedStateOf
 
 val LazyListState.showScrollToTopFab
     get() = derivedStateOf { firstVisibleItemScrollOffset > 0 }.value
 
+val LazyGridState.showScrollToTopFab
+    get() = derivedStateOf { firstVisibleItemScrollOffset > 0 }.value
+
 suspend fun LazyListState.scrollToTop() {
+    val scrollToTop = suspend { animateScrollToItem(0) }
+
+    if (firstVisibleItemIndex <= 2) scrollToTop()
+    else {
+        scrollToItem(2)
+        scrollToTop()
+    }
+}
+
+suspend fun LazyGridState.scrollToTop() {
     val scrollToTop = suspend { animateScrollToItem(0) }
 
     if (firstVisibleItemIndex <= 2) scrollToTop()

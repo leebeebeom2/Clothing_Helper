7099099ac7e19e06638462061eecb16000ce2797
leebeebeom <boole92@naver.com>
2022-11-27 00:12:53 +0900

    레이아웃 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/composables/ButtonWithGoogleButton.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/base/composables/ButtonWithGoogleButton.kt
@@ -1,25 +1,17 @@
 package com.leebeebeom.clothinghelper.signin.base.composables
 
 import androidx.activity.result.ActivityResult
-import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
-import com.leebeebeom.clothinghelper.base.composables.MaxWidthButton
 import com.leebeebeom.clothinghelper.base.composables.SimpleHeightSpacer
 
 @Composable
 fun ButtonWithGoogleButton(
-    @StringRes buttonText: Int,
-    buttonEnabled: () -> Boolean,
-    onButtonClick: () -> Unit,
+    buttons: @Composable () -> Unit,
     googleButtonEnabled: () -> Boolean,
     onActivityResult: (ActivityResult) -> Unit,
     googleButtonDisable: () -> Unit
 ) {
-    MaxWidthButton(
-        text = buttonText,
-        enabled = buttonEnabled,
-        onClick = onButtonClick
-    )
+    buttons()
     SimpleHeightSpacer(dp = 8)
     OrDivider()
     SimpleHeightSpacer(dp = 8)

57d9457f04984103d93aa62e5814c6bec8105f0f
leebeebeom <boole92@naver.com>
2022-11-30 23:23:33 +0900

    생성

---
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/SizeChartRepository.kt
@@ -0,0 +1,5 @@
+package com.leebeebeom.clothinghelperdomain.repository
+
+import com.leebeebeom.clothinghelperdomain.model.data.SizeChart
+
+interface SizeChartRepository : BaseDataRepository<SizeChart>

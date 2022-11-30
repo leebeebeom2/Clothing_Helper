2082dc19ed35948dadd8862d7a9c7e8a893279e4
leebeebeom <boole92@naver.com>
2022-11-30 23:24:58 +0900

    리팩토링

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/addandedit/folder/AddFolder.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/addandedit/folder/AddFolder.kt
@@ -3,7 +3,7 @@ package com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.folder
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.BaseContainerAddAndEdit
 import com.leebeebeom.clothinghelper.map.StableFolder
-import com.leebeebeom.clothinghelper.map.toStable
+import com.leebeebeom.clothinghelper.map.toUnstable
 import com.leebeebeom.clothinghelperdomain.usecase.folder.AddFolderUseCase
 
 interface AddFolder : BaseContainerAddAndEdit {
@@ -11,7 +11,7 @@ interface AddFolder : BaseContainerAddAndEdit {
 
     suspend fun baseAddFolder(folder: StableFolder) {
         uid?.let {
-            val result = addFoldersUseCase.add(folder.toStable(), it)
+            val result = addFoldersUseCase.add(folder.toUnstable(), it)
             showFailToast(
                 result,
                 R.string.network_error_for_add_folder,
--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/addandedit/folder/EditFolder.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/interfaces/addandedit/folder/EditFolder.kt
@@ -3,7 +3,7 @@ package com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.folder
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.main.base.interfaces.addandedit.BaseContainerAddAndEdit
 import com.leebeebeom.clothinghelper.map.StableFolder
-import com.leebeebeom.clothinghelper.map.toStable
+import com.leebeebeom.clothinghelper.map.toUnstable
 import com.leebeebeom.clothinghelperdomain.usecase.folder.EditFolderNameUseCase
 
 interface EditFolder : BaseContainerAddAndEdit {
@@ -11,7 +11,7 @@ interface EditFolder : BaseContainerAddAndEdit {
 
     suspend fun baseEditFolder(folder: StableFolder) {
         uid?.let {
-            val result = editFolderNameUseCase.edit(folder.toStable(), it)
+            val result = editFolderNameUseCase.edit(folder.toUnstable(), it)
             showFailToast(
                 result,
                 R.string.network_error_for_edit_folder,

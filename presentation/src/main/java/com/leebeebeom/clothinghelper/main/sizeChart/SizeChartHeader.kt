6a14e8127c6d45bd530902b30f41c02cb30a3660
leebeebeom <boole92@naver.com>
2022-12-01 00:49:08 +0900

    생성

---
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/sizeChart/SizeChartHeader.kt
@@ -0,0 +1,25 @@
+package com.leebeebeom.clothinghelper.main.sizeChart
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.leebeebeom.clothinghelper.base.composables.SingleLineText
+
+@Composable
+fun SizeChartHeader(path: String) {
+    Row(modifier = Modifier
+        .horizontalScroll(rememberScrollState())
+        .padding(vertical = 8.dp, horizontal = 12.dp)) {
+        SingleLineText(text = path, style = MaterialTheme.typography.h4.copy(fontSize = 26.sp))
+    }
+
+    Divider(modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp))
+}

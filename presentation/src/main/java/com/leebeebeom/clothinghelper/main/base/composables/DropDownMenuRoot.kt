d6ffc9ceb0ddacb49fed9056dbd3779c301c2657
leebeebeom <boole92@naver.com>
2022-12-01 03:45:18 +0900

    Shape 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/composables/DropDownMenuRoot.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/composables/DropDownMenuRoot.kt
@@ -1,11 +1,7 @@
 package com.leebeebeom.clothinghelper.main.base.composables
 
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.DropdownMenu
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun DropDownMenuRoot(
@@ -13,11 +9,7 @@ fun DropDownMenuRoot(
     onDismiss: () -> Unit,
     content: @Composable () -> Unit,
 ) {
-    MaterialTheme(shapes = MaterialTheme.shapes.copy(medium = RoundedCornerShape(20.dp))) {
-        Surface {
-            DropdownMenu(expanded = show(), onDismissRequest = onDismiss) {
-                content()
-            }
-        }
+    DropdownMenu(expanded = show(), onDismissRequest = onDismiss) {
+        content()
     }
 }

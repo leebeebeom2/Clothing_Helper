73848bc4d41cdca60ce1cacfbb1ef95655a17bd6
leebeebeom <boole92@naver.com>
2022-12-01 03:52:06 +0900

    Shape 수정

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/SubCategoryCard.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/subcategory/content/subcategorycard/SubCategoryCard.kt
@@ -1,16 +1,14 @@
 package com.leebeebeom.clothinghelper.main.subcategory.content.subcategorycard
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.main.subcategory.content.subcategorycard.title.SubCategoryCardTitle
 import com.leebeebeom.clothinghelper.map.StableFolder
@@ -18,6 +16,7 @@ import com.leebeebeom.clothinghelper.map.StableSubCategory
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.ImmutableSet
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SubCategoryCard(
     subCategory: () -> StableSubCategory,
@@ -28,12 +27,8 @@ fun SubCategoryCard(
 ) {
     var isExpanded by rememberSaveable { mutableStateOf(false) }
 
-    Card(elevation = 2.dp, shape = RoundedCornerShape(12.dp)) {
-        Column(
-            modifier = Modifier
-                .clip(RoundedCornerShape(12.dp))
-                .clickable(onClick = onClick)
-        ) {
+    Card(elevation = 2.dp, shape = RoundedCornerShape(12.dp), onClick = onClick) {
+        Column {
             SubCategoryCardTitle(
                 subCategory = subCategory,
                 isExpanded = { isExpanded },

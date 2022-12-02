26af2ae414dc2e73e8d51893796c51d91e81e037
leebeebeom <boole92@naver.com>
2022-12-03 03:31:08 +0900

    textAlign 삭제

--- presentation/src/main/java/com/leebeebeom/clothinghelper/base/composables/MaxWidthButton.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/base/composables/MaxWidthButton.kt
@@ -13,13 +13,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MaxWidthButton(
     modifier: Modifier = Modifier,
-    textAlign: TextAlign = TextAlign.Center,
     @StringRes text: Int,
     enabled: () -> Boolean = { true },
     colors: ButtonColors = ButtonDefaults.buttonColors(),
@@ -43,8 +41,7 @@ fun MaxWidthButton(
             SingleLineText(
                 text = text,
                 modifier = Modifier.weight(1f),
-                style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
-                textAlign = textAlign
+                style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold)
             )
         }
     }

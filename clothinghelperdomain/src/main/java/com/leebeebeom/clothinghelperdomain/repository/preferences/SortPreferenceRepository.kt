e6f04b80950b61589344ef50083bbd71e00974a2
leebeebeom <boole92@naver.com>
2022-12-06 23:47:53 +0900

    코드 정리

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/preferences/SortPreferenceRepository.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/repository/preferences/SortPreferenceRepository.kt
@@ -4,6 +4,7 @@ import com.leebeebeom.clothinghelperdomain.model.Order
 import com.leebeebeom.clothinghelperdomain.model.Sort
 import com.leebeebeom.clothinghelperdomain.model.SortPreferences
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Qualifier
 
 interface SortPreferenceRepository {
     val sort: Flow<SortPreferences>
@@ -11,3 +12,11 @@ interface SortPreferenceRepository {
     suspend fun changeSort(sort: Sort)
     suspend fun changeOrder(order: Order)
 }
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class SubCategoryPreferencesRepository
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class FolderPreferencesRepository

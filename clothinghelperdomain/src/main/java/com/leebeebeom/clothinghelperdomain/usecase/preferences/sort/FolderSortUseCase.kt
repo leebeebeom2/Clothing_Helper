e1779fde29154036de8c5904ce6efcddd6080e31
leebeebeom <boole92@naver.com>
2022-12-06 23:46:51 +0900

    코드 정리

--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/sort/FolderSortUseCase.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/sort/FolderSortUseCase.kt
@@ -1,9 +1,11 @@
 package com.leebeebeom.clothinghelperdomain.usecase.preferences.sort
 
 import com.leebeebeom.clothinghelperdomain.repository.preferences.FolderPreferencesRepository
+import com.leebeebeom.clothinghelperdomain.repository.preferences.SortPreferenceRepository
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class FolderSortUseCase @Inject constructor(folderPreferencesRepository: FolderPreferencesRepository) :
-    SortUseCase by SortUseCaseImpl(folderPreferencesRepository)
+class FolderSortUseCase @Inject constructor(
+    @FolderPreferencesRepository folderPreferencesRepository: SortPreferenceRepository
+) : SortUseCase by SortUseCaseImpl(folderPreferencesRepository)
--- clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/sort/SubCategorySortUseCase.kt
+++ clothinghelperdomain/src/main/java/com/leebeebeom/clothinghelperdomain/usecase/preferences/sort/SubCategorySortUseCase.kt
@@ -1,10 +1,11 @@
 package com.leebeebeom.clothinghelperdomain.usecase.preferences.sort
 
+import com.leebeebeom.clothinghelperdomain.repository.preferences.SortPreferenceRepository
 import com.leebeebeom.clothinghelperdomain.repository.preferences.SubCategoryPreferencesRepository
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class SubCategorySortUseCase
-@Inject constructor(subCategoryPreferencesRepository: SubCategoryPreferencesRepository) :
-    SortUseCase by SortUseCaseImpl(subCategoryPreferencesRepository)
+class SubCategorySortUseCase @Inject constructor(
+    @SubCategoryPreferencesRepository subCategoryPreferencesRepository: SortPreferenceRepository
+) : SortUseCase by SortUseCaseImpl(subCategoryPreferencesRepository)

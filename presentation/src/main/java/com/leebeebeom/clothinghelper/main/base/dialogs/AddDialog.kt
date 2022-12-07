cb03c098b197e0f5378219a2a52eb8de2b356980
leebeebeom <boole92@naver.com>
2022-12-07 12:28:03 +0900

    코드 정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/dialogs/AddDialog.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/main/base/dialogs/AddDialog.kt
@@ -2,14 +2,10 @@ package com.leebeebeom.clothinghelper.main.base.dialogs
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.focus.FocusState
-import androidx.compose.ui.text.TextRange
 import com.leebeebeom.clothinghelper.main.base.dialogs.composables.BaseTextFieldDialogState
 import com.leebeebeom.clothinghelper.main.base.dialogs.composables.TextFieldDialog
 import com.leebeebeom.clothinghelperdomain.model.data.BaseModel
@@ -27,11 +23,10 @@ fun AddDialog(
     onDismiss: () -> Unit
 ) {
     if (show()) {
-        val names by remember { derivedStateOf { items().map { it.name } } }
+        val names = remember(items()) { items().map { it.name } }
         val state = rememberSaveable(saver = AddDialogState.Saver) { AddDialogState() }
 
-        TextFieldDialog(
-            label = label,
+        TextFieldDialog(label = label,
             placeHolder = placeHolder,
             title = title,
             error = { state.error },
@@ -52,15 +47,8 @@ fun AddDialog(
 class AddDialogState(
     initialText: String = "", initialError: Int? = null
 ) : BaseTextFieldDialogState(initialText, initialError) {
-    fun onFocusChange(newFocusState: FocusState) {
-        if (newFocusState.hasFocus) textFieldValue =
-            textFieldValue.copy(selection = TextRange(text.length))
-    }
-
     companion object {
-        val Saver: Saver<AddDialogState, *> = listSaver(
-            save = { listOf(it.text, it.error) },
-            restore = { AddDialogState(it[0] as String, it[1] as? Int) }
-        )
+        val Saver: Saver<AddDialogState, *> = listSaver(save = { listOf(it.text, it.error) },
+            restore = { AddDialogState(it[0] as String, it[1] as? Int) })
     }
 }

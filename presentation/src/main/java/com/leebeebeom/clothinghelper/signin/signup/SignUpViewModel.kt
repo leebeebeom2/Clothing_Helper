860ad62cf6e3105197ff9835d5d6291fa7d23418
leebeebeom <boole92@naver.com>
2022-12-07 11:04:01 +0900

    코드 정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signup/SignUpViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/signin/signup/SignUpViewModel.kt
@@ -7,9 +7,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.base.ToastUIState
+import com.leebeebeom.clothinghelper.base.ToastUIStateImpl
 import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
 import com.leebeebeom.clothinghelper.signin.base.interfaces.GoogleButtonUIState
 import com.leebeebeom.clothinghelper.signin.base.interfaces.GoogleButtonUIStateImpl
+import com.leebeebeom.clothinghelper.signin.base.interfaces.PasswordUIState
+import com.leebeebeom.clothinghelper.signin.base.interfaces.PasswordUIStateImpl
 import com.leebeebeom.clothinghelper.signin.base.setFireBaseError
 import com.leebeebeom.clothinghelperdomain.model.AuthResult
 import com.leebeebeom.clothinghelperdomain.usecase.user.GoogleSignInUseCase
@@ -34,6 +38,7 @@ class SignUpViewModel @Inject constructor(
                 onSubCategoryLoadFail = { uiState.showToast(R.string.data_load_failed) }
             )
             when (result) {
+                is AuthResult.Success -> showToast(R.string.sign_up_complete)
                 is AuthResult.Fail -> setFireBaseError(
                     errorCode = result.errorCode,
                     updateEmailError = uiState::updateEmailError,
@@ -52,8 +57,10 @@ class SignUpViewModel @Inject constructor(
 }
 
 class SignUpUIState(
-    private val googleButtonUIStateImpl: GoogleButtonUIStateImpl = GoogleButtonUIStateImpl()
-) : GoogleButtonUIState by googleButtonUIStateImpl {
+    private val passwordUIState: PasswordUIStateImpl = PasswordUIStateImpl()
+) : PasswordUIState by passwordUIState,
+    GoogleButtonUIState by GoogleButtonUIStateImpl(),
+    ToastUIState by ToastUIStateImpl() {
     var passwordConfirmError: Int? by mutableStateOf(null)
         private set
 
@@ -67,7 +74,7 @@ class SignUpUIState(
     }
 
     override fun onPasswordChange(password: String) {
-        googleButtonUIStateImpl.onPasswordChange(password)
+        passwordUIState.onPasswordChange(password)
 
         if (password.isNotBlank()) {
             if (password.length < 6) updatePasswordError(R.string.error_weak_password)
@@ -90,6 +97,6 @@ class SignUpUIState(
     }
 
     override val buttonEnabled by derivedStateOf {
-        googleButtonUIStateImpl.buttonEnabled && passwordConfirm.isNotBlank() && this.passwordConfirmError == null && name.isNotBlank()
+        passwordUIState.buttonEnabled && passwordConfirm.isNotBlank() && this.passwordConfirmError == null && name.isNotBlank()
     }
 }

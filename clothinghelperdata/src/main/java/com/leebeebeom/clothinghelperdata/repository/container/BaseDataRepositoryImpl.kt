b39a3070fb7d873d683e01b67d6ed1024229c21b
leebeebeom <boole92@naver.com>
2022-12-08 23:30:31 +0900

    코드 간소화

--- clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/container/BaseDataRepositoryImpl.kt
+++ clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/container/BaseDataRepositoryImpl.kt
@@ -4,14 +4,13 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.leebeebeom.clothinghelperdata.repository.base.LoadingRepositoryImpl
-import com.leebeebeom.clothinghelperdata.repository.util.getSorted
 import com.leebeebeom.clothinghelperdata.repository.util.logE
-import com.leebeebeom.clothinghelperdata.repository.util.updateMutable
 import com.leebeebeom.clothinghelperdomain.model.FirebaseResult
+import com.leebeebeom.clothinghelperdomain.model.Order
+import com.leebeebeom.clothinghelperdomain.model.Sort
 import com.leebeebeom.clothinghelperdomain.model.SortPreferences
 import com.leebeebeom.clothinghelperdomain.model.data.BaseModel
 import com.leebeebeom.clothinghelperdomain.repository.BaseDataRepository
-import com.leebeebeom.clothinghelperdomain.repository.LoadingRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.flow.Flow
@@ -25,16 +24,13 @@ import kotlinx.coroutines.withTimeout
 private val db = Firebase.database.apply { setPersistenceEnabled(true) }
 private val loadingRepositoryImpl = LoadingRepositoryImpl(true)
 
-abstract class BaseDataRepositoryImpl<T : BaseModel>(
-    private val sortFlow: Flow<SortPreferences>,
-) : LoadingRepository by loadingRepositoryImpl, BaseDataRepository<T> {
-    protected val root = db.reference
+class BaseDataRepositoryImpl<T : BaseModel>(
+    sortFlow: Flow<SortPreferences>, private val refPath: String
+) : LoadingRepositoryImpl(true), BaseDataRepository<T> {
+    val root = db.reference
 
     private val _allData = MutableStateFlow(emptyList<T>())
-    override val allData = getSortedContainers()
-    private fun getSortedContainers() = combine(_allData, sortFlow, transform = ::getSorted)
-
-    protected abstract val refPath: String
+    override val allData = combine(_allData, sortFlow, transform = ::getSortedData)
 
     override suspend fun load(uid: String?, type: Class<T>) = databaseTryWithLoading("update") {
         uid?.let {
@@ -52,76 +48,83 @@ abstract class BaseDataRepositoryImpl<T : BaseModel>(
         }
     }
 
-    override suspend fun add(t: T, uid: String) = databaseTryWithTimeOut(1000, "add") {
-        val containerRef = root.getContainerRef(uid, refPath)
+    override suspend fun add(t: T, uid: String) =
+        databaseTryWithTimeOut(1000, "add") {
+            val containerRef = root.getContainerRef(uid, refPath)
 
-        val newContainer = getNewContainer(
-            t = t, key = getKey(containerRef), createDate = System.currentTimeMillis()
-        )
+            @Suppress("UNCHECKED_CAST")
+            val tWithKey = t.addKey(getKey(containerRef)) as T
 
-        push(containerRef, newContainer).await()
-        _allData.updateMutable { it.add(newContainer) }
-        FirebaseResult.Success
-    }
+            push(containerRef, tWithKey).await()
+            _allData.updateList { it.add(tWithKey) }
+            FirebaseResult.Success
+        }
 
     override suspend fun edit(newT: T, uid: String): FirebaseResult =
         databaseTryWithTimeOut(1000, "edit") {
-            val newContainerWithNewEditDate =
-                getContainerWithNewEditDate(newT = newT, editDate = System.currentTimeMillis())
-            root.getContainerRef(uid, refPath).child(newContainerWithNewEditDate.key)
-                .setValue(newContainerWithNewEditDate).await()
+            root.getContainerRef(uid, refPath).child(newT.key).setValue(newT).await()
 
-            _allData.updateMutable {
-                val oldContainer =
-                    it.first { value -> value.key == newContainerWithNewEditDate.key }
-                it.remove(oldContainer)
-                it.add(newContainerWithNewEditDate)
+            _allData.updateList {
+                val oldT = it.first { oldT -> oldT.key == newT.key }
+                it.remove(oldT)
+                it.add(newT)
             }
             FirebaseResult.Success
         }
 
     private suspend fun databaseTryWithTimeOut(
-        time: Long, site: String, task: suspend () -> FirebaseResult
+        time: Long, callSite: String, task: suspend () -> FirebaseResult
     ) = withContext(Dispatchers.IO) {
         try {
             withContext(NonCancellable) { withTimeout(time) { task() } }
         } catch (e: Exception) {
-            logE(site, e)
+            logE(callSite, e)
             FirebaseResult.Fail(e)
         }
     }
 
     private suspend fun databaseTryWithLoading(
-        site: String, task: suspend () -> FirebaseResult
+        callSite: String, task: suspend () -> FirebaseResult
     ) = withContext(Dispatchers.IO) {
         try {
             loadingRepositoryImpl.loadingOn()
             withContext(NonCancellable) { task() }
         } catch (e: Exception) {
-            logE(site, e)
+            logE(callSite, e)
             FirebaseResult.Fail(e)
         } finally {
             loadingRepositoryImpl.loadingOff()
         }
     }
 
-    protected suspend fun push(containerRef: DatabaseReference, t: T) =
+    private fun getKey(containerRef: DatabaseReference) = containerRef.push().key!!
+
+    private suspend fun push(containerRef: DatabaseReference, t: T) =
         withContext(Dispatchers.IO) { containerRef.child(t.key).setValue(t) }
 
-    protected fun getKey(containerRef: DatabaseReference) = containerRef.push().key!!
-
-    /**
-     * 추가될 객체
-     *
-     * [createDate]를 createDate와 editDate에 할당하여 반환 바람
-     */
-    protected abstract fun getNewContainer(t: T, key: String, createDate: Long): T
-    protected fun DatabaseReference.getContainerRef(uid: String, path: String) =
+    private fun DatabaseReference.getContainerRef(uid: String, path: String) =
         child(uid).child(path)
 
-    /**
-     * 수정될 객체
-     * [editDate]를 editDate에 할당하여 반환 바람
-     */
-    protected abstract fun getContainerWithNewEditDate(newT: T, editDate: Long): T
+    private fun <T : BaseModel> getSortedData(
+        allData: List<T>, sortPreferences: SortPreferences
+    ): List<T> {
+        val sort = sortPreferences.sort
+        val order = sortPreferences.order
+
+        return when {
+            sort == Sort.NAME && order == Order.ASCENDING -> allData.sortedBy { it.name }
+            sort == Sort.NAME && order == Order.DESCENDING -> allData.sortedByDescending { it.name }
+            sort == Sort.CREATE && order == Order.ASCENDING -> allData.sortedBy { it.createDate }
+            sort == Sort.CREATE && order == Order.DESCENDING -> allData.sortedByDescending { it.createDate }
+            sort == Sort.EDIT && order == Order.ASCENDING -> allData.sortedBy { it.editDate }
+            sort == Sort.EDIT && order == Order.DESCENDING -> allData.sortedByDescending { it.editDate }
+            else -> allData
+        }
+    }
+
+    private fun <T> MutableStateFlow<List<T>>.updateList(task: (MutableList<T>) -> Unit) {
+        val temp = value.toMutableList()
+        task(temp)
+        update { temp }
+    }
 }

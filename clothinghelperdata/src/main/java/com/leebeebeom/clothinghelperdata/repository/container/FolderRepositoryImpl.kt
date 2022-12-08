9b32ef3f5f794d78e566799cb10cfe8470d78c21
leebeebeom <boole92@naver.com>
2022-12-08 23:39:16 +0900

    코드 간소화

--- clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/container/FolderRepositoryImpl.kt
+++ clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/container/FolderRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.leebeebeom.clothinghelperdata.repository.container
 
 import com.leebeebeom.clothinghelperdata.repository.base.DatabasePath
 import com.leebeebeom.clothinghelperdomain.model.data.Folder
+import com.leebeebeom.clothinghelperdomain.repository.BaseDataRepository
 import com.leebeebeom.clothinghelperdomain.repository.FolderRepository
 import com.leebeebeom.clothinghelperdomain.repository.preferences.FolderPreferencesRepository
 import com.leebeebeom.clothinghelperdomain.repository.preferences.SortPreferenceRepository
@@ -11,12 +12,7 @@ import javax.inject.Singleton
 @Singleton
 class FolderRepositoryImpl @Inject constructor(
     @FolderPreferencesRepository folderPreferencesRepository: SortPreferenceRepository
-) : BaseDataRepositoryImpl<Folder>(sortFlow = folderPreferencesRepository.sort), FolderRepository {
-    override val refPath = DatabasePath.FOLDERS
-
-    override fun getNewContainer(t: Folder, key: String, createDate: Long) =
-        t.copy(key = key, createDate = createDate, editDate = createDate)
-
-    override fun getContainerWithNewEditDate(newT: Folder, editDate: Long) =
-        newT.copy(editDate = editDate)
-}
+) : BaseDataRepository<Folder> by BaseDataRepositoryImpl(
+    sortFlow = folderPreferencesRepository.sort,
+    refPath = DatabasePath.FOLDERS
+), FolderRepository

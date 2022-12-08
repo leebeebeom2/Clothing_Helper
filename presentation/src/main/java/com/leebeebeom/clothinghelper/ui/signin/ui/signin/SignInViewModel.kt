7f1a83cf8f121f2619d952038b211d42baf1334b
leebeebeom <boole92@naver.com>
2022-12-09 03:19:12 +0900

    코드 간소화

--- presentation/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInViewModel.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInViewModel.kt
@@ -1,15 +1,13 @@
-package com.leebeebeom.clothinghelper.signin.signin
+package com.leebeebeom.clothinghelper.ui.signin.ui.signin
 
 import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.ToastUIState
-import com.leebeebeom.clothinghelper.base.ToastUIStateImpl
-import com.leebeebeom.clothinghelper.signin.base.GoogleSignInUpViewModel
-import com.leebeebeom.clothinghelper.signin.base.interfaces.GoogleButtonUIState
-import com.leebeebeom.clothinghelper.signin.base.interfaces.GoogleButtonUIStateImpl
-import com.leebeebeom.clothinghelper.signin.base.interfaces.PasswordUIState
-import com.leebeebeom.clothinghelper.signin.base.interfaces.PasswordUIStateImpl
-import com.leebeebeom.clothinghelper.signin.base.setFireBaseError
+import com.leebeebeom.clothinghelper.ui.signin.base.GoogleSignInUpViewModel
+import com.leebeebeom.clothinghelper.ui.signin.base.setFireBaseError
+import com.leebeebeom.clothinghelper.ui.signin.state.GoogleButtonUIState
+import com.leebeebeom.clothinghelper.ui.signin.state.GoogleButtonUIStateImpl
+import com.leebeebeom.clothinghelper.ui.signin.state.PasswordUIState
+import com.leebeebeom.clothinghelper.ui.signin.state.PasswordUIStateImpl
 import com.leebeebeom.clothinghelperdomain.model.AuthResult
 import com.leebeebeom.clothinghelperdomain.usecase.user.GoogleSignInUseCase
 import com.leebeebeom.clothinghelperdomain.usecase.user.SignInUseCase
@@ -22,29 +20,23 @@ class SignInViewModel @Inject constructor(
     private val signInUseCase: SignInUseCase, googleSignInUseCase: GoogleSignInUseCase
 ) : GoogleSignInUpViewModel(googleSignInUseCase) {
 
-    val uiState = SignInUIState()
+    override val uiState = SignInUIState()
 
     fun signInWithEmailAndPassword() =
         viewModelScope.launch {
             when (val result = signInUseCase.signIn(uiState.email, uiState.password)) {
-                is AuthResult.Success -> showToast(R.string.sign_in_complete)
+                is AuthResult.Success -> uiState.showToast(R.string.sign_in_complete)
                 is AuthResult.Fail -> setFireBaseError(
                     errorCode = result.errorCode,
                     updateEmailError = uiState::updateEmailError,
                     updatePasswordError = uiState::updatePasswordError,
-                    showToast = ::showToast
+                    showToast = uiState::showToast
                 )
-                is AuthResult.UnknownFail -> showToast(R.string.unknown_error)
+                is AuthResult.UnknownFail -> uiState.showToast(R.string.unknown_error)
             }
         }
-
-    override fun updateGoogleButtonEnabled(enabled: Boolean) =
-        uiState.updateGoogleButtonEnabled(enabled)
-
-    override fun showToast(text: Int) = uiState.showToast(text)
 }
 
 class SignInUIState :
     GoogleButtonUIState by GoogleButtonUIStateImpl(),
-    ToastUIState by ToastUIStateImpl(),
     PasswordUIState by PasswordUIStateImpl()

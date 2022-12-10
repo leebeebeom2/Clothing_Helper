c7923438d5ac903f3c990e5541adcd914bd48a33
leebeebeom <boole92@naver.com>
2022-12-11 05:20:12 +0900

    인포 변경

--- presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/subcategory/content/subcategorycard/SubCategoryInfo.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/subcategory/content/subcategorycard/SubCategoryInfo.kt
@@ -1,8 +1,10 @@
-package com.leebeebeom.clothinghelper.main.subcategory.content.subcategorycard
+package com.leebeebeom.clothinghelper.ui.main.subcategory.content.subcategorycard
 
 import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,8 +13,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.Anime
-import com.leebeebeom.clothinghelper.base.composables.SimpleHeightSpacer
+import com.leebeebeom.clothinghelper.composable.SimpleHeightSpacer
+import com.leebeebeom.clothinghelper.util.Anime
 
 @Composable
 fun SubCategoryInfo(isExpanded: () -> Boolean) {
@@ -22,17 +24,22 @@ fun SubCategoryInfo(isExpanded: () -> Boolean) {
         exit = Anime.SubCategoryCardInfo.shrinkOut
     ) {
         Surface(color = MaterialTheme.colors.background) {
-            Row(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(8.dp)
+                    .padding(vertical = 10.dp)
+                    .padding(horizontal = 16.dp)
             ) {
                 SubCategoryInfoText( // TODO
-                    infoTitle = R.string.average_size, info = R.string.top_info
+                    infoTitle = R.string.average_size, info = R.string.info
                 )
-
+                SimpleHeightSpacer(dp = 4)
                 SubCategoryInfoText(
-                    infoTitle = R.string.most_have_size, info = R.string.top_info
+                    infoTitle = R.string.minimum_size, info = R.string.info
+                )
+                SimpleHeightSpacer(dp = 4)
+                SubCategoryInfoText(
+                    infoTitle = R.string.maximum_size, info = R.string.info
                 )
             }
         }
@@ -40,19 +47,14 @@ fun SubCategoryInfo(isExpanded: () -> Boolean) {
 }
 
 @Composable
-fun RowScope.SubCategoryInfoText(@StringRes infoTitle: Int, @StringRes info: Int) {
+fun SubCategoryInfoText(@StringRes infoTitle: Int, @StringRes info: Int) {
     ProvideTextStyle(
         value = MaterialTheme.typography.caption.copy(
             color = LocalContentColor.current.copy(ContentAlpha.medium), fontSize = 13.sp
         )
     ) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 8.dp)
-        ) {
+        Column {
             Text(text = stringResource(id = infoTitle), fontWeight = FontWeight.Bold)
-            SimpleHeightSpacer(dp = 2)
             Text(text = stringResource(id = info))
         }
     }

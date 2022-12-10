802f1bde5e6a7080454410803b996daf531809b9
leebeebeom <boole92@naver.com>
2022-12-11 00:42:33 +0900

    디바이더 삭제

--- presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/composables/sort/SortIcon.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/composables/sort/SortIcon.kt
@@ -1,38 +1,17 @@
-package com.leebeebeom.clothinghelper.main.base.composables.sort
+package com.leebeebeom.clothinghelper.ui.main.composables.sort
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Divider
 import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.base.composables.CustomIconButton
+import com.leebeebeom.clothinghelper.composable.CustomIconButton
 import com.leebeebeom.clothinghelperdomain.model.Order
 import com.leebeebeom.clothinghelperdomain.model.Sort
 import com.leebeebeom.clothinghelperdomain.model.SortPreferences
 
 @Composable
-fun SortIconWithDivider(
-    sort: () -> SortPreferences,
-    onSortClick: (Sort) -> Unit,
-    onOrderClick: (Order) -> Unit
-) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Divider(modifier = Modifier.weight(1f))
-
-        SortIcon(
-            sort = sort,
-            onSortClick = onSortClick,
-            onOrderClick = onOrderClick
-        )
-    }
-}
-
-@Composable
-private fun SortIcon(
+fun SortIcon(
     sort: () -> SortPreferences,
     onSortClick: (Sort) -> Unit,
     onOrderClick: (Order) -> Unit

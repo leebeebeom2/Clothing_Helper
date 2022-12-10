b7d31777510078025d14d2db870a3edd68cbf964
leebeebeom <boole92@naver.com>
2022-12-11 03:34:48 +0900

    코드 정리

--- presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/root/contents/dropdownmenus/DrawerMainCategoryDropDownMenu.kt
+++ presentation/src/main/java/com/leebeebeom/clothinghelper/ui/main/root/contents/dropdownmenus/DrawerMainCategoryDropDownMenu.kt
@@ -1,14 +1,14 @@
-package com.leebeebeom.clothinghelper.main.root.contents.dropdownmenus
+package com.leebeebeom.clothinghelper.ui.main.root.contents.dropdownmenus
 
+import androidx.compose.material.DropdownMenu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.main.base.composables.DropDownMenuRoot
-import com.leebeebeom.clothinghelper.main.base.dialogs.AddSubCategoryDialog
 import com.leebeebeom.clothinghelper.map.StableSubCategory
+import com.leebeebeom.clothinghelper.ui.main.dialogs.AddSubCategoryDialog
 import kotlinx.collections.immutable.ImmutableList
 
 @Composable
@@ -20,7 +20,7 @@ fun DrawerMainCategoryDropDownMenu(
 ) {
     var showDialog by rememberSaveable { mutableStateOf(false) }
 
-    DropDownMenuRoot(show = show, onDismiss = onDismiss) {
+    DropdownMenu(expanded = show(), onDismissRequest = onDismiss) {
         DrawerDropdownMenuItem(
             text = R.string.add_category,
             onClick = { showDialog = true },

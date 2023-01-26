6ea955d99a79be5e85d5b19fb303188083475998
leebeebeom <boole92@naver.com>
2023-01-27 06:05:53 +0900

    어플리케이션 클래스, 뷰모델 분리

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.leebeebeom.clothinghelper.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.leebeebeom.clothinghelper.ui.main.MainNavHost
+import com.leebeebeom.clothinghelper.ui.signin.ui.SignInNavHost
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { MainActivityScreen() }
+    }
+
+    @Composable
+    fun MainActivityScreen(viewModel: MainActivityViewModel = hiltViewModel()) {
+        val isSignIn by viewModel.isSignIn.collectAsStateWithLifecycle()
+
+        Crossfade(targetState = isSignIn) { if (it) MainNavHost() else SignInNavHost() }
+    }
+}

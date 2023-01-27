6b03256494d543bb3b8519f14c668b5bf16bee5f
leebeebeom <boole92@naver.com>
2023-01-28 06:15:41 +0900

    리팩토링

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/composable/textfield/EmailTextField.kt
@@ -0,0 +1,41 @@
+package com.leebeebeom.clothinghelper.ui.signin.composable.textfield
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.ui.components.ImeActionRoute
+import com.leebeebeom.clothinghelper.ui.components.KeyboardRoute
+import com.leebeebeom.clothinghelper.ui.components.MaxWidthTextFieldWithError
+import com.leebeebeom.clothinghelper.ui.components.rememberMaxWidthTextFieldState
+
+@Composable
+fun EmailTextField(
+    error: () -> Int?,
+    imeActionRoute: ImeActionRoute = ImeActionRoute.NEXT,
+    onInputChange: (String) -> Unit
+) {
+    val state = rememberEmailTextFieldState(initialError = error(), imeActionRoute = imeActionRoute)
+
+    MaxWidthTextFieldWithError(
+        state = state,
+        onValueChange = state::onValueChange,
+        onFocusChanged = state::onFocusChanged
+    )
+
+    LaunchedEffect(state) {
+        snapshotFlow { state.textFieldValue.text }.collect(onInputChange)
+    }
+}
+
+@Composable
+private fun rememberEmailTextFieldState(
+    initialError: Int?, imeActionRoute: ImeActionRoute
+) = rememberMaxWidthTextFieldState(
+    label = R.string.email,
+    placeholder = R.string.email_place_holder,
+    showKeyboard = true,
+    initialError = initialError,
+    keyboardRoute = KeyboardRoute.EMAIL,
+    imeActionRoute = imeActionRoute
+)

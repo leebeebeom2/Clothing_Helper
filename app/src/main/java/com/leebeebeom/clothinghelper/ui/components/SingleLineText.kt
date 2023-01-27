c37697d229432183a237810ceb3420956910f449
leebeebeom <boole92@naver.com>
2023-01-28 02:30:05 +0900

    코드 정리

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/components/SingleLineText.kt
@@ -0,0 +1,58 @@
+package com.leebeebeom.clothinghelper.ui.components
+
+import androidx.annotation.StringRes
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+
+@JvmName("SingleLineTextString")
+@Composable
+fun SingleLineText(
+    modifier: Modifier = Modifier,
+    textString: () -> String?,
+    style: TextStyle = LocalTextStyle.current,
+    textAlign: TextAlign? = null
+) {
+    SingleLineText(modifier = modifier, text = textString(), style = style, textAlign = textAlign)
+}
+
+@Composable
+fun SingleLineText(
+    modifier: Modifier = Modifier,
+    @StringRes text: Int?,
+    style: TextStyle = LocalTextStyle.current,
+    textAlign: TextAlign? = null
+) {
+    text?.let {
+        SingleLineText(
+            modifier = modifier,
+            text = stringResource(id = text),
+            style = style,
+            textAlign = textAlign
+        )
+    }
+}
+
+@Composable
+fun SingleLineText(
+    modifier: Modifier = Modifier,
+    text: String?,
+    style: TextStyle = LocalTextStyle.current,
+    textAlign: TextAlign? = null
+) {
+    text?.let {
+        Text(
+            modifier = modifier,
+            text = text,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            style = style,
+            textAlign = textAlign
+        )
+    }
+}

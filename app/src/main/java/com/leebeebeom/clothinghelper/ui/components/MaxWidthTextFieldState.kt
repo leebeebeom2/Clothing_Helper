6a15fdcfc436d4f067f98a1b3e0c06dcbd3ac6af
leebeebeom <boole92@naver.com>
2023-01-28 06:00:20 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/components/MaxWidthTextFieldState.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/components/MaxWidthTextFieldState.kt
@@ -44,10 +44,17 @@ enum class ImeActionRoute {
 fun MaxWidthTextFieldWithError(
     modifier: Modifier = Modifier,
     state: MaxWidthTextFieldState,
+    onValueChange: (TextFieldValue) -> Unit,
+    onFocusChanged: (FocusState) -> Unit,
     trailingIcon: @Composable (() -> Unit)?
 ) {
     Column(modifier = modifier) {
-        MaxWidthTextField(state = state, trailingIcon = trailingIcon)
+        MaxWidthTextField(
+            state = state,
+            trailingIcon = trailingIcon,
+            onValueChange = onValueChange,
+            onFocusChanged = onFocusChanged
+        )
         ErrorText(state = state)
     }
     ShowKeyboard(state = state)
@@ -57,21 +64,27 @@ fun MaxWidthTextFieldWithError(
 private fun MaxWidthTextField(
     state: MaxWidthTextFieldState,
     trailingIcon: @Composable (() -> Unit)?,
+    onValueChange: (TextFieldValue) -> Unit,
+    onFocusChanged: (FocusState) -> Unit,
     focusManager: FocusManager = LocalFocusManager.current
 ) {
     OutlinedTextField(
         modifier = Modifier
             .fillMaxWidth()
             .focusRequester(state.focusRequester)
-            .onFocusChanged(onFocusChanged = state::onFocusChanged),
+            .onFocusChanged(onFocusChanged = onFocusChanged),
         value = state.textFieldValue,
-        onValueChange = state::onValueChange,
+        onValueChange = onValueChange,
         label = { SingleLineText(text = state.label) },
         placeholder = state.placeholder?.let { { SingleLineText(text = state.placeholder) } },
         isError = state.isError,
         visualTransformation = if (state.isVisible) VisualTransformation.None else PasswordVisualTransformation(),
         keyboardOptions = state.keyboardOptions,
-        trailingIcon = { state.TrailingIcon(trailingIcon = trailingIcon) },
+        trailingIcon = {
+            state.TrailingIcon(
+                trailingIcon = trailingIcon, onValueChange = onValueChange
+            )
+        },
         singleLine = true,
         keyboardActions = state.getKeyboardActions(focusManager = focusManager),
         colors = TextFieldDefaults.outlinedTextFieldColors(
@@ -122,21 +135,22 @@ interface MaxWidthTextFieldState {
     val focusRequester: FocusRequester
     val keyboardRoute: KeyboardRoute
     val imeActionRoute: ImeActionRoute
-    fun onValueChange(newTextField: TextFieldValue)
+
     fun showKeyboard(scope: CoroutineScope, keyboardController: SoftwareKeyboardController?)
-    fun onFocusChanged(focusState: FocusState)
     fun getKeyboardActions(focusManager: FocusManager): KeyboardActions
 
     @Composable
-    fun TrailingIcon(trailingIcon: @Composable (() -> Unit)?) {
+    fun TrailingIcon(
+        trailingIcon: @Composable (() -> Unit)?, onValueChange: (TextFieldValue) -> Unit
+    ) {
         if (trailingIcon != null || cancelIconEnabled) Row {
-            trailingIcon?.run { invoke() } ?: CancelIcon()
+            trailingIcon?.run { invoke() } ?: CancelIcon(onValueChange = onValueChange)
             WidthSpacer(dp = 4)
         }
     }
 
     @Composable
-    private fun CancelIcon() {
+    private fun CancelIcon(onValueChange: (TextFieldValue) -> Unit) {
         AnimatedVisibility(
             visible = textFieldValue.text.isNotEmpty() && hasFocus,
             enter = Anime.CancelIcon.fadeIn,
@@ -153,14 +167,14 @@ interface MaxWidthTextFieldState {
 }
 
 @Stable
-class MutableMaxWidthTextFieldState(
+open class MutableMaxWidthTextFieldState(
     initialText: String = "",
     initialError: Int? = null,
     initialHasFocus: Boolean = false,
     initialEnabled: Boolean = true,
     initialVisibility: Boolean = true,
-    override val keyboardRoute: KeyboardRoute = KeyboardRoute.TEXT,
-    override val imeActionRoute: ImeActionRoute = ImeActionRoute.DONE,
+    final override val keyboardRoute: KeyboardRoute = KeyboardRoute.TEXT,
+    final override val imeActionRoute: ImeActionRoute = ImeActionRoute.DONE,
     override val label: Int,
     override val placeholder: Int?,
     override val showKeyboard: Boolean,
@@ -168,9 +182,9 @@ class MutableMaxWidthTextFieldState(
 ) : MaxWidthTextFieldState {
     override var textFieldValue by mutableStateOf(TextFieldValue(initialText))
     override var error: Int? by mutableStateOf(initialError)
-    override val hasFocus by mutableStateOf(initialHasFocus)
-    override val enabled by mutableStateOf(initialEnabled)
-    override val isVisible by mutableStateOf(initialVisibility)
+    override var hasFocus by mutableStateOf(initialHasFocus)
+    override var enabled by mutableStateOf(initialEnabled)
+    override var isVisible by mutableStateOf(initialVisibility)
 
     private val keyboardType = when (keyboardRoute) {
         KeyboardRoute.TEXT -> KeyboardType.Text
@@ -187,13 +201,12 @@ class MutableMaxWidthTextFieldState(
 
     override val focusRequester: FocusRequester = FocusRequester()
 
-    override fun onValueChange(newTextField: TextFieldValue) {
+    fun onValueChange(newTextField: TextFieldValue) {
         textFieldValue = newTextField
     }
 
     override fun showKeyboard(
-        scope: CoroutineScope,
-        keyboardController: SoftwareKeyboardController?
+        scope: CoroutineScope, keyboardController: SoftwareKeyboardController?
     ) {
         scope.launch {
             focusRequester.requestFocus()
@@ -202,45 +215,44 @@ class MutableMaxWidthTextFieldState(
         }
     }
 
-    override fun onFocusChanged(focusState: FocusState) {}
+    open fun onFocusChanged(focusState: FocusState) {
+        hasFocus = focusState.hasFocus
+    }
 
     override fun getKeyboardActions(focusManager: FocusManager) =
         if (imeAction == ImeAction.Done) KeyboardActions(onDone = { focusManager.clearFocus() })
         else KeyboardActions.Default
 
     companion object {
-        val Saver: Saver<MutableMaxWidthTextFieldState, *> = listSaver(
-            save = {
-                listOf(
-                    it.textFieldValue.text,
-                    it.error,
-                    it.hasFocus,
-                    it.enabled,
-                    it.isVisible,
-                    it.keyboardRoute,
-                    it.imeActionRoute,
-                    it.label,
-                    it.placeholder,
-                    it.showKeyboard,
-                    it.cancelIconEnabled
-                )
-            },
-            restore = {
-                MutableMaxWidthTextFieldState(
-                    initialText = it[0] as String,
-                    initialError = it[1] as Int?,
-                    initialHasFocus = it[2] as Boolean,
-                    initialEnabled = it[3] as Boolean,
-                    initialVisibility = it[4] as Boolean,
-                    keyboardRoute = it[5] as KeyboardRoute,
-                    imeActionRoute = it[6] as ImeActionRoute,
-                    label = it[7] as Int,
-                    placeholder = it[8] as Int?,
-                    showKeyboard = it[9] as Boolean,
-                    cancelIconEnabled = it[10] as Boolean
-                )
-            }
-        )
+        val Saver: Saver<MutableMaxWidthTextFieldState, *> = listSaver(save = {
+            listOf(
+                it.textFieldValue.text,
+                it.error,
+                it.hasFocus,
+                it.enabled,
+                it.isVisible,
+                it.keyboardRoute,
+                it.imeActionRoute,
+                it.label,
+                it.placeholder,
+                it.showKeyboard,
+                it.cancelIconEnabled
+            )
+        }, restore = {
+            MutableMaxWidthTextFieldState(
+                initialText = it[0] as String,
+                initialError = it[1] as Int?,
+                initialHasFocus = it[2] as Boolean,
+                initialEnabled = it[3] as Boolean,
+                initialVisibility = it[4] as Boolean,
+                keyboardRoute = it[5] as KeyboardRoute,
+                imeActionRoute = it[6] as ImeActionRoute,
+                label = it[7] as Int,
+                placeholder = it[8] as Int?,
+                showKeyboard = it[9] as Boolean,
+                cancelIconEnabled = it[10] as Boolean
+            )
+        })
     }
 }
 

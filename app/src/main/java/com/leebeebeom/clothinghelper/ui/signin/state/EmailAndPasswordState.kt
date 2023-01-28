474a3c69784fad3f0b77e164fb1e51d7e7a1047d
leebeebeom <boole92@naver.com>
2023-01-29 06:12:25 +0900

    구조 변경

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/state/EmailAndPasswordState.kt
@@ -0,0 +1,14 @@
+package com.leebeebeom.clothinghelper.ui.signin.state
+
+import androidx.compose.runtime.*
+
+@Stable
+interface EmailAndPasswordState : EmailUiState {
+    val passwordError: Int?
+}
+
+open class MutableEmailAndPasswordUiState : MutableEmailUiState(), EmailAndPasswordState {
+    override var passwordError: Int? by mutableStateOf(null)
+    var password = ""
+    override val buttonEnabled by derivedStateOf { super.buttonEnabled && password.isNotBlank() && passwordError == null }
+}
---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/state/EmailUiState.kt
@@ -0,0 +1,15 @@
+package com.leebeebeom.clothinghelper.ui.signin.state
+
+import androidx.compose.runtime.*
+
+@Stable
+interface EmailUiState {
+    val emailError: Int?
+    val buttonEnabled: Boolean
+}
+
+open class MutableEmailUiState : EmailUiState {
+    override var emailError: Int? by mutableStateOf(null)
+    var email = ""
+    override val buttonEnabled by derivedStateOf { email.isNotBlank() && emailError == null }
+}

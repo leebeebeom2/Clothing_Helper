9e000d1c94f8cfa1326256a43c9388036811ffc8
leebeebeom <boole92@naver.com>
2023-01-30 18:36:52 +0900

    리팩토링

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/main/dialogs/AddSubCategoryDialog.kt
@@ -0,0 +1,24 @@
+package com.leebeebeom.clothinghelper.ui.main.dialogs
+
+import androidx.compose.runtime.Composable
+import com.leebeebeom.clothinghelper.R
+import kotlinx.collections.immutable.ImmutableList
+
+@Composable
+fun AddSubCategoryDialog(
+    subCategoryNames: () -> ImmutableList<String>,
+    onPositiveButtonClick: (String) -> Unit,
+    show: () -> Boolean,
+    onDismiss: () -> Unit
+) {
+    if (show())
+        AddDialog(
+            label = R.string.category,
+            placeHolder = R.string.category_place_holder,
+            title = R.string.add_category,
+            existNameError = R.string.error_exist_category_name,
+            onPositiveButtonClick = onPositiveButtonClick,
+            names = subCategoryNames,
+            onDismiss = onDismiss
+        )
+}

af6c7b2cdf2f10f0c312ec7c9d4ad461b6c44906
leebeebeom <boole92@naver.com>
2023-01-30 15:24:28 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreen.kt
@@ -1,8 +1,7 @@
 package com.leebeebeom.clothinghelper.ui.signin.ui.signin
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TextButton
@@ -10,32 +9,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.ActivityViewModel
+import com.leebeebeom.clothinghelper.ui.activityViewModel
 import com.leebeebeom.clothinghelper.ui.components.HeightSpacer
 import com.leebeebeom.clothinghelper.ui.components.IconWrapper
 import com.leebeebeom.clothinghelper.ui.components.MaxWidthButton
 import com.leebeebeom.clothinghelper.ui.components.SingleLineText
-import com.leebeebeom.clothinghelper.ui.getActivityViewModel
 import com.leebeebeom.clothinghelper.ui.signin.composable.GoogleSignInButton
 import com.leebeebeom.clothinghelper.ui.signin.composable.Logo
 import com.leebeebeom.clothinghelper.ui.signin.composable.OrDivider
+import com.leebeebeom.clothinghelper.ui.signin.composable.SignInBaseColumn
 import com.leebeebeom.clothinghelper.ui.signin.composable.textfield.EmailTextField
 import com.leebeebeom.clothinghelper.ui.signin.composable.textfield.PasswordTextField
 import com.leebeebeom.clothinghelper.ui.signin.state.EmailAndPasswordState
 import com.leebeebeom.clothinghelper.ui.signin.ui.SignInNavUiState
 import com.leebeebeom.clothinghelper.ui.signin.ui.SignInNavViewModel
-
-/*
-에러 애니메이션, 로딩, 스크린 아웃, 로그인 토스트, 구글 로그인 토스트, 테스트
-
-구글 로그인 시 최초 유저라면 유저 데이터와 최초 데이터 푸쉬
-다른 계정으로 로그인 후 구글로 최초 로그인해도 전에 로그인한 유저 데이터 정상인지 확인
-(구글 최초 사용자 데이터 정상인지 확인)
-두 계정 번갈아 로그인해도 데이터 유지되는지 확인
- */
 
 @Composable
 fun SignInScreen(
@@ -45,19 +35,11 @@ fun SignInScreen(
     signInNavUiState: SignInNavUiState = signInNavViewModel.signInNavUiState,
     viewModel: SignInViewModel = viewModel(),
     uiState: EmailAndPasswordState = viewModel.uiState,
-    activityViewModel: ActivityViewModel = getActivityViewModel()
+    activityViewModel: ActivityViewModel = activityViewModel()
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(bottom = 40.dp), verticalArrangement = Arrangement.Center
-    ) {
+    SignInBaseColumn {
         Logo()
-        EmailTextField(
-            error = { uiState.emailError }, onInputChange = viewModel::onEmailChange
-        )
-
+        EmailTextField(error = { uiState.emailError }, onInputChange = viewModel::onEmailChange)
         PasswordTextField(
             error = { uiState.passwordError }, onInputChange = viewModel::onPasswordChange
         )
@@ -75,14 +57,11 @@ fun SignInScreen(
             colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.surface),
             icon = { IconWrapper(drawable = R.drawable.ic_email) })
         HeightSpacer(dp = 12)
-        GoogleSignInButton(enabled = { signInNavUiState.googleButtonEnabled },
-            onActivityResult = {
-                signInNavViewModel.signInWithGoogleEmail(
-                    activityResult = it,
-                    showToast = activityViewModel::showToast
-                )
-            },
-            onClick = { signInNavViewModel.setGoogleButtonEnable(false) })
+        GoogleSignInButton(enabled = { signInNavUiState.googleButtonEnabled }, onActivityResult = {
+            signInNavViewModel.signInWithGoogleEmail(
+                activityResult = it, showToast = activityViewModel::showToast
+            )
+        }, disable = { signInNavViewModel.setGoogleButtonEnable(false) })
     }
 }
 

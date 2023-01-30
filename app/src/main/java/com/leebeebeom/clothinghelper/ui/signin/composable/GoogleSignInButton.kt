861e0f557df411a58a3f29311f335c082547afa6
leebeebeom <boole92@naver.com>
2023-01-30 15:24:21 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/composable/GoogleSignInButton.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/composable/GoogleSignInButton.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -14,37 +15,36 @@ import androidx.compose.ui.res.vectorResource
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.composable.MaxWidthButton
+import com.leebeebeom.clothinghelper.ui.components.MaxWidthButton
 
 @Composable
 fun GoogleSignInButton(
-    enabled: () -> Boolean,
-    onActivityResult: (ActivityResult) -> Unit,
-    onClick: () -> Unit
+    enabled: () -> Boolean, onActivityResult: (ActivityResult) -> Unit, disable: () -> Unit
 ) {
     val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.StartActivityForResult(),
-        onResult = onActivityResult
+        contract = ActivityResultContracts.StartActivityForResult(), onResult = onActivityResult
     )
 
     val intent = GoogleSignIn.getClient(LocalContext.current, gso()).signInIntent
 
-    MaxWidthButton(
-        text = R.string.starts_with_google_email,
+    MaxWidthButton(text = R.string.starts_with_google_email,
         enabled = enabled,
         colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.surface),
         icon = { GoogleIcon() },
         onClick = {
-            onClick()
+            disable()
             launcher.launch(intent)
-        }
-    )
+        })
 }
 
 @Composable
 private fun gso(): GoogleSignInOptions {
-    return GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestIdToken(stringResource(id = R.string.default_web_client_id)).requestEmail().build()
+    val clientId = stringResource(id = R.string.default_web_client_id)
+
+    return remember {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(clientId)
+            .requestEmail().build()
+    }
 }
 
 @Composable

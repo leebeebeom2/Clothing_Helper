d967a1d895c730543dff53d6fe8fe29849ac9b0a
leebeebeom <boole92@naver.com>
2023-01-30 15:46:37 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/theme/Shape.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.leebeebeom.clothinghelper.theme
+package com.leebeebeom.clothinghelper.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes

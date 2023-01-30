114a19f258db3706f0f084f9faba2e15c54eb70b
leebeebeom <boole92@naver.com>
2023-01-31 08:08:39 +0900

    테스트 코드 추가

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/MainActivityTest.kt
@@ -1,15 +1,14 @@
 package com.leebeebeom.clothinghelper.ui
 
-import android.util.Log
 import androidx.activity.compose.setContent
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodesWithText
-import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.leebeebeom.clothinghelper.ui.ActivityDestinations.MAIN_ROUTE
 import com.leebeebeom.clothinghelper.ui.ActivityDestinations.SIGN_IN_ROUTE
+import com.leebeebeom.clothinghelper.ui.main.drawer.SETTING_ICON
 import org.junit.Rule
 import org.junit.Test
 
@@ -17,15 +16,32 @@ class MainActivityTest {
     @get:Rule
     val rule = createAndroidComposeRule<MainActivity>()
 
-    // 로그인 상태로 앱 실행 시 로그인 화면 안 보이는지
+    // 로그인 상태로 앱 실행 시 로그인 화면 안 보이는 지
     @Test
     fun doesSeeSignInScreenTest() {
         if (user == null) signIn()
-        rule.activity.setContent { MainActivityScreen() }
-        rule.waitUntil {
-            rule.onAllNodesWithText("이메일").fetchSemanticsNodes().isEmpty()
-        }
+        setContent()
         emailTextField.assertDoesNotExist()
+    }
+
+    // 로그인 시 메인 스크린 이동
+    @Test
+    fun signInTest() {
+        signOut()
+        setContent()
+        waitSignInScreen()
+        uiSignIn()
+        waitMainScreen()
+    }
+
+    // 로그아웃 시 로그인 스크린 이동
+    @Test
+    fun signOutTest() {
+        signIn()
+        setContent()
+        waitMainScreen()
+        uiSignOut()
+        waitMainScreen()
     }
 
     // 로그인, 로그아웃 시 백스택은 하나여야함.
@@ -57,7 +73,6 @@ class MainActivityTest {
             assert(false)
         } catch (e: IllegalArgumentException) {
             assert(true)
-            Log.d("TAG", "assertDoesNotExistSignInBackStack: 테스트 1 통과")
         }
     }
 
@@ -67,8 +82,18 @@ class MainActivityTest {
             assert(false)
         } catch (e: IllegalArgumentException) {
             assert(true)
-            Log.d("TAG", "assertDoesNotExistSignInBackStack: 테스트 2 통과")
         }
+    }
+
+    private fun uiSignIn() {
+        emailTextField.performTextInput("1@a.com")
+        passwordTextField.performTextInput("111111")
+        signInButton.performClick()
+    }
+
+    private fun uiSignOut() {
+        rule.onRoot().performTouchInput { swipeRight() }
+        rule.onNodeWithContentDescription(SETTING_ICON).performClick()
     }
 
     private fun signOut() = FirebaseAuth.getInstance().signOut()
@@ -78,15 +103,19 @@ class MainActivityTest {
 
     val user get() = FirebaseAuth.getInstance().currentUser
     val emailTextField get() = rule.onNodeWithText("이메일")
-    fun waitSignInScreen() {
+    val passwordTextField get() = rule.onNodeWithText("비밀번호")
+    val signInButton get() = rule.onNodeWithText("로그인")
+    private fun waitSignInScreen() {
         rule.waitUntil {
             rule.onAllNodesWithText("이메일").fetchSemanticsNodes().isNotEmpty()
         }
     }
 
-    fun waitMainScreen() {
+    private fun waitMainScreen() {
         rule.waitUntil {
             rule.onAllNodesWithText("이메일").fetchSemanticsNodes().isEmpty()
         }
     }
+
+    private fun setContent() = rule.activity.setContent { MainActivityScreen() }
 }

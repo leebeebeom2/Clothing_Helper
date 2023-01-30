b86204518b89a41c023e03a2c181a2af94d379c8
leebeebeom <boole92@naver.com>
2023-01-30 12:51:06 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/theme/Theme.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.leebeebeom.clothinghelper.theme
+package com.leebeebeom.clothinghelper.ui.theme
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.lightColors

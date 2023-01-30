fcdae7bf911eeb4ac5acddb4b92bbf02c621f0e9
leebeebeom <boole92@naver.com>
2023-01-30 15:46:05 +0900

    어노테이션 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/resetpassword/ResetPasswordScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/resetpassword/ResetPasswordScreen.kt
@@ -1,46 +1,33 @@
 package com.leebeebeom.clothinghelper.ui.signin.ui.resetpassword
 
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.ActivityViewModel
+import com.leebeebeom.clothinghelper.ui.activityViewModel
 import com.leebeebeom.clothinghelper.ui.components.HeightSpacer
 import com.leebeebeom.clothinghelper.ui.components.ImeActionRoute
 import com.leebeebeom.clothinghelper.ui.components.MaxWidthButton
-import com.leebeebeom.clothinghelper.ui.getActivityViewModel
 import com.leebeebeom.clothinghelper.ui.signin.composable.SignInBaseColumn
 import com.leebeebeom.clothinghelper.ui.signin.composable.textfield.EmailTextField
 
-/*
-이메일 필드가 비어있거나 에러 메세지 표시 중일 경우 확인 버튼 비활성화
-에러가 표시중인 경우 텍스트가 변경되면 에러 숨김
-
-확인 버튼 클릭 시 로딩화면 출력, 끝나면 로딩화면 숨김
-
-이메일 형식이 올바르지 않은 경우 "이메일 형식이 올바르지 않아요" 에러 표시
-이메일을 찾을 수 없는 경우 "이메일을 찾을 수 없어요" 에러 표시
-
-정상적으로 이메일이 발송된 경우 "이메일이 전송되었습니다" 토스트 출력
-로그인 화면으로 이동
-
- */
-
 @Composable
 fun ResetPasswordScreen(
+    popBackStack: () -> Unit,
     viewModel: ResetPasswordViewModel = hiltViewModel(),
     uiState: ResetPasswordUiState = viewModel.uiState,
-    activityViewModel: ActivityViewModel = getActivityViewModel()
+    activityViewModel: ActivityViewModel = activityViewModel()
 ) {
-    GoBack(
-        taskSuccess = { uiState.isTaskSuccess }, consumeTaskSuccess = viewModel::consumeTaskSuccess
+    PopBackStack(
+        taskSuccess = { uiState.isTaskSuccess },
+        popBackStack = popBackStack,
+        consumeTaskSuccess = viewModel::consumeTaskSuccess
     )
 
     SignInBaseColumn {
@@ -64,9 +51,13 @@ fun ResetPasswordScreen(
 }
 
 @Composable
-private fun GoBack(taskSuccess: () -> Boolean, consumeTaskSuccess: () -> Unit) {
+private fun PopBackStack(
+    taskSuccess: () -> Boolean,
+    popBackStack: () -> Unit,
+    consumeTaskSuccess: () -> Unit
+) {
     if (taskSuccess()) {
-        (LocalContext.current as ComponentActivity).onBackPressedDispatcher.onBackPressed()
+        popBackStack()
         consumeTaskSuccess()
     }
 }

11551f99070880c455653a8b2058d10f474cec61
leebeebeom <boole92@naver.com>
2023-01-30 15:37:21 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/composable/SignInBaseColumn.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/composable/SignInBaseColumn.kt
@@ -5,15 +5,24 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
+import com.leebeebeom.clothinghelper.ui.util.noRippleClickable
 
 @Composable
-fun SignInBaseColumn(content: @Composable ColumnScope.() -> Unit) {
+fun SignInBaseColumn(
+    focusManager: FocusManager = LocalFocusManager.current,
+    content: @Composable ColumnScope.() -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .padding(bottom = 40.dp), verticalArrangement = Arrangement.Center
+            .noRippleClickable(onClick = focusManager::clearFocus)
+            .padding(horizontal = 40.dp)
+            .padding(bottom = 40.dp),
+        verticalArrangement = Arrangement.Center
     ) {
         content()
     }

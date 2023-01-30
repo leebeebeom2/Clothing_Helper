bf22d53d1c9213fcf526deee6025b2f165328215
leebeebeom <boole92@naver.com>
2023-01-30 13:24:25 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/ActivityViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/ActivityViewModel.kt
@@ -6,15 +6,20 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
+import com.leebeebeom.clothinghelper.domain.usecase.user.GetSignInLoadingStateUseCase
 import com.leebeebeom.clothinghelper.domain.usecase.user.GetSignInStateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ActivityViewModel @Inject constructor(private val getSignInStateUseCase: GetSignInStateUseCase) :
-    ViewModel() {
+class ActivityViewModel @Inject constructor(
+    private val getSignInStateUseCase: GetSignInStateUseCase,
+    private val getSignInLoadingStateUseCase: GetSignInLoadingStateUseCase
+) : ViewModel() {
+
     private val _activityUiState = MutableActivityUiState()
     val activityUiState: ActivityUiState = _activityUiState
+
 
     fun showToast(toastText: Int) {
         _activityUiState.toastText = toastText
@@ -30,10 +35,12 @@ class ActivityViewModel @Inject constructor(private val getSignInStateUseCase: G
 @Stable
 interface ActivityUiState {
     val toastText: Int?
+    val isSignInLoading: Boolean
 }
 
 private class MutableActivityUiState : ActivityUiState {
     override var toastText: Int? by mutableStateOf(null)
+    override var isSignInLoading by mutableStateOf(false)
 }
 
 @Composable

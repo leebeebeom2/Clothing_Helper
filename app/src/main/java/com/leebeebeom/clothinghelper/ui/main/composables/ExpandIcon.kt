919349cd2fbba62e9aa170a111d6a659ade2ac2d
leebeebeom <boole92@naver.com>
2023-01-30 12:12:01 +0900

    최적화

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/main/composables/ExpandIcon.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/main/composables/ExpandIcon.kt
@@ -5,10 +5,10 @@ import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.graphicsLayer
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.composable.CustomIconButton
-import com.leebeebeom.clothinghelper.util.Anime.ExpandIcon.rotateSpec
+import com.leebeebeom.clothinghelper.ui.components.CustomIconButton
+import com.leebeebeom.clothinghelper.ui.util.Anime.ExpandIcon.rotateSpec
 
 @Composable
 fun ExpandIcon(
@@ -22,7 +22,7 @@ fun ExpandIcon(
     )
 
     CustomIconButton(
-        modifier = modifier.rotate(rotate),
+        modifier = modifier.graphicsLayer { rotationZ = rotate },
         onClick = onClick,
         drawable = R.drawable.ic_expand_more,
         tint = LocalContentColor.current.copy(alpha = 0.6f),

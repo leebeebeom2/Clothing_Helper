609d6813dac579f402643a3aa02bf9a3ce31b455
leebeebeom <boole92@naver.com>
2023-01-30 18:35:54 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/main/dialogs/composables/DialogTextButtons.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/main/dialogs/composables/DialogTextButtons.kt
@@ -1,18 +1,15 @@
 package com.leebeebeom.clothinghelper.ui.main.dialogs.composables
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.composable.SingleLineText
+import com.leebeebeom.clothinghelper.ui.components.SingleLineText
 
 @Composable
 fun DialogTextButtons(
@@ -21,40 +18,41 @@ fun DialogTextButtons(
     onDismiss: () -> Unit
 ) {
     Row(modifier = Modifier.fillMaxWidth()) {
+        val weightModifier = Modifier.weight(1f)
+
         DialogTextButton(
+            modifier = weightModifier,
             text = R.string.cancel,
             textColor = MaterialTheme.colors.error,
             onClick = onDismiss
         )
-        DialogTextButton(text = R.string.check,
-            textColor = Color.Unspecified,
-            enabled = positiveButtonEnabled,
-            onClick = {
-                onPositiveButtonClick()
-                onDismiss()
-            })
+        DialogTextButton(
+            modifier = weightModifier,
+            text = R.string.check,
+            enabled = positiveButtonEnabled
+        ) {
+            onPositiveButtonClick()
+            onDismiss()
+        }
     }
 }
 
 @Composable
-fun RowScope.DialogTextButton(
+fun DialogTextButton(
+    modifier: Modifier = Modifier,
     @StringRes text: Int,
     textColor: Color = Color.Unspecified,
     enabled: () -> Boolean = { true },
     onClick: () -> Unit
 ) {
-    Box(modifier = Modifier.weight(1f)) {
-        TextButton(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .fillMaxWidth(),
-            onClick = onClick,
-            enabled = enabled()
-        ) {
-            SingleLineText(
-                text = text,
-                style = MaterialTheme.typography.body1.copy(color = textColor),
-            )
-        }
+    TextButton(
+        modifier = modifier.fillMaxWidth(),
+        onClick = onClick,
+        enabled = enabled()
+    ) {
+        SingleLineText(
+            text = text,
+            style = MaterialTheme.typography.body1.copy(color = textColor),
+        )
     }
 }

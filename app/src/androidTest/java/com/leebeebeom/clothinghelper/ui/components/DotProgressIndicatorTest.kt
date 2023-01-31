ff758bf49e2d349fd00f2bc4adc029c362e7584a
leebeebeom <boole92@naver.com>
2023-02-01 02:49:57 +0900

    테스트 코드 작성

---
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/components/DotProgressIndicatorTest.kt
@@ -0,0 +1,34 @@
+package com.leebeebeom.clothinghelper.ui.components
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import com.leebeebeom.clothinghelper.ui.theme.ClothingHelperTheme
+import org.junit.Rule
+import org.junit.Test
+
+class DotProgressIndicatorTest {
+    @get:Rule
+    val rule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun centerDotProgressIndicatorTest() {
+        var isLoading by mutableStateOf(false)
+        rule.setContent {
+            ClothingHelperTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    CenterDotProgressIndicator { isLoading }
+                }
+            }
+        }
+        isLoading = true
+        rule.onNodeWithTag(CENTER_DOT_PROGRESS_INDICATOR).assertExists()
+        isLoading = false
+        rule.onNodeWithTag(CENTER_DOT_PROGRESS_INDICATOR).assertDoesNotExist()
+    }
+}

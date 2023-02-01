c538754f69fb0e0a1c4356edbf0b4b136fa08496
leebeebeom <boole92@naver.com>
2023-02-02 06:17:33 +0900

    상태 복원 코드 수정, 코드 간소화

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/components/MaxWidthButtonTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/components/MaxWidthButtonTest.kt
@@ -3,9 +3,10 @@ package com.leebeebeom.clothinghelper.ui.components
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.signin.composable.textfield.EmailTextField
+import com.leebeebeom.clothinghelper.ui.signin.components.textfield.EmailTextField
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -13,22 +14,22 @@ import org.junit.Test
 class MaxWidthButtonTest {
     @get:Rule
     val rule = createComposeRule()
+    lateinit var restoreTester: StateRestorationTester
     private val onClickTestText = "onClick"
+    private var onClickTest = ""
     private lateinit var enable: MutableState<Boolean>
-    private lateinit var onClickTest: String
 
     @Before
     fun init() {
         enable = mutableStateOf(true)
         onClickTest = ""
 
-        rule.setContent {
+        restoreTester = StateRestorationTester(rule)
+        restoreTester.setContent {
             EmailTextField(error = { null }, onInputChange = {})
-            MaxWidthButton(
-                text = R.string.check,
+            MaxWidthButton(text = R.string.check,
                 enabled = { enable.value },
-                onClick = { onClickTest = onClickTestText }
-            )
+                onClick = { onClickTest = onClickTestText })
         }
     }
 
@@ -41,14 +42,30 @@ class MaxWidthButtonTest {
 
     @Test
     fun enabledTest() {
+        button.assertIsEnabled()
+        restoreTester.emulateSavedInstanceStateRestore()
+        button.assertIsEnabled()
+
         enable.value = false
+
         button.assertIsNotEnabled()
+        restoreTester.emulateSavedInstanceStateRestore()
+        button.assertIsNotEnabled()
+
         enable.value = true
+
+        button.assertIsEnabled()
+        restoreTester.emulateSavedInstanceStateRestore()
         button.assertIsEnabled()
     }
 
     @Test
     fun onClickTest() {
+        button.performClick()
+        assert(onClickTest == onClickTestText)
+        onClickTest = ""
+
+        restoreTester.emulateSavedInstanceStateRestore()
         button.performClick()
         assert(onClickTest == onClickTestText)
     }

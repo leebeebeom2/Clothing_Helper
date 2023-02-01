c543a0966844862f612f2ac80403626b6b21f84e
leebeebeom <boole92@naver.com>
2023-02-02 06:05:05 +0900

    상태 복원 코드 수정, 코드 간소화

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/SignInNavHostTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/SignInNavHostTest.kt
@@ -1,6 +1,6 @@
 package com.leebeebeom.clothinghelper.ui.signin.ui
 
-import androidx.activity.compose.setContent
+import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
@@ -11,7 +11,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.uiautomator.UiDevice
 import com.leebeebeom.clothinghelper.ui.ActivityDestinations.SIGN_IN_ROUTE
-import com.leebeebeom.clothinghelper.ui.MainActivity
+import com.leebeebeom.clothinghelper.ui.HiltTestActivity
 import com.leebeebeom.clothinghelper.ui.components.CENTER_DOT_PROGRESS_INDICATOR_TAG
 import com.leebeebeom.clothinghelper.ui.components.isKeyboardShown
 import org.junit.Before
@@ -20,21 +20,19 @@ import org.junit.Test
 
 class SignInNavHostTest {
     @get:Rule
-    val rule = createAndroidComposeRule<MainActivity>()
+    val rule = createAndroidComposeRule<HiltTestActivity>()
+    private val device = UiDevice.getInstance(getInstrumentation())
+    lateinit var restoreTester: StateRestorationTester
 
     private lateinit var uiState: MutableSignInNavUiState
 
     @Before
     fun init() {
         uiState = MutableSignInNavUiState()
-        setContent()
-    }
-
-    private fun setContent() {
-        rule.activity.setContent {
-            val navController = rememberNavController()
+        restoreTester = StateRestorationTester(rule)
+        restoreTester.setContent {
             NavHost(
-                navController = navController,
+                navController = rememberNavController(),
                 startDestination = SIGN_IN_ROUTE,
             ) {
                 composable(SIGN_IN_ROUTE) { SignInNavHost(uiState = uiState) }
@@ -43,37 +41,44 @@ class SignInNavHostTest {
     }
 
     // centerDotProgressIndicator 테스트와 동시 진행
-
     @Test
     fun signInBlockBacKPressTest() {
-        rule.onNodeWithText("이메일").assertExists()
+        val emailTextField = rule.onNodeWithText("이메일")
+
+        emailTextField.assertExists()
         blockBackPressTest()
-        rule.onNodeWithText("이메일").assertExists()
+        emailTextField.assertExists()
     }
 
     @Test
     fun signUpBlockBacKPressTest() {
         rule.waitUntil { isKeyboardShown() }
-        UiDevice.getInstance(getInstrumentation()).pressBack() // 키보드 내리기
-        rule.onNodeWithText("이메일로 가입하기").performClick()
-        rule.onNodeWithText("닉네임").assertExists()
+        device.pressBack() // 키보드 내리기
+        rule.onNodeWithText("이메일로 가입하기").performClick() // 이동
+
+        val nickNameTextField = rule.onNodeWithText("닉네임")
+        nickNameTextField.assertExists()
         blockBackPressTest()
+        nickNameTextField.assertExists()
     }
 
     @Test
     fun resetPasswordBlockBacKPressTest() {
         rule.onNodeWithText("비밀번호를 잊으셨나요?").performClick()
-        rule.onNodeWithText("보내기").assertExists()
+
+        val sendButton = rule.onNodeWithText("보내기")
+        sendButton.assertExists()
         blockBackPressTest()
+        sendButton.assertExists()
     }
 
     private fun blockBackPressTest() {
         centerDotProgressIndicator.assertDoesNotExist()
         uiState.isSignInLoading = true
         centerDotProgressIndicator.assertExists()
-        UiDevice.getInstance(getInstrumentation()).pressBack()
-        UiDevice.getInstance(getInstrumentation()).pressBack()
-        UiDevice.getInstance(getInstrumentation()).pressBack()
+        device.pressBack()
+        device.pressBack()
+        device.pressBack()
         centerDotProgressIndicator.assertExists()
     }
 

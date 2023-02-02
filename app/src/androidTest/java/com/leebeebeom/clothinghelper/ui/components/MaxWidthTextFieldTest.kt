dc0d8f42ace7ad07b8a5d2e0226403611919f727
leebeebeom <boole92@naver.com>
2023-02-03 04:27:23 +0900

    공통 코드 추출

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/components/MaxWidthTextFieldTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/components/MaxWidthTextFieldTest.kt
@@ -1,7 +1,5 @@
 package com.leebeebeom.clothinghelper.ui.components
 
-import android.content.Context
-import android.view.inputmethod.InputMethodManager
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,46 +9,44 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.StateRestorationTester
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.test.platform.app.InstrumentationRegistry
-import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.signin.components.textfield.INVISIBLE_ICON_TAG
-import com.leebeebeom.clothinghelper.ui.signin.components.textfield.VISIBLE_ICON_TAG
+import com.leebeebeom.clothinghelper.*
+import com.leebeebeom.clothinghelper.R.string.*
 import com.leebeebeom.clothinghelper.ui.signin.components.textfield.VisibleIcon
 import com.leebeebeom.clothinghelper.ui.theme.ClothingHelperTheme
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+private const val MOVE_BUTTON_TEXT = "이동"
+private const val MOVE_BACK_BUTTON_TEXT = "뒤로 이동"
+private const val SCREEN_1_ROUTE = "screen1"
+private const val SCREEN_2_ROUTE = "screen2"
+
 class MaxWidthTextFieldTest {
-    private val moveText = "이동"
-    private val moveBackText = "뒤로 이동"
-    private val testInput = "테스트 인풋"
-    private var input = ""
-    private val screen1Route = "screen1"
-    private val screen2Route = "screen2"
+    private lateinit var emailInput: String
+    private lateinit var passwordInput: String
     private lateinit var state: MutableMaxWidthTextFieldState
     private lateinit var passwordState: MutableMaxWidthTextFieldState
+    private val moveButton get() = rule.onNodeWithText(MOVE_BUTTON_TEXT)
+    private val moveBackButton get() = rule.onNodeWithText(MOVE_BACK_BUTTON_TEXT)
 
     @get:Rule
-    val rule = createComposeRule()
-    private val restoreTester = StateRestorationTester(rule)
+    val rule = composeRule
+    private val restoreTester = restoreTester(rule)
 
     @Before
     fun init() {
-        input = ""
+        emailInput = ""
+        passwordInput = ""
 
         restoreTester.setContent {
             ClothingHelperTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = screen1Route) {
-                    composable(screen1Route) {
+                NavHost(navController = navController, startDestination = SCREEN_1_ROUTE) {
+                    composable(SCREEN_1_ROUTE) {
                         Column(
                             modifier = Modifier.fillMaxSize(),
                             verticalArrangement = Arrangement.Center
@@ -58,15 +54,15 @@ class MaxWidthTextFieldTest {
                             emailTextField()()
                             passwordTextField()()
 
-                            Button(onClick = { navController.navigate(screen2Route) }) {
-                                Text(text = moveText)
+                            Button(onClick = { navController.navigate(SCREEN_2_ROUTE) }) {
+                                Text(text = MOVE_BUTTON_TEXT)
                             }
                         }
                     }
-                    composable(screen2Route) {
+                    composable(SCREEN_2_ROUTE) {
                         Box(modifier = Modifier.fillMaxSize()) {
                             Button(onClick = { navController.popBackStack() }) {
-                                Text(text = moveBackText)
+                                Text(text = MOVE_BACK_BUTTON_TEXT)
                             }
                         }
                     }
@@ -75,172 +71,70 @@ class MaxWidthTextFieldTest {
         }
     }
 
+    // 키보드는 최초 실행 시에만 보여야 함
     @Test
-    fun showKeyboardTest() {
-        // 키보드는 최초 실행 시에만 보여야 함
-        assert(isKeyboardShown())
-        moveButton.performClick()
-        moveBackButton.performClick()
-        assert(!isKeyboardShown())
-    }
-
-    @Test
-    fun textChangeTest() {
-        textField.performTextInput(testInput)
-
-        assertInputChange()
-        restoreTester.emulateSavedInstanceStateRestore()
-        assertInputChange()
-
-        textField.performClick() // 포커스
-        // 커서 위치 변경
-        state.onValueChange(newTextField = state.textFieldValue.copy(selection = TextRange(1)))
-        textField.performTextInput("스")
-
-        assertCursorPosition()
-        restoreTester.emulateSavedInstanceStateRestore()
-        assertCursorPosition()
-
-        textField.performTextClearance()
-
-        assertTextClear()
-        restoreTester.emulateSavedInstanceStateRestore()
-        assertTextClear()
-    }
-
-    private fun assertTextClear() {
-        rule.onNodeWithText(testInput).assertDoesNotExist()
-        assert(input.isBlank())
-    }
-
-    private fun assertCursorPosition() {
-        rule.onNodeWithText("테스스트 인풋").assertExists()
-        assert(input == "테스스트 인풋")
-    }
-
-    private fun assertInputChange() {
-        rule.onNodeWithText(testInput).assertExists()
-        assert(input == testInput)
-    }
+    fun showKeyboardOnceTest() =
+        showKeyboardOnceTest(
+            move = { moveButton.performClick() },
+            moveBack = { moveBackButton.performClick() }
+        )
 
     @Test
-    fun passwordTextFieldTest() {
-        passwordTextField.performTextInput(testInput)
-
-        assertInVisible()
-        restoreTester.emulateSavedInstanceStateRestore()
-        assertInVisible()
-
-        visibleIcon.performClick()
-
-        assertVisible()
-        restoreTester.emulateSavedInstanceStateRestore()
-        assertVisible()
-
-        invisibleIcon.performClick()
-
-        assertInVisible()
-        restoreTester.emulateSavedInstanceStateRestore()
-        assertInVisible()
-    }
-
-    private fun assertVisible() {
-        visibleIcon.assertDoesNotExist()
-        invisibleIcon.assertExists()
-        rule.onNodeWithText(testInput).assertExists()
-    }
-
-    private fun assertInVisible() {
-        visibleIcon.assertExists()
-        invisibleIcon.assertDoesNotExist()
-        rule.onNodeWithText(testInput).assertDoesNotExist()
-    }
+    fun textFieldInputChangeTest() =
+        textFieldInputChangeTest(
+            textField = rule.emailTextField,
+            rule = rule,
+            input = { emailInput },
+            restoreTester = restoreTester,
+            state = { state })
 
     @Test
-    fun cancelIconTest() {
-        textField.performTextInput(testInput)
-        cancelIcon.assertExists()
-
-        passwordTextField.performClick()
-        cancelIcon.assertDoesNotExist()
-
-        textField.performClick()
-        cancelIcon.assertExists()
-        cancelIcon.performClick()
-
-        rule.onNodeWithText(testInput).assertDoesNotExist()
-    }
+    fun passwordTextFieldVisibleTest() =
+        passwordTextFieldVisibleTest(
+            rule = rule,
+            input = { passwordInput },
+            restoreTester = restoreTester
+        )
 
     @Test
-    fun errorTest() {
-        state.error = R.string.error_test
-        rule.waitForIdle()
+    fun cancelIconTest() =
+        cancelIconTest(
+            cancelIconTestField = rule.emailTextField,
+            noCancelIconTestField = rule.passwordTextField,
+            rule = rule,
+            cancelIconTextFieldInput = { emailInput },
+            restorationTester = restoreTester
+        )
 
-        errorText.assertExists()
-        restoreTester.emulateSavedInstanceStateRestore()
-        errorText.assertExists()
-
-        textField.performTextInput(testInput)
-
-        errorText.assertDoesNotExist()
-        restoreTester.emulateSavedInstanceStateRestore()
-        errorText.assertDoesNotExist()
-    }
+    @Test
+    fun errorTest() = errorTest(
+        rule,
+        errorTextField = rule.emailTextField,
+        setError = { state.error = error_test },
+        restoreTester = restoreTester
+    )
 
     @Test
     fun cursorTest() {
-        textField.performTextInput(testInput)
-        changeFocus()
-        textField.performTextInput("1")
-
-        rule.onNodeWithText(testInput + 1)
-
-        changeFocus()
-        changeFocus()
-        textField.performTextInput("1")
-
-        rule.onNodeWithText(testInput + 1 + 1)
-    }
-
-    private fun changeFocus() {
-        passwordTextField.performClick()
-        passwordTextField.assertIsFocused()
-        textField.assertIsNotFocused()
-
-        textField.performClick()
-        textField.assertIsFocused()
-        passwordTextField.assertIsNotFocused()
+        rule.visibleIcon.performClick()
+        cursorTest(
+            restoreTester = restoreTester,
+            textField1 = rule.emailTextField,
+            textField2 = rule.passwordTextField
+        )
+        cursorTest(
+            restoreTester = restoreTester,
+            textField1 = rule.passwordTextField,
+            textField2 = rule.emailTextField
+        )
     }
 
     @Test
-    fun imeTest() {
-        textField.assertIsFocused()
-        textField.performImeAction()
+    fun imeTest() = imeTest(rule.emailTextField, doneTextField = rule.passwordTextField)
 
-        textField.assertIsNotFocused()
-        passwordTextField.assertIsFocused()
-
-        passwordTextField.performImeAction()
-        textField.assertIsNotFocused()
-        passwordTextField.assertIsNotFocused()
-        assert(!isKeyboardShown())
-    }
-
-    private val moveButton get() = rule.onNodeWithText(moveText)
-    private val moveBackButton get() = rule.onNodeWithText(moveBackText)
-    private val textField get() = rule.onNodeWithText("이메일")
-    private val passwordTextField get() = rule.onNodeWithText("비밀번호")
-    private val visibleIcon get() = rule.onNodeWithContentDescription(VISIBLE_ICON_TAG)
-    private val invisibleIcon get() = rule.onNodeWithContentDescription(INVISIBLE_ICON_TAG)
-    private val cancelIcon get() = rule.onNodeWithContentDescription(CANCEL_ICON_TAG)
-    private val errorText get() = rule.onNodeWithText("에러")
-
-    /**
-     * showKeyboard
-     */
     private fun emailTextField(): @Composable () -> Unit = {
         state = rememberMaxWidthTextFieldState(
-            label = R.string.email,
+            label = email,
             showKeyboard = true,
             imeActionRoute = ImeActionRoute.NEXT
         )
@@ -249,13 +143,13 @@ class MaxWidthTextFieldTest {
             state = state,
             onValueChange = state::onValueChange,
             onFocusChanged = state::onFocusChanged,
-            onInputChange = { input = it }
+            onInputChange = { emailInput = it }
         )
     }
 
     private fun passwordTextField(): @Composable () -> Unit = {
         passwordState = rememberMaxWidthTextFieldState(
-            label = R.string.password,
+            label = password,
             initialVisibility = false,
             keyboardRoute = KeyboardRoute.PASSWORD,
             imeActionRoute = ImeActionRoute.DONE,
@@ -266,7 +160,7 @@ class MaxWidthTextFieldTest {
             state = passwordState,
             onValueChange = passwordState::onValueChange,
             onFocusChanged = passwordState::onFocusChanged,
-            onInputChange = {},
+            onInputChange = { passwordInput = it },
             trailingIcon = {
                 VisibleIcon(
                     isVisible = { passwordState.isVisible },
@@ -275,11 +169,4 @@ class MaxWidthTextFieldTest {
             }
         )
     }
-}
-
-fun isKeyboardShown(): Boolean {
-    val inputMethodManager =
-        InstrumentationRegistry.getInstrumentation().targetContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    KeyboardType
-    return inputMethodManager.isAcceptingText
 }

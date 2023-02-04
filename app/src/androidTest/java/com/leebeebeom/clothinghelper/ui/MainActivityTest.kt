7815fc35b543105829a2fb2c65a65b8589b64fcb
leebeebeom <boole92@naver.com>
2023-02-05 00:39:41 +0900

    CustomTestRule.kt 생성

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/MainActivityTest.kt
@@ -1,12 +1,13 @@
 package com.leebeebeom.clothinghelper.ui
 
-import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeRight
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.leebeebeom.clothinghelper.*
 import com.leebeebeom.clothinghelper.ui.main.MAIN_NAV_TAG
+import com.leebeebeom.clothinghelper.ui.signin.ui.SIGN_IN_NAV_TAG
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -14,79 +15,64 @@ import org.junit.Test
 class MainActivitySignInStartTest {
     @get:Rule
     val rule = activityRule
-    private val restoreTester = restoreTester(rule)
+    private val customTestRule = CustomTestRule(rule = rule)
 
     @Before
-    fun signIn() {
+    fun init() {
         FirebaseAuth.getInstance().signInWithEmailAndPassword(EMAIL, PASSWORD)
-        restoreTester.setContent { MainActivityScreen() }
+        customTestRule.setContent { MainActivityScreen() }
     }
 
+    // 로그인 상태로 앱 실행 시 로그인 화면 안 보이는 지
+    // TODO 구글은 따로 테스트 필요
     @Test
-    fun doesSeeSignInScreenTest() {
-        // 로그인 상태로 앱 실행 시 로그인 화면 안 보이는 지
-        // TODO 구글은 따로 테스트 필요
-        rule.waitMainScreen()
-        rule.emailTextField.assertDoesNotExist()
-        restoreTester.emulateSavedInstanceStateRestore()
-        rule.emailTextField.assertDoesNotExist()
-    }
+    fun doesSeeSignInScreenTest() = customTestRule.waitMainNav()
 
     @Test
     fun signOutTest() {
         // 로그아웃 시 로그인 스크린 이동
-        rule.emailTextField.assertDoesNotExist()
-        rule.uiSignOut()
-        rule.emailTextField.assertExists()
-        restoreTester.emulateSavedInstanceStateRestore()
-        rule.emailTextField.assertExists()
+        customTestRule.uiSignOut()
+        customTestRule.waitSignInNav()
     }
 }
 
 class MainActivitySignOutStartTest {
     @get:Rule
     val rule = activityRule
-    private val restoreTester = restoreTester(rule)
+    private val customTestRule = CustomTestRule(rule = rule)
     private lateinit var navController: NavHostController
 
     @Before
-    fun signOut() {
-        restoreTester.setContent {
+    fun init() {
+        FirebaseAuth.getInstance().signOut()
+        customTestRule.setContent {
             navController = rememberNavController()
             MainActivityScreen(navController = navController)
         }
-        FirebaseAuth.getInstance().signOut()
     }
 
     @Test
-    fun signInTest() {
-        // 로그인 시 메인 스크린 이동
-        rule.emailTextField.assertExists()
-        rule.uiSignIn()
-
-        assertSignIn(rule = rule, restoreTester = restoreTester)
-    }
+    fun signInTest() = customTestRule.uiSignIn()
 
     @Test
     fun backStackTest() {
         // 로그인, 로그아웃 시 백스택은 하나여야함.
-        rule.waitSignInScreen()
-        assertDoesNotExistMainBackStack()
+        notExistMainBackStack()
 
-        rule.uiSignIn()
-        assertDoesNotExistSignInBackStack()
+        customTestRule.uiSignIn()
+        notExistSignInBackStack()
 
-        rule.uiSignOut()
-        assertDoesNotExistMainBackStack()
+        customTestRule.uiSignOut()
+        notExistMainBackStack()
 
-        rule.uiSignIn()
-        rule.uiSignOut()
-        rule.uiSignIn()
-        rule.uiSignOut()
-        assertDoesNotExistMainBackStack()
+        customTestRule.uiSignIn()
+        customTestRule.uiSignOut()
+        customTestRule.uiSignIn()
+        customTestRule.uiSignOut()
+        notExistMainBackStack()
     }
 
-    private fun assertDoesNotExistSignInBackStack() {
+    private fun notExistSignInBackStack() {
         try {
             navController.getBackStackEntry(ActivityDestinations.SIGN_IN_ROUTE)
             assert(false)
@@ -95,7 +81,7 @@ class MainActivitySignOutStartTest {
         }
     }
 
-    private fun assertDoesNotExistMainBackStack() {
+    private fun notExistMainBackStack() {
         try {
             navController.getBackStackEntry(ActivityDestinations.MAIN_ROUTE)
             assert(false)
@@ -105,21 +91,20 @@ class MainActivitySignOutStartTest {
     }
 }
 
-private fun ComposeContentTestRule.uiSignIn() {
-    emailTextField.performTextInput(EMAIL)
-    passwordTextField.performTextInput(PASSWORD)
-    signInButton.performClick()
-    waitMainScreen()
+private fun CustomTestRule.uiSignIn() {
+    emailTextField.input(EMAIL)
+    passwordTextField.invisibleInput(PASSWORD)
+    signInButton.click()
+    centerDotProgressIndicator.exist(false)
+    waitMainNav()
 }
 
-private fun ComposeContentTestRule.uiSignOut() {
-    onRoot().performTouchInput { swipeRight() }
-    settingIcon.performClick()
-    signOutButton.performClick()
-    waitSignInScreen()
+private fun CustomTestRule.uiSignOut() {
+    root.performTouchInput { swipeRight() }
+    settingIcon.click()
+    signOutButton.click()
+    waitSignInNav()
 }
 
-fun ComposeContentTestRule.waitSignInScreen() = waitTextNodeIsNotEmpty(text = EMAIL_LABEL)
-
-private fun ComposeContentTestRule.waitMainScreen() =
-    waitTagNodeIsNotEmpty(tag = MAIN_NAV_TAG)
+private fun CustomTestRule.waitSignInNav() = waitTagExist(tag = SIGN_IN_NAV_TAG)
+private fun CustomTestRule.waitMainNav() = waitTagExist(tag = MAIN_NAV_TAG)

3b3cd6544cdb7b90a48dc4b9a4ba2900868f72ff
leebeebeom <boole92@naver.com>
2023-02-06 02:58:30 +0900

    CustomTestRule.kt 생성

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/components/textfield/PasswordTextFieldTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/components/textfield/PasswordTextFieldTest.kt
@@ -1,14 +1,10 @@
 package com.leebeebeom.clothinghelper.ui.signin.components.textfield
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.ui.test.junit4.ComposeContentTestRule
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.leebeebeom.clothinghelper.*
-import com.leebeebeom.clothinghelper.ui.components.ImeActionRoute
-import com.leebeebeom.clothinghelper.ui.components.MaxWidthTextFieldWithError
-import com.leebeebeom.clothinghelper.ui.components.rememberMaxWidthTextFieldState
+import com.leebeebeom.clothinghelper.ui.components.*
 import com.leebeebeom.clothinghelper.ui.signin.state.MutableEmailAndPasswordUiState
 import com.leebeebeom.clothinghelper.ui.signin.ui.signin.SignInViewModel
 import com.leebeebeom.clothinghelper.ui.theme.ClothingHelperTheme
@@ -19,14 +15,13 @@ import org.junit.Test
 class PasswordTextFieldTest {
     @get:Rule
     val rule = activityRule
-    private val restoreTester = restoreTester(rule)
+    private val customTestRule = CustomTestRule(rule = rule)
     private lateinit var viewModel: SignInViewModel
     private lateinit var uiState: MutableEmailAndPasswordUiState
-    private val ComposeContentTestRule.dummyTextField get() = onNodeWithText("확인")
 
     @Before
     fun init() {
-        restoreTester.setContent {
+        customTestRule.setContent {
             ClothingHelperTheme {
                 viewModel = hiltViewModel()
                 uiState = viewModel.uiState as MutableEmailAndPasswordUiState
@@ -58,60 +53,74 @@ class PasswordTextFieldTest {
     fun showKeyboardTest() = isKeyboardNotShown()
 
     @Test
-    fun inputChangeTest() {
-        rule.visibleIcon.performClick()
-
-        inputChangeTest(
-            textField = rule.passwordTextField,
-            rule = rule,
-            input = { uiState.password },
-            restoreTester = restoreTester
-        )
-    }
+    fun inputChangeTest() =
+        invisibleTest {
+            inputChangeTest(
+                rule = customTestRule,
+                textField = { customTestRule.passwordTextField },
+                input = { uiState.password },
+                invisible = it
+            )
+        }
 
     @Test
     fun textFieldVisibleTest() = textFieldVisibleTest(
-        rule = rule,
-        input = { uiState.password },
-        restoreTester = restoreTester
+        rule = customTestRule,
+        textField = { customTestRule.passwordTextField }
     )
 
     @Test
-    fun errorTest() = errorTest(
-        rule = rule,
-        errorTextField = rule.passwordTextField,
-        setError = { uiState.passwordError = R.string.error_test },
-        restoreTester = restoreTester,
-        blockBlank = true
-    )
+    fun errorTest() =
+        invisibleTest {
+            errorTest(
+                customTestRule,
+                { customTestRule.passwordTextField },
+                rule.activity.getString(R.string.error_test),
+                { uiState.passwordError = R.string.error_test },
+                blockBlank = true,
+                invisible = it
+            )
+        }
 
     @Test
     fun cursorTest() {
-        rule.visibleIcon.performClick()
+        invisibleTest {
+            cursorTest(
+                rule = customTestRule,
+                textField1 = { customTestRule.passwordTextField },
+                textField2 = { customTestRule.dummyTextField },
+                invisible = it
+            )
+        }
 
-        cursorTest(
-            restoreTester,
-            textField1 = rule.passwordTextField,
-            textField2 = rule.dummyTextField
-        )
-        cursorTest(
-            restoreTester,
-            textField1 = rule.dummyTextField,
-            textField2 = rule.passwordTextField
-        )
+        customTestRule.invisibleIcon.click()
+
+        invisibleTest {
+            cursorTest(
+                rule = customTestRule,
+                textField1 = { customTestRule.dummyTextField },
+                textField2 = { customTestRule.passwordTextField }
+            )
+        }
     }
 
     @Test
-    fun imeTest() = imeTest(rule.dummyTextField, doneTextField = rule.passwordTextField)
+    fun imeTest() = imeTest({ customTestRule.dummyTextField },
+        doneTextField = { customTestRule.passwordTextField })
 
     @Test
     fun blockBlankTest() {
         rule.visibleIcon.performClick()
 
         blockBlankTest(
-            rule = rule,
-            textField = rule.passwordTextField,
-            restoreTester = restoreTester
+            rule = customTestRule,
+            textField = { customTestRule.passwordTextField }
         )
+    }
+
+    private fun invisibleTest(test: (Boolean) -> Unit) {
+        test(true)
+        customTestRule.visibleIcon.click()
+        test(false)
     }
 }

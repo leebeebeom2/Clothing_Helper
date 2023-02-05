b3cd5f635f1aa7d87f7105435355a9e777f63521
leebeebeom <boole92@naver.com>
2023-02-06 04:34:11 +0900

    CustomTestRule.kt 생성

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/components/GoogleSignInButtonTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/components/GoogleSignInButtonTest.kt
@@ -1,12 +1,9 @@
 package com.leebeebeom.clothinghelper.ui.signin.components
 
-import androidx.compose.ui.test.assertIsNotEnabled
-import androidx.compose.ui.test.performClick
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.leebeebeom.clothinghelper.CustomTestRule
 import com.leebeebeom.clothinghelper.activityRule
-import com.leebeebeom.clothinghelper.googleSignInButton
-import com.leebeebeom.clothinghelper.restoreTester
 import com.leebeebeom.clothinghelper.ui.signin.ui.SignInNavViewModel
 import org.junit.Before
 import org.junit.Rule
@@ -15,18 +12,18 @@ import org.junit.Test
 class GoogleSignInButtonTest {
     @get:Rule
     val rule = activityRule
-    val restoreTester = restoreTester(rule)
+    private val customTestRule = CustomTestRule(rule)
 
     @Before
     fun init() {
         FirebaseAuth.getInstance().signOut()
 
-        restoreTester.setContent {
+        customTestRule.setContent {
             val viewModel: SignInNavViewModel = hiltViewModel()
 
             GoogleSignInButton(
                 enabled = { viewModel.uiState.googleButtonEnabled },
-                onActivityResult = { viewModel.signInWithGoogleEmail(it) {} },
+                onActivityResult = { },
                 disable = { viewModel.setGoogleButtonEnable(false) }
             )
         }
@@ -34,9 +31,8 @@ class GoogleSignInButtonTest {
 
     @Test
     fun enabledTest() {
-        rule.googleSignInButton.performClick()
-        rule.waitForIdle()
-        rule.googleSignInButton.assertIsNotEnabled()
+        customTestRule.googleSignInButton.click()
+        customTestRule.googleSignInButton.notEnabled(false)
     }
 
     // 구글 로그인 테스트 불가

62733bfc43c44038d4211e96f9bd963a3baf0336
leebeebeom <boole92@naver.com>
2023-02-09 04:43:09 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/LoadingStateProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.leebeebeom.clothinghelper.domain.repository
-
-import kotlinx.coroutines.flow.StateFlow
-
-interface LoadingStateProvider {
-    val isLoading: StateFlow<Boolean>
-}
---
+++ app/src/main/java/com/leebeebeom/clothinghelper/util/LoadingStateProvider.kt
@@ -0,0 +1,21 @@
+package com.leebeebeom.clothinghelper.util
+
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.withContext
+
+interface LoadingStateProvider {
+    val isLoading: StateFlow<Boolean>
+}
+
+class LoadingStateProviderImpl(initialLoading: Boolean) : LoadingStateProvider {
+    private val _isLoading = MutableStateFlow(initialLoading)
+    override val isLoading = _isLoading.asStateFlow()
+
+    fun loadingOn() = _isLoading.update { true }
+
+    suspend fun loadingOff() = withContext(NonCancellable) { _isLoading.update { false } }
+}

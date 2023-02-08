f2743ae8fa1bda2c437c8e2925dae624fa213a80
leebeebeom <boole92@naver.com>
2023-02-09 04:32:01 +0900

    리팩토링

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/ResetPasswordUseCase.kt
@@ -0,0 +1,11 @@
+package com.leebeebeom.clothinghelper.domain.usecase.user
+
+import com.leebeebeom.clothinghelper.domain.repository.UserRepository
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class ResetPasswordUseCase @Inject constructor(private val userRepository: UserRepository) {
+    suspend fun sendResetPasswordEmail(email: String) =
+        userRepository.resetPasswordEmail(email = email)
+}

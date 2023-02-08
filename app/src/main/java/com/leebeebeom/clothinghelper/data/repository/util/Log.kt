8880f79ba9916597685b4dde533c74599502cbcb
leebeebeom <boole92@naver.com>
2023-02-09 04:37:20 +0900

    리팩토링

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/Log.kt
@@ -0,0 +1,7 @@
+package com.leebeebeom.clothinghelper.data.repository.util
+
+import android.util.Log
+
+const val TAG = "TAG"
+
+fun logE(site: String, e: Throwable) = Log.e(TAG, site, e)
--- clothinghelperdata/src/main/java/com/leebeebeom/clothinghelperdata/repository/util/Log.kt
+++
@@ -1,7 +0,0 @@
-package com.leebeebeom.clothinghelperdata.repository.util
-
-import android.util.Log
-
-const val TAG = "TAG"
-
-fun logE(site: String, e: Throwable) = Log.e(TAG, "site: $site", e)

b0506b85fa560f45f721ce07d98c08d6106e00d2
leebeebeom <boole92@naver.com>
2023-02-09 04:47:16 +0900

    리팩토링

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -0,0 +1,13 @@
+package com.leebeebeom.clothinghelper.domain.repository
+
+import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
+import com.leebeebeom.clothinghelper.util.LoadingStateProvider
+import kotlinx.coroutines.flow.Flow
+
+interface BaseDataRepository<T> : LoadingStateProvider {
+    val allData: Flow<List<T>>
+
+    suspend fun load(uid: String?, type: Class<T>): FirebaseResult
+    suspend fun add(data: T, uid: String): FirebaseResult
+    suspend fun edit(newData: T, uid: String): FirebaseResult
+}

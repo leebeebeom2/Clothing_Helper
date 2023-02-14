4c4e47a97861e07d8ecc29c2c586859c125f7cf0
leebeebeom <boole92@naver.com>
2023-02-15 04:01:02 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/preference/SubCategoryPreferencesRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/preference/SubCategoryPreferencesRepositoryImpl.kt
@@ -1,15 +1,14 @@
-package com.leebeebeom.clothinghelperdata.repository.preference
+package com.leebeebeom.clothinghelper.data.repository.preference
 
 import android.content.Context
 import androidx.datastore.preferences.preferencesDataStore
-import com.leebeebeom.clothinghelperdomain.repository.preference.SortPreferenceRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class SubCategoryPreferencesRepositoryImpl @Inject constructor(@ApplicationContext context: Context) :
-    SortPreferenceRepository by SortPreferenceRepositoryImpl(context.subCategoryDatastore)
+    SortPreferenceRepositoryImpl(dataStore = context.subCategoryDatastore)
 
-private const val SUBCATEGORY = "subCategory_preferences"
+private const val SUBCATEGORY = "subCategory preferences"
 private val Context.subCategoryDatastore by preferencesDataStore(name = SUBCATEGORY)

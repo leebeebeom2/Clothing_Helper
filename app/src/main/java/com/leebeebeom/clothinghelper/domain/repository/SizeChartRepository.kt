8818d65dc715fdcbe5bfe25ef480e42b39d406d3
leebeebeom <boole92@naver.com>
2023-02-15 04:02:35 +0900

    리팩토링

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SizeChartRepository.kt
@@ -0,0 +1,5 @@
+package com.leebeebeom.clothinghelper.domain.repository
+
+import com.leebeebeom.clothinghelper.domain.model.data.SizeChart
+
+interface SizeChartRepository : BaseDataRepository<SizeChart>

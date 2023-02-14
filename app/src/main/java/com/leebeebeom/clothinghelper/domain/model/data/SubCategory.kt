980c166a26f596e69ceb02721066b25c982767ba
leebeebeom <boole92@naver.com>
2023-02-15 04:02:11 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/SubCategory.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/SubCategory.kt
@@ -1,19 +1,16 @@
-package com.leebeebeom.clothinghelperdomain.model.data
+package com.leebeebeom.clothinghelper.domain.model.data
 
 import android.os.Parcelable
+import com.leebeebeom.clothinghelper.ui.main.drawer.MainCategoryType
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class SubCategory(
-    override val parent: SubCategoryParent = SubCategoryParent.TOP,
+    override val parent: MainCategoryType = MainCategoryType.TOP,
     override val key: String = "",
     override val name: String = "",
     override val createDate: Long = 0,
     override val editDate: Long = 0
 ) : BaseModel(), Parcelable {
     override fun addKey(key: String) = copy(key = key)
-}
-
-enum class SubCategoryParent {
-    TOP, BOTTOM, OUTER, ETC
 }

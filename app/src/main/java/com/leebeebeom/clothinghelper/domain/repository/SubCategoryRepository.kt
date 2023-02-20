daba5dfed28ca38d787b66f40b1091262f60b9b1
leebeebeom <boole92@naver.com>
2023-02-21 01:08:50 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
@@ -1,8 +1,5 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
-import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
 import com.leebeebeom.clothinghelper.domain.model.data.SubCategory
 
-interface SubCategoryRepository : BaseDataRepository<SubCategory> {
-    suspend fun pushInitialSubCategories(uid: String) : FirebaseResult
-}
+interface SubCategoryRepository : BaseDataRepository<SubCategory>

e76fe523f92d3e7dc570d63886d1036a93f65a55
leebeebeom <boole92@naver.com>
2023-02-21 02:38:00 +0900

    SignUpScreenTest.kt 작성

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/signup/SignUpScreenTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/signup/SignUpScreenTest.kt
@@ -1,14 +1,20 @@
 package com.leebeebeom.clothinghelper.ui.signin.ui.signup
 
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.leebeebeom.clothinghelper.*
-import com.leebeebeom.clothinghelper.R.string.error_password_confirm_not_same
+import com.leebeebeom.clothinghelper.R.string.*
 import com.leebeebeom.clothinghelper.ui.components.*
 import com.leebeebeom.clothinghelper.ui.signin.ui.SignInNavViewModel
 import com.leebeebeom.clothinghelper.ui.theme.ClothingHelperTheme
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+
+// TODO 구글 로그인 테스트
 
 class SignUpScreenTest {
     @get:Rule
@@ -39,9 +45,7 @@ class SignUpScreenTest {
                 uiState = viewModel.uiState as MutableSignUpUiState
 
                 SignUpScreen(
-                    signInNavViewModel = navViewModel,
-                    viewModel = viewModel,
-                    uiState = uiState
+                    signInNavViewModel = navViewModel, viewModel = viewModel, uiState = uiState
                 )
             }
         }
@@ -52,12 +56,10 @@ class SignUpScreenTest {
 
     @Test
     fun inputChangeTest() {
-        inputChangeTest(
-            rule = customTestRule,
+        inputChangeTest(rule = customTestRule,
             textField = { emailTextField },
             input = { uiState.email })
-        inputChangeTest(
-            rule = customTestRule,
+        inputChangeTest(rule = customTestRule,
             textField = { nickNameTextField },
             input = { uiState.nickName })
 
@@ -82,36 +84,28 @@ class SignUpScreenTest {
 
     @Test
     fun passwordTextFieldVisibleTest() {
-        textFieldVisibleTest(
-            rule = customTestRule,
+        textFieldVisibleTest(rule = customTestRule,
             textField = { passwordTextField },
             input = { uiState.password },
             visibleIcon = { passwordVisibleIcon },
-            invisibleIcon = { passwordInvisibleIcon }
-        )
+            invisibleIcon = { passwordInvisibleIcon })
 
-        textFieldVisibleTest(
-            rule = customTestRule,
+        textFieldVisibleTest(rule = customTestRule,
             textField = { passwordConfirmTextField },
             input = { uiState.passwordConfirm },
             visibleIcon = { passwordConfirmVisibleIcon },
-            invisibleIcon = { passwordConfirmInvisibleIcon }
-        )
+            invisibleIcon = { passwordConfirmInvisibleIcon })
     }
 
     @Test
     fun cancelIconTest() {
-        cancelIconTest(
-            rule = customTestRule,
+        cancelIconTest(rule = customTestRule,
             cancelIconTextField = { emailTextField },
-            looseFocus = { passwordTextField.click() }
-        )
+            looseFocus = { passwordTextField.click() })
 
-        cancelIconTest(
-            rule = customTestRule,
+        cancelIconTest(rule = customTestRule,
             cancelIconTextField = { nickNameTextField },
-            looseFocus = { passwordTextField.click() }
-        )
+            looseFocus = { passwordTextField.click() })
 
         invisibleTest(rule = customTestRule, visibleIcon = { passwordVisibleIcon }) {
             cancelIconTest(
@@ -138,15 +132,8 @@ class SignUpScreenTest {
         errorTest(
             rule = customTestRule,
             errorTextField = { emailTextField },
-            errorTextRes = R.string.error_email_already_in_use,
-            setError = {
-                allTextFieldInput(
-                    email = EMAIL,
-                    password = PASSWORD,
-                    passwordConfirm = PASSWORD
-                )
-                signUpButton.click()
-            },
+            errorTextRes = error_email_already_in_use,
+            setError = ::setAlreadyUseEmailError,
             beforeText = EMAIL,
             blockBlank = true,
             clearOtherTextField = ::allTextFieldClear
@@ -154,15 +141,8 @@ class SignUpScreenTest {
         errorTest(
             rule = customTestRule,
             errorTextField = { emailTextField },
-            errorTextRes = R.string.error_invalid_email,
-            setError = {
-                allTextFieldInput(
-                    email = INVALID_EMAIL,
-                    password = PASSWORD,
-                    passwordConfirm = PASSWORD
-                )
-                signUpButton.click()
-            },
+            errorTextRes = error_invalid_email,
+            setError = ::setInvalidEmailError,
             beforeText = INVALID_EMAIL,
             blockBlank = true,
             clearOtherTextField = ::allTextFieldClear
@@ -175,6 +155,16 @@ class SignUpScreenTest {
         passwordNotSameErrorTest2()
     }
 
+    private fun setInvalidEmailError() {
+        allTextFieldInput(email = INVALID_EMAIL, password = PASSWORD, passwordConfirm = PASSWORD)
+        signUpButton.click()
+    }
+
+    private fun setAlreadyUseEmailError() {
+        allTextFieldInput(email = EMAIL, password = PASSWORD, passwordConfirm = PASSWORD)
+        signUpButton.click()
+    }
+
     private fun allTextFieldClear() {
         emailTextField.textClear()
         nickNameTextField.textClear()
@@ -183,15 +173,13 @@ class SignUpScreenTest {
     }
 
     private fun weakPasswordErrorTest() {
-        fun weakPasswordError() =
-            customTestRule.getNodeWithStringRes(R.string.error_weak_password)
+        fun weakPasswordError() = customTestRule.getNodeWithStringRes(error_weak_password)
 
         fun errorExist() = weakPasswordError().exist()
         fun errorNotExist() = weakPasswordError().notExist()
 
         allTextFieldInput(
-            email = NOT_EXIST_EMAIL,
-            password = WEAK_PASSWORD
+            email = NOT_EXIST_EMAIL, password = WEAK_PASSWORD
         )
 
         errorExist()
@@ -225,9 +213,7 @@ class SignUpScreenTest {
     private fun passwordNotSameErrorTest1() {
 
         allTextFieldInput(
-            email = EMAIL,
-            password = PASSWORD,
-            passwordConfirm = ""
+            email = EMAIL, password = PASSWORD, passwordConfirm = ""
         )
 
         passwordNotSameErrorNotExist()
@@ -250,9 +236,7 @@ class SignUpScreenTest {
      */
     private fun passwordNotSameErrorTest2() {
         allTextFieldInput(
-            email = EMAIL,
-            password = PASSWORD,
-            passwordConfirm = PASSWORD
+            email = EMAIL, password = PASSWORD, passwordConfirm = PASSWORD
         )
 
         passwordTextField.input(text = "1", beforeText = PASSWORD, invisible = true)
@@ -267,27 +251,24 @@ class SignUpScreenTest {
 
     private fun allTextFieldInput(
         email: String,
+        nickName: String = NICK_NAME,
         password: String,
         passwordConfirm: String = "",
     ) {
         emailTextField.input(email)
-        nickNameTextField.input(NICK_NAME)
+        nickNameTextField.input(nickName)
         passwordTextField.input(password, invisible = true)
         passwordConfirmTextField.input(passwordConfirm, invisible = true)
     }
 
     @Test
     fun cursorTest() {
-        cursorTest(
-            rule = customTestRule,
+        cursorTest(rule = customTestRule,
             textField = { emailTextField },
-            looseFocus = { passwordTextField.click() }
-        )
-        cursorTest(
-            rule = customTestRule,
+            looseFocus = { passwordTextField.click() })
+        cursorTest(rule = customTestRule,
             textField = { nickNameTextField },
-            looseFocus = { passwordTextField.click() }
-        )
+            looseFocus = { passwordTextField.click() })
         invisibleTest(rule = customTestRule, visibleIcon = { passwordVisibleIcon }) {
             cursorTest(
                 rule = customTestRule,
@@ -306,30 +287,138 @@ class SignUpScreenTest {
         }
     }
 
-    @Test
-    fun imeTest() {
-        imeTest(
-            { emailTextField },
-            { nickNameTextField },
-            { passwordTextField },
-            doneTextField = { passwordConfirmTextField }
-        )
-        imeTest(
-            { nickNameTextField },
-            { passwordTextField },
-            doneTextField = { passwordConfirmTextField }
-        )
-        imeTest(
-            { passwordTextField },
-            doneTextField = { passwordConfirmTextField }
-        )
-    }
+//    @Test TODO 테스트 불가
+//    fun imeTest() {
+//        imeTest(
+//            { emailTextField },
+//            { nickNameTextField },
+//            { passwordTextField },
+//            doneTextField = { passwordConfirmTextField }
+//        )
+//        imeTest(
+//            { nickNameTextField },
+//            { passwordTextField },
+//            doneTextField = { passwordConfirmTextField }
+//        )
+//        imeTest(
+//            { passwordTextField },
+//            doneTextField = { passwordConfirmTextField }
+//        )
+//    }
 
     @Test
     fun blockBlankTest() {
-//        com.leebeebeom.clothinghelper.ui.components.blockBlankTest(
-//            customTestRule,
-//
-//        )
+        blockBlankTest(rule = customTestRule, textField = { emailTextField })
+
+        invisibleTest(rule = customTestRule, visibleIcon = { passwordVisibleIcon }) {
+            blockBlankTest(
+                rule = customTestRule, textField = { passwordTextField }, invisible = it
+            )
+        }
+
+        invisibleTest(rule = customTestRule, visibleIcon = { passwordConfirmVisibleIcon }) {
+            blockBlankTest(
+                rule = customTestRule, textField = { passwordConfirmTextField }, invisible = it
+            )
+        }
+    }
+
+    @Test
+    fun nickNameTextFieldBlankTest() {
+        val blankNickName = "  $NICK_NAME  "
+        allTextFieldInput(
+            email = NOT_EXIST_EMAIL,
+            nickName = blankNickName,
+            password = PASSWORD,
+            passwordConfirm = PASSWORD
+        )
+        signUpButton.click()
+
+        customTestRule.wait(5000) {
+            FirebaseAuth.getInstance().currentUser != null && FirebaseAuth.getInstance().currentUser?.displayName != null
+        }
+        assert(FirebaseAuth.getInstance().currentUser != null)
+        val user = FirebaseAuth.getInstance().currentUser!!
+        val uid = user.uid
+        assert(user.displayName!! != blankNickName)
+        assert(user.displayName!! == NICK_NAME)
+
+        user.delete() // 삭제 확인
+        Firebase.database.getReference(uid).removeValue() // 삭제 확인
+    }
+
+    @Test
+    fun buttonEnableTest() {
+        val invalidEmailErrorText = customTestRule.getString(error_invalid_email)
+        val alreadyUseEmailErrorText = customTestRule.getString(error_email_already_in_use)
+        val weakPasswordErrorText = customTestRule.getString(error_weak_password)
+        val passwordNotSameErrorText = customTestRule.getString(error_password_confirm_not_same)
+
+        fun blankTextFieldTest() {
+            val inputFunctions = arrayOf({ emailTextField.input(EMAIL) },
+                { nickNameTextField.input(NICK_NAME) },
+                { passwordTextField.input(PASSWORD, invisible = true) },
+                { passwordConfirmTextField.input(PASSWORD, invisible = true) })
+            val order = arrayListOf(0, 1, 2, 3)
+
+            repeat(20) {
+                order.shuffle()
+                order.forEach { inputFunctions[it]() }
+                signUpButton.enabled()
+                allTextFieldClear()
+            }
+        }
+
+        blankTextFieldTest()
+
+        setInvalidEmailError()
+        customTestRule.waitTextExist(text = invalidEmailErrorText, time = 5000)
+        signUpButton.notEnabled()
+        allTextFieldClear()
+
+        setAlreadyUseEmailError()
+        customTestRule.waitTextExist(text = alreadyUseEmailErrorText, time = 5000)
+        signUpButton.notEnabled()
+        allTextFieldClear()
+
+        allTextFieldInput(EMAIL, password = PASSWORD, passwordConfirm = PASSWORD)
+        signUpButton.enabled()
+
+        passwordConfirmTextField.delete()
+        passwordTextField.delete()
+        customTestRule.waitTextExist(text = weakPasswordErrorText)
+        signUpButton.notEnabled()
+
+        passwordTextField.input("1", "11111", invisible = true)
+        customTestRule.getNodeWithText(text = weakPasswordErrorText).notExist()
+        customTestRule.waitTextExist(text = passwordNotSameErrorText)
+        signUpButton.notEnabled()
+
+        passwordConfirmTextField.input("1", "11111", invisible = true)
+        customTestRule.getNodeWithText(text = passwordNotSameErrorText).notExist()
+        signUpButton.enabled()
+    }
+
+    @Test
+    fun signUpTest() {
+        val auth = FirebaseAuth.getInstance()
+        fun getUser() = auth.currentUser
+
+        allTextFieldInput(email = NOT_EXIST_EMAIL, password = PASSWORD, passwordConfirm = PASSWORD)
+        signUpButton.click()
+        customTestRule.wait(3000) {
+            getUser() != null && getUser()!!.displayName != null && getUser()!!.displayName!!.isNotBlank()
+        }
+
+        val user = getUser()!!
+        assert(user.email == NOT_EXIST_EMAIL)
+        assert(user.displayName == NICK_NAME)
+
+        auth.signOut()
+        val task = auth.signInWithEmailAndPassword(NOT_EXIST_EMAIL, PASSWORD)
+        customTestRule.wait(10000) { task.isSuccessful }
+
+        FirebaseDatabase.getInstance().reference.child(user.uid).removeValue()
+        user.delete()
     }
 }

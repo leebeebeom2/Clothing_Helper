ae9a8e62a1d8f9a71d5bfb381261bacba596e260
leebeebeom <boole92@naver.com>
2023-02-21 01:08:43 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/FolderRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/FolderRepository.kt
@@ -1,6 +1,5 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.domain.model.data.Folder
-import com.leebeebeom.clothinghelper.util.LoadingStateProvider
 
-interface FolderRepository : BaseDataRepository<Folder>, LoadingStateProvider
+interface FolderRepository : BaseDataRepository<Folder>

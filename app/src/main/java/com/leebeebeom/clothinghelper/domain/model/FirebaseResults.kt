03bc434c492b0bc8879c03dbd02d9a25212dfd22
leebeebeom <boole92@naver.com>
2023-02-21 01:32:10 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/model/FirebaseResults.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/model/FirebaseResults.kt
@@ -1,18 +1,12 @@
 package com.leebeebeom.clothinghelper.domain.model
 
-import com.leebeebeom.clothinghelper.domain.model.data.User
-
 sealed class FirebaseResult {
     object Success : FirebaseResult()
     data class Fail(val exception: Exception) : FirebaseResult()
 }
 
 sealed class AuthResult {
-    object EmptySuccess : AuthResult()
-    data class Success(
-        val user: User = User(),
-        val isNewer: Boolean = false,
-    ) : AuthResult()
+    object Success : AuthResult()
 
     data class Fail(val errorCode: String) : AuthResult()
     object UnknownFail : AuthResult()

9d1bb2291c09041c91e37d74870095084d118d69
leebeebeom <boole92@naver.com>
2023-02-22 01:16:33 +0900

    콜백 구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -1,12 +1,11 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
-import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import kotlinx.coroutines.flow.Flow
 
 interface BaseDataRepository<T> : LoadingStateProvider {
     val allData: Flow<List<T>>
-    suspend fun load(uid: String?, type: Class<T>, firebaseResult: FirebaseResult)
-    suspend fun add(data: T, uid: String, firebaseResult: FirebaseResult)
-    suspend fun edit(newData: T, uid: String, firebaseResult: FirebaseResult)
+    suspend fun load(uid: String?, type: Class<T>, onFail: (Exception) -> Unit)
+    suspend fun add(data: T, uid: String, onFail: (Exception) -> Unit)
+    suspend fun edit(newData: T, uid: String, onFail: (Exception) -> Unit)
 }

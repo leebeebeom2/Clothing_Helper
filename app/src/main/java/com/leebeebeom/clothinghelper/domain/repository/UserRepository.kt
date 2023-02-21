e4c1d320b40ef28b07b0095633b3ce4ee7d770ab
leebeebeom <boole92@naver.com>
2023-02-22 00:11:03 +0900

    콜백 구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
@@ -1,27 +1,29 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
 import com.google.firebase.auth.AuthCredential
-import com.leebeebeom.clothinghelper.domain.model.AuthResult
-import com.leebeebeom.clothinghelper.domain.model.data.User
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
+import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
+import com.leebeebeom.clothinghelper.domain.model.data.User
 import kotlinx.coroutines.flow.StateFlow
 
 interface UserRepository : LoadingStateProvider {
     val isSignIn: StateFlow<Boolean>
     val user: StateFlow<User?>
 
-    suspend fun googleSignIn(credential: AuthCredential): AuthResult
+    suspend fun googleSignIn(credential: AuthCredential, firebaseResult: FirebaseResult)
     suspend fun signIn(
         email: String,
         password: String,
-    ): AuthResult
+        firebaseResult: FirebaseResult,
+    )
 
     suspend fun signUp(
         email: String,
         password: String,
         name: String,
-    ): AuthResult
+        firebaseResult: FirebaseResult,
+    )
 
-    suspend fun resetPasswordEmail(email: String): AuthResult
-    suspend fun signOut(): AuthResult
+    suspend fun resetPasswordEmail(email: String, firebaseResult: FirebaseResult)
+    suspend fun signOut(firebaseResult: FirebaseResult)
 }

67e71ebbe3e2fa4a87de4376f5bb4828e65efc16
leebeebeom <boole92@naver.com>
2023-02-22 00:42:37 +0900

    에러 구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInViewModel.kt
@@ -2,19 +2,22 @@ package com.leebeebeom.clothinghelper.ui.signin.ui.signin
 
 import androidx.lifecycle.viewModelScope
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.domain.model.AuthResult.*
+import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
+import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorUseCase
 import com.leebeebeom.clothinghelper.domain.usecase.user.SignInUseCase
 import com.leebeebeom.clothinghelper.ui.signin.base.EmailAndPasswordViewModel
 import com.leebeebeom.clothinghelper.ui.signin.state.EmailAndPasswordState
 import com.leebeebeom.clothinghelper.ui.signin.state.MutableEmailAndPasswordUiState
 import com.leebeebeom.clothinghelper.ui.util.ShowToast
-import com.leebeebeom.clothinghelper.ui.util.fireBaseError
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModel @Inject constructor(private val signInUseCase: SignInUseCase) :
+class SignInViewModel @Inject constructor(
+    private val signInUseCase: SignInUseCase,
+    private val firebaseAuthErrorUseCase: FirebaseAuthErrorUseCase,
+) :
     EmailAndPasswordViewModel() {
 
     override val mutableUiState: MutableEmailAndPasswordUiState = MutableEmailAndPasswordUiState()
@@ -22,19 +25,22 @@ class SignInViewModel @Inject constructor(private val signInUseCase: SignInUseCa
 
     fun signInWithEmailAndPassword(showToast: ShowToast) =
         viewModelScope.launch {
-            when (val result =
-                signInUseCase.signIn(
-                    email = mutableUiState.email,
-                    password = mutableUiState.password
-                )) {
-                is Success -> showToast(R.string.sign_in_complete)
-                is Fail -> fireBaseError(
-                    errorCode = result.errorCode,
-                    updateEmailError = { mutableUiState.emailError = it },
-                    updatePasswordError = { mutableUiState.passwordError = it },
-                    showToast = showToast
-                )
-                is UnknownFail -> showToast(R.string.unknown_error)
-            }
+            signInUseCase.signIn(
+                email = mutableUiState.email,
+                password = mutableUiState.password,
+                firebaseResult = object : FirebaseResult {
+                    override fun success() {
+                        showToast(R.string.sign_in_complete)
+                    }
+
+                    override fun fail(exception: Exception) =
+                        firebaseAuthErrorUseCase.firebaseAuthError(
+                            exception = exception,
+                            updateEmailError = { mutableUiState.emailError = it },
+                            updatePasswordError = { mutableUiState.passwordError = it },
+                            showToast = showToast
+                        )
+                }
+            )
         }
 }

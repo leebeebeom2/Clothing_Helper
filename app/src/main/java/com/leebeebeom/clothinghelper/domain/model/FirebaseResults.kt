2fba656d5e56cc8b5ae0f964398334a8449c47c8
leebeebeom <boole92@naver.com>
2023-02-22 00:54:04 +0900

    결과 콜백 구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/model/FirebaseResults.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/model/FirebaseResults.kt
@@ -1,13 +1,6 @@
 package com.leebeebeom.clothinghelper.domain.model
 
-sealed class FirebaseResult {
-    object Success : FirebaseResult()
-    data class Fail(val exception: Exception) : FirebaseResult()
-}
-
-sealed class AuthResult {
-    object Success : AuthResult()
-
-    data class Fail(val errorCode: String) : AuthResult()
-    object UnknownFail : AuthResult()
+interface FirebaseResult {
+    fun success()
+    fun fail(exception: Exception)
 }

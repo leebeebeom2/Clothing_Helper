a5e86dd5a4822be059b1d9ed801f9e662bbb8d08
leebeebeom <boole92@naver.com>
2023-02-22 01:19:21 +0900

    콜백 구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/LoadDataUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/LoadDataUseCase.kt
@@ -1,14 +1,11 @@
 package com.leebeebeom.clothinghelper.domain.usecase
 
-import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
 import com.leebeebeom.clothinghelper.domain.model.data.Folder
 import com.leebeebeom.clothinghelper.domain.model.data.SubCategory
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelper.domain.usecase.user.GetUserUseCase
 import dagger.hilt.android.scopes.ViewModelScoped
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.collectLatest
 import javax.inject.Inject
 
@@ -18,25 +15,20 @@ class LoadDataUseCase @Inject constructor(
     private val subCategoryRepository: SubCategoryRepository,
     private val folderRepository: FolderRepository,
 ) {
-    suspend fun load(onLoadDataFail: (FirebaseResult.Fail) -> Unit) =
-        getUserUseCase.user.collectLatest {
-            coroutineScope {
-                val subCategoryDeferred = async {
-                    subCategoryRepository.load(
-                        uid = it?.uid,
-                        type = SubCategory::class.java
-                    )
-                }
-                val folderDeferred =
-                    async { folderRepository.load(uid = it?.uid, type = Folder::class.java) }
+    suspend fun load(
+        subCategoryLoadFail: (Exception) -> Unit,
+        folderLoadFail: (Exception) -> Unit,
+    ) = getUserUseCase.user.collectLatest {// TODO 속도 테스트
+        subCategoryRepository.load(
+            uid = it?.uid,
+            type = SubCategory::class.java,
+            onFail = subCategoryLoadFail
+        )
 
-                val subCategoryResult = subCategoryDeferred.await()
-                val folderResult = folderDeferred.await()
-
-                when {
-                    subCategoryResult is FirebaseResult.Fail -> onLoadDataFail(subCategoryResult)
-                    folderResult is FirebaseResult.Fail -> onLoadDataFail(folderResult)
-                }
-            }
-        }
+        folderRepository.load(
+            uid = it?.uid,
+            type = Folder::class.java,
+            onFail = folderLoadFail
+        )
+    }
 }

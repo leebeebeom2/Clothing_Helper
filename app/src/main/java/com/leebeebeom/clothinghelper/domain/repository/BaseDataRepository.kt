3e9c1adbc2d5961896acf1484c96a4152fb8ad68
leebeebeom <boole92@naver.com>
2023-02-22 01:01:10 +0900

    콜백 구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface BaseDataRepository<T> : LoadingStateProvider {
     val allData: Flow<List<T>>
-    suspend fun load(uid: String?, type: Class<T>): FirebaseResult
-    suspend fun add(data: T, uid: String): FirebaseResult
-    suspend fun edit(newData: T, uid: String): FirebaseResult
+    suspend fun load(uid: String?, type: Class<T>, firebaseResult: FirebaseResult)
+    suspend fun add(data: T, uid: String, firebaseResult: FirebaseResult)
+    suspend fun edit(newData: T, uid: String, firebaseResult: FirebaseResult)
 }

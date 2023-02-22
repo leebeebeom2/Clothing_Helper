7e000a4fcb4732775890b9d01487363117bf1c4b
leebeebeom <boole92@naver.com>
2023-02-23 01:42:42 +0900

    룸 코드 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/container/SubCategoryRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/container/SubCategoryRepositoryImpl.kt
@@ -1,16 +1,30 @@
 package com.leebeebeom.clothinghelper.data.repository.container
 
+import android.content.Context
+import com.leebeebeom.clothinghelper.data.datasourse.subcategory.SubCategoryFirebaseDataSource
+import com.leebeebeom.clothinghelper.data.datasourse.subcategory.SubCategoryRoomDataSource
 import com.leebeebeom.clothinghelper.data.repository.DatabasePath
+import com.leebeebeom.clothinghelper.data.repository.preference.NetworkPreferenceRepository
 import com.leebeebeom.clothinghelper.domain.model.data.SubCategory
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.SortPreferenceRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.SubCategoryPreferencesRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class SubCategoryRepositoryImpl @Inject constructor(@SubCategoryPreferencesRepository subCategoryPreferencesRepository: SortPreferenceRepository) :
-    BaseDataRepositoryImpl<SubCategory>(
-        sortFlow = subCategoryPreferencesRepository.sort,
-        refPath = DatabasePath.SUB_CATEGORIES
-    ), SubCategoryRepository
+class SubCategoryRepositoryImpl @Inject constructor(
+    @SubCategoryPreferencesRepository subCategoryPreferencesRepository: SortPreferenceRepository,
+    @ApplicationContext context: Context,
+    subCategoryFirebaseDataSource: SubCategoryFirebaseDataSource,
+    subCateRoomDataSource: SubCategoryRoomDataSource,
+    networkPreferenceRepository: NetworkPreferenceRepository,
+) : BaseContainerRepository<SubCategory>(
+    sortFlow = subCategoryPreferencesRepository.sort,
+    refPath = DatabasePath.SUB_CATEGORIES,
+    context = context,
+    baseFirebaseDataSource = subCategoryFirebaseDataSource,
+    baseRoomDataSource = subCateRoomDataSource,
+    networkPreferenceRepository = networkPreferenceRepository
+), SubCategoryRepository

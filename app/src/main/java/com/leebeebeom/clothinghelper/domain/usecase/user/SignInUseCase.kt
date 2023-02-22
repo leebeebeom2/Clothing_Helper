6d5a9bd7016cf3383a69f3183e32b8788e948101
leebeebeom <boole92@naver.com>
2023-02-22 23:14:22 +0900

    로그인 시 모든 로컬 데이터 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignInUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignInUseCase.kt
@@ -2,11 +2,18 @@ package com.leebeebeom.clothinghelper.domain.usecase.user
 
 import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
+import com.leebeebeom.clothinghelper.domain.usecase.AllLocalDataClearUseCase
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class SignInUseCase @Inject constructor(private val userRepository: UserRepository) {
+class SignInUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val allLocalDataClearUseCase: AllLocalDataClearUseCase,
+) {
     suspend fun signIn(email: String, password: String, firebaseResult: FirebaseResult) =
-        userRepository.signIn(email = email, password = password, firebaseResult = firebaseResult)
+        userRepository.signIn(
+            email = email, password = password, firebaseResult = firebaseResult,
+            allLocalDataClear = allLocalDataClearUseCase::allLocalDataClear
+        )
 }

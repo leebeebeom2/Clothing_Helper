6389e632974d4adffc05159f47b457d9853adec9
leebeebeom <boole92@naver.com>
2023-02-23 00:29:25 +0900

    isSynced 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/SubCategory.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/SubCategory.kt
@@ -1,16 +1,20 @@
 package com.leebeebeom.clothinghelper.domain.model.data
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.leebeebeom.clothinghelper.ui.main.drawer.MainCategoryType
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity
 data class SubCategory(
+    @PrimaryKey override val key: String = "",
     override val parent: MainCategoryType = MainCategoryType.TOP,
-    override val key: String = "",
     override val name: String = "",
     override val createDate: Long = 0,
-    override val editDate: Long = 0
-) : BaseModel(), Parcelable {
+    override val editDate: Long = 0,
+    override val isSynced: Boolean = false,
+) : BaseContainerModel(), Parcelable {
     override fun addKey(key: String) = copy(key = key)
 }

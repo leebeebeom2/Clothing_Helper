260cda4029921338b383d5eb6eb4b46dfb9bee20
leebeebeom <boole92@naver.com>
2023-02-23 00:29:44 +0900

    isSynced 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/Folder.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/Folder.kt
@@ -1,18 +1,22 @@
 package com.leebeebeom.clothinghelper.domain.model.data
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.leebeebeom.clothinghelper.ui.main.drawer.MainCategoryType
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity
 data class Folder(
+    @PrimaryKey override val key: String = "",
     override val parentKey: String = "",
     override val subCategoryKey: String = "",
     override val createDate: Long = 0,
     override val name: String = "",
-    override val key: String = "",
     override val editDate: Long = 0,
-    override val parent: MainCategoryType = MainCategoryType.TOP
+    override val parent: MainCategoryType = MainCategoryType.TOP,
+    override val isSynced: Boolean = false,
 ) : BaseFolderModel(), Parcelable {
     override fun addKey(key: String) = copy(key = key)
 }

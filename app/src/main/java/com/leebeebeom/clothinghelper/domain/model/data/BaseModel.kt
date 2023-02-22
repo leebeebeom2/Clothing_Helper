1b224c1263cc6fcc9cfa3eec8ced976d09b15389
leebeebeom <boole92@naver.com>
2023-02-23 01:09:20 +0900

    BaseModel 추상화, synced 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/BaseModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/BaseModel.kt
@@ -3,16 +3,20 @@ package com.leebeebeom.clothinghelper.domain.model.data
 import com.leebeebeom.clothinghelper.ui.main.drawer.MainCategoryType
 
 abstract class BaseModel {
-    abstract val parent: MainCategoryType
-    abstract val name: String
     abstract val key: String
-    abstract val createDate: Long
-    abstract val editDate: Long
-
+    abstract val isSynced: Boolean
     abstract fun addKey(key: String): BaseModel
+    abstract fun synced(): BaseModel
 }
 
-abstract class BaseFolderModel : BaseModel() {
+abstract class BaseContainerModel : BaseModel() {
+    abstract val parent: MainCategoryType
+    abstract val name: String
+    abstract val createDate: Long
+    abstract val editDate: Long
+}
+
+abstract class BaseFolderModel : BaseContainerModel() {
     abstract val parentKey: String
     abstract val subCategoryKey: String
 }

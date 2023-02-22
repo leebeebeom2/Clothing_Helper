43858f3c346e2555a6a63faf0ccbace20f9bcfb4
leebeebeom <boole92@naver.com>
2023-02-23 02:40:08 +0900

    allLocalDataClear 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -8,5 +8,4 @@ interface BaseDataRepository<T> : LoadingStateProvider {
     suspend fun load(uid: String?, type: Class<T>, onFail: (Exception) -> Unit)
     suspend fun add(data: T, uid: String, onFail: (Exception) -> Unit)
     suspend fun edit(newData: T, uid: String, onFail: (Exception) -> Unit)
-    suspend fun allLocalDataClear()
 }

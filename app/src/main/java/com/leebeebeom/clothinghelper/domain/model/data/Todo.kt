e55f61dd7c4ab9fae39debc0b382b120bb2e1220
leebeebeom <boole92@naver.com>
2023-02-23 01:45:35 +0900

    상송

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/Todo.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/model/data/Todo.kt
@@ -1,7 +1,13 @@
 package com.leebeebeom.clothinghelper.domain.model.data
 
 data class Todo(
-    val key: String = "",
+    override val key: String = "",
     val text: String = "",
     val done: Boolean = false,
-)
+    val order: Int = 0,
+    override val isSynced: Boolean = false,
+) : BaseModel() {
+    override fun addKey(key: String) = copy(key = key)
+
+    override fun synced() = copy(isSynced = true)
+}

39fd1844106f79dc29820249fdaf4c9ea0da0b80
leebeebeom <boole92@naver.com>
2023-02-23 02:20:48 +0900

    TodoDao 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/LocalDataBase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/LocalDataBase.kt
@@ -4,11 +4,14 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.leebeebeom.clothinghelper.data.datasourse.folder.FolderDao
 import com.leebeebeom.clothinghelper.data.datasourse.subcategory.SubCategoryDao
+import com.leebeebeom.clothinghelper.data.datasourse.todo.TodoDao
 import com.leebeebeom.clothinghelper.domain.model.data.Folder
 import com.leebeebeom.clothinghelper.domain.model.data.SubCategory
+import com.leebeebeom.clothinghelper.domain.model.data.Todo
 
-@Database(entities = [SubCategory::class, Folder::class], version = 1)
+@Database(entities = [SubCategory::class, Folder::class, Todo::class], version = 1)
 abstract class LocalDataBase : RoomDatabase() {
     abstract fun subCategoryDao(): SubCategoryDao
-    abstract fun folderDao():FolderDao
+    abstract fun folderDao(): FolderDao
+    abstract fun todoDao(): TodoDao
 }

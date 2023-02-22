db2dd13dfbc1cd83d535795e6b763360e4b2af10
leebeebeom <boole92@naver.com>
2023-02-23 04:05:25 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/TodoRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/TodoRepositoryImpl.kt
@@ -3,10 +3,9 @@ package com.leebeebeom.clothinghelper.data.repository
 import android.content.Context
 import com.leebeebeom.clothinghelper.data.datasourse.todo.TodoFirebaseDataSource
 import com.leebeebeom.clothinghelper.data.datasourse.todo.TodoRoomDataSource
-import com.leebeebeom.clothinghelper.data.repository.container.BaseDataRepositoryImpl
-import com.leebeebeom.clothinghelper.data.repository.preference.NetworkPreferenceRepository
 import com.leebeebeom.clothinghelper.domain.model.data.Todo
 import com.leebeebeom.clothinghelper.domain.repository.TodoRepository
+import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton

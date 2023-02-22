7fedbc975e3b03e8bd967c6aabc58f1dc2dba0da
leebeebeom <boole92@naver.com>
2023-02-22 23:14:50 +0900

    로그인 시 모든 로컬 데이터 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignOutUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignOutUseCase.kt
@@ -1,10 +1,17 @@
 package com.leebeebeom.clothinghelper.domain.usecase.user
 
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
+import com.leebeebeom.clothinghelper.domain.usecase.AllLocalDataClearUseCase
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class SignOutUseCase @Inject constructor(private val userRepository: UserRepository) {
-    suspend fun signOut(onFail: (Exception) -> Unit) = userRepository.signOut(onFail = onFail)
+class SignOutUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val allLocalDataClearUseCase: AllLocalDataClearUseCase,
+) {
+    suspend fun signOut(onFail: (Exception) -> Unit) = userRepository.signOut(
+        onFail = onFail,
+        allLocalDataClear = allLocalDataClearUseCase::allLocalDataClear
+    )
 }

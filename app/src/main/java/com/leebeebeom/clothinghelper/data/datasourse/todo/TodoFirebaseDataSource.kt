37c85c50afd673fef20c7b3445f14b949ac826da
leebeebeom <boole92@naver.com>
2023-02-23 02:30:57 +0900

    상속

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/todo/TodoFirebaseDataSource.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/todo/TodoFirebaseDataSource.kt
@@ -1,4 +1,9 @@
 package com.leebeebeom.clothinghelper.data.datasourse.todo
 
-class TodoFirebaseDataSource {
-}
+import com.leebeebeom.clothinghelper.data.datasourse.BaseFirebaseDataSource
+import com.leebeebeom.clothinghelper.domain.model.data.Todo
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TodoFirebaseDataSource @Inject constructor() : BaseFirebaseDataSource<Todo>()
--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/todo/TodoRoomDataSource.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/todo/TodoRoomDataSource.kt
@@ -1,4 +1,9 @@
 package com.leebeebeom.clothinghelper.data.datasourse.todo
 
-class TodoRoomDataSource {
-}
+import com.leebeebeom.clothinghelper.data.datasourse.BaseRoomDataSource
+import com.leebeebeom.clothinghelper.domain.model.data.Todo
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TodoRoomDataSource @Inject constructor(dao: TodoDao) : BaseRoomDataSource<Todo>(dao = dao)

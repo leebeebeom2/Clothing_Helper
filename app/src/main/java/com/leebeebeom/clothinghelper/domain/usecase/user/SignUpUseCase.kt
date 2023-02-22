3ad63b363a971e3e1eec7ac6b7cc58cca83c6126
leebeebeom <boole92@naver.com>
2023-02-22 23:22:57 +0900

    로컬 데이터 삭제 로직 제거

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignUpUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignUpUseCase.kt
@@ -2,15 +2,11 @@ package com.leebeebeom.clothinghelper.domain.usecase.user
 
 import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
-import com.leebeebeom.clothinghelper.domain.usecase.AllLocalDataClearUseCase
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 @ViewModelScoped
-class SignUpUseCase @Inject constructor(
-    private val userRepository: UserRepository,
-    private val allLocalDataClearUseCase: AllLocalDataClearUseCase
-) {
+class SignUpUseCase @Inject constructor(private val userRepository: UserRepository) {
     suspend fun signUp(
         email: String,
         password: String,
@@ -21,6 +17,5 @@ class SignUpUseCase @Inject constructor(
         password = password,
         name = name,
         firebaseResult = firebaseResult,
-        allLocalDataClear = allLocalDataClearUseCase::allLocalDataClear
     )
 }

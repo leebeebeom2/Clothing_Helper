730bf71e5aa41d86168c6926896728bb605aa5f2
leebeebeom <boole92@naver.com>
2023-02-24 03:40:02 +0900

    withContext 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/BaseFirebaseDataSource.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/BaseFirebaseDataSource.kt
@@ -3,18 +3,20 @@ package com.leebeebeom.clothinghelper.data.datasourse
 import com.leebeebeom.clothinghelper.data.repository.firebaseDbRoot
 import com.leebeebeom.clothinghelper.data.repository.getContainerRef
 import com.leebeebeom.clothinghelper.domain.model.data.BaseModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 abstract class BaseFirebaseDataSource<T : BaseModel>(private val refPath: String) {
     suspend fun getAll(
         uid: String,
         type: Class<T>,
-    ): List<T> {
+    ): List<T> = withContext(Dispatchers.IO) {
         val temp = mutableListOf<T>()
 
         firebaseDbRoot.getContainerRef(uid = uid, path = refPath).get()
             .await().children.forEach { temp.add((it.getValue(type))!!) }
-        return temp
+        temp
     }
 
     suspend fun push(uid: String, t: T) {

9a4d5eea9dc3afdfb4eb2869cad652074be6120a
leebeebeom <boole92@naver.com>
2023-02-25 05:06:48 +0900

    모델 소스 분리

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/folder/FolderDao.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/folder/FolderDao.kt
@@ -3,17 +3,15 @@ package com.leebeebeom.clothinghelper.data.datasourse.folder
 import androidx.room.Dao
 import androidx.room.Query
 import com.leebeebeom.clothinghelper.data.datasourse.BaseDao
-import com.leebeebeom.clothinghelper.domain.model.data.Folder
+import com.leebeebeom.clothinghelper.domain.model.Folder
+import com.leebeebeom.clothinghelper.domain.model.RoomFolder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface FolderDao : BaseDao<Folder> {
-    @Query("SELECT * FROM Folder")
+interface FolderDao : BaseDao<RoomFolder> {
+    @Query("SELECT `key`,name,parentKey,subCategoryKey FROM RoomFolder")
     fun getAll(): Flow<List<Folder>>
 
-    @Query("SELECT * FROM Folder WHERE isSynced = 0")
-    suspend fun getAllAsync(): List<Folder>
-
-    @Query("DELETE FROM Folder")
+    @Query("DELETE FROM RoomFolder")
     suspend fun deleteAll()
 }
--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/folder/FolderRoomDataSource.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/folder/FolderRoomDataSource.kt
@@ -1,14 +1,14 @@
 package com.leebeebeom.clothinghelper.data.datasourse.folder
 
 import com.leebeebeom.clothinghelper.data.datasourse.BaseRoomDataSource
-import com.leebeebeom.clothinghelper.domain.model.data.Folder
+import com.leebeebeom.clothinghelper.domain.model.Folder
+import com.leebeebeom.clothinghelper.domain.model.RoomFolder
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class FolderRoomDataSource @Inject constructor(private val dao: FolderDao) :
-    BaseRoomDataSource<Folder>(dao = dao) {
+    BaseRoomDataSource<RoomFolder, Folder>(dao = dao) {
     override fun getAll() = dao.getAll()
-    override suspend fun getAllAsync() = dao.getAllAsync()
     override suspend fun deleteAll() = dao.deleteAll()
 }

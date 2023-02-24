cb87cdc3cb820de0141542392bf769c378ff9ee8
leebeebeom <boole92@naver.com>
2023-02-25 05:04:49 +0900

    모델 소스 분리

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/subcategory/SubCategoryRoomDataSource.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/subcategory/SubCategoryRoomDataSource.kt
@@ -1,14 +1,14 @@
 package com.leebeebeom.clothinghelper.data.datasourse.subcategory
 
 import com.leebeebeom.clothinghelper.data.datasourse.BaseRoomDataSource
-import com.leebeebeom.clothinghelper.domain.model.data.SubCategory
+import com.leebeebeom.clothinghelper.domain.model.RoomSubCategory
+import com.leebeebeom.clothinghelper.domain.model.SubCategory
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class SubCategoryRoomDataSource @Inject constructor(private val dao: SubCategoryDao) :
-    BaseRoomDataSource<SubCategory>(dao = dao) {
+    BaseRoomDataSource<RoomSubCategory, SubCategory>(dao = dao) {
     override fun getAll() = dao.getAll()
-    override suspend fun getAllAsync() = dao.getAllAsync()
     override suspend fun deleteAll() = dao.deleteAll()
 }

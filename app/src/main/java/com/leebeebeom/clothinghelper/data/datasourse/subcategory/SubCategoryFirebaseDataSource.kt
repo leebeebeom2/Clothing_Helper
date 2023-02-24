5451635b192eea8e33c7b598e93371693c7fc01d
leebeebeom <boole92@naver.com>
2023-02-25 05:04:55 +0900

    모델 소스 분리

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/subcategory/SubCategoryFirebaseDataSource.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/subcategory/SubCategoryFirebaseDataSource.kt
@@ -2,10 +2,10 @@ package com.leebeebeom.clothinghelper.data.datasourse.subcategory
 
 import com.leebeebeom.clothinghelper.data.datasourse.BaseFirebaseDataSource
 import com.leebeebeom.clothinghelper.data.repository.DatabasePath
-import com.leebeebeom.clothinghelper.domain.model.data.SubCategory
+import com.leebeebeom.clothinghelper.domain.model.FirebaseSubCategory
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class SubCategoryFirebaseDataSource @Inject constructor() :
-    BaseFirebaseDataSource<SubCategory>(refPath = DatabasePath.SUB_CATEGORIES)
+    BaseFirebaseDataSource<FirebaseSubCategory>(refPath = DatabasePath.SUB_CATEGORIES)

d5a41b4bc0d2cb0bc66c3bce825a87d719182198
leebeebeom <boole92@naver.com>
2023-02-25 04:49:24 +0900

    onConflict 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/BaseDao.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/BaseDao.kt
@@ -2,13 +2,14 @@ package com.leebeebeom.clothinghelper.data.datasourse
 
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Update
 
 interface BaseDao<T> {
     @Insert
     suspend fun insert(data: T)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(data: List<T>)
 
     @Update

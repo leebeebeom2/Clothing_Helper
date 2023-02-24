1a892af5121381c04e044118488494ee286dac8d
leebeebeom <boole92@naver.com>
2023-02-25 02:53:04 +0900

    Flow -> StateFlow 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
@@ -4,10 +4,10 @@ import com.google.firebase.auth.AuthCredential
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
 import com.leebeebeom.clothinghelper.domain.model.data.User
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 interface UserRepository : LoadingStateProvider {
-    val user: Flow<User?>
+    val user: StateFlow<User?>
 
     suspend fun googleSignIn(credential: AuthCredential, firebaseResult: FirebaseResult)
 

e6494ecf23b2be540d38511ce193c846d833db07
leebeebeom <boole92@naver.com>
2023-02-25 05:02:48 +0900

    모델 소스 분리

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/todo/TodoRoomDataSource.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/todo/TodoRoomDataSource.kt
@@ -1,14 +1,14 @@
 package com.leebeebeom.clothinghelper.data.datasourse.todo
 
 import com.leebeebeom.clothinghelper.data.datasourse.BaseRoomDataSource
-import com.leebeebeom.clothinghelper.domain.model.data.Todo
+import com.leebeebeom.clothinghelper.domain.model.RoomTodo
+import com.leebeebeom.clothinghelper.domain.model.Todo
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class TodoRoomDataSource @Inject constructor(private val dao: TodoDao) :
-    BaseRoomDataSource<Todo>(dao = dao) {
+    BaseRoomDataSource<RoomTodo, Todo>(dao = dao) {
     override fun getAll() = dao.getAll()
-    override suspend fun getAllAsync() = dao.getAllAsync()
     override suspend fun deleteAll() = dao.deleteAll()
 }

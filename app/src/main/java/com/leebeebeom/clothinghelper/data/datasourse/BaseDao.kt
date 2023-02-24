438680bf0041ab7e202cfb4020ca264201478f4a
leebeebeom <boole92@naver.com>
2023-02-25 04:49:39 +0900

    모델 소스 분리

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/BaseDao.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/BaseDao.kt
@@ -4,8 +4,9 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Update
+import com.leebeebeom.clothinghelper.domain.model.BaseRoomModel
 
-interface BaseDao<T> {
+interface BaseDao<T : BaseRoomModel> {
     @Insert
     suspend fun insert(data: T)
 

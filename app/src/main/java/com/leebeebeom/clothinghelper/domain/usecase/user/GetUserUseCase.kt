1fe3119e507c140b4cd3eecddf4c61fdb4b7fdd2
leebeebeom <boole92@naver.com>
2023-02-25 05:39:10 +0900

    모델 소스 분리

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetUserUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetUserUseCase.kt
@@ -1,10 +1,29 @@
 package com.leebeebeom.clothinghelper.domain.usecase.user
 
+import com.leebeebeom.clothinghelper.domain.model.User
+import com.leebeebeom.clothinghelper.domain.model.toUser
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
-import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
+import javax.inject.Singleton
 
-@ViewModelScoped
-class GetUserUseCase @Inject constructor(private val userRepository: UserRepository) {
-    val user get() = userRepository.user
+@Singleton
+class GetUserUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val appCoroutineScope: CoroutineScope,
+) {
+    private lateinit var user: StateFlow<User?>
+    fun getUser(): StateFlow<User?> = if (::user.isInitialized) user
+    else {
+        user = userRepository.user.map { it?.toUser() }.stateIn(
+            scope = appCoroutineScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = null
+        )
+        user
+    }
 }

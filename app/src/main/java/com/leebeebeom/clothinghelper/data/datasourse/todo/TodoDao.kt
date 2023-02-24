426a120bc1dc33c291885e750a2523c43eb5c356
leebeebeom <boole92@naver.com>
2023-02-25 05:02:23 +0900

    모델 소스 분리

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/todo/TodoDao.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/todo/TodoDao.kt
@@ -3,11 +3,12 @@ package com.leebeebeom.clothinghelper.data.datasourse.todo
 import androidx.room.Dao
 import androidx.room.Query
 import com.leebeebeom.clothinghelper.data.datasourse.BaseDao
+import com.leebeebeom.clothinghelper.domain.model.RoomTodo
 import com.leebeebeom.clothinghelper.domain.model.Todo
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface TodoDao : BaseDao<Todo> {
+interface TodoDao : BaseDao<RoomTodo> {
     @Query("SELECT * FROM RoomTodo")
     fun getAll(): Flow<List<Todo>>
 

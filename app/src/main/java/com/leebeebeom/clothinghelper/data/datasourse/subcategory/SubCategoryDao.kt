351ee8ec9332cbec6cc6d3f923df75c9592b67ec
leebeebeom <boole92@naver.com>
2023-02-25 05:04:24 +0900

    모델 소스 분리

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/subcategory/SubCategoryDao.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/subcategory/SubCategoryDao.kt
@@ -2,17 +2,15 @@ package com.leebeebeom.clothinghelper.data.datasourse.subcategory
 
 import androidx.room.*
 import com.leebeebeom.clothinghelper.data.datasourse.BaseDao
-import com.leebeebeom.clothinghelper.domain.model.data.SubCategory
+import com.leebeebeom.clothinghelper.domain.model.RoomSubCategory
+import com.leebeebeom.clothinghelper.domain.model.SubCategory
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface SubCategoryDao : BaseDao<SubCategory> {
-    @Query("SELECT * FROM SubCategory")
+interface SubCategoryDao : BaseDao<RoomSubCategory> {
+    @Query("SELECT `key`, name, parent FROM RoomSubCategory")
     fun getAll(): Flow<List<SubCategory>>
 
-    @Query("SELECT * FROM SubCategory WHERE isSynced = 0")
-    suspend fun getAllAsync(): List<SubCategory>
-
-    @Query("DELETE FROM SubCategory")
+    @Query("DELETE FROM RoomSubCategory")
     suspend fun deleteAll()
 }

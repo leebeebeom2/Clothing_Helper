0731c7ea5f61b8d7b1a4c817d70350f1c632ed74
leebeebeom <boole92@naver.com>
2023-02-26 05:26:22 +0900

    USER_INFO DB 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/DatabasePath.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/DatabasePath.kt
@@ -2,7 +2,6 @@ package com.leebeebeom.clothinghelper.data.repository
 
 object DatabasePath {
     const val SUB_CATEGORIES = "sub categories"
-    const val USER_INFO = "user info"
     const val FOLDERS = "folders"
     const val TODOS = "todos"
 }

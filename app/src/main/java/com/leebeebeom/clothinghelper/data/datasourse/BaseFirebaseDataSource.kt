3717d4752b278acea2b7d6b255038bce58c6abc8
leebeebeom <boole92@naver.com>
2023-02-26 04:14:49 +0900

    dispatcher 분리

--- app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/BaseFirebaseDataSource.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/datasourse/BaseFirebaseDataSource.kt
@@ -3,6 +3,7 @@ package com.leebeebeom.clothinghelper.data.datasourse
 import com.leebeebeom.clothinghelper.data.repository.firebaseDbRoot
 import com.leebeebeom.clothinghelper.data.repository.getContainerRef
 import com.leebeebeom.clothinghelper.domain.model.BaseFirebaseModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
@@ -11,7 +12,8 @@ abstract class BaseFirebaseDataSource<T : BaseFirebaseModel>(private val refPath
     suspend fun getAll(
         uid: String,
         type: Class<T>,
-    ): List<T> = withContext(Dispatchers.IO) {
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    ): List<T> = withContext(dispatcher) {
         val temp = mutableListOf<T>()
 
         firebaseDbRoot.getContainerRef(uid = uid, path = refPath).get()
@@ -19,6 +21,8 @@ abstract class BaseFirebaseDataSource<T : BaseFirebaseModel>(private val refPath
         temp
     }
 
-    suspend fun push(uid: String, t: T): Void =
-        firebaseDbRoot.getContainerRef(uid, refPath).child(t.key).setValue(t).await()
+    suspend fun push(uid: String, t: T, dispatcher: CoroutineDispatcher = Dispatchers.IO): Void =
+        withContext(dispatcher) {
+            firebaseDbRoot.getContainerRef(uid, refPath).child(t.key).setValue(t).await()
+        }
 }

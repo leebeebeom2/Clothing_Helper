a86a08913db51209057ad6048b0a50d43ca12367
leebeebeom <boole92@naver.com>
2023-03-01 04:24:46 +0900

    힐트 범위 제거

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetAllFoldersUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetAllFoldersUseCase.kt
@@ -1,7 +1,6 @@
 package com.leebeebeom.clothinghelper.domain.usecase.folder
 
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
-import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
 class GetAllFoldersUseCase @Inject constructor(private val folderRepository: FolderRepository) {

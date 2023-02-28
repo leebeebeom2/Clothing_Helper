8035512e25d1c725ba77ad38d524631f39cbcc31
leebeebeom <boole92@naver.com>
2023-03-01 04:36:29 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/TodoRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/TodoRepositoryImpl.kt
@@ -3,16 +3,13 @@ package com.leebeebeom.clothinghelper.data.repository
 import com.leebeebeom.clothinghelper.data.repository.util.NetworkChecker
 import com.leebeebeom.clothinghelper.domain.model.DatabaseTodo
 import com.leebeebeom.clothinghelper.domain.repository.TodoRepository
-import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class TodoRepositoryImpl @Inject constructor(
-    networkPreferenceRepository: NetworkPreferenceRepository,
     networkChecker: NetworkChecker
 ) : BaseDataRepositoryImpl<DatabaseTodo>(
     refPath = DatabasePath.TODOS,
-    networkPreferences = networkPreferenceRepository,
     networkChecker = networkChecker
 ), TodoRepository

eb65fcadb39414fa835517e29371b6d50ba185ec
leebeebeom <boole92@naver.com>
2023-03-01 04:07:59 +0900

    힐트 범위 제거

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignOutUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignOutUseCase.kt
@@ -1,10 +1,8 @@
 package com.leebeebeom.clothinghelper.domain.usecase.user
 
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
-import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-@ViewModelScoped
 class SignOutUseCase @Inject constructor(private val userRepository: UserRepository) {
     suspend fun signOut(onFail: (Exception) -> Unit) = userRepository.signOut(onFail = onFail)
 }

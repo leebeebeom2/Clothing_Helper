b5f163e31613beb8c7cd272fd17df935070b6305
leebeebeom <boole92@naver.com>
2023-03-01 04:07:09 +0900

    힐트 범위 제거

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/ResetPasswordUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/ResetPasswordUseCase.kt
@@ -1,11 +1,9 @@
 package com.leebeebeom.clothinghelper.domain.usecase.user
 
-import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
+import com.leebeebeom.clothinghelper.data.repository.FirebaseResult
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
-import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-@ViewModelScoped
 class ResetPasswordUseCase @Inject constructor(private val userRepository: UserRepository) {
     suspend fun sendResetPasswordEmail(email: String, firebaseResult: FirebaseResult) =
         userRepository.resetPasswordEmail(email = email, firebaseResult = firebaseResult)

943e1a25b7d5dfcb975071d822c61fbaf79b5216
leebeebeom <boole92@naver.com>
2023-03-01 03:15:47 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/container/FolderRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/container/FolderRepositoryImpl.kt
@@ -1,30 +1,27 @@
 package com.leebeebeom.clothinghelper.data.repository.container
 
-import android.content.Context
-import com.leebeebeom.clothinghelper.data.datasourse.folder.FolderFirebaseDataSource
-import com.leebeebeom.clothinghelper.data.datasourse.folder.FolderRoomDataSource
 import com.leebeebeom.clothinghelper.data.repository.DatabasePath
-import com.leebeebeom.clothinghelper.domain.model.data.Folder
+import com.leebeebeom.clothinghelper.data.repository.util.NetworkChecker
+import com.leebeebeom.clothinghelper.di.AppScope
+import com.leebeebeom.clothinghelper.domain.model.DatabaseFolder
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.FolderPreferencesRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.SortPreferenceRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class FolderRepositoryImpl @Inject constructor(
     @FolderPreferencesRepository folderPreferencesRepository: SortPreferenceRepository,
-    @ApplicationContext context: Context,
-    folderRoomDataSource: FolderRoomDataSource,
-    folderFirebaseDataSource: FolderFirebaseDataSource,
     networkPreferenceRepository: NetworkPreferenceRepository,
-) : BaseContainerRepositoryImpl<Folder>(
+    @AppScope appScope: CoroutineScope,
+    networkChecker: NetworkChecker,
+) : BaseContainerRepositoryImpl<DatabaseFolder>(
     sortFlow = folderPreferencesRepository.sort,
     refPath = DatabasePath.FOLDERS,
-    context = context,
-    baseFirebaseDataSource = folderFirebaseDataSource,
-    baseRoomDataSource = folderRoomDataSource,
-    networkPreferenceRepository = networkPreferenceRepository
+    networkPreferenceRepository = networkPreferenceRepository,
+    appScope = appScope,
+    networkChecker = networkChecker
 ), FolderRepository

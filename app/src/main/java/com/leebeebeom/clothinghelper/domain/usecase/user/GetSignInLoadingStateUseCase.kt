0741bc73d468aee53c99810aa3c75cab1e7953a7
leebeebeom <boole92@naver.com>
2023-03-01 04:04:20 +0900

    힐트 범위 제거

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetSignInLoadingStateUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetSignInLoadingStateUseCase.kt
@@ -4,7 +4,6 @@ import com.leebeebeom.clothinghelper.domain.repository.UserRepository
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-@ViewModelScoped
 class GetSignInLoadingStateUseCase @Inject constructor(private val userRepository: UserRepository) {
     val isLoading get() = userRepository.isLoading
 }

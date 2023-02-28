2e72bba3592bbd2b7c254b1759ebd064e5a52fd4
leebeebeom <boole92@naver.com>
2023-03-01 04:23:30 +0900

    힐트 범위 제거

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetAllSubCategoriesUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetAllSubCategoriesUseCase.kt
@@ -1,10 +1,8 @@
 package com.leebeebeom.clothinghelper.domain.usecase.subcategory
 
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
-import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-@ViewModelScoped
 class GetAllSubCategoriesUseCase @Inject constructor(private val subCategoryRepository: SubCategoryRepository) {
     val allSubCategories get() = subCategoryRepository.allData
 }

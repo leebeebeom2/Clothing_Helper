0f72cce1e8fe0297e74f5d49d83a264fa28c0d4d
leebeebeom <boole92@naver.com>
2023-03-01 03:41:14 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/GetDataLoadingStateUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/GetDataLoadingStateUseCase.kt
@@ -1,29 +1,39 @@
 package com.leebeebeom.clothinghelper.domain.usecase
 
+import com.leebeebeom.clothinghelper.di.AppScope
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelper.domain.repository.TodoRepository
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class GetDataLoadingStateUseCase @Inject constructor(
-    subCategoryRepository: SubCategoryRepository,
-    folderRepository: FolderRepository,
-    todoRepository: TodoRepository
+    private val subCategoryRepository: SubCategoryRepository,
+    private val folderRepository: FolderRepository,
+    private val todoRepository: TodoRepository,
+    @AppScope private val appScope: CoroutineScope,
 ) {
-    var isLoading: Flow<Boolean>
-        private set
+    private lateinit var isLoading: StateFlow<Boolean>
 
-    init {
-        isLoading = combine(
-            flow = subCategoryRepository.isLoading,
-            flow2 = folderRepository.isLoading,
-            flow3 = todoRepository.isLoading
-        ) { isSubCategoryLoading, isFolderLoading, isTodoLoading ->
-            isSubCategoryLoading && isFolderLoading && isTodoLoading
-        }
+    fun getIsLoading(): StateFlow<Boolean> {
+        if (!::isLoading.isInitialized)
+            isLoading = combine(
+                flow = subCategoryRepository.isLoading,
+                flow2 = folderRepository.isLoading,
+                flow3 = todoRepository.isLoading
+            ) { isSubCategoryLoading, isFolderLoading, isTodoLoading ->
+                isSubCategoryLoading && isFolderLoading && isTodoLoading
+            }.stateIn(
+                scope = appScope,
+                started = SharingStarted.WhileSubscribed(),
+                initialValue = true
+            )
+        return isLoading
     }
 }

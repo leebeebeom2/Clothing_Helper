53410a795a3445ddafbd118999126c085300fab2
leebeebeom <boole92@naver.com>
2023-03-01 04:07:39 +0900

    힐트 범위 제거

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignInUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignInUseCase.kt
@@ -1,11 +1,9 @@
 package com.leebeebeom.clothinghelper.domain.usecase.user
 
-import com.leebeebeom.clothinghelper.domain.model.FirebaseResult
+import com.leebeebeom.clothinghelper.data.repository.FirebaseResult
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
-import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-@ViewModelScoped
 class SignInUseCase @Inject constructor(private val userRepository: UserRepository) {
     suspend fun signIn(email: String, password: String, firebaseResult: FirebaseResult) =
         userRepository.signIn(email = email, password = password, firebaseResult = firebaseResult)

7053b5c178b3c3d51bd6a5475e6e90d19169347e
leebeebeom <boole92@naver.com>
2023-03-01 04:11:58 +0900

    힐트 범위 수정, 로직 수정

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/AddSubCategoryUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/AddSubCategoryUseCase.kt
@@ -1,12 +1,11 @@
 package com.leebeebeom.clothinghelper.domain.usecase.subcategory
 
-import com.leebeebeom.clothinghelper.domain.model.data.SubCategory
+import com.leebeebeom.clothinghelper.domain.model.SubCategory
+import com.leebeebeom.clothinghelper.domain.model.toDatabaseModel
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelper.ui.main.drawer.MainCategoryType
-import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-@ViewModelScoped
 class AddSubCategoryUseCase @Inject constructor(private val subCategoryRepository: SubCategoryRepository) {
     suspend fun add(
         name: String,
@@ -14,15 +13,8 @@ class AddSubCategoryUseCase @Inject constructor(private val subCategoryRepositor
         uid: String,
         onFail: (Exception) -> Unit,
     ) {
-        val currentTime = System.currentTimeMillis()
+        val subCategory = SubCategory(name = name, parent = parent)
 
-        val subCategory = SubCategory(
-            name = name,
-            parent = parent,
-            createDate = currentTime,
-            editDate = currentTime
-        )
-
-        subCategoryRepository.add(data = subCategory, uid = uid, onFail = onFail)
+        subCategoryRepository.add(data = subCategory.toDatabaseModel(), uid = uid, onFail = onFail)
     }
 }

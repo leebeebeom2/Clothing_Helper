1e0756fc539982701dbd7c616870f2df5ca92d98
leebeebeom <boole92@naver.com>
2023-03-01 03:59:40 +0900

    네트워크 체크 로직 추가,  onFail 로직 수정

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/LoadDataUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/LoadDataUseCase.kt
@@ -1,37 +1,66 @@
 package com.leebeebeom.clothinghelper.domain.usecase
 
-import com.leebeebeom.clothinghelper.domain.model.data.Folder
-import com.leebeebeom.clothinghelper.domain.model.data.SubCategory
+import com.leebeebeom.clothinghelper.data.repository.util.NetworkChecker
+import com.leebeebeom.clothinghelper.data.repository.util.logE
+import com.leebeebeom.clothinghelper.domain.model.DatabaseFolder
+import com.leebeebeom.clothinghelper.domain.model.DatabaseSubCategory
+import com.leebeebeom.clothinghelper.domain.model.DatabaseTodo
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
-import com.leebeebeom.clothinghelper.domain.usecase.user.GetUserUseCase
+import com.leebeebeom.clothinghelper.domain.repository.TodoRepository
+import com.leebeebeom.clothinghelper.domain.repository.UserRepository
 import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.collectLatest
 import javax.inject.Inject
 
 @ViewModelScoped
 class LoadDataUseCase @Inject constructor(
-    private val getUserUseCase: GetUserUseCase,
+    private val userRepository: UserRepository,
     private val subCategoryRepository: SubCategoryRepository,
     private val folderRepository: FolderRepository,
+    private val todoRepository: TodoRepository,
+    private val networkChecker: NetworkChecker,
 ) {
-    /**
-     * 유저 상태 변경(로그인, 로그아웃) 시 모든 데이터 다시 로드
-     */
-    suspend fun load(
-        subCategoryLoadFail: (Exception) -> Unit,
-        folderLoadFail: (Exception) -> Unit,
-    ) = getUserUseCase.user.collectLatest {// TODO 속도 테스트
-        subCategoryRepository.load(
-            uid = it?.uid,
-            type = SubCategory::class.java,
-            onFail = subCategoryLoadFail
-        )
+    suspend fun load(onFail: (Exception) -> Unit) {
+        // 인터넷 확인 여기서, 실패 여기서
+        userRepository.firebaseUser.collectLatest {
+            try {
+                networkChecker.checkNetWork()
 
-        folderRepository.load(
-            uid = it?.uid,
-            type = Folder::class.java,
-            onFail = folderLoadFail
-        )
+                coroutineScope {
+
+                    val deferreds = listOf(
+                        async {
+                            subCategoryRepository.load(
+                                uid = it?.uid,
+                                type = DatabaseSubCategory::class.java,
+                                onFail = { throw it }
+                            )
+                        },
+                        async {
+                            folderRepository.load(
+                                uid = it?.uid,
+                                type = DatabaseFolder::class.java,
+                                onFail = { throw it }
+                            )
+                        },
+                        async {
+                            todoRepository.load(
+                                uid = it?.uid,
+                                type = DatabaseTodo::class.java,
+                                onFail = { throw it }
+                            )
+                        }
+                    )
+                    deferreds.awaitAll()
+                }
+            } catch (e: Exception) {
+                onFail(e)
+                logE("LoadDataUseCase:", e)
+            }
+        }
     }
 }

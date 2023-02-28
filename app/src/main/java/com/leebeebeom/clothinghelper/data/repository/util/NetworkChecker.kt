69db4265d8b855691cd9b77af31ecf6679792627
leebeebeom <boole92@naver.com>
2023-03-01 03:55:27 +0900

    checkNetWork 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/NetworkChecker.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/NetworkChecker.kt
@@ -3,17 +3,31 @@ package com.leebeebeom.clothinghelper.data.repository.util
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import com.google.firebase.FirebaseNetworkException
+import com.leebeebeom.clothinghelper.data.repository.WifiException
+import com.leebeebeom.clothinghelper.data.repository.preference.NetworkPreferences
+import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class NetworkChecker @Inject constructor(@ApplicationContext context: Context) {
+class NetworkChecker @Inject constructor(
+    @ApplicationContext context: Context,
+    private val networkPreferenceRepository: NetworkPreferenceRepository,
+) {
     private val connectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
     private val networkCapabilities = connectivityManager.activeNetwork
     private val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities)
+
+    private suspend fun checkNetWork() {
+        if (!networkCheck()) throw FirebaseNetworkException("인터넷 미연결")
+        // 와이파이 선택 시 와이파이에 연결되지 않은 경우
+        if (networkPreferenceRepository.network.first() == NetworkPreferences.WIFI && !isWifiConnected()) throw WifiException
+    }
 
     fun isWifiConnected() =
         actNw?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ?: false

39a3b10dd5ef59bf0c04c6aec96fc91115add829
leebeebeom <boole92@naver.com>
2023-03-01 03:11:59 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -1,9 +1,10 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
+import com.leebeebeom.clothinghelper.domain.model.BaseDatabaseModel
 import kotlinx.coroutines.flow.StateFlow
 
-interface BaseDataRepository<T> : LoadingStateProvider {
+interface BaseDataRepository<T : BaseDatabaseModel> : LoadingStateProvider {
     val allData: StateFlow<List<T>>
     suspend fun load(uid: String?, type: Class<T>, onFail: (Exception) -> Unit)
     suspend fun add(data: T, uid: String, onFail: (Exception) -> Unit)

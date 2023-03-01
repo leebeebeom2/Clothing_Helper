ccebf4ad817bb2d3c06c9265bdf966e8b1966c24
leebeebeom <boole92@naver.com>
2023-03-02 05:32:09 +0900

    리팩토링(테스터블)

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/LoadDataUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/LoadDataUseCase.kt
@@ -1,6 +1,8 @@
 package com.leebeebeom.clothinghelper.domain.usecase
 
+import com.google.firebase.FirebaseNetworkException
 import com.leebeebeom.clothinghelper.data.repository.util.NetworkChecker
+import com.leebeebeom.clothinghelper.data.repository.util.WifiException
 import com.leebeebeom.clothinghelper.data.repository.util.logE
 import com.leebeebeom.clothinghelper.domain.model.DatabaseFolder
 import com.leebeebeom.clothinghelper.domain.model.DatabaseSubCategory
@@ -8,56 +10,59 @@ import com.leebeebeom.clothinghelper.domain.model.DatabaseTodo
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelper.domain.repository.TodoRepository
-import com.leebeebeom.clothinghelper.domain.repository.UserRepository
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 class LoadDataUseCase @Inject constructor(
-    private val userRepository: UserRepository,
     private val subCategoryRepository: SubCategoryRepository,
     private val folderRepository: FolderRepository,
     private val todoRepository: TodoRepository,
     private val networkChecker: NetworkChecker,
 ) {
-    suspend fun load(onFail: (Exception) -> Unit) {
-        userRepository.firebaseUser.collectLatest {
-            try {
+    /**
+     * @throws FirebaseNetworkException 인터넷 미 연결 시
+     * @throws WifiException 사용자가 와이파이로만 연결 선택 시 와이파이 미 연결됐을 경우
+     */
+    suspend fun load(
+        uid: String?,
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        onFail: (Exception) -> Unit,
+    ) {
+        try {
+            coroutineScope {
                 networkChecker.checkNetWork()
 
-                coroutineScope {
-
-                    val deferred = listOf(
-                        async {
-                            subCategoryRepository.load(
-                                uid = it?.uid,
-                                type = DatabaseSubCategory::class.java,
-                                onFail = { throw it }
-                            )
-                        },
-                        async {
-                            folderRepository.load(
-                                uid = it?.uid,
-                                type = DatabaseFolder::class.java,
-                                onFail = { throw it }
-                            )
-                        },
-                        async {
-                            todoRepository.load(
-                                uid = it?.uid,
-                                type = DatabaseTodo::class.java,
-                                onFail = { throw it }
-                            )
-                        }
-                    )
-                    deferred.awaitAll()
-                }
-            } catch (e: Exception) {
-                onFail(e)
-                logE("LoadDataUseCase:", e)
+                val deferred = listOf(
+                    async {
+                        subCategoryRepository.load(
+                            dispatcher = dispatcher,
+                            uid = uid,
+                            type = DatabaseSubCategory::class.java,
+                            onFail = { throw it }
+                        )
+                    },
+                    async {
+                        folderRepository.load(
+                            dispatcher = dispatcher,
+                            uid = uid,
+                            type = DatabaseFolder::class.java,
+                            onFail = { throw it }
+                        )
+                    },
+                    async {
+                        todoRepository.load(
+                            dispatcher = dispatcher,
+                            uid = uid,
+                            type = DatabaseTodo::class.java,
+                            onFail = { throw it }
+                        )
+                    }
+                )
+                deferred.awaitAll()
             }
+        } catch (e: Exception) {
+            onFail(e)
+            logE("LoadDataUseCase:", e)
         }
     }
 }

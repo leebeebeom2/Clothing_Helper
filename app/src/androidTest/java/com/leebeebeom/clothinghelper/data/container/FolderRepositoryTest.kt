bfd69add3db26408ebfc00db14b8eee349c6262d
leebeebeom <boole92@naver.com>
2023-03-02 03:16:46 +0900

    RepositoryProvider 생성

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/data/container/FolderRepositoryTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/container/FolderRepositoryTest.kt
@@ -2,28 +2,17 @@ package com.leebeebeom.clothinghelper.data.container
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import com.leebeebeom.clothinghelper.data.repository.container.FolderRepositoryImpl
-import com.leebeebeom.clothinghelper.data.repository.preference.FolderPreferencesRepositoryImpl
-import com.leebeebeom.clothinghelper.data.repository.preference.NetworkPreferenceRepositoryImpl
-import com.leebeebeom.clothinghelper.data.repository.util.NetworkChecker
+import com.leebeebeom.clothinghelper.RepositoryProvider
 import com.leebeebeom.clothinghelper.data.repositoryCrudTest
 import com.leebeebeom.clothinghelper.data.repositorySignOutTest
 import com.leebeebeom.clothinghelper.domain.model.DatabaseFolder
 import com.leebeebeom.clothinghelper.domain.model.Folder
 import com.leebeebeom.clothinghelper.domain.model.toDatabaseModel
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
-import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
-import com.leebeebeom.clothinghelper.domain.repository.preference.SortPreferenceRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import org.junit.Before
 import org.junit.Test
 
 class FolderRepositoryTest {
-    private lateinit var networkPreferenceRepository: NetworkPreferenceRepository
-    private lateinit var networkChecker: NetworkChecker
-    private lateinit var folderPreferenceRepository: SortPreferenceRepository
     private lateinit var folderRepository: FolderRepository
 
     private val uid = "folder test"
@@ -31,24 +20,14 @@ class FolderRepositoryTest {
     @Before
     fun init() {
         val context = ApplicationProvider.getApplicationContext<Context>()
-        networkPreferenceRepository = NetworkPreferenceRepositoryImpl(context = context)
-        networkChecker = NetworkChecker(
-            context = context, networkPreferenceRepository = networkPreferenceRepository
-        )
-        folderPreferenceRepository = FolderPreferencesRepositoryImpl(context = context)
-        folderRepository = FolderRepositoryImpl(
-            folderPreferencesRepository = folderPreferenceRepository,
-            appScope = CoroutineScope(SupervisorJob() + Dispatchers.Default),
-            networkChecker = networkChecker
-        )
+        folderRepository = RepositoryProvider.getFolderRepository(context)
     }
 
     @Test
     fun crudTest() {
         val folder = Folder("folder")
 
-        repositoryCrudTest(
-            data = folder.toDatabaseModel(),
+        repositoryCrudTest(data = folder.toDatabaseModel(),
             repository = folderRepository,
             uid = uid,
             type = DatabaseFolder::class.java,
@@ -66,8 +45,7 @@ class FolderRepositoryTest {
                 assert(origin.createDate == new.createDate)
                 assert(origin.editDate != new.editDate)
                 assert(origin.editDate < new.editDate)
-            }
-        )
+            })
     }
 
     @Test
--- app/src/androidTest/java/com/leebeebeom/clothinghelper/data/container/SubCategoryRepositoryTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/container/SubCategoryRepositoryTest.kt
@@ -2,26 +2,17 @@ package com.leebeebeom.clothinghelper.data.container
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import com.leebeebeom.clothinghelper.data.repository.container.SubCategoryRepositoryImpl
-import com.leebeebeom.clothinghelper.data.repository.preference.NetworkPreferenceRepositoryImpl
-import com.leebeebeom.clothinghelper.data.repository.preference.SubCategoryPreferencesRepositoryImpl
-import com.leebeebeom.clothinghelper.data.repository.util.NetworkChecker
+import com.leebeebeom.clothinghelper.RepositoryProvider
 import com.leebeebeom.clothinghelper.data.repositoryCrudTest
 import com.leebeebeom.clothinghelper.data.repositorySignOutTest
-import com.leebeebeom.clothinghelper.domain.model.*
+import com.leebeebeom.clothinghelper.domain.model.DatabaseSubCategory
+import com.leebeebeom.clothinghelper.domain.model.SubCategory
+import com.leebeebeom.clothinghelper.domain.model.toDatabaseModel
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
-import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
-import com.leebeebeom.clothinghelper.domain.repository.preference.SortPreferenceRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import org.junit.Before
 import org.junit.Test
 
 class SubCategoryRepositoryTest {
-    private lateinit var networkPreferenceRepository: NetworkPreferenceRepository
-    private lateinit var networkChecker: NetworkChecker
-    private lateinit var subCategoryPreferencesRepository: SortPreferenceRepository
     private lateinit var subCategoryRepository: SubCategoryRepository
 
     private val uid = "subcategory test"
@@ -29,16 +20,7 @@ class SubCategoryRepositoryTest {
     @Before
     fun init() {
         val context = ApplicationProvider.getApplicationContext<Context>()
-        networkPreferenceRepository = NetworkPreferenceRepositoryImpl(context = context)
-        networkChecker = NetworkChecker(
-            context = context, networkPreferenceRepository = networkPreferenceRepository
-        )
-        subCategoryPreferencesRepository = SubCategoryPreferencesRepositoryImpl(context = context)
-        subCategoryRepository = SubCategoryRepositoryImpl(
-            subCategoryPreferencesRepository = subCategoryPreferencesRepository,
-            appScope = CoroutineScope(SupervisorJob() + Dispatchers.Default),
-            networkChecker = networkChecker
-        )
+        subCategoryRepository = RepositoryProvider.getSubCategoryRepository(context = context)
     }
 
 

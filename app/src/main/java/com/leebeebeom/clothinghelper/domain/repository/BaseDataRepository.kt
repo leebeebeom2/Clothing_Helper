56d27cb38f5b8e20d58f80a06f0d7ce3d850009b
leebeebeom <boole92@naver.com>
2023-03-02 00:34:30 +0900

    dispatcher 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -2,11 +2,30 @@ package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.BaseDatabaseModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.StateFlow
 
 interface BaseDataRepository<T : BaseDatabaseModel> : LoadingStateProvider {
     val allData: StateFlow<List<T>>
-    suspend fun load(uid: String?, type: Class<T>, onFail: (Exception) -> Unit)
-    suspend fun add(data: T, uid: String, onFail: (Exception) -> Unit)
-    suspend fun edit(newData: T, uid: String, onFail: (Exception) -> Unit)
+    suspend fun load(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        uid: String?,
+        type: Class<T>,
+        onFail: (Exception) -> Unit,
+    )
+
+    suspend fun add(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        data: T,
+        uid: String,
+        onFail: (Exception) -> Unit,
+    )
+
+    suspend fun edit(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        newData: T,
+        uid: String,
+        onFail: (Exception) -> Unit,
+    )
 }

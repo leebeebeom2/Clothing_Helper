a743af3b47eb9c4004680b2919cf4bb87face83e
leebeebeom <boole92@naver.com>
2023-03-02 00:07:50 +0900

    예외 주석 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
@@ -1,6 +1,9 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuthException
 import com.leebeebeom.clothinghelper.data.repository.FirebaseResult
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.FirebaseUser
@@ -17,6 +20,11 @@ interface UserRepository : LoadingStateProvider {
         dispatcher: CoroutineDispatcher = Dispatchers.IO,
     )
 
+    /**
+     * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
+     * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
+     * @throws FirebaseAuthException - InvalidEmail, NotFoundUser, WrongPassword 등
+     */
     suspend fun signIn(
         email: String,
         password: String,
@@ -24,6 +32,11 @@ interface UserRepository : LoadingStateProvider {
         dispatcher: CoroutineDispatcher = Dispatchers.IO,
     )
 
+    /**
+     * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
+     * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
+     * @throws FirebaseAuthException - InvalidEmail, EmailAlreadyInUse 등
+     */
     suspend fun signUp(
         email: String,
         password: String,
@@ -32,7 +45,12 @@ interface UserRepository : LoadingStateProvider {
         dispatcher: CoroutineDispatcher = Dispatchers.IO,
     )
 
-    suspend fun resetPasswordEmail(
+    /**
+     * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
+     * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
+     * @throws FirebaseAuthException - InvalidEmail, NotFoundUser 등
+     */
+    suspend fun sendResetPasswordEmail(
         email: String, firebaseResult: FirebaseResult,
         dispatcher: CoroutineDispatcher = Dispatchers.IO,
     )

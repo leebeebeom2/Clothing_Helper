b45da83317a7321723d1b6fc29cefd15299d3deb
leebeebeom <boole92@naver.com>
2023-03-01 23:31:44 +0900

    예외 주석 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/container/BaseContainerRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/container/BaseContainerRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package com.leebeebeom.clothinghelper.data.repository.container
 
+import com.google.firebase.FirebaseNetworkException
 import com.leebeebeom.clothinghelper.data.repository.BaseDataRepositoryImpl
 import com.leebeebeom.clothinghelper.data.repository.preference.Order.ASCENDING
 import com.leebeebeom.clothinghelper.data.repository.preference.Order.DESCENDING
 import com.leebeebeom.clothinghelper.data.repository.preference.Sort.*
 import com.leebeebeom.clothinghelper.data.repository.preference.SortPreferences
 import com.leebeebeom.clothinghelper.data.repository.util.NetworkChecker
+import com.leebeebeom.clothinghelper.data.repository.util.WifiException
 import com.leebeebeom.clothinghelper.domain.model.BaseDatabaseContainerModel
 import com.leebeebeom.clothinghelper.domain.repository.BaseContainerRepository
 import kotlinx.coroutines.CoroutineScope
@@ -56,6 +58,10 @@ abstract class BaseContainerRepositoryImpl<T : BaseDatabaseContainerModel>(
         }
     }
 
+    /**
+     * @throws FirebaseNetworkException 인터넷 미 연결 시
+     * @throws WifiException 사용자가 와이파이로만 연결 선택 시 와이파이 미 연결됐을 경우
+     */
     override suspend fun add(data: T, uid: String, onFail: (Exception) -> Unit) {
         val dataWithCreateDate = data.addCreateData()
         val dataWithEditDate = dataWithCreateDate.addEditDate()
@@ -64,8 +70,12 @@ abstract class BaseContainerRepositoryImpl<T : BaseDatabaseContainerModel>(
         super.add(data = dataWithKey, uid = uid, onFail = onFail)
     }
 
-    // TODO NoSuchElementException 처리
-    // TODO 주석에 Throw 추가
+    /**
+     * @throws FirebaseNetworkException 인터넷 미 연결 시
+     * @throws WifiException 사용자가 와이파이로만 연결 선택 시 와이파이 미 연결됐을 경우
+     * @throws NoSuchElementException 본래 데이터를 찾지 못했을 경우
+     * @throws IllegalArgumentException 본래 데이터를 삭제하지 못했을 경우
+     */
     override suspend fun edit(newData: T, uid: String, onFail: (Exception) -> Unit) {
         val value = allData.value
         val oldData = value.first { it.key == newData.key }

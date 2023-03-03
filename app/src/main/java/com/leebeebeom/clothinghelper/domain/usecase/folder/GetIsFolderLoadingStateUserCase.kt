ad8efcbdadeac4ae7bf678b2cb5200baef760d89
leebeebeom <boole92@naver.com>
2023-03-04 03:01:44 +0900

    Flow -> StateFlow로 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetIsFolderLoadingStateUserCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetIsFolderLoadingStateUserCase.kt
@@ -1,9 +1,12 @@
 package com.leebeebeom.clothinghelper.domain.usecase.folder
 
+import com.leebeebeom.clothinghelper.di.AppScope
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import com.leebeebeom.clothinghelper.domain.usecase.BaseGetIsDataLoadingStateUseCase
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
 class GetIsFolderLoadingStateUserCase @Inject constructor(
     folderRepository: FolderRepository,
-) : BaseGetIsDataLoadingStateUseCase(repository = folderRepository)
+    @AppScope appScope: CoroutineScope,
+) : BaseGetIsDataLoadingStateUseCase(repository = folderRepository, appScope = appScope)
--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetIsSubCategoryLoadingStateUserCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetIsSubCategoryLoadingStateUserCase.kt
@@ -1,9 +1,12 @@
 package com.leebeebeom.clothinghelper.domain.usecase.subcategory
 
+import com.leebeebeom.clothinghelper.di.AppScope
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelper.domain.usecase.BaseGetIsDataLoadingStateUseCase
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
 class GetIsSubCategoryLoadingStateUserCase @Inject constructor(
     subCategoryRepository: SubCategoryRepository,
-) : BaseGetIsDataLoadingStateUseCase(repository = subCategoryRepository)
+    @AppScope appScope: CoroutineScope
+) : BaseGetIsDataLoadingStateUseCase(repository = subCategoryRepository, appScope = appScope)

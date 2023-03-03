78084dc339d4f5a4c5cb73b91c0c2cde5eedc970
leebeebeom <boole92@naver.com>
2023-03-04 03:02:44 +0900

    생성

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/BaseGetAllDataUseCase.kt
@@ -0,0 +1,29 @@
+package com.leebeebeom.clothinghelper.domain.usecase
+
+import com.leebeebeom.clothinghelper.domain.model.BaseModel
+import com.leebeebeom.clothinghelper.domain.repository.BaseDataRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+
+abstract class BaseGetAllDataUseCase<T : BaseModel>(
+    private val repository: BaseDataRepository<T>,
+    private val appScope: CoroutineScope,
+) {
+    private lateinit var allDataFlow: StateFlow<List<T>>
+
+    suspend fun getAllData(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        uid: String,
+        onFail: (Exception) -> Unit,
+    ): StateFlow<List<T>> {
+        if (::allDataFlow.isInitialized) return allDataFlow
+
+        allDataFlow = repository.getAllData(dispatcher = dispatcher, uid = uid, onFail = onFail)
+            .stateIn(appScope, SharingStarted.WhileSubscribed(5000), emptyList())
+        return allDataFlow
+    }
+}

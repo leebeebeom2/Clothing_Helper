c3407d9d8dc5224fe26ba18c6387dc979ef51283
leebeebeom <boole92@naver.com>
2023-03-04 03:07:55 +0900

    stateIn 로직 수정

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetUserUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetUserUseCase.kt
@@ -2,12 +2,10 @@ package com.leebeebeom.clothinghelper.domain.usecase.user
 
 import com.leebeebeom.clothinghelper.di.AppScope
 import com.leebeebeom.clothinghelper.domain.model.User
-import com.leebeebeom.clothinghelper.domain.model.toUser
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -19,12 +17,10 @@ class GetUserUseCase @Inject constructor(
 ) {
     private lateinit var user: StateFlow<User?>
     fun getUser(): StateFlow<User?> {
-        if (!::user.isInitialized)
-            user = userRepository.firebaseUser.map { it?.toUser() }.stateIn(
-                scope = appScope,
-                started = SharingStarted.WhileSubscribed(),
-                initialValue = null
-            )
+        if (::user.isInitialized) return user
+        user = userRepository.user.stateIn(
+            scope = appScope, started = SharingStarted.WhileSubscribed(5000), initialValue = null
+        )
         return user
     }
 }

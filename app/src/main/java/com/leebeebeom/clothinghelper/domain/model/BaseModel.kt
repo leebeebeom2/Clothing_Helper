f4454772ea0c3776c6770b90f8936c16f791a3bd
leebeebeom <boole92@naver.com>
2023-03-04 01:01:41 +0900

    DatabaseModel 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/model/BaseModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/model/BaseModel.kt
@@ -4,33 +4,19 @@ import com.leebeebeom.clothinghelper.ui.main.drawer.MainCategoryType
 
 abstract class BaseModel {
     abstract val key: String
-}
 
-abstract class BaseDatabaseModel : BaseModel() {
     abstract fun addKey(key: String): BaseModel
 }
 
 abstract class BaseContainerModel : BaseModel() {
     abstract val name: String
-}
-
-abstract class BaseDatabaseContainerModel : BaseDatabaseModel() {
-    abstract val name: String
-
     abstract val createDate: Long
     abstract val editDate: Long
 
-    abstract fun addCreateData(date: Long = System.currentTimeMillis()): BaseDatabaseContainerModel
-    abstract fun addEditDate(): BaseDatabaseContainerModel
+    abstract fun changeEditDate(editDate: Long): BaseContainerModel
 }
 
 abstract class BaseFolderModel : BaseContainerModel() {
-    abstract val parentKey: String
-    abstract val subCategoryKey: String
-    abstract val mainCategoryType: MainCategoryType
-}
-
-abstract class BaseDatabaseFolderModel : BaseDatabaseContainerModel() {
     abstract val parentKey: String
     abstract val subCategoryKey: String
     abstract val mainCategoryType: MainCategoryType

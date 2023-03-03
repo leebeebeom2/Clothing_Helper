1581ffcc8747615603d2004a4ac4cee9eaaa0c94
leebeebeom <boole92@naver.com>
2023-03-04 03:57:53 +0900

    테스트 로직 수정

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/data/repository/preference/SortFlowTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/repository/preference/SortFlowTest.kt
@@ -6,25 +6,25 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 
 @OptIn(ExperimentalCoroutinesApi::class)
-fun preferenceSortFlowTest(repository: SortPreferenceRepository) =
-    runTest {
-        suspend fun assert(sort: SortPreferences) =
-            assert(repository.sort.first() == sort)
+fun preferenceSortFlowTest(repository: SortPreferenceRepository) = runTest {
+    val sortFlow = repository.sort
+    suspend fun assert(sort: SortPreferences) =
+        assert(sortFlow.first() == sort)
 
-        assert(SortPreferences(sort = Sort.NAME, order = Order.ASCENDING))
+    assert(sort = SortPreferences(sort = Sort.NAME, order = Order.ASCENDING))
 
-        repository.changeSort(sort = Sort.EDIT)
-        assert(SortPreferences(sort = Sort.EDIT, order = Order.ASCENDING))
+    repository.changeSort(sort = Sort.EDIT)
+    assert(sort = SortPreferences(sort = Sort.EDIT, order = Order.ASCENDING))
 
-        repository.changeSort(sort = Sort.CREATE)
-        assert(SortPreferences(sort = Sort.CREATE, order = Order.ASCENDING))
+    repository.changeSort(sort = Sort.CREATE)
+    assert(sort = SortPreferences(sort = Sort.CREATE, order = Order.ASCENDING))
 
-        repository.changeSort(sort = Sort.NAME)
-        assert(SortPreferences(sort = Sort.NAME, order = Order.ASCENDING))
+    repository.changeSort(sort = Sort.NAME)
+    assert(sort = SortPreferences(sort = Sort.NAME, order = Order.ASCENDING))
 
-        repository.changeOrder(Order.DESCENDING)
-        assert(SortPreferences(sort = Sort.NAME, order = Order.DESCENDING))
+    repository.changeOrder(Order.DESCENDING)
+    assert(sort = SortPreferences(sort = Sort.NAME, order = Order.DESCENDING))
 
-        repository.changeOrder(Order.ASCENDING)
-        assert(SortPreferences(sort = Sort.NAME, order = Order.ASCENDING))
-    }
+    repository.changeOrder(Order.ASCENDING)
+    assert(sort = SortPreferences(sort = Sort.NAME, order = Order.ASCENDING))
+}

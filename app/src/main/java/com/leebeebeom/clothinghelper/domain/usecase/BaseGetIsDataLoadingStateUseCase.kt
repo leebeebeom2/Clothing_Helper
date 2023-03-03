c12828161a12e62445fbee858af29f410be54a29
leebeebeom <boole92@naver.com>
2023-03-04 03:02:18 +0900

    Flow -> StateFlow로 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/BaseGetIsDataLoadingStateUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/BaseGetIsDataLoadingStateUseCase.kt
@@ -1,9 +1,24 @@
 package com.leebeebeom.clothinghelper.domain.usecase
 
 import com.leebeebeom.clothinghelper.domain.repository.BaseDataRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
 abstract class BaseGetIsDataLoadingStateUseCase(
     private val repository: BaseDataRepository<*>,
+    private val appScope: CoroutineScope,
 ) {
-    val isLoading get() = repository.isLoading
+    private lateinit var isLoading: StateFlow<Boolean>
+    fun getLoadingFlow(): StateFlow<Boolean> {
+        if (::isLoading.isInitialized) return isLoading
+
+        isLoading = repository.isLoading.stateIn(
+            scope = appScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = true
+        )
+        return isLoading
+    }
 }

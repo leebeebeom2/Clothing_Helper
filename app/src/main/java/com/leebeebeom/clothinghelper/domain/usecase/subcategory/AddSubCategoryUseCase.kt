9e7202eb68b1b760907b198fcd70d0280b953668
leebeebeom <boole92@naver.com>
2023-03-04 03:20:01 +0900

    dispatcher 하드코딩 제거, 스레드 세이프 작업

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/AddSubCategoryUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/AddSubCategoryUseCase.kt
@@ -1,20 +1,23 @@
 package com.leebeebeom.clothinghelper.domain.usecase.subcategory
 
 import com.leebeebeom.clothinghelper.domain.model.SubCategory
-import com.leebeebeom.clothinghelper.domain.model.toDatabaseModel
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelper.ui.main.drawer.MainCategoryType
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AddSubCategoryUseCase @Inject constructor(private val subCategoryRepository: SubCategoryRepository) {
     suspend fun add(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
         name: String,
-        parent: MainCategoryType,
+        mainCategoryType: MainCategoryType,
         uid: String,
         onFail: (Exception) -> Unit,
-    ) {
-        val subCategory = SubCategory(name = name, parent = parent)
+    ) = withContext(dispatcher) {
+        val subCategory = SubCategory(name = name, mainCategoryType = mainCategoryType)
 
-        subCategoryRepository.add(data = subCategory.toDatabaseModel(), uid = uid, onFail = onFail)
+        subCategoryRepository.add(data = subCategory, uid = uid, onFail = onFail, dispatcher = dispatcher)
     }
 }
--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/EditSubCategoryUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/EditSubCategoryUseCase.kt
@@ -1,12 +1,14 @@
 package com.leebeebeom.clothinghelper.domain.usecase.subcategory
 
 import com.leebeebeom.clothinghelper.domain.model.SubCategory
-import com.leebeebeom.clothinghelper.domain.model.toDatabaseModel
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 class EditSubCategoryUseCase @Inject constructor(private val subCategoryRepository: SubCategoryRepository) {
     suspend fun nameEdit(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
         oldSubCategory: SubCategory,
         name: String,
         uid: String,
@@ -14,6 +16,6 @@ class EditSubCategoryUseCase @Inject constructor(private val subCategoryReposito
     ) {
         val newSubCategory = oldSubCategory.copy(name = name)
 
-        subCategoryRepository.edit(newData = newSubCategory.toDatabaseModel(), uid = uid, onFail = onFail)
+        subCategoryRepository.edit(newData = newSubCategory, uid = uid, onFail = onFail, dispatcher = dispatcher)
     }
 }

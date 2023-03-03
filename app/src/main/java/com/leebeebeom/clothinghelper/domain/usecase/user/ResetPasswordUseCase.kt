dd624e3ab45c9ea4fcf5441f8937ab0de42a11b7
leebeebeom <boole92@naver.com>
2023-03-04 03:09:11 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/ResetPasswordUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/ResetPasswordUseCase.kt
@@ -6,5 +6,5 @@ import javax.inject.Inject
 
 class ResetPasswordUseCase @Inject constructor(private val userRepository: UserRepository) {
     suspend fun sendResetPasswordEmail(email: String, firebaseResult: FirebaseResult) =
-        userRepository.resetPasswordEmail(email = email, firebaseResult = firebaseResult)
+        userRepository.sendResetPasswordEmail(email = email, firebaseResult = firebaseResult)
 }

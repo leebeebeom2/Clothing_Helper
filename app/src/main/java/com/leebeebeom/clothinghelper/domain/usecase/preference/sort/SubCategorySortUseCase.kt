74bfa29a308a0ad79db036451a4f7705f8ab5bb3
leebeebeom <boole92@naver.com>
2023-03-04 05:44:35 +0900

    스코프 범위 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/preference/sort/SubCategorySortUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/preference/sort/SubCategorySortUseCase.kt
@@ -2,7 +2,6 @@ package com.leebeebeom.clothinghelper.domain.usecase.preference.sort
 
 import com.leebeebeom.clothinghelper.domain.repository.preference.SortPreferenceRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.SubCategoryPreferencesRepository
-import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 import javax.inject.Singleton
 

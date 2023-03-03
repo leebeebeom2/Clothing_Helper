2b0140d56be1603f854df6d13d31656fb21fb35d
leebeebeom <boole92@naver.com>
2023-03-04 05:40:35 +0900

    singOut 수정

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignOutUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignOutUseCase.kt
@@ -1,13 +1,8 @@
 package com.leebeebeom.clothinghelper.domain.usecase.user
 
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 class SignOutUseCase @Inject constructor(private val userRepository: UserRepository) {
-    suspend fun signOut(
-        dispatcher: CoroutineDispatcher = Dispatchers.IO,
-        onFail: (Exception) -> Unit,
-    ) = userRepository.signOut(onFail = onFail, dispatcher = dispatcher)
+    fun signOut() = userRepository.signOut()
 }

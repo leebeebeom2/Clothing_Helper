0a34e72bda1da1777ba27cf17a09fe9cf00b74ff
leebeebeom <boole92@naver.com>
2023-03-04 03:04:55 +0900

    getAllData 구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetAllFoldersUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetAllFoldersUseCase.kt
@@ -1,8 +1,15 @@
 package com.leebeebeom.clothinghelper.domain.usecase.folder
 
+import com.leebeebeom.clothinghelper.di.AppScope
+import com.leebeebeom.clothinghelper.domain.model.Folder
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
+import com.leebeebeom.clothinghelper.domain.usecase.BaseGetAllDataUseCase
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class GetAllFoldersUseCase @Inject constructor(private val folderRepository: FolderRepository) {
-    val allFolders get() = folderRepository.allData
-}
+@Singleton
+class GetAllFoldersUseCase @Inject constructor(
+    folderRepository: FolderRepository,
+    @AppScope appScope: CoroutineScope,
+) : BaseGetAllDataUseCase<Folder>(repository = folderRepository, appScope = appScope)
--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetAllSubCategoriesUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetAllSubCategoriesUseCase.kt
@@ -1,8 +1,15 @@
 package com.leebeebeom.clothinghelper.domain.usecase.subcategory
 
+import com.leebeebeom.clothinghelper.di.AppScope
+import com.leebeebeom.clothinghelper.domain.model.SubCategory
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
+import com.leebeebeom.clothinghelper.domain.usecase.BaseGetAllDataUseCase
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class GetAllSubCategoriesUseCase @Inject constructor(private val subCategoryRepository: SubCategoryRepository) {
-    val allSubCategories get() = subCategoryRepository.allData
-}
+@Singleton
+class GetAllSubCategoriesUseCase @Inject constructor(
+    subCategoryRepository: SubCategoryRepository,
+    @AppScope appScope: CoroutineScope,
+) : BaseGetAllDataUseCase<SubCategory>(repository = subCategoryRepository, appScope = appScope)

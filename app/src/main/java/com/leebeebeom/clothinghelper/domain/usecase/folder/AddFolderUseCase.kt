edf4b32712143d3ea273e37643054f403bb77e1a
leebeebeom <boole92@naver.com>
2023-03-04 03:21:40 +0900

    dispatcher 하드코딩 제거, 스레드 세이프 작업

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/AddFolderUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/AddFolderUseCase.kt
@@ -1,20 +1,23 @@
 package com.leebeebeom.clothinghelper.domain.usecase.folder
 
 import com.leebeebeom.clothinghelper.domain.model.Folder
-import com.leebeebeom.clothinghelper.domain.model.toDatabaseModel
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import com.leebeebeom.clothinghelper.ui.main.drawer.MainCategoryType
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AddFolderUseCase @Inject constructor(private val folderRepository: FolderRepository) {
     suspend fun add(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
         parentKey: String,
         subCategoryKey: String,
         name: String,
         mainCategoryType: MainCategoryType,
         uid: String,
         onFail: (Exception) -> Unit,
-    ) {
+    ) = withContext(dispatcher) {
         val folder = Folder(
             parentKey = parentKey,
             subCategoryKey = subCategoryKey,
@@ -22,6 +25,6 @@ class AddFolderUseCase @Inject constructor(private val folderRepository: FolderR
             mainCategoryType = mainCategoryType
         )
 
-        folderRepository.add(data = folder.toDatabaseModel(), uid = uid, onFail = onFail)
+        folderRepository.add(data = folder, uid = uid, onFail = onFail, dispatcher = dispatcher)
     }
 }
--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/EditFolderNameUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/EditFolderNameUseCase.kt
@@ -2,17 +2,26 @@ package com.leebeebeom.clothinghelper.domain.usecase.folder
 
 import com.leebeebeom.clothinghelper.domain.model.Folder
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class EditFolderNameUseCase @Inject constructor(private val folderRepository: FolderRepository) {
     suspend fun editName(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
         oldFolder: Folder,
         name: String,
         uid: String,
         onFail: (Exception) -> Unit,
-    ) {
+    ) = withContext(dispatcher) {
         val newFolder = oldFolder.copy(name = name)
 
-        folderRepository.edit(newData = newFolder, uid = uid, onFail = onFail)
+        folderRepository.edit(
+            newData = newFolder,
+            uid = uid,
+            onFail = onFail,
+            dispatcher = dispatcher
+        )
     }
 }

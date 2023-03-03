4029f511886e845ce022244e0379724aea5efeec
leebeebeom <boole92@naver.com>
2023-03-04 01:10:46 +0900

    DatabaseModel 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/EditFolderNameUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/EditFolderNameUseCase.kt
@@ -1,7 +1,6 @@
 package com.leebeebeom.clothinghelper.domain.usecase.folder
 
 import com.leebeebeom.clothinghelper.domain.model.Folder
-import com.leebeebeom.clothinghelper.domain.model.toDatabaseModel
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import javax.inject.Inject
 
@@ -14,6 +13,6 @@ class EditFolderNameUseCase @Inject constructor(private val folderRepository: Fo
     ) {
         val newFolder = oldFolder.copy(name = name)
 
-        folderRepository.edit(newData = newFolder.toDatabaseModel(), uid = uid, onFail = onFail)
+        folderRepository.edit(newData = newFolder, uid = uid, onFail = onFail)
     }
 }

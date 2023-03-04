9fa96ab52f245f95fc8291f7e158789eeb2efe85
leebeebeom <boole92@naver.com>
2023-03-05 06:10:24 +0900

    콜백 로직 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetAllFoldersUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetAllFoldersUseCase.kt
@@ -1,11 +1,10 @@
 package com.leebeebeom.clothinghelper.domain.usecase.folder
 
-import com.leebeebeom.clothinghelper.domain.model.Folder
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
-import com.leebeebeom.clothinghelper.domain.usecase.BaseGetAllDataUseCase
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class GetAllFoldersUseCase @Inject constructor(folderRepository: FolderRepository) :
-    BaseGetAllDataUseCase<Folder>(repository = folderRepository)
+class GetAllFoldersUseCase @Inject constructor(private val folderRepository: FolderRepository) {
+    val allFolders get() = folderRepository.allData
+}
--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetAllSubCategoriesUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetAllSubCategoriesUseCase.kt
@@ -1,11 +1,10 @@
 package com.leebeebeom.clothinghelper.domain.usecase.subcategory
 
-import com.leebeebeom.clothinghelper.domain.model.SubCategory
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
-import com.leebeebeom.clothinghelper.domain.usecase.BaseGetAllDataUseCase
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class GetAllSubCategoriesUseCase @Inject constructor(subCategoryRepository: SubCategoryRepository) :
-    BaseGetAllDataUseCase<SubCategory>(repository = subCategoryRepository)
+class GetAllSubCategoriesUseCase @Inject constructor(private val subCategoryRepository: SubCategoryRepository) {
+    val allSubCategories get() = subCategoryRepository.allData
+}

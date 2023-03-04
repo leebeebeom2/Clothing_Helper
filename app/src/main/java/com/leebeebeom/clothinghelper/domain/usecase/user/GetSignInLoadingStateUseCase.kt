e7c67213e43364e13da0688650d225b137ed151d
leebeebeom <boole92@naver.com>
2023-03-05 02:35:18 +0900

    로직 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetSignInLoadingStateUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetSignInLoadingStateUseCase.kt
@@ -1,12 +1,8 @@
 package com.leebeebeom.clothinghelper.domain.usecase.user
 
-import com.leebeebeom.clothinghelper.di.AppScope
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
-import com.leebeebeom.clothinghelper.domain.usecase.BaseGetIsDataLoadingStateUseCase
-import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
-class GetSignInLoadingStateUseCase @Inject constructor(
-    userRepository: UserRepository,
-    @AppScope appScope: CoroutineScope,
-) : BaseGetIsDataLoadingStateUseCase(repository = userRepository, appScope = appScope)
+class GetSignInLoadingStateUseCase @Inject constructor(private val userRepository: UserRepository) {
+    val isLoading get() = userRepository.isLoading
+}

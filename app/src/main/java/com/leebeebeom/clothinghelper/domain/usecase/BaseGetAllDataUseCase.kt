4dd78c9f0d43c2a44b158a7fbd6006f717fb2d3e
leebeebeom <boole92@naver.com>
2023-03-05 02:34:16 +0900

    로직 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/BaseGetAllDataUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/BaseGetAllDataUseCase.kt
@@ -3,16 +3,9 @@ package com.leebeebeom.clothinghelper.domain.usecase
 import com.leebeebeom.clothinghelper.domain.model.BaseModel
 import com.leebeebeom.clothinghelper.domain.repository.BaseDataRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.StateFlow
 
-abstract class BaseGetAllDataUseCase<T : BaseModel>(
-    private val repository: BaseDataRepository<T>,
-    private val appScope: CoroutineScope,
-) {
-    private lateinit var allDataFlow: StateFlow<List<T>>
-
+abstract class BaseGetAllDataUseCase<T : BaseModel>(private val repository: BaseDataRepository<T>) {
     suspend fun getAllData(
         dispatcher: CoroutineDispatcher = Dispatchers.IO,
         uid: String,

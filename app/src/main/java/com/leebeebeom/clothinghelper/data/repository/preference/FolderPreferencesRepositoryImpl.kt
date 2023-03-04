b2df07eaacaaaca71e1e0ed64dbe7318382a1a3e
leebeebeom <boole92@naver.com>
2023-03-05 04:42:39 +0900

    Flow -> StateFlow 전환

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/preference/FolderPreferencesRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/preference/FolderPreferencesRepositoryImpl.kt
@@ -2,13 +2,17 @@ package com.leebeebeom.clothinghelper.data.repository.preference
 
 import android.content.Context
 import androidx.datastore.preferences.preferencesDataStore
+import com.leebeebeom.clothinghelper.di.AppScope
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class FolderPreferencesRepositoryImpl @Inject constructor(@ApplicationContext context: Context) :
-    SortPreferenceRepositoryImpl(dataStore = context.folderDatastore)
+class FolderPreferencesRepositoryImpl @Inject constructor(
+    @ApplicationContext context: Context,
+    @AppScope appScope: CoroutineScope,
+) : SortPreferenceRepositoryImpl(dataStore = context.folderDatastore, appScope = appScope)
 
 private const val FOLDER = "folder preferences"
 private val Context.folderDatastore by preferencesDataStore(name = FOLDER)

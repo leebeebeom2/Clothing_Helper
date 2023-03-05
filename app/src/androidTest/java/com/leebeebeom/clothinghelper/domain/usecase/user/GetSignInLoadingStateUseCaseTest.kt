a87898052829eae5116498050434cf052319e4e0
leebeebeom <boole92@naver.com>
2023-03-06 04:30:35 +0900

    테스트 완

---
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetSignInLoadingStateUseCaseTest.kt
@@ -0,0 +1,68 @@
+package com.leebeebeom.clothinghelper.domain.usecase.user
+
+import com.google.firebase.auth.FirebaseAuth
+import com.leebeebeom.clothinghelper.RepositoryProvider
+import com.leebeebeom.clothinghelper.data.repository.FirebaseResult
+import com.leebeebeom.clothinghelper.domain.repository.UserRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class GetSignInLoadingStateUseCaseTest {
+    private lateinit var userRepository: UserRepository
+    private lateinit var getSignInLoadingStateUseCase: GetSignInLoadingStateUseCase
+    private val dispatcher = StandardTestDispatcher()
+    private val repositoryProvider = RepositoryProvider(dispatcher)
+
+
+    @Before
+    fun init() {
+        userRepository = repositoryProvider.getUserRepository()
+        getSignInLoadingStateUseCase = GetSignInLoadingStateUseCase(userRepository = userRepository)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun loadingFlowTest() = runTest(dispatcher) {
+        backgroundScope.launch(dispatcher) { getSignInLoadingStateUseCase.isLoading.collectLatest {} }
+
+        userRepository.signIn(
+            email = "1@a.com", password = "111111",
+            firebaseResult = successResult,
+            dispatcher = dispatcher
+        )
+        assert(getSignInLoadingStateUseCase.isLoading.value)
+        advanceUntilIdle()
+        assert(!getSignInLoadingStateUseCase.isLoading.value)
+
+        userRepository.signUp(
+            email = "notExistEmail@a.com", password = "111111", name = "test",
+            firebaseResult = successResult,
+            dispatcher = dispatcher
+        )
+        assert(getSignInLoadingStateUseCase.isLoading.value)
+        advanceUntilIdle()
+        assert(!getSignInLoadingStateUseCase.isLoading.value)
+        FirebaseAuth.getInstance().currentUser!!.delete()
+
+        userRepository.sendResetPasswordEmail(
+            email = "1@a.com", firebaseResult = successResult, dispatcher = dispatcher
+        )
+        assert(getSignInLoadingStateUseCase.isLoading.value)
+        advanceUntilIdle()
+        assert(!getSignInLoadingStateUseCase.isLoading.value)
+    }
+
+    private val successResult = object : FirebaseResult {
+        override fun success() = assert(true)
+
+        override fun fail(exception: Exception) = assert(false)
+
+    }
+}

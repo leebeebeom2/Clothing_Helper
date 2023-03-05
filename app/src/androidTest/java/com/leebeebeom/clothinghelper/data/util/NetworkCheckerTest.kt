6657a65cc5def68402cd9640874b5ca5ce879db4
leebeebeom <boole92@naver.com>
2023-03-06 04:18:37 +0900

    테스트 로직 변경

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/data/util/NetworkCheckerTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/util/NetworkCheckerTest.kt
@@ -1,13 +1,11 @@
 package com.leebeebeom.clothinghelper.data.util
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
 import com.leebeebeom.clothinghelper.RepositoryProvider
-import com.leebeebeom.clothinghelper.data.repository.preference.NetworkPreferenceRepositoryImpl
 import com.leebeebeom.clothinghelper.data.repository.preference.NetworkPreferences
 import com.leebeebeom.clothinghelper.data.repository.util.NetworkChecker
 import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
@@ -16,29 +14,25 @@ import org.junit.Test
 class NetworkCheckerTest {
     private lateinit var networkPreferenceRepository: NetworkPreferenceRepository
     private lateinit var networkChecker: NetworkChecker
+    private val dispatcher = StandardTestDispatcher()
+    private val repositoryProvider = RepositoryProvider(dispatcher)
 
     @Before
     fun init() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        networkPreferenceRepository = NetworkPreferenceRepositoryImpl(
-            context = context,
-            appScope = RepositoryProvider.getAppScope()
-        )
-        networkChecker = NetworkChecker(
-            context = context,
-            networkPreferenceRepository = networkPreferenceRepository
-        )
+        networkPreferenceRepository = repositoryProvider.getNetworkPreferenceRepository()
+        networkChecker =
+            repositoryProvider.getNetWorkChecker(networkPreferenceRepository = networkPreferenceRepository)
     }
 
     @Test
-    fun wifiCheckTest() = runTest {
+    fun wifiCheckTest() = runTest(dispatcher) {
         networkPreferenceRepository.networkSelected(network = NetworkPreferences.WIFI)
         networkChecker.checkNetWork()
         assert(true)
     }
 
     @Test
-    fun connectivityTest() = runTest {
+    fun connectivityTest() = runTest(dispatcher) {
         networkPreferenceRepository.networkSelected(network = NetworkPreferences.ANY)
         networkChecker.checkNetWork()
         assert(true)

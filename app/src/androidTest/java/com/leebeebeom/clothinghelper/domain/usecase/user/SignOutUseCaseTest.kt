728bbf21240133db7f54f9938b2c83574b921cfd
leebeebeom <boole92@naver.com>
2023-03-06 05:48:03 +0900

    테스트 완

---
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/domain/usecase/user/SignOutUseCaseTest.kt
@@ -0,0 +1,48 @@
+package com.leebeebeom.clothinghelper.domain.usecase.user
+
+import com.leebeebeom.clothinghelper.RepositoryProvider
+import com.leebeebeom.clothinghelper.data.successResult
+import com.leebeebeom.clothinghelper.domain.repository.UserRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SignOutUseCaseTest {
+    lateinit var userRepository: UserRepository
+    lateinit var signOutUseCase: SignOutUseCase
+    private val dispatcher = StandardTestDispatcher()
+    private val repositoryProvider = RepositoryProvider(dispatcher)
+
+    @Before
+    fun init() {
+        userRepository = repositoryProvider.getUserRepository()
+        signOutUseCase = SignOutUseCase(userRepository)
+    }
+
+    @Test
+    fun signOutTest() = runTest(dispatcher) {
+        backgroundScope.launch(dispatcher) { userRepository.user.collectLatest { } }
+
+        assert(userRepository.user.value == null)
+
+        userRepository.signIn(
+            email = "1@a.com",
+            password = "111111",
+            dispatcher = dispatcher,
+            firebaseResult = successResult
+        )
+        advanceUntilIdle()
+        assert(userRepository.user.value != null)
+        assert(userRepository.user.value!!.email == "1@a.com")
+
+        userRepository.signOut()
+        advanceUntilIdle()
+        assert(userRepository.user.value == null)
+    }
+}

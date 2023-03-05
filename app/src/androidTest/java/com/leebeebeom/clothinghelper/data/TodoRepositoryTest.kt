945444e187cd39e718a3af75ebaa3dae97e6c3f0
leebeebeom <boole92@naver.com>
2023-03-06 04:06:33 +0900

    테스트 로직 변경

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/data/TodoRepositoryTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/TodoRepositoryTest.kt
@@ -1,45 +1,47 @@
 package com.leebeebeom.clothinghelper.data
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
 import com.leebeebeom.clothinghelper.RepositoryProvider
-import com.leebeebeom.clothinghelper.data.repository.UserRepositoryImpl
 import com.leebeebeom.clothinghelper.domain.model.Todo
 import com.leebeebeom.clothinghelper.domain.repository.TodoRepository
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class TodoRepositoryTest {
     private lateinit var userRepository: UserRepository
     private lateinit var todoRepository: TodoRepository
+    private val dispatcher = StandardTestDispatcher()
+    private val repositoryProvider = RepositoryProvider(dispatcher = dispatcher)
 
     @Before
     fun init() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        todoRepository = RepositoryProvider.getTodoRepository(context = context)
-        userRepository = UserRepositoryImpl(appScope = RepositoryProvider.getAppScope())
+        todoRepository = repositoryProvider.getTodoRepository()
+        userRepository = repositoryProvider.getUserRepository()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun crudTest() {
+    fun crudTest() = runTest(dispatcher) {
         val todo = Todo(text = "todo")
 
-        repositoryCrudTest(
-            userRepository = userRepository,
-            data = todo,
-            repository = todoRepository,
-            addAssert = {
-                assert(it.text == todo.text)
-                assert(it.done == todo.done)
-                assert(it.order == todo.order)
-            },
-            newData = { it.copy(text = "new todo", done = true, order = 1) }
-        ) { origin, new ->
-            assert(origin.key == new.key)
-            assert(new.done)
-            assert(new.order == 1)
-            assert(new.text == "new todo")
-        }
+          repositoryCrudTest(dispatcher = dispatcher,
+              userRepository = userRepository,
+              data = todo,
+              repository = todoRepository,
+              addAssert = {
+                  assert(it.text == todo.text)
+                  assert(it.done == todo.done)
+                  assert(it.order == todo.order)
+              },
+              newData = { it.copy(text = "new todo", done = true, order = 1) }) { origin, new ->
+              assert(origin.key == new.key)
+              assert(new.done)
+              assert(new.order == 1)
+              assert(new.text == "new todo")
+          }
     }
 }

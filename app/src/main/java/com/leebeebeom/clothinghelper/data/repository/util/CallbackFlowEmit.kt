e24ab522023a09ca665803ce1d23d62182a8475d
leebeebeom <boole92@naver.com>
2023-03-06 04:18:47 +0900

    생성

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/CallbackFlowEmit.kt
@@ -0,0 +1,32 @@
+package com.leebeebeom.clothinghelper.data.repository.util
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+suspend fun <T, U> callbackFlowEmit(
+    callback: () -> U?,
+    flow: StateFlow<T>,
+    emit: suspend (callback: U) -> Unit,
+) {
+    val callback1 = callback()
+    if (callback1 != null) {
+        emit(callback1)
+        return
+    }
+
+    coroutineScope {
+        val collectJob = launch { flow.collect() }
+
+        while (collectJob.isActive) {
+            delay(500)
+            val callback2 = callback()
+            if (callback2 != null) {
+                emit(callback2)
+                collectJob.cancel()
+            }
+        }
+    }
+}

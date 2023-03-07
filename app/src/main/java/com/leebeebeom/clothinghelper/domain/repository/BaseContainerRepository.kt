16e6ecabdfb410c95377eb07d2082e190552363d
leebeebeom <boole92@naver.com>
2023-03-08 02:24:18 +0900

    코드 정리ㅣ

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseContainerRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.leebeebeom.clothinghelper.domain.repository
-
-import com.leebeebeom.clothinghelper.domain.model.BaseDatabaseContainerModel
-import kotlinx.coroutines.flow.StateFlow
-
-interface BaseContainerRepository<T : BaseDatabaseContainerModel> : BaseDataRepository<T> {
-    val allSortedData: StateFlow<List<T>>
-}
--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SizeChartRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.leebeebeom.clothinghelper.domain.repository
-
-import com.leebeebeom.clothinghelper.domain.model.data.SizeChart
-
-interface SizeChartRepository : BaseDataRepository<SizeChart>

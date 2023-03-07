ecfa150e3dc66806e61b8f613b87f0f2b014e367
leebeebeom <boole92@naver.com>
2023-03-07 11:26:49 +0900

    테스트 완

---
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/EditSubCategoryUseCaseTest.kt
@@ -0,0 +1,52 @@
+package com.leebeebeom.clothinghelper.domain.usecase.subcategory
+
+import com.leebeebeom.clothinghelper.RepositoryProvider
+import com.leebeebeom.clothinghelper.data.DataRepositoryTestUtil
+import com.leebeebeom.clothinghelper.data.editUseCaseTest
+import com.leebeebeom.clothinghelper.domain.model.SubCategory
+import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class EditSubCategoryUseCaseTest {
+    private lateinit var dataRepositoryTestUtil: DataRepositoryTestUtil<SubCategory, SubCategoryRepository>
+    private lateinit var addSubCategoryUseCase: AddSubCategoryUseCase
+    private lateinit var editSubCategoryUseCase: EditSubCategoryUseCase
+    private val dispatcher = StandardTestDispatcher()
+
+    @Before
+    fun init() {
+        val repositoryProvider = RepositoryProvider(dispatcher)
+        dataRepositoryTestUtil = DataRepositoryTestUtil(
+            repositoryProvider,
+            repositoryProvider.createSubCategoryRepository()
+        )
+        addSubCategoryUseCase =
+            AddSubCategoryUseCase(subCategoryRepository = dataRepositoryTestUtil.repository)
+        editSubCategoryUseCase =
+            EditSubCategoryUseCase(subCategoryRepository = dataRepositoryTestUtil.repository)
+
+    }
+
+    @Test
+    fun nameEditTest() = runTest(dispatcher) {
+        val subCategory = SubCategory(name = "test")
+
+        editUseCaseTest(
+            dataRepositoryTestUtil,
+            addData = subCategory,
+            edit = { oldData, name ->
+                editSubCategoryUseCase.nameEdit(
+                    oldSubCategory = oldData,
+                    name = name,
+                    uid = dataRepositoryTestUtil.userRepositoryTestUtil.uid!!,
+                    onFail = { assert(false) }
+                )
+            }
+        )
+    }
+}

1bdc15e877832a9bf4f0945217125f01b7b2faac
leebeebeom <boole92@naver.com>
2023-03-07 11:29:14 +0900

    Hilt 구성 범위 수정

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetUserUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetUserUseCase.kt
@@ -2,9 +2,7 @@ package com.leebeebeom.clothinghelper.domain.usecase.user
 
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class GetUserUseCase @Inject constructor(private val userRepository: UserRepository) {
     val user get() = userRepository.user
 }

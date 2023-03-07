d3dccbcdb4be2af90c0e22f6fb15734a4d49e02a
leebeebeom <boole92@naver.com>
2023-03-08 02:24:26 +0900

    코드 정리ㅣ

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/preference/NetworkPreferenceRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/preference/NetworkPreferenceRepositoryImpl.kt
@@ -5,31 +5,23 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import com.leebeebeom.clothinghelper.di.AppScope
 import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class NetworkPreferenceRepositoryImpl @Inject constructor(
-    @ApplicationContext context: Context,
-    @AppScope appScope: CoroutineScope,
-) : NetworkPreferenceRepository {
+class NetworkPreferenceRepositoryImpl @Inject constructor(@ApplicationContext context: Context) :
+    NetworkPreferenceRepository {
     private val dataStore = context.networkDatastore
     private val key = stringPreferencesKey("network")
 
-    override val network: StateFlow<NetworkPreferences> = dataStore.data.catch {
+    override val network = dataStore.data.catch {
         if (it is IOException) emit(emptyPreferences()) else throw it
     }.map { enumValueOf<NetworkPreferences>(it[key] ?: NetworkPreferences.ANY.name) }
-        .stateIn(
-            scope = appScope,
-            started = SharingStarted.WhileSubscribed(5000),
-            initialValue = NetworkPreferences.ANY
-        )
 
     override suspend fun networkSelected(network: NetworkPreferences) {
         dataStore.edit { it[key] = network.name }

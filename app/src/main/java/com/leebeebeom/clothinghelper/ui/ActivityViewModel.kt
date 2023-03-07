669993c9a6ca457684e942d8d245a05cf59224b9
leebeebeom <boole92@naver.com>
2023-03-08 04:10:46 +0900

    State 구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/ActivityViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/ActivityViewModel.kt
@@ -2,39 +2,53 @@ package com.leebeebeom.clothinghelper.ui
 
 import android.content.Context
 import androidx.activity.ComponentActivity
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.leebeebeom.clothinghelper.domain.model.User
 import com.leebeebeom.clothinghelper.domain.usecase.user.GetUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class ActivityViewModel @Inject constructor(private val getUserUseCase: GetUserUseCase) :
+class ActivityViewModel @Inject constructor(getUserUseCase: GetUserUseCase) :
     ViewModel() {
-    private val _activityUiState = MutableActivityUiState()
-    val activityUiState: ActivityUiState = _activityUiState
+
+    private val _toastTest: MutableStateFlow<Int?> = MutableStateFlow(null)
+
+    val activityUiState = combine(
+        flow = _toastTest,
+        flow2 = getUserUseCase.user
+    ) { toastText, user ->
+        ActivityUiState(
+            toastText = toastText,
+            user = user
+        )
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000),
+        ActivityUiState()
+    )
 
     fun showToast(toastText: Int) {
-        _activityUiState.toastText = toastText
+        _toastTest.value = toastText
     }
 
     fun toastShown() {
-        _activityUiState.toastText = null
+        _toastTest.value = null
     }
-
-    val user get() = getUserUseCase.getUser()
 }
 
-@Stable
-interface ActivityUiState {
-    val toastText: Int?
-}
-
-private class MutableActivityUiState : ActivityUiState {
-    override var toastText: Int? by mutableStateOf(null)
-}
+data class ActivityUiState(
+    val toastText: Int? = null,
+    val user: User? = null,
+)
 
 @Composable
 fun activityViewModel(context: Context = LocalContext.current): ActivityViewModel =

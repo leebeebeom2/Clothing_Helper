0d9377f587ea3b057ecaafa148634c6f76e4d757
leebeebeom <boole92@naver.com>
2023-03-08 02:15:14 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetIsFolderLoadingStateUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/GetIsFolderLoadingStateUseCase.kt
@@ -3,6 +3,6 @@ package com.leebeebeom.clothinghelper.domain.usecase.folder
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import javax.inject.Inject
 
-class GetIsFolderLoadingStateUserCase @Inject constructor(private val folderRepository: FolderRepository) {
+class GetIsFolderLoadingStateUseCase @Inject constructor(private val folderRepository: FolderRepository) {
     val isLoading get() = folderRepository.isLoading
 }
--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetIsSubCategoryLoadingStateUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetIsSubCategoryLoadingStateUseCase.kt
@@ -3,6 +3,6 @@ package com.leebeebeom.clothinghelper.domain.usecase.subcategory
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import javax.inject.Inject
 
-class GetIsSubCategoryLoadingStateUserCase @Inject constructor(private val subCategoryRepository: SubCategoryRepository) {
+class GetIsSubCategoryLoadingStateUseCase @Inject constructor(private val subCategoryRepository: SubCategoryRepository) {
     val isLoading get() = subCategoryRepository.isLoading
 }

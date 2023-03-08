53c531dfc270cac965d88bd0208cf11ef0ad469e
leebeebeom <boole92@naver.com>
2023-03-08 12:54:55 +0900

    StateFlow -> SharedFlow 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/LoadingStateProvider.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/LoadingStateProvider.kt
@@ -1,47 +1,23 @@
 package com.leebeebeom.clothinghelper.data.repository.util
 
-import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProviderImpl.LoadingStateCallBack
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 
 interface LoadingStateProvider {
-    val isLoading: StateFlow<Boolean>
+    val isLoading: SharedFlow<Boolean>
 }
 
-open class LoadingStateProviderImpl(
-    initialValue: Boolean,
-    appScope: CoroutineScope,
-) : LoadingStateProvider {
-    override val isLoading = callbackFlow {
+open class LoadingStateProviderImpl : LoadingStateProvider {
 
-        val callBack = LoadingStateCallBack { trySend(it) }
-
-        this@LoadingStateProviderImpl.loadingStateCallBack = callBack
-        awaitClose { this@LoadingStateProviderImpl.loadingStateCallBack = null }
-    }.stateIn(
-        scope = appScope,
-        started = SharingStarted.WhileSubscribed(5000),
-        initialValue = initialValue
+    private val _isLoading = MutableSharedFlow<Boolean>(
+        replay = 0,
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
     )
+    override val isLoading = _isLoading
 
-    private var loadingStateCallBack: LoadingStateCallBack? = null
+    suspend fun loadingOn() = _isLoading.emit(true)
 
-    suspend fun loadingOn() = callbackFlowEmitWrapper { it(true) }
-
-    suspend fun loadingOff() = callbackFlowEmitWrapper { it(false) }
-    private fun interface LoadingStateCallBack {
-        operator fun invoke(loading: Boolean)
-    }
-
-    private suspend fun callbackFlowEmitWrapper(
-        emit: suspend (LoadingStateCallBack) -> Unit,
-    ) = callbackFlowEmit(
-        { loadingStateCallBack },
-        flow = isLoading,
-        emit = emit
-    )
+    suspend fun loadingOff() = _isLoading.emit(false)
 }

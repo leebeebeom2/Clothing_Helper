fa75bfbdfbfd5cd74171cf36cc6f0351096f7c2c
leebeebeom <boole92@naver.com>
2023-03-09 01:25:48 +0900

    Flow -> SharedFlow 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/preference/NetworkPreferenceRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/preference/NetworkPreferenceRepositoryImpl.kt
@@ -5,16 +5,23 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.leebeebeom.clothinghelper.di.AppScope
 import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.shareIn
 import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class NetworkPreferenceRepositoryImpl @Inject constructor(@ApplicationContext context: Context) :
+class NetworkPreferenceRepositoryImpl @Inject constructor(
+    @ApplicationContext context: Context,
+    @AppScope appScope: CoroutineScope,
+) :
     NetworkPreferenceRepository {
     private val dataStore = context.networkDatastore
     private val key = stringPreferencesKey("network")
@@ -22,6 +29,10 @@ class NetworkPreferenceRepositoryImpl @Inject constructor(@ApplicationContext co
     override val network = dataStore.data.catch {
         if (it is IOException) emit(emptyPreferences()) else throw it
     }.map { enumValueOf<NetworkPreferences>(it[key] ?: NetworkPreferences.ANY.name) }
+        .shareIn(
+            scope = appScope,
+            started = SharingStarted.WhileSubscribed(5000)
+        )
 
     override suspend fun networkSelected(network: NetworkPreferences) {
         dataStore.edit { it[key] = network.name }

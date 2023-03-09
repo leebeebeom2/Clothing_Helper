e34f068767b74099f9807c16648d5c6cb3b15172
leebeebeom <boole92@naver.com>
2023-03-10 02:59:03 +0900

    WifiException 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -2,37 +2,19 @@ package com.leebeebeom.clothinghelper.domain.repository
 
 import com.google.firebase.FirebaseNetworkException
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
-import com.leebeebeom.clothinghelper.data.repository.util.WifiException
 import com.leebeebeom.clothinghelper.domain.model.BaseModel
 import kotlinx.coroutines.flow.SharedFlow
 
 interface BaseDataRepository<T : BaseModel> : LoadingStateProvider {
     val allData: SharedFlow<List<T>>
-    suspend fun load(
-        uid: String?,
-        onFail: (Exception) -> Unit,
-    )
 
     /**
      * @throws FirebaseNetworkException 인터넷 미 연결 시
-     * @throws WifiException 사용자가 와이파이로만 연결 선택 시 와이파이 미 연결됐을 경우
      */
-    suspend fun add(
-        data: T,
-        uid: String,
-        onFail: (Exception) -> Unit,
-    )
+    suspend fun add(data: T)
 
     /**
      * @throws FirebaseNetworkException 인터넷 미 연결 시
-     * @throws WifiException 사용자가 와이파이로만 연결 선택 시 와이파이 미 연결됐을 경우
-     * @throws NoSuchElementException 본래 데이터를 찾지 못했을 경우
-     * @throws IllegalArgumentException 본래 데이터를 삭제하지 못했을 경우
      */
-    suspend fun edit(
-        oldData: T,
-        newData: T,
-        uid: String,
-        onFail: (Exception) -> Unit,
-    )
+    suspend fun push(data: T)
 }

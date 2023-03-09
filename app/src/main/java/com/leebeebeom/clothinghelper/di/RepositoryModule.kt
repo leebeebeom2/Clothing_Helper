d3d4bc9e9912dbb6d2dba771b607a047d14404cf
leebeebeom <boole92@naver.com>
2023-03-10 02:55:44 +0900

    네트워크 프리퍼런스 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/di/RepositoryModule.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/di/RepositoryModule.kt
@@ -5,14 +5,12 @@ import com.leebeebeom.clothinghelper.data.repository.UserRepositoryImpl
 import com.leebeebeom.clothinghelper.data.repository.container.FolderRepositoryImpl
 import com.leebeebeom.clothinghelper.data.repository.container.SubCategoryRepositoryImpl
 import com.leebeebeom.clothinghelper.data.repository.preference.FolderPreferencesRepositoryImpl
-import com.leebeebeom.clothinghelper.data.repository.preference.NetworkPreferenceRepositoryImpl
 import com.leebeebeom.clothinghelper.data.repository.preference.SubCategoryPreferencesRepositoryImpl
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import com.leebeebeom.clothinghelper.domain.repository.TodoRepository
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.FolderPreferencesRepository
-import com.leebeebeom.clothinghelper.domain.repository.preference.NetworkPreferenceRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.SortPreferenceRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.SubCategoryPreferencesRepository
 import dagger.Binds
@@ -40,10 +38,6 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun todoRepository(impl: TodoRepositoryImpl): TodoRepository
-
-    @Singleton
-    @Binds
-    abstract fun networkPreferenceRepository(impl: NetworkPreferenceRepositoryImpl): NetworkPreferenceRepository
 
     @Singleton
     @Binds

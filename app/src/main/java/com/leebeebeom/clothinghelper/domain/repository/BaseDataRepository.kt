1d89c4346bcd83128fa703fad13146f3babfed43
leebeebeom <boole92@naver.com>
2023-03-10 03:05:49 +0900

    주석 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -1,6 +1,5 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
-import com.google.firebase.FirebaseNetworkException
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.BaseModel
 import kotlinx.coroutines.flow.SharedFlow
@@ -8,13 +7,7 @@ import kotlinx.coroutines.flow.SharedFlow
 interface BaseDataRepository<T : BaseModel> : LoadingStateProvider {
     val allData: SharedFlow<List<T>>
 
-    /**
-     * @throws FirebaseNetworkException 인터넷 미 연결 시
-     */
     suspend fun add(data: T)
 
-    /**
-     * @throws FirebaseNetworkException 인터넷 미 연결 시
-     */
     suspend fun push(data: T)
 }

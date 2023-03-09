d52d2cce69e4e8d4628891b63606bcc45eb15554
leebeebeom <boole92@naver.com>
2023-03-10 02:56:48 +0900

    네트워크 프리퍼런스 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/preference/NetworkPreferenceRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.leebeebeom.clothinghelper.domain.repository.preference
-
-import com.leebeebeom.clothinghelper.data.repository.preference.NetworkPreferences
-import kotlinx.coroutines.flow.SharedFlow
-
-interface NetworkPreferenceRepository {
-    val network: SharedFlow<NetworkPreferences>
-    suspend fun networkSelected(network: NetworkPreferences)
-}

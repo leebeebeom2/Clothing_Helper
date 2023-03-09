10e7b4e43e269f18a44acf6e92884cb621a303a6
leebeebeom <boole92@naver.com>
2023-03-10 02:31:28 +0900

    firebaseResult 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
@@ -4,7 +4,6 @@ import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuthException
-import com.leebeebeom.clothinghelper.data.repository.FirebaseResult
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.User
 import kotlinx.coroutines.flow.SharedFlow
@@ -12,33 +11,28 @@ import kotlinx.coroutines.flow.SharedFlow
 interface UserRepository : LoadingStateProvider {
     val user: SharedFlow<User?>
 
-    suspend fun googleSignIn(credential: AuthCredential, firebaseResult: FirebaseResult)
+    suspend fun googleSignIn(credential: AuthCredential)
 
     /**
      * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
      * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
      * @throws FirebaseAuthException - InvalidEmail, NotFoundUser, WrongPassword 등
      */
-    suspend fun signIn(email: String, password: String, firebaseResult: FirebaseResult)
+    suspend fun signIn(email: String, password: String)
 
     /**
      * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
      * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
      * @throws FirebaseAuthException - InvalidEmail, EmailAlreadyInUse 등
      */
-    suspend fun signUp(
-        email: String,
-        password: String,
-        name: String,
-        firebaseResult: FirebaseResult,
-    )
+    suspend fun signUp(email: String, password: String, name: String)
 
     /**
      * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
      * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
      * @throws FirebaseAuthException - InvalidEmail, NotFoundUser 등
      */
-    suspend fun sendResetPasswordEmail(email: String, firebaseResult: FirebaseResult)
+    suspend fun sendResetPasswordEmail(email: String)
 
-    suspend fun signOut()
+    fun signOut()
 }

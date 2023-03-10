371dce59a8568a07946f596614500a246b4ab00f
leebeebeom <boole92@naver.com>
2023-03-11 04:28:20 +0900

    DataResult 사용

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -1,11 +1,12 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
+import com.leebeebeom.clothinghelper.data.repository.DataResult
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.BaseModel
 import kotlinx.coroutines.flow.SharedFlow
 
 interface BaseDataRepository<T : BaseModel> : LoadingStateProvider {
-    val allData: SharedFlow<List<T>>
+    val allData: SharedFlow<DataResult<T>>
 
     suspend fun add(data: T)
 

1b58a90ac91664a4b58f2b7d9e9ba0aeaa46fb55
leebeebeom <boole92@naver.com>
2023-03-13 13:35:31 +0900

    생성

---
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/Wait.kt
@@ -0,0 +1,11 @@
+package com.leebeebeom.clothinghelper.data
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+/**
+ * wait [time] second
+ */
+suspend fun wait(time: Long = 1000L) =
+    withContext(Dispatchers.Default) { delay(time) }

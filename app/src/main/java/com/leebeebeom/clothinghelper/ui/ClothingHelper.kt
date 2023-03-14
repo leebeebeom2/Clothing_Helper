10af82d444a79b4b8e88e17062af2939b4db26b1
leebeebeom <boole92@naver.com>
2023-03-15 02:28:42 +0900

    파이어베이스 오프라인 코드 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/ClothingHelper.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/ClothingHelper.kt
@@ -1,7 +1,14 @@
 package com.leebeebeom.clothinghelper.ui
 
 import android.app.Application
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ClothingHelper : Application()
+class ClothingHelper : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Firebase.database.setPersistenceEnabled(true)
+    }
+}

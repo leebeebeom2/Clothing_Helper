15334a0a0baabe69ac393ffff4714bdd86c684d8
leebeebeom <boole92@naver.com>
2023-03-15 04:10:14 +0900

    테스트 완

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/ActivityViewModelTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/ActivityViewModelTest.kt
@@ -1,9 +1,11 @@
 package com.leebeebeom.clothinghelper.ui
 
+import androidx.lifecycle.SavedStateHandle
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.RepositoryProvider
 import com.leebeebeom.clothinghelper.data.UserRepositoryTestUtil
 import com.leebeebeom.clothinghelper.data.signInEmail
+import com.leebeebeom.clothinghelper.data.wait
 import com.leebeebeom.clothinghelper.domain.usecase.user.GetUserUseCase
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collectLatest
@@ -24,49 +26,41 @@ class ActivityViewModelTest {
         val repositoryProvider = RepositoryProvider(dispatcher)
         userRepositoryTestUtil = UserRepositoryTestUtil(repositoryProvider)
         val getUserUseCase = GetUserUseCase(userRepositoryTestUtil.userRepository)
-        activityViewModel = ActivityViewModel(getUserUseCase)
+        activityViewModel = ActivityViewModel(
+            getUserUseCase = getUserUseCase,
+            savedStateHandle = SavedStateHandle(mapOf(toastTextKey to emptyList<Int>()))
+        )
     }
 
     @Test
     fun activityUiStateTest() = runTest(dispatcher) {
         val uiState = activityViewModel.activityUiState
-        backgroundScope.launch(dispatcher) {
-            uiState.collectLatest { }
-        }
+        backgroundScope.launch(dispatcher) { uiState.collectLatest { } }
 
         assert(uiState.value.user == null)
-        assert(uiState.value.toastText == null)
 
         userRepositoryTestUtil.signIn()
         advanceUntilIdle()
         assert(uiState.value.user != null)
         assert(uiState.value.user!!.email == signInEmail)
-        assert(uiState.value.toastText == null)
 
         userRepositoryTestUtil.signOut()
         advanceUntilIdle()
-        withContext(Dispatchers.Default) {
-            delay(1000)
-            assert(uiState.value.user == null)
-            assert(uiState.value.toastText == null)
-        }
+        wait()
+        assert(uiState.value.user == null)
 
         val toastText = R.string.sign_in_complete
         activityViewModel.showToast(toastText = toastText)
         advanceUntilIdle()
-        withContext(Dispatchers.Default) {
-            delay(1000)
-            assert(uiState.value.user == null)
-            assert(uiState.value.toastText != null)
-            assert(uiState.value.toastText == toastText)
-        }
+        wait()
+        assert(uiState.value.user == null)
+        assert(uiState.value.toastText.isNotEmpty())
+        assert(uiState.value.toastText.first() == toastText)
 
         activityViewModel.toastShown()
         advanceUntilIdle()
-        withContext(Dispatchers.Default) {
-            delay(1000)
-            assert(uiState.value.user == null)
-            assert(uiState.value.toastText == null)
-        }
+        wait()
+        assert(uiState.value.user == null)
+        assert(uiState.value.toastText.isEmpty())
     }
 }

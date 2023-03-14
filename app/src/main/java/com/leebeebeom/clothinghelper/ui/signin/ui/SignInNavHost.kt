82b167df61d42c60a6add8135296a5a24e646014
leebeebeom <boole92@naver.com>
2023-03-15 04:57:18 +0900

    코드 정리

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/SignInNavHost.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/SignInNavHost.kt
@@ -1,62 +1,65 @@
 package com.leebeebeom.clothinghelper.ui.signin.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.leebeebeom.clothinghelper.ui.components.BlockBacKPressWhenLoading
 import com.leebeebeom.clothinghelper.ui.components.CenterDotProgressIndicator
-import com.leebeebeom.clothinghelper.ui.signin.ui.SignInDestinations.ResetPassword_Route
-import com.leebeebeom.clothinghelper.ui.signin.ui.SignInDestinations.SignInNav_Route
-import com.leebeebeom.clothinghelper.ui.signin.ui.SignInDestinations.SignIn_Route
-import com.leebeebeom.clothinghelper.ui.signin.ui.SignInDestinations.SignUp_Route
+import com.leebeebeom.clothinghelper.ui.signin.ui.SignInDestinations.ResetPasswordRoute
+import com.leebeebeom.clothinghelper.ui.signin.ui.SignInDestinations.SignInNavRoute
+import com.leebeebeom.clothinghelper.ui.signin.ui.SignInDestinations.SignInRoute
+import com.leebeebeom.clothinghelper.ui.signin.ui.SignInDestinations.SignUpRoute
 import com.leebeebeom.clothinghelper.ui.signin.ui.resetpassword.ResetPasswordScreen
 import com.leebeebeom.clothinghelper.ui.signin.ui.signin.SignInScreen
 import com.leebeebeom.clothinghelper.ui.signin.ui.signup.SignUpScreen
 
-const val SIGN_IN_NAV_TAG = "sign in nav"
+const val signInNavTag = "sign in nav"
 
 object SignInDestinations {
-    const val SignInNav_Route = "signInNav"
-    const val SignIn_Route = "signIn"
-    const val SignUp_Route = "signUp"
-    const val ResetPassword_Route = "resetPassword"
+    const val SignInNavRoute = "signInNav"
+    const val SignInRoute = "signIn"
+    const val SignUpRoute = "signUp"
+    const val ResetPasswordRoute = "resetPassword"
 }
 
 @Composable
 fun SignInNavHost(
     navController: NavHostController = rememberNavController(),
     viewModel: SignInNavViewModel = hiltViewModel(),
-    uiState: SignInNavUiState = viewModel.uiState,
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     NavHost(
-        modifier = Modifier.testTag(SIGN_IN_NAV_TAG),
+        modifier = Modifier.testTag(signInNavTag),
         navController = navController,
-        startDestination = SignIn_Route,
-        route = SignInNav_Route
+        startDestination = SignInRoute,
+        route = SignInNavRoute
     ) {
-        composable(route = SignIn_Route) {
+        composable(route = SignInRoute) {
             SignInScreen(
                 navigateToResetPassword = navController::navigateToResetPassword,
                 navigateToSignUp = navController::navigateToSignUp,
                 signInNavViewModel = viewModel
             )
         }
-        composable(route = SignUp_Route) {
+        composable(route = SignUpRoute) {
             SignUpScreen(signInNavViewModel = viewModel)
         }
-        composable(route = ResetPassword_Route) {
+        composable(route = ResetPasswordRoute) {
             ResetPasswordScreen(popBackStack = { navController.popBackStack() })
         }
     }
 
-    CenterDotProgressIndicator(show = { uiState.isSignInLoading })
-    BlockBacKPressWhenLoading { uiState.isSignInLoading }
+    CenterDotProgressIndicator(show = { uiState.isLoading })
+    BlockBacKPressWhenLoading { uiState.isLoading }
 }
 
-private fun NavHostController.navigateToSignUp() = navigate(SignUp_Route)
-private fun NavHostController.navigateToResetPassword() = navigate(ResetPassword_Route)
+private fun NavHostController.navigateToSignUp() = navigate(SignUpRoute)
+private fun NavHostController.navigateToResetPassword() = navigate(ResetPasswordRoute)

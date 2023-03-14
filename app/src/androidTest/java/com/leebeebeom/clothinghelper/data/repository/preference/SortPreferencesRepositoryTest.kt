6a77dde9997b6f5f113b50edcf1779cd45763933
leebeebeom <boole92@naver.com>
2023-03-15 02:17:48 +0900

    테스트 완

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/data/repository/preference/SortPreferencesRepositoryTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/repository/preference/SortPreferencesRepositoryTest.kt
@@ -3,7 +3,11 @@ package com.leebeebeom.clothinghelper.data.repository.preference
 import com.leebeebeom.clothinghelper.RepositoryProvider
 import com.leebeebeom.clothinghelper.domain.repository.preference.SortPreferenceRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
@@ -19,14 +23,40 @@ class SortPreferencesRepositoryTest {
     fun init() {
         val repositoryProvider = RepositoryProvider(dispatcher)
         folderPreferencesRepository = repositoryProvider.createFolderPreferenceRepository()
-        subCategoryPreferencesRepository = repositoryProvider.createSubCategoryPreferenceRepository()
+        subCategoryPreferencesRepository =
+            repositoryProvider.createSubCategoryPreferenceRepository()
     }
 
     @Test
     fun sotFlowTest() = runTest(dispatcher) {
-        preferenceSortFlowTest(dispatcher = dispatcher, repository = folderPreferencesRepository)
-        preferenceSortFlowTest(
-            dispatcher = dispatcher, repository = subCategoryPreferencesRepository
-        )
+        preferenceSortFlowTest(repository = folderPreferencesRepository)
+        preferenceSortFlowTest(repository = subCategoryPreferencesRepository)
+    }
+
+    private suspend fun TestScope.preferenceSortFlowTest(repository: SortPreferenceRepository) {
+        val sortFlow = repository.sort
+
+        backgroundScope.launch(dispatcher) { sortFlow.collect() }
+
+        assert(sortFlow.first() == SortPreferences()) // initial
+
+        suspend fun assert(sort: SortPreferences) = assert(sortFlow.first() == sort)
+
+        assert(sort = SortPreferences(sort = Sort.NAME, order = Order.ASCENDING))
+
+        repository.changeSort(sort = Sort.EDIT)
+        assert(sort = SortPreferences(sort = Sort.EDIT, order = Order.ASCENDING))
+
+        repository.changeSort(sort = Sort.CREATE)
+        assert(sort = SortPreferences(sort = Sort.CREATE, order = Order.ASCENDING))
+
+        repository.changeSort(sort = Sort.NAME)
+        assert(sort = SortPreferences(sort = Sort.NAME, order = Order.ASCENDING))
+
+        repository.changeOrder(Order.DESCENDING)
+        assert(sort = SortPreferences(sort = Sort.NAME, order = Order.DESCENDING))
+
+        repository.changeOrder(Order.ASCENDING)
+        assert(sort = SortPreferences(sort = Sort.NAME, order = Order.ASCENDING))
     }
 }

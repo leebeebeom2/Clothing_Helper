e4810600312edcb715a9aba59ac37778730c87bd
leebeebeom <boole92@naver.com>
2023-03-15 23:07:33 +0900

    태그 제거

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/ClothingHelper.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/ClothingHelper.kt
@@ -5,8 +5,6 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 
-const val Tag = "태그"
-
 @HiltAndroidApp
 class ClothingHelper : Application() {
     override fun onCreate() {

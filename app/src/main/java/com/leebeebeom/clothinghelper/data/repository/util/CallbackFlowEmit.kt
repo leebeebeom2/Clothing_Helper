8447d14bf32ec444abb43ab6980261231a666fdd
leebeebeom <boole92@naver.com>
2023-03-15 12:51:49 +0900

    코드 정리

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/CallbackFlowEmit.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/CallbackFlowEmit.kt
@@ -6,10 +6,10 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
-suspend fun <T, U> callbackFlowEmit(
-    callback: () -> U?,
-    flow: Flow<T>,
-    emit: suspend (callback: U) -> Unit,
+suspend fun <T> callbackFlowEmit(
+    callback: () -> T?,
+    flow: Flow<*>,
+    emit: suspend (callback: T) -> Unit,
 ) {
     val callback1 = callback()
     if (callback1 != null) {
@@ -20,11 +20,12 @@ suspend fun <T, U> callbackFlowEmit(
     coroutineScope {
         val collectJob = launch { flow.collect() }
 
-        while (!collectJob.isCancelled) {
+        while (true) {
             val callback2 = callback()
             if (callback2 != null) {
                 emit(callback2)
                 collectJob.cancel()
+                return@coroutineScope
             }
             delay(500)
         }

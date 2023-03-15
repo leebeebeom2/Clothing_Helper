c972852d1ee897b077c993a9c6a0553360213748
leebeebeom <boole92@naver.com>
2023-03-15 23:04:46 +0900

    태그 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/FirebaseAuthErrorUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/FirebaseAuthErrorUseCase.kt
@@ -1,14 +1,15 @@
 package com.leebeebeom.clothinghelper.domain.usecase.user
 
+import android.util.Log
 import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.FirebaseAuthException
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.data.repository.util.logE
 import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorCode.ERROR_EMAIL_ALREADY_IN_USE
 import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorCode.ERROR_INVALID_EMAIL
 import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorCode.ERROR_USER_NOT_FOUND
 import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorCode.ERROR_WRONG_PASSWORD
+import com.leebeebeom.clothinghelper.ui.Tag
 import com.leebeebeom.clothinghelper.ui.util.ShowToast
 import javax.inject.Inject
 
@@ -21,7 +22,7 @@ object FirebaseAuthErrorCode {
 
 class FirebaseAuthErrorUseCase @Inject constructor() {
     fun firebaseAuthError(
-        exception: Exception,
+        exception: Throwable,
         updateEmailError: (error: Int) -> Unit = {},
         updatePasswordError: (error: Int) -> Unit = {},
         showToast: ShowToast,
@@ -39,7 +40,7 @@ class FirebaseAuthErrorUseCase @Inject constructor() {
             is FirebaseNetworkException -> showToast(R.string.network_error)
             else -> {
                 showToast(R.string.unknown_error)
-                logE("FirebaseAuthErrorUseCase: firebaseAuthError", exception)
+                Log.e(Tag, "firebaseAuthError: ", exception)
             }
         }
     }

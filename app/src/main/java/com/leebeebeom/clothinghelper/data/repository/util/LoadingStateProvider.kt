ae42e0807cde6b2f1994ed13b6564f03e7d6c5f0
leebeebeom <boole92@naver.com>
2023-03-18 01:41:30 +0900

    onEmpty 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/LoadingStateProvider.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/util/LoadingStateProvider.kt
@@ -4,19 +4,21 @@ import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.onEmpty
 
 interface LoadingStateProvider {
     val isLoading: Flow<Boolean>
 }
 
-open class LoadingStateProviderImpl : LoadingStateProvider {
+open class LoadingStateProviderImpl(initialState: Boolean) : LoadingStateProvider {
 
     private val _isLoading = MutableSharedFlow<Boolean>(
         replay = 1,
         extraBufferCapacity = 0,
         onBufferOverflow = BufferOverflow.DROP_OLDEST
     )
-    override val isLoading = _isLoading.distinctUntilChanged()
+
+    override val isLoading = _isLoading.onEmpty { emit(initialState) }.distinctUntilChanged()
 
     protected suspend fun loadingOn() = _isLoading.emit(true)
 

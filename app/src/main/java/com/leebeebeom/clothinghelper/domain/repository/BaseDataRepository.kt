3d30c30cb424e7ff273fedc0a49cdb4d38caafd4
leebeebeom <boole92@naver.com>
2023-03-21 04:03:48 +0900

    코드 정리

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -3,13 +3,12 @@ package com.leebeebeom.clothinghelper.domain.repository
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.BaseModel
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 interface BaseDataRepository<T : BaseModel> : LoadingStateProvider {
     val allDataStream: StateFlow<List<T>>
 
-    suspend fun add(data: T) : Job
+    suspend fun add(data: T): Job
 
-    suspend fun push(data: T) : Job
+    suspend fun push(data: T): Job
 }

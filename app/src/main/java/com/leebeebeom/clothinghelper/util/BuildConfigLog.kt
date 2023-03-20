0306b678e397790eca76e28ebf7fdebc1511b803
leebeebeom <boole92@naver.com>
2023-03-21 04:13:34 +0900

    수정

--- app/src/main/java/com/leebeebeom/clothinghelper/util/BuildConfigLog.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/util/BuildConfigLog.kt
@@ -6,10 +6,6 @@ import com.leebeebeom.clothinghelper.BuildConfig
 const val Tag = "태그"
 
 fun buildConfigLog(site: String, msg: String = "", throwable: Throwable? = null) {
-    if (BuildConfig.DEBUG) {
-        throwable?.let {
-            Log.e(Tag, "$site: $msg", throwable)
-        }
-        Log.d(Tag, "$site: $msg")
-    }
+    if (BuildConfig.DEBUG)
+        throwable?.let { Log.e(Tag, "$site: $msg", throwable) } ?: Log.d(Tag, "$site: $msg")
 }

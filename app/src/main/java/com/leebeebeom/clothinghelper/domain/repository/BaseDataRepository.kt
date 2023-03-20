ded24d208ebfcc1eb48c518457ad3b2b9dee85e1
leebeebeom <boole92@naver.com>
2023-03-21 07:28:44 +0900

    DataResult 생성

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -3,10 +3,15 @@ package com.leebeebeom.clothinghelper.domain.repository
 import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.BaseModel
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.SharedFlow
 
 interface BaseDataRepository<T : BaseModel> : LoadingStateProvider {
-    val allDataStream: StateFlow<List<T>>
+    val allDataStream: SharedFlow<DataResult<T>>
     suspend fun add(data: T): Job
     suspend fun push(data: T): Job
+}
+
+sealed class DataResult<T> {
+    data class Success<T>(val data: List<T>) : DataResult<T>()
+    data class Fail<T>(val data: List<T>, val exception: Throwable) : DataResult<T>()
 }

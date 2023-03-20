921a032f857125a69e41df9e92c53ae4111aea5b
leebeebeom <boole92@naver.com>
2023-03-21 04:15:30 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetUserUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetUserUseCase.kt
@@ -2,7 +2,9 @@ package com.leebeebeom.clothinghelper.domain.usecase.user
 
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class GetUserUseCase @Inject constructor(private val userRepository: UserRepository) {
-    val user get() = userRepository.user
+    val userStream get() = userRepository.userStream
 }

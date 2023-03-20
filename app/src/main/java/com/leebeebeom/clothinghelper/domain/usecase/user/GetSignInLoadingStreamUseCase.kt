06c6e80753f6716e9175c7d15e2a2d0f525a4a2e
leebeebeom <boole92@naver.com>
2023-03-21 04:42:14 +0900

    생성

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetSignInLoadingStreamUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/user/GetSignInLoadingStreamUseCase.kt
@@ -3,6 +3,6 @@ package com.leebeebeom.clothinghelper.domain.usecase.user
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
 import javax.inject.Inject
 
-class GetSignInLoadingStateUseCase @Inject constructor(private val userRepository: UserRepository) {
+class GetSignInLoadingStreamUseCase @Inject constructor(private val userRepository: UserRepository) {
     val signInLoadingStream get() = userRepository.loadingStream
 }

b7e701ec165bc867e17d65ee0776f48c7df203a9
leebeebeom <boole92@naver.com>
2023-03-21 04:16:13 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/ActivityViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/ActivityViewModel.kt
@@ -8,7 +8,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.domain.model.User
 import com.leebeebeom.clothinghelper.domain.usecase.user.GetUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -26,13 +25,13 @@ class ActivityViewModel @Inject constructor(
 ) : ViewModel() {
 
     val activityUiState = combine(
-        flow = savedStateHandle.getStateFlow(ToastTextKey, emptyList<Int>()),
-        flow2 = getUserUseCase.getUser(onFail = { addToastTextAtLast(R.string.error_fail_get_user_info_by_unknow_error) }),
-    ) { toastTexts, userResult ->
-        ActivityUiState(toastTexts = toastTexts, user = userResult)
+        flow = savedStateHandle.getStateFlow(key = ToastTextKey, initialValue = emptyList<Int>()),
+        flow2 = getUserUseCase.userStream,
+    ) { toastTexts, user ->
+        ActivityUiState(toastTexts = toastTexts, user = user)
     }.stateIn(
         scope = viewModelScope, started = SharingStarted.WhileSubscribed(5000),
-        initialValue = ActivityUiState(user = getUserUseCase.getUserImmediate())
+        initialValue = ActivityUiState()
     )
 
     fun addToastTextAtLast(toastText: Int) {

f548a27dbbc51c67cc271fb95c1375a69d8ec6c0
leebeebeom <boole92@naver.com>
2023-03-22 04:15:14 +0900

    테스트 완

--- app/src/test/java/com/leebeebeom/clothinghelper/domain/usecase/user/FirebaseAuthErrorUseCaseTest.kt
+++ app/src/test/java/com/leebeebeom/clothinghelper/domain/usecase/user/FirebaseAuthErrorUseCaseTest.kt
@@ -8,62 +8,55 @@ import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorCode.E
 import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorCode.ERROR_INVALID_EMAIL
 import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorCode.ERROR_USER_NOT_FOUND
 import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorCode.ERROR_WRONG_PASSWORD
-import org.junit.Before
 import org.junit.Test
 
 class FirebaseAuthErrorUseCaseTest {
-    private lateinit var firebaseAuthErrorUseCase: FirebaseAuthErrorUseCase
-
-    @Before
-    fun init() {
-        firebaseAuthErrorUseCase = FirebaseAuthErrorUseCase()
-    }
+    private val firebaseAuthErrorUseCase = FirebaseAuthErrorUseCase()
 
     @Test
-    fun test() {
-
+    fun errorTest() {
         firebaseAuthErrorUseCase.firebaseAuthError( // invalidEmail
-            exception = FirebaseAuthException(ERROR_INVALID_EMAIL, ""),
-            updateEmailError = { assert(it == R.string.error_invalid_email) },
-            updatePasswordError = { assert(false) },
+            throwable = FirebaseAuthException(ERROR_INVALID_EMAIL, ""),
+            setEmailError = { assert(it == R.string.error_invalid_email) },
+            setPasswordError = { assert(false) },
             showToast = { assert(false) })
 
         firebaseAuthErrorUseCase.firebaseAuthError( // emailAlreadyInUse
-            exception = FirebaseAuthException(ERROR_EMAIL_ALREADY_IN_USE, ""),
-            updateEmailError = { assert(it == R.string.error_email_already_in_use) },
-            updatePasswordError = { assert(false) },
+            throwable = FirebaseAuthException(ERROR_EMAIL_ALREADY_IN_USE, ""),
+            setEmailError = { assert(it == R.string.error_email_already_in_use) },
+            setPasswordError = { assert(false) },
             showToast = { assert(false) })
 
         firebaseAuthErrorUseCase.firebaseAuthError( // userNotFound
-            exception = FirebaseAuthException(ERROR_USER_NOT_FOUND, ""),
-            updateEmailError = { assert(it == R.string.error_user_not_found) },
-            updatePasswordError = { assert(false) },
+            throwable = FirebaseAuthException(ERROR_USER_NOT_FOUND, ""),
+            setEmailError = { assert(it == R.string.error_user_not_found) },
+            setPasswordError = { assert(false) },
             showToast = { assert(false) })
 
         firebaseAuthErrorUseCase.firebaseAuthError( // wrongPassword
-            exception = FirebaseAuthException(ERROR_WRONG_PASSWORD, ""),
-            updateEmailError = { assert(false) },
-            updatePasswordError = { assert(it == R.string.error_wrong_password) },
+            throwable = FirebaseAuthException(ERROR_WRONG_PASSWORD, ""),
+            setEmailError = { assert(false) },
+            setPasswordError = { assert(it == R.string.error_wrong_password) },
             showToast = { assert(false) })
 
         firebaseAuthErrorUseCase.firebaseAuthError( // TooManyRequest
-            exception = FirebaseTooManyRequestsException(""),
-            updateEmailError = { assert(false) },
-            updatePasswordError = { assert(false) },
+            throwable = FirebaseTooManyRequestsException(""),
+            setEmailError = { assert(false) },
+            setPasswordError = { assert(false) },
             showToast = { assert(it == R.string.too_many_requst_error) }
         )
 
         firebaseAuthErrorUseCase.firebaseAuthError( // TooManyRequest
-            exception = FirebaseNetworkException(""),
-            updateEmailError = { assert(false) },
-            updatePasswordError = { assert(false) },
+            throwable = FirebaseNetworkException(""),
+            setEmailError = { assert(false) },
+            setPasswordError = { assert(false) },
             showToast = { assert(it == R.string.network_error) }
         )
 
         firebaseAuthErrorUseCase.firebaseAuthError( // unknown
-            exception = Exception(),
-            updateEmailError = { assert(false) },
-            updatePasswordError = { assert(false) },
+            throwable = Exception(),
+            setEmailError = { assert(false) },
+            setPasswordError = { assert(false) },
             showToast = { assert(it == R.string.unknown_error) }
         )
     }

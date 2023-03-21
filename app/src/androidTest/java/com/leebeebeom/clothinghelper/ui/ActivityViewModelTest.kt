ba4c0be70531dc932ef446fb37cd2314e19642b5
leebeebeom <boole92@naver.com>
2023-03-22 07:17:37 +0900

    테스트 완

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/ActivityViewModelTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/ActivityViewModelTest.kt
@@ -1,65 +1,52 @@
 package com.leebeebeom.clothinghelper.ui
 
-import androidx.lifecycle.SavedStateHandle
-import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.RepositoryProvider
-import com.leebeebeom.clothinghelper.data.UserRepositoryTestUtil
 import com.leebeebeom.clothinghelper.data.SignInEmail
-import com.leebeebeom.clothinghelper.data.wait
+import com.leebeebeom.clothinghelper.data.SignInPassword
+import com.leebeebeom.clothinghelper.data.repository.UserRepositoryImpl
+import com.leebeebeom.clothinghelper.data.waitTime
 import com.leebeebeom.clothinghelper.domain.usecase.user.GetUserUseCase
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
-import org.junit.Before
 import org.junit.Test
+import com.leebeebeom.clothinghelper.R
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class ActivityViewModelTest {
-    private lateinit var userRepositoryTestUtil: UserRepositoryTestUtil
-    private lateinit var activityViewModel: ActivityViewModel
     private val dispatcher = StandardTestDispatcher()
-
-    @Before
-    fun init() {
-        val repositoryProvider = RepositoryProvider(dispatcher = dispatcher)
-        userRepositoryTestUtil = UserRepositoryTestUtil(repositoryProvider = repositoryProvider)
-        val getUserUseCase = GetUserUseCase(userRepository = userRepositoryTestUtil.userRepository)
-        activityViewModel = ActivityViewModel(
-            getUserUseCase = getUserUseCase,
-            savedStateHandle = SavedStateHandle(mapOf(ToastTextKey to emptyList<Int>()))
-        )
-    }
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    private val userRepository = UserRepositoryImpl(
+        appScope = scope,
+        dispatcher = dispatcher
+    )
+    private val getUserUseCase = GetUserUseCase(userRepository = userRepository)
+    private val activityViewModel = ActivityViewModel(getUserUseCase)
 
     @Test
     fun activityUiStateTest() = runTest(dispatcher) {
+        userRepository.signOut()
+        waitTime()
+
         val uiState = activityViewModel.activityUiState
-        backgroundScope.launch(dispatcher) { uiState.collectLatest { } }
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) { uiState.collect() }
 
-        assert(uiState.value.user == null)
-
-        userRepositoryTestUtil.signIn()
-        advanceUntilIdle()
+        userRepository.signIn(email = SignInEmail, password = SignInPassword).join()
+        waitTime()
         assert(uiState.value.user != null)
         assert(uiState.value.user!!.email == SignInEmail)
 
-        userRepositoryTestUtil.signOut()
-        advanceUntilIdle()
-        wait()
+        userRepository.signOut()
+        waitTime()
         assert(uiState.value.user == null)
 
         val toastText = R.string.sign_in_complete
         activityViewModel.addToastTextAtLast(toastText = toastText)
-        advanceUntilIdle()
-        wait()
-        assert(uiState.value.user == null)
         assert(uiState.value.toastTexts.isNotEmpty())
         assert(uiState.value.toastTexts.first() == toastText)
 
         activityViewModel.removeFirstToastText()
-        advanceUntilIdle()
-        wait()
         assert(uiState.value.user == null)
         assert(uiState.value.toastTexts.isEmpty())
     }

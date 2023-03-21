31301337592b0fbd5604ffe33f64abf13097cc87
leebeebeom <boole92@naver.com>
2023-03-21 23:50:23 +0900

    enum으로 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/DatabasePath.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/DatabasePath.kt
@@ -1,7 +1,5 @@
 package com.leebeebeom.clothinghelper.data.repository
 
-object DatabasePath {
-    const val SUB_CATEGORIES = "sub categories"
-    const val FOLDERS = "folders"
-    const val TODOS = "todos"
+enum class DataBasePath(val path: String) {
+    SubCategory("sub categories"), Folder("folder"), Todo("todos")
 }

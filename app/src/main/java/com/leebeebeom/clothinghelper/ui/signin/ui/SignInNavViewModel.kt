7e7f37597608607840797c0592ba854d4270252c
leebeebeom <boole92@naver.com>
2023-03-21 13:40:00 +0900

    savedStateHandle 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/SignInNavViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/SignInNavViewModel.kt
@@ -2,6 +2,10 @@ package com.leebeebeom.clothinghelper.ui.signin.ui
 
 import android.app.Activity
 import androidx.activity.result.ActivityResult
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -11,7 +15,7 @@ import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.GoogleAuthProvider
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorUseCase
-import com.leebeebeom.clothinghelper.domain.usecase.user.GetSignInLoadingStateUseCase
+import com.leebeebeom.clothinghelper.domain.usecase.user.GetSignInLoadingStreamUseCase
 import com.leebeebeom.clothinghelper.domain.usecase.user.GoogleSignInUseCase
 import com.leebeebeom.clothinghelper.ui.util.ShowToast
 import com.leebeebeom.clothinghelper.util.buildConfigLog
@@ -31,14 +35,15 @@ const val GoogleButtonEnabledKey = "google button enable"
 @HiltViewModel
 class SignInNavViewModel @Inject constructor(
     private val googleSignInUseCase: GoogleSignInUseCase,
-    getSignInLoadingStateUseCase: GetSignInLoadingStateUseCase,
+    getSignInLoadingStreamUseCase: GetSignInLoadingStreamUseCase,
     private val firebaseAuthErrorUseCase: FirebaseAuthErrorUseCase,
     private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
+    private var googleButtonEnabled by mutableStateOf(true)
 
     val uiState = combine(
-        flow = savedStateHandle.getStateFlow(key = GoogleButtonEnabledKey, initialValue = true),
-        flow2 = getSignInLoadingStateUseCase.signInLoadingStream
+        flow = snapshotFlow { googleButtonEnabled },
+        flow2 = getSignInLoadingStreamUseCase.signInLoadingStream
     ) { googleButtonEnabled, isLoading ->
         SignInNavUiState(googleButtonEnabled = googleButtonEnabled, isLoading = isLoading)
     }.stateIn(
@@ -70,7 +75,7 @@ class SignInNavViewModel @Inject constructor(
     private fun googleSignIn(activityResult: ActivityResult, showToast: ShowToast) {
         val handler = CoroutineExceptionHandler { _, throwable ->
             firebaseAuthErrorUseCase.firebaseAuthError(throwable = throwable, showToast = showToast)
-            savedStateHandle[GoogleButtonEnabledKey] = true
+            googleButtonEnabled = true
         }
 
         viewModelScope.launch(handler) {
@@ -86,7 +91,7 @@ class SignInNavViewModel @Inject constructor(
     }
 
     fun setGoogleButtonEnable(enable: Boolean) {
-        savedStateHandle[GoogleButtonEnabledKey] = enable
+        googleButtonEnabled = enable
     }
 }
 

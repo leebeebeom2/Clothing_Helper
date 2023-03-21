1eedaaacab47c441a27e020e859dca00bf6fdc0f
leebeebeom <boole92@naver.com>
2023-03-22 01:25:21 +0900

    folder 수정

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/DatabasePath.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/DatabasePath.kt
@@ -1,5 +1,5 @@
 package com.leebeebeom.clothinghelper.data.repository
 
 enum class DataBasePath(val path: String) {
-    SubCategory("sub categories"), Folder("folder"), Todo("todos")
+    SubCategory("sub categories"), Folder("folders"), Todo("todos")
 }

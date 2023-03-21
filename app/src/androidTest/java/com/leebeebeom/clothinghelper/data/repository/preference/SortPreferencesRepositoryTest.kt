3003bac2e9c87fa0a8e2234654286bcc169fa682
leebeebeom <boole92@naver.com>
2023-03-22 03:37:45 +0900

    리팩토링

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/data/repository/preference/SortPreferencesRepositoryTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/repository/preference/SortPreferencesRepositoryTest.kt
@@ -1,13 +1,14 @@
 package com.leebeebeom.clothinghelper.data.repository.preference
 
-import com.leebeebeom.clothinghelper.RepositoryProvider
+import androidx.test.core.app.ApplicationProvider
+import com.leebeebeom.clothinghelper.data.waitTime
 import com.leebeebeom.clothinghelper.domain.repository.preference.SortPreferenceRepository
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Test
@@ -15,10 +16,15 @@ import org.junit.Test
 @OptIn(ExperimentalCoroutinesApi::class)
 class SortPreferencesRepositoryTest {
     private val dispatcher = StandardTestDispatcher()
-    private val repositoryProvider = RepositoryProvider(dispatcher)
-    private val folderPreferencesRepository = repositoryProvider.createFolderPreferenceRepository()
-    private val subCategoryPreferencesRepository =
-        repositoryProvider.createSubCategoryPreferenceRepository()
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    private val folderPreferencesRepository = FolderPreferencesRepositoryImpl(
+        context = ApplicationProvider.getApplicationContext(),
+        appScope = scope
+    )
+    private val subCategoryPreferencesRepository = SubCategoryPreferencesRepositoryImpl(
+        context = ApplicationProvider.getApplicationContext(),
+        appScope = scope
+    )
 
     @After
     fun init() = runTest(dispatcher) {
@@ -35,29 +41,32 @@ class SortPreferencesRepositoryTest {
     }
 
     private suspend fun TestScope.preferenceSortFlowTest(repository: SortPreferenceRepository) {
-        val sortFlow = repository.sort
+        val sortFlow = repository.sortStream
 
-        backgroundScope.launch(dispatcher) { sortFlow.collect() }
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) { sortFlow.collect() }
 
         assert(sortFlow.first() == SortPreferences()) // initial
 
         suspend fun assertSort(sort: SortPreferences) = assert(sortFlow.first() == sort)
 
-        assertSort(sort = SortPreferences(sort = Sort.Name, order = Order.Ascending)) // initial
-
         repository.changeSort(sort = Sort.Edit)
+        waitTime()
         assertSort(sort = SortPreferences(sort = Sort.Edit, order = Order.Ascending))
 
         repository.changeSort(sort = Sort.Create)
+        waitTime()
         assertSort(sort = SortPreferences(sort = Sort.Create, order = Order.Ascending))
 
         repository.changeSort(sort = Sort.Name)
+        waitTime()
         assertSort(sort = SortPreferences(sort = Sort.Name, order = Order.Ascending))
 
         repository.changeOrder(Order.Descending)
+        waitTime()
         assertSort(sort = SortPreferences(sort = Sort.Name, order = Order.Descending))
 
         repository.changeOrder(Order.Ascending)
+        waitTime()
         assertSort(sort = SortPreferences(sort = Sort.Name, order = Order.Ascending))
     }
 }

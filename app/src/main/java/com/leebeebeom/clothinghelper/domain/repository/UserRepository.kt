2920cfd20d6b1f33bd336d17c41b9370cd336400
leebeebeom <boole92@naver.com>
2023-03-23 08:29:22 +0900

    Job 반환

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
@@ -4,34 +4,34 @@ import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuthException
-import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.User
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.SharedFlow
 
-interface UserRepository : LoadingStateProvider {
+interface UserRepository {
     val userStream: SharedFlow<User?>
     fun getInitialUser(): User?
-    suspend fun googleSignIn(credential: AuthCredential)
+    suspend fun googleSignIn(credential: AuthCredential): Job
 
     /**
      * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
      * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
      * @throws FirebaseAuthException - InvalidEmail, NotFoundUser, WrongPassword 등
      */
-    suspend fun signIn(email: String, password: String)
+    suspend fun signIn(email: String, password: String): Job
 
     /**
      * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
      * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
      * @throws FirebaseAuthException - InvalidEmail, EmailAlreadyInUse 등
      */
-    suspend fun signUp(email: String, password: String, name: String)
+    suspend fun signUp(email: String, password: String, name: String): Job
 
     /**
      * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
      * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
      * @throws FirebaseAuthException - InvalidEmail, NotFoundUser 등
      */
-    suspend fun sendResetPasswordEmail(email: String)
+    suspend fun sendResetPasswordEmail(email: String): Job
     fun signOut()
 }

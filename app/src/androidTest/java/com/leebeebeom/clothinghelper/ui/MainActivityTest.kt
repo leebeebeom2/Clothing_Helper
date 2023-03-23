9a93f1ee14f6e6c2e28bc5f62847d6b8ee7232ed
leebeebeom <boole92@naver.com>
2023-03-23 10:25:49 +0900

    테스트 완

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/MainActivityTest.kt
@@ -1,20 +1,17 @@
 package com.leebeebeom.clothinghelper.ui
 
-import androidx.compose.ui.test.performTouchInput
-import androidx.compose.ui.test.swipeRight
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.leebeebeom.clothinghelper.CustomTestRule
-import com.leebeebeom.clothinghelper.R.string.*
 import com.leebeebeom.clothinghelper.activityRule
 import com.leebeebeom.clothinghelper.data.SignInEmail
 import com.leebeebeom.clothinghelper.data.SignInPassword
 import com.leebeebeom.clothinghelper.ui.MainActivityRoutes.MainGraphRoute
 import com.leebeebeom.clothinghelper.ui.MainActivityRoutes.SignInGraphRoute
-import com.leebeebeom.clothinghelper.ui.components.CENTER_DOT_PROGRESS_INDICATOR_TAG
 import com.leebeebeom.clothinghelper.ui.main.MainNavTag
-import com.leebeebeom.clothinghelper.ui.main.drawer.SETTING_ICON
 import com.leebeebeom.clothinghelper.ui.signin.ui.SignInNavTag
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
@@ -24,8 +21,7 @@ import org.junit.Test
 
 class MainActivitySignInStartTest {
     @get:Rule
-    val rule = activityRule
-    private val customTestRule = CustomTestRule(rule = rule)
+    val rule = createAndroidComposeRule<HiltTestActivity>()
 
     @Before
     fun init() {
@@ -33,33 +29,26 @@ class MainActivitySignInStartTest {
             FirebaseAuth.getInstance().signInWithEmailAndPassword(SignInEmail, SignInPassword)
                 .await()
         }
-        customTestRule.setContent { MainActivityScreen() }
+        rule.setContent { MainActivityScreen() }
     }
 
     // 로그인 상태로 앱 실행 시 로그인 화면 안 보이는 지
     // TODO 구글은 따로 테스트 필요
     @Test
     fun mainScreenTest() {
-        customTestRule.getNodeWithTag(SignInNavTag).notExist()
-    }
-
-    @Test
-    fun signOutTest() {
-        customTestRule.uiSignOut()
-        customTestRule.waitSignInNav()
+        rule.onNodeWithTag(SignInNavTag).assertDoesNotExist()
     }
 }
 
 class MainActivitySignOutStartTest {
     @get:Rule
-    val rule = activityRule
-    private val customTestRule = CustomTestRule(rule = rule)
+    val rule = createAndroidComposeRule<HiltTestActivity>()
     private lateinit var navController: NavHostController
 
     @Before
     fun init() {
         FirebaseAuth.getInstance().signOut()
-        customTestRule.setContent {
+        rule.setContent {
             navController = rememberNavController()
             MainActivityScreen(navController = navController)
         }
@@ -67,13 +56,7 @@ class MainActivitySignOutStartTest {
 
     @Test
     fun signInScreenTest() {
-        customTestRule.getNodeWithTag(SignInNavTag).exist()
-    }
-
-    @Test
-    fun signInTest() {
-        customTestRule.uiSignIn()
-        customTestRule.waitMainNav()
+        rule.onNodeWithTag(SignInNavTag).assertExists()
     }
 
     @Test
@@ -89,36 +72,20 @@ class MainActivitySignOutStartTest {
         // 로그인, 로그아웃 시 백스택은 하나여야함.
         assert(!isBackStackExist(MainGraphRoute))
 
-        customTestRule.uiSignIn()
-        customTestRule.waitMainNav()
+        rule.signIn()
+        rule.waitTagExit(MainNavTag)
         assert(!isBackStackExist(SignInGraphRoute))
 
-        customTestRule.uiSignOut()
-        customTestRule.waitSignInNav()
+        rule.signOut()
+        rule.waitTagExit(SignInNavTag)
         assert(!isBackStackExist(MainGraphRoute))
 
         repeat(2) {
-            customTestRule.uiSignIn()
-            customTestRule.waitMainNav()
-            customTestRule.uiSignOut()
-            customTestRule.waitSignInNav()
+            rule.signIn()
+            rule.waitTagExit(MainNavTag)
+            rule.signOut()
+            rule.waitTagExit(SignInNavTag)
         }
         assert(!isBackStackExist(MainGraphRoute))
     }
 }
-
-private fun CustomTestRule.uiSignIn() {
-    getNodeWithStringRes(email).input(SignInEmail)
-    getNodeWithStringRes(password).input(SignInPassword, invisible = true)
-    getNodeWithStringRes(sign_in).click()
-    getNodeWithTag(CENTER_DOT_PROGRESS_INDICATOR_TAG).exist(false)
-}
-
-private fun CustomTestRule.uiSignOut() {
-    root.performTouchInput { swipeRight() }
-    getNodeWithDescription(SETTING_ICON).click()
-    getNodeWithStringRes(sign_out).click()
-}
-
-private fun CustomTestRule.waitSignInNav() = waitTagExist(tag = SignInNavTag)
-private fun CustomTestRule.waitMainNav() = waitTagExist(tag = MainNavTag)

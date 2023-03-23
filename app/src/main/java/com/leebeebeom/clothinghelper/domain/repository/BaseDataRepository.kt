d9211f04ef1e556a281b62a803a7a8dbe53f2914
leebeebeom <boole92@naver.com>
2023-03-23 10:25:38 +0900

    로딩 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -1,11 +1,10 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
-import com.leebeebeom.clothinghelper.data.repository.util.LoadingStateProvider
 import com.leebeebeom.clothinghelper.domain.model.BaseModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.SharedFlow
 
-interface BaseDataRepository<T : BaseModel> : LoadingStateProvider {
+interface BaseDataRepository<T : BaseModel> {
     val allDataStream: SharedFlow<DataResult<T>>
     suspend fun add(data: T): Job
     suspend fun push(data: T): Job

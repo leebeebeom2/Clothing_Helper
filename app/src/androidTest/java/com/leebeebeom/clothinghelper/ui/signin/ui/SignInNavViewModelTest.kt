e34ef38890f9f3dbce105ef628022825f0d6f847
leebeebeom <boole92@naver.com>
2023-03-25 02:26:20 +0900

    테스트 완

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/SignInNavViewModelTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/SignInNavViewModelTest.kt
@@ -1,52 +1,61 @@
 package com.leebeebeom.clothinghelper.ui.signin.ui
 
-import androidx.lifecycle.SavedStateHandle
-import com.leebeebeom.clothinghelper.RepositoryProvider
-import com.leebeebeom.clothinghelper.data.UserRepositoryTestUtil
-import com.leebeebeom.clothinghelper.data.wait
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.leebeebeom.clothinghelper.data.repository.UserRepositoryImpl
+import com.leebeebeom.clothinghelper.data.waitTime
+import com.leebeebeom.clothinghelper.domain.repository.UserRepository
 import com.leebeebeom.clothinghelper.domain.usecase.user.FirebaseAuthErrorUseCase
-import com.leebeebeom.clothinghelper.domain.usecase.user.GetSignInLoadingStateUseCase
 import com.leebeebeom.clothinghelper.domain.usecase.user.GoogleSignInUseCase
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
+import com.leebeebeom.clothinghelper.ui.HiltTestActivity
+import kotlinx.coroutines.*
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class SignInNavViewModelTest {
+    @get:Rule
+    val rule = createAndroidComposeRule<HiltTestActivity>()
+
     private val dispatcher = StandardTestDispatcher()
-    private val repositoryProvider = RepositoryProvider(dispatcher)
-    private val userRepositoryTestUtil = UserRepositoryTestUtil(repositoryProvider)
-    private lateinit var signInNavViewModel: SignInNavViewModel
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    private val userRepository: UserRepository =
+        UserRepositoryImpl(appScope = scope, dispatcher = dispatcher)
+    private val signInNavViewModel =
+        SignInNavViewModel(
+            googleSignInUseCase = GoogleSignInUseCase(userRepository),
+            firebaseAuthErrorUseCase = FirebaseAuthErrorUseCase()
+        )
+    private val uiState = signInNavViewModel.uiState
 
     @Before
     fun init() {
-        val googleSignInUseCase =
-            GoogleSignInUseCase(userRepository = userRepositoryTestUtil.userRepository)
-        val getSignInLoadingStateUseCase =
-            GetSignInLoadingStateUseCase(userRepository = userRepositoryTestUtil.userRepository)
-        signInNavViewModel = SignInNavViewModel(
-            googleSignInUseCase = googleSignInUseCase,
-            getSignInLoadingStateUseCase = getSignInLoadingStateUseCase,
-            firebaseAuthErrorUseCase = FirebaseAuthErrorUseCase(),
-            savedStateHandle = SavedStateHandle(mapOf(GoogleButtonEnabledKey to true))
-        )
+        rule.setContent {
+            uiState.collectAsStateWithLifecycle()
+        }
     }
 
     @Test
     fun signInNavUiStateTest() = runTest(dispatcher) {
-        val uiState = signInNavViewModel.uiState
-        backgroundScope.launch(dispatcher) { uiState.collectLatest {} }
+        val state = signInNavViewModel.signInNavState
 
-        signInNavViewModel.setGoogleButtonEnable(false)
-        wait()
+        state.googleButtonDisable()
+        waitTime()
         assert(!uiState.value.googleButtonEnabled)
 
-        signInNavViewModel.setGoogleButtonEnable(true)
-        wait()
+        state.googleButtonEnabled()
+        waitTime()
         assert(uiState.value.googleButtonEnabled)
+
+        state.setLoading(true)
+        waitTime()
+        assert(uiState.value.isLoading)
+
+        state.setLoading(false)
+        waitTime()
+        assert(!uiState.value.isLoading)
     }
 }

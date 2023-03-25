e2c8969ed255ee4f116943372cbf30fa275efea0
leebeebeom <boole92@naver.com>
2023-03-26 05:51:34 +0900

    로딩 테스트 추가

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreenTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreenTest.kt
@@ -1,5 +1,6 @@
 package com.leebeebeom.clothinghelper.ui.signin.ui.signin
 
+import androidx.annotation.StringRes
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
@@ -9,9 +10,11 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.data.InvalidEmail
+import com.leebeebeom.clothinghelper.data.NotFoundEmail
 import com.leebeebeom.clothinghelper.data.SignInEmail
 import com.leebeebeom.clothinghelper.data.SignInPassword
 import com.leebeebeom.clothinghelper.ui.*
+import com.leebeebeom.clothinghelper.ui.components.CenterDotProgressIndicatorTag
 import com.leebeebeom.clothinghelper.ui.main.mainScreen.MainScreenTag
 import com.leebeebeom.clothinghelper.ui.signin.ui.resetpassword.ResetPasswordScreenTag
 import com.leebeebeom.clothinghelper.ui.signin.ui.signup.SignUpScreenTag
@@ -46,33 +49,46 @@ class SignInScreenTest {
     }
 
     @Test
-    fun signInRestoreTest() {
-        emailTextField.performTextInput(InvalidEmail)
-        passwordTestField.performTextInput(SignInPassword)
-        signInButton.performClick()
+    fun signInRestoreTest() { // with error, loading
+        fun localSignInRestoreTest(
+            email: String,
+            password: String,
+            @StringRes error: Int
+        ) {
+            emailTextField.performTextInput(email)
+            passwordTestField.performTextInput(password)
+            signInButton.performClick()
+            rule.waitTagExist(CenterDotProgressIndicatorTag)
 
-        repeat(2) {
-            rule.waitStringResExist(R.string.error_invalid_email)
-            rule.onNodeWithText(InvalidEmail).assertExists()
-            rule.onNodeWithText(getInvisibleText(SignInPassword.length)).assertExists()
+            repeat(2) {
+                rule.waitStringResExist(error)
+                rule.onNodeWithText(email).assertExists()
+                rule.onNodeWithText(getInvisibleText(password.length)).assertExists()
 
-            restorationTester.emulateSavedInstanceStateRestore()
+                restorationTester.emulateSavedInstanceStateRestore()
+            }
+
+            emailTextField.performTextClearance()
+            passwordTestField.performTextClearance()
         }
 
-        emailTextField.performTextClearance()
-        passwordTestField.performTextClearance()
+        localSignInRestoreTest(
+            email = InvalidEmail,
+            password = SignInPassword,
+            error = R.string.error_invalid_email
+        )
 
-        emailTextField.performTextInput(SignInEmail)
-        passwordTestField.performTextInput("123456")
-        signInButton.performClick()
+        localSignInRestoreTest(
+            email = NotFoundEmail,
+            password = SignInPassword,
+            error = R.string.error_user_not_found
+        )
 
-        repeat(2) {
-            rule.waitStringResExist(R.string.error_wrong_password)
-            rule.onNodeWithText(SignInEmail).assertExists()
-            rule.onNodeWithText(getInvisibleText("123456".length)).assertExists()
-
-            restorationTester.emulateSavedInstanceStateRestore()
-        }
+        localSignInRestoreTest(
+            email = SignInEmail,
+            password = "123456",
+            error = R.string.error_wrong_password
+        )
     }
 
     @Test

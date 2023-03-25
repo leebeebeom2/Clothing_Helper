e700b3790debc0dafe0201416a9c6befe6cc206e
leebeebeom <boole92@naver.com>
2023-03-26 00:05:14 +0900

    Job 반환 취소

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/UserRepository.kt
@@ -5,33 +5,32 @@ import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuthException
 import com.leebeebeom.clothinghelper.domain.model.User
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.SharedFlow
 
 interface UserRepository {
     val userStream: SharedFlow<User?>
     fun getUser(): User?
-    suspend fun googleSignIn(credential: AuthCredential): Job
+    suspend fun googleSignIn(credential: AuthCredential)
 
     /**
      * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
      * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
      * @throws FirebaseAuthException - InvalidEmail, NotFoundUser, WrongPassword 등
      */
-    suspend fun signIn(email: String, password: String): Job
+    suspend fun signIn(email: String, password: String)
 
     /**
      * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
      * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
      * @throws FirebaseAuthException - InvalidEmail, EmailAlreadyInUse 등
      */
-    suspend fun signUp(email: String, password: String, name: String): Job
+    suspend fun signUp(email: String, password: String, name: String)
 
     /**
      * @throws FirebaseNetworkException - 인터넷에 연결되지 않았을 경우
      * @throws FirebaseTooManyRequestsException - 너무 많은 요청이 발생했을 경우
      * @throws FirebaseAuthException - InvalidEmail, NotFoundUser 등
      */
-    suspend fun sendResetPasswordEmail(email: String): Job
+    suspend fun sendResetPasswordEmail(email: String)
     fun signOut()
 }

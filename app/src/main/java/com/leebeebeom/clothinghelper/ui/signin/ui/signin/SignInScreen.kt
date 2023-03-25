077c6552f19fa7245e9c8a1baf10fd95d05d9016
leebeebeom <boole92@naver.com>
2023-03-26 05:32:05 +0900

    테스트 완

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreen.kt
@@ -1,58 +1,70 @@
 package com.leebeebeom.clothinghelper.ui.signin.ui.signin
 
+import androidx.activity.result.ActivityResult
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.ActivityViewModel
-import com.leebeebeom.clothinghelper.ui.activityViewModel
-import com.leebeebeom.clothinghelper.ui.components.HeightSpacer
-import com.leebeebeom.clothinghelper.ui.components.IconWrapper
-import com.leebeebeom.clothinghelper.ui.components.MaxWidthButton
-import com.leebeebeom.clothinghelper.ui.components.SingleLineText
+import com.leebeebeom.clothinghelper.ui.components.*
 import com.leebeebeom.clothinghelper.ui.signin.components.GoogleSignInButton
 import com.leebeebeom.clothinghelper.ui.signin.components.Logo
 import com.leebeebeom.clothinghelper.ui.signin.components.OrDivider
 import com.leebeebeom.clothinghelper.ui.signin.components.SignInBaseColumn
 import com.leebeebeom.clothinghelper.ui.signin.components.textfield.EmailTextField
 import com.leebeebeom.clothinghelper.ui.signin.components.textfield.PasswordTextField
-import com.leebeebeom.clothinghelper.ui.signin.state.EmailAndPasswordState
-import com.leebeebeom.clothinghelper.ui.signin.ui.SignInNavUiState
-import com.leebeebeom.clothinghelper.ui.signin.ui.SignInNavViewModel
+import com.leebeebeom.clothinghelper.ui.util.ShowToast
 
-const val SIGN_IN_SCREEN_TAG = "sign in screen"
+const val SignInScreenTag = "sign in screen"
 
 @Composable
 fun SignInScreen(
     navigateToResetPassword: () -> Unit,
     navigateToSignUp: () -> Unit,
-    signInNavViewModel: SignInNavViewModel,
-    signInNavUiState: SignInNavUiState = signInNavViewModel.uiState,
     viewModel: SignInViewModel = hiltViewModel(),
-    uiState: EmailAndPasswordState = viewModel.uiState,
-    activityViewModel: ActivityViewModel = activityViewModel(),
+    showToast: ShowToast
 ) {
-    SignInBaseColumn(Modifier.testTag(SIGN_IN_SCREEN_TAG)) {
+    val state = viewModel.signInState
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    SignInBaseColumn(modifier = Modifier.testTag(SignInScreenTag)) {
         Logo()
-        EmailTextField(error = { uiState.emailError }, onInputChange = viewModel::onEmailChange)
+        EmailTextField(
+            initialEmail = state.savedEmail,
+            error = { uiState.emailError },
+            onEmailChange = state::setEmail
+        )
         PasswordTextField(
-            error = { uiState.passwordError }, onInputChange = viewModel::onPasswordChange
+            initialPassword = state.savedPassword,
+            error = { uiState.passwordError },
+            onInputChange = state::setPassword,
+            imeAction = ImeAction.Done
         )
 
         ForgotPasswordText(navigateToResetPassword = navigateToResetPassword)
 
+        val onSignInButtonClick: () -> Unit = remember {
+            {
+                viewModel.signInWithEmailAndPassword(
+                    showToast = showToast
+                )
+            }
+        }
         MaxWidthButton(
             text = R.string.sign_in,
-            enabled = { uiState.buttonEnabled },
-            onClick = { viewModel.signInWithEmailAndPassword(activityViewModel::showToast) },
+            enabled = { uiState.buttonEnable },
+            onClick = onSignInButtonClick,
         )
         OrDivider()
         MaxWidthButton(text = R.string.sign_up_with_email,
@@ -60,12 +72,17 @@ fun SignInScreen(
             colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.surface),
             icon = { IconWrapper(drawable = R.drawable.ic_email) })
         HeightSpacer(dp = 12)
-        GoogleSignInButton(enabled = { signInNavUiState.googleButtonEnabled }, onActivityResult = {
-            signInNavViewModel.signInWithGoogleEmail(
-                activityResult = it, showToast = activityViewModel::showToast
-            )
-        }, disable = { signInNavViewModel.setGoogleButtonEnable(false) })
+
+        val onActivityResult: (ActivityResult) -> Unit = remember {
+            { viewModel.signInWithGoogleEmail(activityResult = it, showToast = showToast) }
+        }
+        GoogleSignInButton(
+            enabled = { uiState.googleButtonEnabled },
+            onActivityResult = onActivityResult,
+            disable = state::googleButtonDisable
+        )
     }
+    CenterDotProgressIndicator(show = { uiState.isLoading })
 }
 
 @Composable

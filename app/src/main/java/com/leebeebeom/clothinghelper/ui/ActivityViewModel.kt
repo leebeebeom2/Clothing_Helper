6a3a089797ebed64dfce52e6d32a07c26dbc9ceb
leebeebeom <boole92@naver.com>
2023-03-28 06:51:20 +0900

    토스트 제거

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/ActivityViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/ActivityViewModel.kt
@@ -3,52 +3,26 @@ package com.leebeebeom.clothinghelper.ui
 import android.content.Context
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.leebeebeom.clothinghelper.domain.model.User
 import com.leebeebeom.clothinghelper.domain.usecase.user.GetUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class ActivityViewModel @Inject constructor(getUserUseCase: GetUserUseCase) :
     ViewModel() {
-    private val toastTexts = mutableStateListOf<Int>()
-    private val toastTextsFlow = snapshotFlow { toastTexts }
-
-    val activityUiState =
-        combine(
-            flow = toastTextsFlow,
-            flow2 = getUserUseCase.userFlow
-        ) { toastTexts, user ->
-            ActivityUiState(toastTexts = toastTexts, user = user)
-        }.distinctUntilChanged().stateIn(
+    val user =
+        getUserUseCase.userFlow.stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5000),
-            initialValue = ActivityUiState(user = getUserUseCase.getUser())
+            initialValue = getUserUseCase.getUser()
         )
-
-    fun addToastTextAtLast(toastText: Int) {
-        toastTexts.add(toastText)
-    }
-
-    fun removeFirstToastText() {
-        toastTexts.removeFirst()
-    }
 }
-
-data class ActivityUiState(
-    val toastTexts: List<Int> = emptyList(),
-    val user: User? = null,
-)
 
 @Composable
 fun activityViewModel(context: Context = LocalContext.current): ActivityViewModel =

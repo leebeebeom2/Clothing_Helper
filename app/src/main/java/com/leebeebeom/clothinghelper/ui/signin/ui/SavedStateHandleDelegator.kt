b450e34b77498b39b14fea17a8b0c4de3558748b
leebeebeom <boole92@naver.com>
2023-03-27 15:40:54 +0900

    nullable 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/SavedStateHandleDelegator.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/SavedStateHandleDelegator.kt
@@ -4,20 +4,15 @@ import androidx.lifecycle.SavedStateHandle
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-class SavedStateHandleDelegator<T : Any>(
+class SavedStateHandleDelegator<T : Any?>(
     private val savedStateHandle: SavedStateHandle,
     private val key: String,
     private val initial: T
 ) : ReadWriteProperty<Any, T> {
-    private lateinit var value: T
-    override fun getValue(thisRef: Any, property: KProperty<*>): T {
-        if (!::value.isInitialized)
-            value = savedStateHandle.get<T>(key) ?: initial
-        return value
-    }
+    override fun getValue(thisRef: Any, property: KProperty<*>) =
+        savedStateHandle.get<T>(key) ?: initial
 
     override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
         savedStateHandle[key] = value
-        this.value = value
     }
 }

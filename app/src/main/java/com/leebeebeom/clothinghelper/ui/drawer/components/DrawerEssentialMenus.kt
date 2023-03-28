139a41dd069dba1faaa169a8f14b208ce9bf8573
leebeebeom <boole92@naver.com>
2023-03-28 23:56:47 +0900

    수정

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/components/DrawerEssentialMenus.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/components/DrawerEssentialMenus.kt
@@ -1,13 +1,13 @@
 package com.leebeebeom.clothinghelper.ui.drawer.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -17,7 +17,6 @@ import com.leebeebeom.clothinghelper.ui.components.IconWrapper
 import com.leebeebeom.clothinghelper.ui.components.SingleLineText
 import com.leebeebeom.clothinghelper.ui.components.WidthSpacer
 import com.leebeebeom.clothinghelper.ui.drawer.drawer.components.DrawerRow
-import com.leebeebeom.clothinghelper.ui.theme.DarkGray
 import com.leebeebeom.clothinghelper.ui.theme.Disabled
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
@@ -25,25 +24,17 @@ import kotlinx.collections.immutable.toImmutableList
 const val DrawerEssentialMenuHeight = 40
 const val DrawerEssentialMenuIconSize = 22
 
-@Composable
-fun DrawerEssentialMenus(
+fun LazyListScope.drawerEssentialMenus(
     onEssentialMenuClick: (EssentialMenuType) -> Unit,
-    essentialMenus: ImmutableList<EssentialMenu> = remember { getEssentialMenus() }
+    essentialMenus: ImmutableList<EssentialMenu>
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = DarkGray),
-        contentPadding = PaddingValues(top = 4.dp, bottom = 40.dp)
-    ) {
-        this.items(items = essentialMenus, key = { it.name }) { essentialMenu ->
-            EssentialMenu(
-                essentialMenu = essentialMenu,
-                onClick = { essentialMenuType -> onEssentialMenuClick(essentialMenuType) },
-            )
-        }
-        item { Divider(color = Disabled, modifier = Modifier.padding(vertical = 8.dp)) }
+    this.items(items = essentialMenus, key = { it.name }) { essentialMenu ->
+        EssentialMenu(
+            essentialMenu = essentialMenu,
+            onClick = { essentialMenuType -> onEssentialMenuClick(essentialMenuType) },
+        )
     }
+    item { Divider(color = Disabled, modifier = Modifier.padding(vertical = 8.dp)) }
 }
 
 @Composable

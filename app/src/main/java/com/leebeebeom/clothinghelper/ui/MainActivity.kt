62340e830cc50c3d6acf6b32ea2c108ca6cfa41a
leebeebeom <boole92@naver.com>
2023-03-29 03:09:42 +0900

    skippable 체크

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/MainActivity.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -37,7 +38,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
+@Composable // skippable
 fun MainNavHost(
     viewModel: ActivityViewModel = activityViewModel(),
     navController: NavHostController = rememberNavController(),

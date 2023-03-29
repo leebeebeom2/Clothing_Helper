eb40c53118bd91d5a10191b62f17043262500263
leebeebeom <boole92@naver.com>
2023-03-30 04:34:33 +0900

    state 구조변경

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreen.kt
@@ -7,6 +7,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -15,14 +18,15 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.components.*
-import com.leebeebeom.clothinghelper.ui.main.composables.ToastWrapper
-import com.leebeebeom.clothinghelper.ui.signin.components.GoogleSignInButton
-import com.leebeebeom.clothinghelper.ui.signin.components.Logo
-import com.leebeebeom.clothinghelper.ui.signin.components.OrDivider
-import com.leebeebeom.clothinghelper.ui.signin.components.SignInBaseColumn
-import com.leebeebeom.clothinghelper.ui.signin.components.textfield.EmailTextField
-import com.leebeebeom.clothinghelper.ui.signin.components.textfield.PasswordTextField
+import com.leebeebeom.clothinghelper.ui.component.*
+import com.leebeebeom.clothinghelper.ui.main.component.ToastWrapper
+import com.leebeebeom.clothinghelper.ui.signin.component.GoogleSignInButton
+import com.leebeebeom.clothinghelper.ui.signin.component.Logo
+import com.leebeebeom.clothinghelper.ui.signin.component.OrDivider
+import com.leebeebeom.clothinghelper.ui.signin.component.SignInBaseColumn
+import com.leebeebeom.clothinghelper.ui.signin.component.textfield.EmailTextField
+import com.leebeebeom.clothinghelper.ui.signin.component.textfield.PasswordTextField
+import com.leebeebeom.clothinghelper.ui.signin.state.MutablePasswordState
 
 const val SignInScreenTag = "sign in screen"
 
@@ -32,26 +36,36 @@ fun SignInScreen(
     navigateToSignUp: () -> Unit,
     viewModel: SignInViewModel = hiltViewModel()
 ) {
-    val state = viewModel.signInState
+    val state = rememberSignInScreenState()
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     SignInBaseColumn(modifier = Modifier.testTag(SignInScreenTag)) {
         Logo()
         EmailTextField(
-            error = { uiState.emailError }, onEmailChange = state::setEmail
+            error = { state.emailError }, onEmailChange = state::onEmailChange
         )
         PasswordTextField(
-            error = { uiState.passwordError },
-            onInputChange = state::setPassword,
+            error = { state.passwordError },
+            onInputChange = state::onPasswordChange,
             imeAction = ImeAction.Done
         )
 
         ForgotPasswordText(navigateToResetPassword = navigateToResetPassword)
 
+        val signInButtonClick = remember {
+            {
+                viewModel.signInWithEmailAndPassword(
+                    email = state.email,
+                    password = state.password,
+                    setEmailError = { state.emailError = it },
+                    setPasswordError = { state.passwordError = it }
+                )
+            }
+        }
         MaxWidthButton(
             text = R.string.sign_in,
-            enabled = { uiState.buttonEnable },
-            onClick = viewModel::signInWithEmailAndPassword,
+            enabled = { state.buttonEnabled },
+            onClick = signInButtonClick,
         )
         OrDivider()
         MaxWidthButton(text = R.string.sign_up_with_email,
@@ -78,3 +92,32 @@ private fun ForgotPasswordText(navigateToResetPassword: () -> Unit) {
         }
     }
 }
+
+class MutableSignInScreenState(
+    initialEmail: String = "",
+    initialEmailError: Int? = null,
+    initialPassword: String = "",
+    initialPasswordError: Int? = null,
+) : MutablePasswordState(
+    initialEmail = initialEmail,
+    initialEmailError = initialEmailError,
+    initialPassword = initialPassword,
+    initialPasswordError = initialPasswordError
+) {
+    companion object {
+        val Saver = listSaver<MutableSignInScreenState, Any?>(save = {
+            arrayListOf(it.email, it.emailError, it.password, it.passwordError)
+        }, restore = {
+            MutableSignInScreenState(
+                it[0] as String,
+                it[1] as Int?,
+                it[2] as String,
+                it[3] as Int?,
+            )
+        })
+    }
+}
+
+@Composable
+fun rememberSignInScreenState() =
+    rememberSaveable(saver = MutableSignInScreenState.Saver) { MutableSignInScreenState() }

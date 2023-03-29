4e981aed2d8d474e2cce9494792851993ecad4c0
leebeebeom <boole92@naver.com>
2023-03-29 23:58:27 +0900

    toImmutableList() 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/ToastViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/ToastViewModel.kt
@@ -3,10 +3,11 @@ package com.leebeebeom.clothinghelper.ui
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
+import kotlinx.collections.immutable.toImmutableList
 
 abstract class ToastViewModel : ViewModel() {
     private val toastTexts = mutableStateListOf<Int>()
-    protected val toastTextsFlow = snapshotFlow { toastTexts }
+    protected val toastTextsFlow = snapshotFlow { toastTexts.toImmutableList() }
 
     fun addToastTextAtLast(toastText: Int) {
         toastTexts.add(toastText)

795a421c16a50484e2059a9f4a929a904b98ad88
leebeebeom <boole92@naver.com>
2023-03-30 05:32:16 +0900

    state 구조변경

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signup/SignUpScreen.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/signup/SignUpScreen.kt
@@ -1,8 +1,9 @@
 package com.leebeebeom.clothinghelper.ui.signin.ui.signup
 
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.ImeAction
@@ -10,55 +11,66 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.components.CenterDotProgressIndicator
-import com.leebeebeom.clothinghelper.ui.components.HeightSpacer
-import com.leebeebeom.clothinghelper.ui.components.MaxWidthButton
-import com.leebeebeom.clothinghelper.ui.components.StatefulMaxWidthTestFieldWithCancelIcon
-import com.leebeebeom.clothinghelper.ui.main.composables.ToastWrapper
-import com.leebeebeom.clothinghelper.ui.signin.components.GoogleSignInButton
-import com.leebeebeom.clothinghelper.ui.signin.components.Logo
-import com.leebeebeom.clothinghelper.ui.signin.components.OrDivider
-import com.leebeebeom.clothinghelper.ui.signin.components.SignInBaseColumn
-import com.leebeebeom.clothinghelper.ui.signin.components.textfield.EmailTextField
-import com.leebeebeom.clothinghelper.ui.signin.components.textfield.PasswordTextField
+import com.leebeebeom.clothinghelper.ui.component.CenterDotProgressIndicator
+import com.leebeebeom.clothinghelper.ui.component.HeightSpacer
+import com.leebeebeom.clothinghelper.ui.component.MaxWidthButton
+import com.leebeebeom.clothinghelper.ui.component.StatefulMaxWidthTestFieldWithCancelIcon
+import com.leebeebeom.clothinghelper.ui.main.component.ToastWrapper
+import com.leebeebeom.clothinghelper.ui.signin.component.GoogleSignInButton
+import com.leebeebeom.clothinghelper.ui.signin.component.Logo
+import com.leebeebeom.clothinghelper.ui.signin.component.OrDivider
+import com.leebeebeom.clothinghelper.ui.signin.component.SignInBaseColumn
+import com.leebeebeom.clothinghelper.ui.signin.component.textfield.EmailTextField
+import com.leebeebeom.clothinghelper.ui.signin.component.textfield.PasswordTextField
+import com.leebeebeom.clothinghelper.ui.signin.state.PasswordState2
 
 const val SignUpScreenTag = "sign up screen"
 
 @Composable // skippable
 fun SignUpScreen(viewModel: SignUpViewModel = hiltViewModel()) {
-    val state = viewModel.signUpState
+    val state = rememberSignUpState()
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     SignInBaseColumn(modifier = Modifier.testTag(SignUpScreenTag)) {
         Logo()
         EmailTextField(
-            error = { uiState.emailError }, onEmailChange = state::setEmail
+            error = { state.emailError }, onEmailChange = state::onEmailChange
         )
 
         StatefulMaxWidthTestFieldWithCancelIcon(
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Text, imeAction = ImeAction.Next
-            ), onInputChange = state::setName, blockBlank = false, label = R.string.nickname
+            ), onInputChange = state::onNameChange, blockBlank = false, label = R.string.nickname
         )
 
         PasswordTextField(
-            error = { uiState.passwordError },
+            error = { state.passwordError },
             imeAction = ImeAction.Next,
-            onInputChange = state::setPassword
+            onInputChange = state::onPasswordChange
         )
 
         PasswordTextField(
             label = R.string.password_confirm,
-            error = { uiState.passwordConfirmError },
+            error = { state.passwordConfirmError },
             imeAction = ImeAction.Done,
-            onInputChange = state::setPasswordConfirm
+            onInputChange = state::onPasswordConfirmChange
         )
-
         HeightSpacer(dp = 12)
+
+        val signUpButtonClick = remember {
+            {
+                viewModel.signUpWithEmailAndPassword(
+                    email = state.email,
+                    password = state.password,
+                    name = state.name,
+                    setEmailError = state::setEmailError
+                )
+            }
+        }
         MaxWidthButton(
             text = R.string.sign_up,
-            enabled = { uiState.buttonEnabled },
-            onClick = viewModel::signUpWithEmailAndPassword,
+            enabled = { state.buttonEnabled },
+            onClick = signUpButtonClick,
         )
         OrDivider()
         GoogleSignInButton(onResult = viewModel::signInWithGoogleEmail)
@@ -66,3 +78,90 @@ fun SignUpScreen(viewModel: SignUpViewModel = hiltViewModel()) {
     CenterDotProgressIndicator(show = { uiState.isLoading })
     ToastWrapper(toastTexts = { uiState.toastTexts }, toastShown = viewModel::removeFirstToastText)
 }
+
+class SignUpScreenState(
+    initialEmail: String = "",
+    initialEmailError: Int? = null,
+    initialName: String = "",
+    initialPassword: String = "",
+    initialPasswordError: Int? = null,
+    initialPasswordConfirm: String = "",
+    initialPasswordConfirmError: Int? = null,
+) : PasswordState2(
+    initialEmail = initialEmail,
+    initialEmailError = initialEmailError,
+    initialPassword = initialPassword,
+    initialPasswordError = initialPasswordError
+) {
+    var name by mutableStateOf(initialName)
+        private set
+    var passwordConfirm by mutableStateOf(initialPasswordConfirm)
+        private set
+    var passwordConfirmError: Int? by mutableStateOf(initialPasswordConfirmError)
+        private set
+    override val buttonEnabled by derivedStateOf {
+        super.buttonEnabled && name.isNotBlank() && passwordConfirm.isNotBlank() && passwordConfirmError == null
+    }
+
+    fun onNameChange(name: String) {
+        this.name = name
+    }
+
+    override fun onPasswordChange(password: String) {
+        super.onPasswordChange(password)
+
+        if (password.isBlank()) {
+            setPasswordNotSameError()
+            return
+        }
+        if (password.length < 6) passwordError = R.string.error_weak_password
+
+        setPasswordNotSameError()
+    }
+
+    fun onPasswordConfirmChange(passwordConfirm: String) {
+        if (this.passwordConfirm == passwordConfirm) return
+        this.passwordConfirm = passwordConfirm
+        setPasswordNotSameError()
+    }
+
+    private fun setPasswordNotSameError() {
+        if (passwordConfirm.isBlank()) {
+            passwordConfirmError = null
+            return
+        }
+        passwordConfirmError =
+            if (password != passwordConfirm) R.string.error_password_confirm_not_same else null
+    }
+
+    companion object {
+        val Saver = listSaver<SignUpScreenState, Any?>(
+            save = {
+                listOf(
+                    it.email,
+                    it.emailError,
+                    it.name,
+                    it.password,
+                    it.passwordError,
+                    it.passwordConfirm,
+                    it.passwordConfirmError,
+                )
+            },
+            restore = {
+                SignUpScreenState(
+                    initialEmail = it[0] as String,
+                    initialEmailError = it[1] as Int?,
+                    initialName = it[2] as String,
+                    initialPassword = it[3] as String,
+                    initialPasswordError = it[4] as Int?,
+                    initialPasswordConfirm = it[5] as String,
+                    initialPasswordConfirmError = it[6] as Int?,
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun rememberSignUpState() =
+    rememberSaveable(saver = SignUpScreenState.Saver) { SignUpScreenState() }

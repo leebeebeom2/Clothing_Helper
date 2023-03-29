5ca716df816b7b9d5404bea491f429aec0683e10
leebeebeom <boole92@naver.com>
2023-03-29 23:02:21 +0900

    initialText 삭제, state 구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/components/textfield/PasswordTextField.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/components/textfield/PasswordTextField.kt
@@ -3,45 +3,45 @@ package com.leebeebeom.clothinghelper.ui.signin.components.textfield
 import androidx.annotation.StringRes
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.LocalContentColor
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.components.CustomIconButton
-import com.leebeebeom.clothinghelper.ui.components.StatefulMaxWidthTestField
+import com.leebeebeom.clothinghelper.ui.components.StatefulMaxWidthTextField
 
 @Composable // skippable
 fun PasswordTextField(
     @StringRes label: Int = R.string.password,
-    initialPassword: String,
     error: () -> Int?,
     imeAction: ImeAction,
     onInputChange: (String) -> Unit,
 ) {
     var isVisible by rememberSaveable { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
 
-    StatefulMaxWidthTestField(initialText = initialPassword,
+    StatefulMaxWidthTextField(
+        blockBlank = true,
+        label = label,
+        error = error,
+        isVisible = { isVisible },
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Password, imeAction = imeAction
         ),
         onInputChange = onInputChange,
-        blockBlank = true,
-        error = error,
-        label = label,
-        isVisible = { isVisible },
-        trailingIcon = { focusRequester ->
+        trailingIcon = {
             VisibleIcon(isVisible = { isVisible }) {
                 focusRequester.requestFocus()
                 isVisible = !isVisible
             }
-        })
+        },
+        focusRequester = focusRequester
+    )
 }
 
 const val VisibleIconTag = "visible icon"

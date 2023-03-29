5f6c5000fd8fd34d811945802c621643b2761d83
leebeebeom <boole92@naver.com>
2023-03-29 23:14:58 +0900

    GoogleSignInState 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/GoogleSignInViewModel.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/ui/GoogleSignInViewModel.kt
@@ -2,11 +2,6 @@ package com.leebeebeom.clothinghelper.ui.signin.ui
 
 import android.app.Activity
 import androidx.activity.result.ActivityResult
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
@@ -17,7 +12,6 @@ import com.leebeebeom.clothinghelper.domain.usecase.user.GoogleSignInUseCase
 import com.leebeebeom.clothinghelper.ui.ToastViewModel
 import com.leebeebeom.clothinghelper.util.buildConfigLog
 import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 /**
@@ -25,37 +19,38 @@ import kotlinx.coroutines.launch
  */
 abstract class GoogleSignInViewModel(private val googleSignInUseCase: GoogleSignInUseCase) :
     ToastViewModel() {
-    abstract val googleSignInState: GoogleSignInState
+    abstract fun setLoading(loading: Boolean)
 
-    fun signInWithGoogleEmail(activityResult: ActivityResult) =
-        when (activityResult.resultCode) {
-            Activity.RESULT_OK -> googleSignIn(activityResult = activityResult)
-            Activity.RESULT_CANCELED -> {
-                addToastTextAtLast(R.string.canceled)
-                googleSignInState.googleButtonEnabled()
-                null
-            }
-            else -> {
-                buildConfigLog(
-                    site = "signInWithGoogleEmail",
-                    msg = "resultCode = ${activityResult.resultCode}",
-                )
-                addToastTextAtLast(R.string.unknown_error)
-                googleSignInState.googleButtonEnabled()
-                null
-            }
+    fun signInWithGoogleEmail(
+        activityResult: ActivityResult, googleSignInButtonEnable: () -> Unit
+    ) = when (activityResult.resultCode) {
+        Activity.RESULT_OK -> googleSignIn(
+            activityResult = activityResult, googleSignInButtonEnable = googleSignInButtonEnable
+        )
+        Activity.RESULT_CANCELED -> {
+            addToastTextAtLast(R.string.canceled)
+            googleSignInButtonEnable()
         }
+        else -> {
+            buildConfigLog(
+                site = "signInWithGoogleEmail",
+                msg = "resultCode = ${activityResult.resultCode}",
+            )
+            addToastTextAtLast(R.string.unknown_error)
+            googleSignInButtonEnable()
+        }
+    }
 
-    private fun googleSignIn(activityResult: ActivityResult): Job {
+    private fun googleSignIn(activityResult: ActivityResult, googleSignInButtonEnable: () -> Unit) {
         val handler = CoroutineExceptionHandler { _, throwable ->
             buildConfigLog("GoogleSignInViewModel", "$throwable")
             addToastTextAtLast(R.string.unknown_error)
-            googleSignInState.setLoading(false)
-            googleSignInState.googleButtonEnabled()
+            setLoading(false)
+            googleSignInButtonEnable()
         }
 
-        return viewModelScope.launch(handler) {
-            googleSignInState.setLoading(true)
+        viewModelScope.launch(handler) {
+            setLoading(true)
             googleSignInUseCase.googleSignIn(credential = getGoogleCredential(activityResult = activityResult))
         }
     }
@@ -64,35 +59,5 @@ abstract class GoogleSignInViewModel(private val googleSignInUseCase: GoogleSign
         val account = GoogleSignIn.getSignedInAccountFromIntent(activityResult.data)
             .getResult(ApiException::class.java)
         return GoogleAuthProvider.getCredential(account.idToken, null)
-    }
-}
-
-abstract class GoogleSignInUiState {
-    abstract val googleButtonEnabled: Boolean
-    abstract val isLoading: Boolean
-}
-
-abstract class GoogleSignInState(
-    savedStateHandle: SavedStateHandle,
-    savedEmailKey: String,
-    emailErrorKey: String,
-    savedPasswordKey: String,
-    passwordErrorKey: String
-) : PasswordState(
-    savedStateHandle = savedStateHandle,
-    emailKey = savedEmailKey,
-    emailErrorKey = emailErrorKey,
-    passwordKey = savedPasswordKey,
-    passwordErrorKey = passwordErrorKey
-) {
-    private var googleButtonEnabledState by mutableStateOf(true)
-    val googleButtonEnabledFlow = snapshotFlow { googleButtonEnabledState }
-
-    fun googleButtonEnabled() {
-        googleButtonEnabledState = true
-    }
-
-    fun googleButtonDisable() {
-        googleButtonEnabledState = false
     }
 }

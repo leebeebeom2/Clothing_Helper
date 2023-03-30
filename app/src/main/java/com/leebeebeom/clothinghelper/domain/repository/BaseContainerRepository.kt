101b588e2c2f4df26f1b0c64fad59e283d96ce84
leebeebeom <boole92@naver.com>
2023-03-30 16:26:24 +0900

    List -> ImmutableList 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseContainerRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseContainerRepository.kt
@@ -2,10 +2,11 @@ package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.domain.model.BaseContainerModel
 import com.leebeebeom.clothinghelper.ui.drawer.contents.MainCategoryType
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.ImmutableMap
 import kotlinx.coroutines.flow.SharedFlow
 
 interface BaseContainerRepository<T : BaseContainerModel> : BaseDataRepository<T> {
-    val allDataNames: SharedFlow<ImmutableMap<MainCategoryType, List<String>>>
+    val allDataNames: SharedFlow<ImmutableMap<MainCategoryType, ImmutableList<String>>>
     val allDataSizeFlow: SharedFlow<ImmutableMap<MainCategoryType, Int>>
 }

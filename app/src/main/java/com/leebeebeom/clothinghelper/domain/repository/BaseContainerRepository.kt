74473d806985cc2cb1ec6c19def9e358df9ab569
leebeebeom <boole92@naver.com>
2023-03-30 16:09:55 +0900

    ImmutableMap 패키지 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseContainerRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseContainerRepository.kt
@@ -1,8 +1,8 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
-import com.google.common.collect.ImmutableMap
 import com.leebeebeom.clothinghelper.domain.model.BaseContainerModel
 import com.leebeebeom.clothinghelper.ui.drawer.contents.MainCategoryType
+import kotlinx.collections.immutable.ImmutableMap
 import kotlinx.coroutines.flow.SharedFlow
 
 interface BaseContainerRepository<T : BaseContainerModel> : BaseDataRepository<T> {

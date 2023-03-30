ada57f98858c86e7cc2c5096008471c8d9f8d6a0
leebeebeom <boole92@naver.com>
2023-03-30 15:43:18 +0900

    BaseContainerRepository 생성

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/FolderRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/FolderRepository.kt
@@ -2,4 +2,4 @@ package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.domain.model.Folder
 
-interface FolderRepository : BaseDataRepository<Folder>
+interface FolderRepository : BaseContainerRepository<Folder>
--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
@@ -2,4 +2,4 @@ package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.domain.model.SubCategory
 
-interface SubCategoryRepository : BaseDataRepository<SubCategory>
+interface SubCategoryRepository : BaseContainerRepository<SubCategory>

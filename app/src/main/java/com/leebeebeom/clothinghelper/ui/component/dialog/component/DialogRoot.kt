25d48f3c2c8fee97a8b29d4d968228c0b2c587a9
leebeebeom <boole92@naver.com>
2023-03-31 04:05:50 +0900

    Tag 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/component/dialog/component/DialogRoot.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/component/dialog/component/DialogRoot.kt
@@ -6,9 +6,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.leebeebeom.clothinghelper.ui.component.HeightSpacer
+
+const val DialogRootTag = "dialog root"
 
 @Composable // skippable
 fun DialogRoot(onDismiss: () -> Unit, content: @Composable () -> Unit) {
@@ -18,7 +21,9 @@ fun DialogRoot(onDismiss: () -> Unit, content: @Composable () -> Unit) {
             shape = MaterialTheme.shapes.medium
         ) {
             Column(
-                modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 20.dp)
+                modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp, top = 20.dp)
+                    .testTag(DialogRootTag)
             ) {
                 content()
                 HeightSpacer(dp = 20)

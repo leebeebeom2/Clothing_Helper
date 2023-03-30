324ee6a6486a35ec8b6911e7a6c0a5f200c471d7
leebeebeom <boole92@naver.com>
2023-03-30 15:37:44 +0900

    List -> ImmutableList 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseDataRepository.kt
@@ -1,15 +1,17 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
+import com.google.common.collect.ImmutableList
 import com.leebeebeom.clothinghelper.domain.model.BaseModel
 import kotlinx.coroutines.flow.SharedFlow
 
 interface BaseDataRepository<T : BaseModel> {
     val allDataFlow: SharedFlow<DataResult<T>>
+    val allDataSizeFlow: SharedFlow<Int>
     suspend fun add(data: T)
     suspend fun push(data: T)
 }
 
-sealed class DataResult<T>(val data: List<T>) {
-    class Success<T>(data: List<T>) : DataResult<T>(data)
-    class Fail<T>(data: List<T>, val exception: Throwable) : DataResult<T>(data)
+sealed class DataResult<T>(val data: ImmutableList<T>) {
+    class Success<T>(data: ImmutableList<T>) : DataResult<T>(data)
+    class Fail<T>(data: ImmutableList<T>, val exception: Throwable) : DataResult<T>(data)
 }

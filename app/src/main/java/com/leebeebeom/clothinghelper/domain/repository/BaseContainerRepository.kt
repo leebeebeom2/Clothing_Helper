0aeeeac882c3eaf90ad227e84752bfe0bd8aa7c3
leebeebeom <boole92@naver.com>
2023-03-30 15:59:40 +0900

    size 추가, List -> Map 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseContainerRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/BaseContainerRepository.kt
@@ -1,8 +1,11 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
+import com.google.common.collect.ImmutableMap
 import com.leebeebeom.clothinghelper.domain.model.BaseContainerModel
+import com.leebeebeom.clothinghelper.ui.drawer.contents.MainCategoryType
 import kotlinx.coroutines.flow.SharedFlow
 
 interface BaseContainerRepository<T : BaseContainerModel> : BaseDataRepository<T> {
-    val allDataNames: SharedFlow<List<String>>
+    val allDataNames: SharedFlow<ImmutableMap<MainCategoryType, List<String>>>
+    val allDataSizeFlow: SharedFlow<ImmutableMap<MainCategoryType, Int>>
 }

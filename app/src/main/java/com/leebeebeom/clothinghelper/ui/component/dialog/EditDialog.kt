1bc2e611b3998eca83e4f06a463018c432ea872b
leebeebeom <boole92@naver.com>
2023-04-01 03:59:53 +0900

    완ㅅ어

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/component/dialog/EditDialog.kt
@@ -0,0 +1,39 @@
+package com.leebeebeom.clothinghelper.ui.component.dialog
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import com.leebeebeom.clothinghelper.ui.component.dialog.component.TextFieldDialog
+import com.leebeebeom.clothinghelper.ui.component.dialog.component.rememberTextFieldDialogState
+import kotlinx.collections.immutable.ImmutableSet
+
+@Composable // skippable
+fun EditDialog(
+    @StringRes label: Int,
+    @StringRes placeHolder: Int?,
+    @StringRes title: Int,
+    @StringRes existNameError: Int,
+    names: () -> ImmutableSet<String>,
+    onPositiveButtonClick: (String) -> Unit,
+    onDismiss: () -> Unit,
+    initialName: String
+) {
+    val state = rememberTextFieldDialogState(initialText = initialName)
+    val localNames by remember { derivedStateOf(names) }
+    val error by remember {
+        derivedStateOf { if (localNames.contains(state.input.trim())) existNameError else null }
+    }
+
+    TextFieldDialog(
+        state = state,
+        label = label,
+        placeHolder = placeHolder,
+        title = title,
+        onPositiveButtonClick = { onPositiveButtonClick(state.input.trim()) },
+        onDismiss = onDismiss,
+        onInputChange = state::onInputChange,
+        error = { error }
+    )
+}

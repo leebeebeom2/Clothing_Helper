622956ae36ab9d6af274e6299a5b81534966cfac
leebeebeom <boole92@naver.com>
2023-04-01 04:59:18 +0900

    완성

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/DrawerCount.kt
@@ -0,0 +1,25 @@
+package com.leebeebeom.clothinghelper.ui.drawer.component
+
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.ui.component.SingleLineText
+
+@Composable // skippable
+fun DrawerCount(
+    foldersSize: () -> Int,
+    chartsSize: () -> Int
+) {
+    SingleLineText(
+        text = stringResource(
+            id = R.string.folders_charts,
+            arrayOf(foldersSize(), chartsSize())
+        ),
+        style = MaterialTheme.typography.caption.copy(
+            LocalContentColor.current.copy(ContentAlpha.disabled)
+        )
+    )
+}

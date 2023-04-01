e0a6008b92df31907c5ddeddb0e60c6585eb8953
leebeebeom <boole92@naver.com>
2023-04-02 01:01:53 +0900

    allDataNames,allDataSizeFlow 수정

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
@@ -1,5 +1,12 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.domain.model.SubCategory
+import com.leebeebeom.clothinghelper.ui.drawer.content.MainCategoryType
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.coroutines.flow.SharedFlow
 
-interface SubCategoryRepository : BaseContainerRepository<SubCategory>
+interface SubCategoryRepository : BaseContainerRepository<SubCategory> {
+    val allDataNamesMapFlow: SharedFlow<ImmutableMap<MainCategoryType, ImmutableSet<String>>>
+    val allDataSizeMapFlow: SharedFlow<ImmutableMap<MainCategoryType, Int>>
+}

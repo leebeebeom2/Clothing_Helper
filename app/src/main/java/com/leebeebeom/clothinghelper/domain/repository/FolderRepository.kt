0c03b60c2c147de39dcbd6a52661ae54acff7fde
leebeebeom <boole92@naver.com>
2023-04-03 03:30:54 +0900

    folderNamesMapFlow, folderSizeMapFlow 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/FolderRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/FolderRepository.kt
@@ -1,5 +1,11 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.domain.model.Folder
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.coroutines.flow.SharedFlow
 
-interface FolderRepository : BaseContainerRepository<Folder>
+interface FolderRepository : BaseContainerRepository<Folder> {
+    val folderNamesMapFlow: SharedFlow<ImmutableMap<String, ImmutableSet<String>>>
+    val folderSizeMapFlow: SharedFlow<ImmutableMap<String, Int>>
+}

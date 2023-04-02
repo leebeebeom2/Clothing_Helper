56dd2ec92e2902e38907ebe8a153e7fef7dfa0a1
leebeebeom <boole92@naver.com>
2023-04-03 00:56:04 +0900

    LazyColumn -> Column 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/DrawerContent.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/DrawerContent.kt
@@ -1,20 +1,22 @@
 package com.leebeebeom.clothinghelper.ui.drawer.content
 
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable // skippable
-fun DrawerContent(
-    content: (LazyListScope).() -> Unit
-) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(top = 4.dp, bottom = 40.dp)
+fun DrawerContent(content: @Composable () -> Unit) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .fillMaxSize()
+            .padding(top = 4.dp, bottom = 40.dp)
     ) {
         content()
     }

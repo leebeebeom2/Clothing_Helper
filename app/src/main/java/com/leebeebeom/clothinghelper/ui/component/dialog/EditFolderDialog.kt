f805df6abe8785e51b15356c4b1b92b9827975b0
leebeebeom <boole92@naver.com>
2023-04-03 03:15:10 +0900

    완성

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/component/dialog/EditFolderDialog.kt
@@ -0,0 +1,26 @@
+package com.leebeebeom.clothinghelper.ui.component.dialog
+
+import androidx.compose.runtime.Composable
+import com.leebeebeom.clothinghelper.R
+import kotlinx.collections.immutable.ImmutableSet
+
+@Composable
+fun EditFolderDialog(
+    show: () -> Boolean,
+    onDismiss: () -> Unit,
+    onPositiveButtonClick: (String) -> Unit,
+    initialName: () -> String,
+    folderNames: () -> ImmutableSet<String>
+) {
+    if (show())
+        EditNameDialog(
+            label = R.string.folder,
+            placeHolder = R.string.folder_place_holder,
+            title = R.string.edit_folder,
+            existNameError = R.string.error_exist_folder_name,
+            onDismiss = onDismiss,
+            onPositiveButtonClick = onPositiveButtonClick,
+            initialName = initialName(),
+            names = folderNames
+        )
+}

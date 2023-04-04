7faed25429ec455f42179e66f7ba1b4d6453af82
leebeebeom <boole92@naver.com>
2023-04-04 23:04:27 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetSubCategoriesMapFlowUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/subcategory/GetSubCategoriesMapFlowUseCase.kt
@@ -3,7 +3,7 @@ package com.leebeebeom.clothinghelper.domain.usecase.subcategory
 import com.leebeebeom.clothinghelper.domain.repository.SubCategoryRepository
 import javax.inject.Inject
 
-class GetSubCategoriesMapFlow @Inject constructor(
+class GetSubCategoriesMapFlowUseCase @Inject constructor(
     private val subCategoryRepository: SubCategoryRepository
 ) {
     val subCategoriesMapFlow get() = subCategoryRepository.allDataMapFlow

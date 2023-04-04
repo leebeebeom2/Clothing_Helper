9b2ac49a2f01f5fb16924ffcaab8c5d63cf87965
leebeebeom <boole92@naver.com>
2023-04-05 03:24:55 +0900

    로그인 시 에러 버그 수정

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/MainActivity.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/MainActivity.kt
@@ -4,32 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.leebeebeom.clothinghelper.domain.model.User
-import com.leebeebeom.clothinghelper.ui.component.CenterDotProgressIndicator
 import com.leebeebeom.clothinghelper.ui.drawer.Drawer
 import com.leebeebeom.clothinghelper.ui.drawer.content.EssentialMenuType
-import com.leebeebeom.clothinghelper.ui.main.*
-import com.leebeebeom.clothinghelper.ui.setting.SettingDestination
+import com.leebeebeom.clothinghelper.ui.main.MainGraphRoute
+import com.leebeebeom.clothinghelper.ui.main.mainGraph
+import com.leebeebeom.clothinghelper.ui.main.navigateToSizeChartList
+import com.leebeebeom.clothinghelper.ui.main.navigateToSubCategory
+import com.leebeebeom.clothinghelper.ui.setting.SettingGraphRoute
+import com.leebeebeom.clothinghelper.ui.setting.settingGraph
 import com.leebeebeom.clothinghelper.ui.signin.ui.signInGraph
 import com.leebeebeom.clothinghelper.ui.theme.ClothingHelperTheme
 import com.leebeebeom.clothinghelper.ui.util.getCurrentRoute
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
 
 const val TAG = "태그"
 
 object MainNavRoute {
-    const val LoadingScreen = "loading screen"
     const val SignInGraph = "sign in graph"
     const val MainGraph = "main graph"
+    const val SettingGraph = "setting graph"
 }
 
 @AndroidEntryPoint
@@ -42,7 +44,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable // skippable
 fun MainNavHost(
-    viewModel: ActivityViewModel = activityViewModel(),
+    viewModel: ActivityViewModel = hiltViewModel(),
     navController: NavHostController = rememberNavController(),
 ) {
     val user by viewModel.userFlow.collectAsStateWithLifecycle()
@@ -59,25 +61,19 @@ fun MainNavHost(
             NavHost(
                 modifier = Modifier.padding(paddingValues),
                 navController = navController,
-                startDestination = MainNavRoute.LoadingScreen
+                startDestination = MainNavRoute.MainGraph
             ) {
-                composable(route = MainNavRoute.LoadingScreen) {
-                    CenterDotProgressIndicator(
-                        backGround = MaterialTheme.colors.background,
-                        show = { true }
-                    )
-                }
                 mainGraph(navController = navController)
+                settingGraph(navController = navController)
                 signInGraph(navController = navController)
             }
-
+            MainNavHostNavigateWrapper(
+                user = { user },
+                navigateToMainGraph = navController::navigateToMainGraph,
+                navigateToSignInGraph = navController::navigateToSignInGraph
+            )
         }
     }
-    MainNavHostNavigateWrapper(
-        user = { user },
-        navigateToMainGraph = navController::navigateToMainGraph,
-        navigateToSignInGraph = navController::navigateToSignInGraph
-    )
 }
 
 @Composable
@@ -86,12 +82,7 @@ private fun MainNavHostNavigateWrapper(
     navigateToMainGraph: () -> Unit,
     navigateToSignInGraph: () -> Unit
 ) {
-    val localUser by remember { derivedStateOf(user) }
-
-    LaunchedEffect(key1 = localUser) {
-        delay(500)
-        localUser?.let { navigateToMainGraph() } ?: navigateToSignInGraph()
-    }
+    user()?.let { navigateToMainGraph() } ?: navigateToSignInGraph()
 }
 
 private fun NavHostController.navigateToSignInGraph() = navigate(MainNavRoute.SignInGraph) {
@@ -113,11 +104,11 @@ private fun onEssentialMenuClick(navController: NavHostController, type: Essenti
     }
 
 private fun NavHostController.navigateToMain() {
-    if (currentBackStackEntry.getCurrentRoute() != DetailDestination.CategoryRoute.route)
+    if (currentBackStackEntry.getCurrentRoute() != MainGraphRoute.Dashboard.route)
         navigate(route = MainNavRoute.MainGraph)
 }
 
 private fun NavHostController.navigateToSetting() {
-    if (currentBackStackEntry.getCurrentRoute() != SettingDestination.Setting.route)
-        navigate(route = MainGraphRoute.SettingGraph)
+    if (currentBackStackEntry.getCurrentRoute() != SettingGraphRoute.Setting.route)
+        navigate(route = MainNavRoute.SettingGraph)
 }

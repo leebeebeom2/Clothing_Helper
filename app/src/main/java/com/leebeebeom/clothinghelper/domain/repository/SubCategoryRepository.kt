7acc41762c361b0ace9b4614b612fe36f52d4e4b
leebeebeom <boole92@naver.com>
2023-04-06 03:39:32 +0900

    DataResultMap 생성

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
@@ -2,11 +2,5 @@ package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.domain.model.SubCategory
 import com.leebeebeom.clothinghelper.ui.drawer.content.MainCategoryType
-import kotlinx.collections.immutable.ImmutableMap
-import kotlinx.collections.immutable.ImmutableSet
-import kotlinx.coroutines.flow.SharedFlow
 
-interface SubCategoryRepository : BaseContainerRepository<SubCategory> {
-    val subCategoryNamesMapFlow: SharedFlow<ImmutableMap<MainCategoryType, ImmutableSet<String>>>
-    val subCategorySizeMapFlow: SharedFlow<ImmutableMap<MainCategoryType, Int>>
-}
+interface SubCategoryRepository : BaseContainerRepository<SubCategory, MainCategoryType>

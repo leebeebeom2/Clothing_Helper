ff571282c51515a254470ede9662b6086397d9ad
leebeebeom <boole92@naver.com>
2023-04-06 03:47:10 +0900

    Black 컬러들 생성

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/theme/Color.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/theme/Color.kt
@@ -2,13 +2,15 @@ package com.leebeebeom.clothinghelper.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Black = Color.Black
+val Black11 = Color(0xFF111111)
+val Black18 = Color(0xFF181818)
+val BlackA3 = Color(0xFF3A3A3A)
 val Teal200 = Color(0xFF03DAC5)
 val Disabled = Color(0x50888888)
-val Black = Color.Black
 val WhiteSmoke = Color(0xFFf5f5f5)
-val DarkGray = Color(0xFF121212)
 val Gainsboro = Color(0xFFDADADA)
 val DarkGrayishBlue = Color(0xFF8391A1)
 val LightGrayishBlue = Color(0xFFF7F8F9)
 val VeryDarkGray = Color(0xFF555555)
-val LightGray = Color(0xffcccccc)
+val LightGray = Color(0xFFCCCCCC)

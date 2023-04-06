d0915274db91e640703d83040f52c01b383f8966
leebeebeom <boole92@naver.com>
2023-04-07 03:37:48 +0900

    완성

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/dropdownmenus/DrawerDropdownMenu.kt
@@ -0,0 +1,56 @@
+package com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus
+
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import com.leebeebeom.clothinghelper.ui.theme.BlackA3
+
+@Composable
+fun DrawerDropdownMenu(
+    state: DrawerDropdownMenuState,
+    onDismiss: () -> Unit,
+    density: Density = LocalDensity.current,
+    dropdownMenuItems: @Composable () -> Unit
+) {
+    val offset by remember {
+        derivedStateOf {
+            with(density) {
+                DpOffset(
+                    x = state.longClickOffsetX.toDp(),
+                    y = state.longClickOffsetY.toDp() - state.itemHeight.toDp()
+                )
+            }
+        }
+    }
+
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(
+            surface = BlackA3,
+            onSurface = Color.White
+        )
+    ) {
+        DropdownMenu(
+            modifier = Modifier.widthIn(120.dp),
+            expanded = state.showDropdownMenu,
+            onDismissRequest = onDismiss,
+            offset = offset
+        ) {
+            dropdownMenuItems()
+        }
+    }
+}
+
+@Stable
+interface DrawerDropdownMenuState {
+    val showDropdownMenu: Boolean
+    val longClickOffsetX: Float
+    val longClickOffsetY: Float
+    val itemHeight: Int
+}

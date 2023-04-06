172ffcc7b08a7b98bb588340faeea11dd74816a9
leebeebeom <boole92@naver.com>
2023-04-07 04:40:15 +0900

    단일 카운트 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/DrawerCount.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/DrawerCount.kt
@@ -1,10 +1,13 @@
 package com.leebeebeom.clothinghelper.ui.drawer.component
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.ui.component.SingleLineText
 
@@ -18,8 +21,19 @@ fun DrawerCount(
             id = R.string.folders_items,
             folderSize(), itemSize()
         ),
-        style = MaterialTheme.typography.caption.copy(
-            LocalContentColor.current.copy(ContentAlpha.disabled)
-        )
+        style = drawerCountStyle()
     )
 }
+
+@Composable // skippable
+fun DrawerCount(dataSize: () -> Int) {
+    SingleLineText(
+        modifier = Modifier.padding(start = 4.dp),
+        text = "(${dataSize()})",
+        style = drawerCountStyle()
+    )
+}
+
+@Composable
+private fun drawerCountStyle() =
+    MaterialTheme.typography.caption.copy(LocalContentColor.current.copy(ContentAlpha.disabled))

0987ca437be380ad2b1e379c5bfc0776444fe5a5
leebeebeom <boole92@naver.com>
2023-04-07 04:27:40 +0900

    함수 람다화

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/DrawerRow.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/DrawerRow.kt
@@ -1,6 +1,7 @@
 package com.leebeebeom.clothinghelper.ui.drawer.component
 
 import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.gestures.PressGestureScope
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -18,44 +19,53 @@ import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 
 @Composable // skippable
 fun DrawerRow(
     modifier: Modifier = Modifier,
+    height: Dp = 48.dp,
     onClick: () -> Unit,
-    onLongClick: (Offset) -> Unit = {},
+    onLongClick: ((Offset) -> Unit)? = null,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     haptic: HapticFeedback = LocalHapticFeedback.current,
     onSizeChange: (IntSize) -> Unit = {},
     content: @Composable RowScope.() -> Unit
 ) {
-    Row(
-        modifier = modifier
-            .heightIn(48.dp)
-            .onSizeChanged(onSizeChange)
-            .fillMaxWidth()
-            .padding(horizontal = 8.dp)
-            .clip(MaterialTheme.shapes.small)
-            .indication(interactionSource = interactionSource, LocalIndication.current)
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onPress = {
-                        val press = PressInteraction.Press(it)
-                        interactionSource.emit(press)
-                        tryAwaitRelease()
-                        interactionSource.emit(PressInteraction.Release(press))
-                    },
-                    onTap = { onClick() },
-                    onLongPress = {
-                        onLongClick(it)
-                        haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                    },
-                )
+    val localOnLongClick = remember<(Offset) -> Unit>(onLongClick, haptic) {
+        { offset ->
+            if (onLongClick != null) {
+                onLongClick(offset)
+                haptic.performHapticFeedback(HapticFeedbackType.LongPress)
             }
-            .padding(start = 4.dp),
+        }
+    }
+    val onPress = remember<suspend PressGestureScope.(Offset) -> Unit> {
+        {
+            val press = PressInteraction.Press(it)
+            interactionSource.emit(press)
+            tryAwaitRelease()
+            interactionSource.emit(PressInteraction.Release(press))
+        }
+    }
+
+    Row(modifier = modifier
+        .heightIn(height)
+        .onSizeChanged(onSizeChange)
+        .fillMaxWidth()
+        .padding(horizontal = 8.dp)
+        .clip(MaterialTheme.shapes.small)
+        .indication(interactionSource = interactionSource, LocalIndication.current)
+        .pointerInput(Unit) {
+            detectTapGestures(
+                onPress = onPress,
+                onTap = { onClick() },
+                onLongPress = localOnLongClick
+            )
+        }
+        .padding(start = 8.dp),
         verticalAlignment = Alignment.CenterVertically,
-        content = content
-    )
+        content = content)
 }

194087de63779306cd388c1539e16ff55b265a6e
leebeebeom <boole92@naver.com>
2023-04-08 02:04:40 +0900

    state 외부로 이동

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/mainmenu/DrawerArchive.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/mainmenu/DrawerArchive.kt
@@ -2,12 +2,14 @@ package com.leebeebeom.clothinghelper.ui.drawer.component.mainmenu
 
 import androidx.compose.runtime.Composable
 import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.ui.drawer.DrawerItemDropdownMenuState
 import com.leebeebeom.clothinghelper.ui.drawer.component.DrawerContentWithDoubleCount
 import com.leebeebeom.clothinghelper.ui.util.AddFolder
 import kotlinx.collections.immutable.ImmutableSet
 
 @Composable
 fun DrawerArchive(
+    state: DrawerItemDropdownMenuState,
     onClick: (MainMenuType) -> Unit,
     folderNames: (parentKey: String) -> ImmutableSet<String>,
     foldersSize: (parentKey: String) -> Int,
@@ -24,6 +26,7 @@ fun DrawerArchive(
         folderNames = folderNames,
         itemsSize = itemsSize,
         addFolder = addFolder,
-        subMenus = archiveFolders
+        subMenus = archiveFolders,
+        state = state
     )
 }

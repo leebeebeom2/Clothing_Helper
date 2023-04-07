87e1583a535c1edc2fca5c9b4ea4a60f4fe80b20
leebeebeom <boole92@naver.com>
2023-04-08 02:55:50 +0900

    리컴포지션 최적화

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/folder/DrawerFolderDropdownMenu.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/folder/DrawerFolderDropdownMenu.kt
@@ -1,7 +1,14 @@
-package com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus
+package com.leebeebeom.clothinghelper.ui.drawer.component.folder
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import com.leebeebeom.clothinghelper.domain.model.Folder
+import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenu
+import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenuAddFolder
+import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenuEditFolder
+import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenuState
 import com.leebeebeom.clothinghelper.ui.util.AddFolder
 import com.leebeebeom.clothinghelper.ui.util.EditFolder
 import kotlinx.collections.immutable.ImmutableSet
@@ -17,18 +24,20 @@ fun DrawerFolderDropDownMenu(
     editFolder: EditFolder,
     selectedFolder: () -> Folder
 ) {
+    val localSelectedFolder by remember(selectedFolder) { derivedStateOf(selectedFolder) }
+
     DrawerDropdownMenu(state = state, onDismiss = onDismiss) {
         DrawerDropdownMenuEditFolder(
             onDismiss = onDismiss,
             folderNames = folderNames,
-            onPositiveButtonClick = { name -> editFolder(selectedFolder(), name) },
-            initialName = { selectedFolder().name }
+            onPositiveButtonClick = { name -> editFolder(localSelectedFolder, name) },
+            initialName = { localSelectedFolder.name }
         )
 
         DrawerDropdownMenuAddFolder(
             onDismissDropdownMenu = onDismiss,
             folderNames = childFolderNames,
-            onPositiveButtonClick = { name -> addFolder(selectedFolder().key, name) },
+            onPositiveButtonClick = { name -> addFolder(localSelectedFolder.key, name) },
             expand = expand
         )
     }

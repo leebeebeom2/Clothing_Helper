7ffb9e94af15a6986ac2142718683b63c9698c2f
leebeebeom <boole92@naver.com>
2023-04-08 03:15:20 +0900

    완성

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/folder/DrawerFolderDropdownMenu.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/folder/DrawerFolderDropdownMenu.kt
@@ -1,20 +1,19 @@
 package com.leebeebeom.clothinghelper.ui.drawer.component.folder
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.domain.model.Folder
+import com.leebeebeom.clothinghelper.ui.component.dialog.AddFolderDialog
+import com.leebeebeom.clothinghelper.ui.component.dialog.EditFolderDialog
 import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenu
-import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenuAddFolder
-import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenuEditFolder
+import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenuItem
 import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenuState
 import com.leebeebeom.clothinghelper.ui.util.AddFolder
 import com.leebeebeom.clothinghelper.ui.util.EditFolder
 import kotlinx.collections.immutable.ImmutableSet
 
 @Composable
-fun DrawerFolderDropDownMenu(
+fun DrawerDownMenuEditAndAddFolder(
     state: DrawerDropdownMenuState,
     onDismiss: () -> Unit,
     expand: () -> Unit,
@@ -24,21 +23,35 @@ fun DrawerFolderDropDownMenu(
     editFolder: EditFolder,
     selectedFolder: () -> Folder
 ) {
+    var showEditDialog by remember { mutableStateOf(false) }
+    var showAddDialog by remember { mutableStateOf(false) }
+
     val localSelectedFolder by remember(selectedFolder) { derivedStateOf(selectedFolder) }
 
     DrawerDropdownMenu(state = state, onDismiss = onDismiss) {
-        DrawerDropdownMenuEditFolder(
-            onDismiss = onDismiss,
-            folderNames = folderNames,
-            onPositiveButtonClick = { name -> editFolder(localSelectedFolder, name) },
-            initialName = { localSelectedFolder.name }
-        )
+        DrawerDropdownMenuItem(text = R.string.dropdown_menu_edit) {
+            showEditDialog = true
+            onDismiss()
+        }
 
-        DrawerDropdownMenuAddFolder(
-            onDismissDropdownMenu = onDismiss,
-            folderNames = childFolderNames,
-            onPositiveButtonClick = { name -> addFolder(localSelectedFolder.key, name) },
-            expand = expand
-        )
+        DrawerDropdownMenuItem(text = R.string.dropdown_menu_add_folder) {
+            showAddDialog = true
+            onDismiss()
+        }
     }
+
+    EditFolderDialog(
+        folderNames = folderNames,
+        onPositiveButtonClick = { name -> editFolder(localSelectedFolder, name) },
+        show = { showEditDialog },
+        onDismiss = { showEditDialog = false },
+        initialName = { localSelectedFolder.name }
+    )
+
+    AddFolderDialog(
+        folderNames = childFolderNames,
+        onPositiveButtonClick = { name ->
+            addFolder(localSelectedFolder.key, name)
+            expand()
+        }, show = { showAddDialog }, onDismiss = { showAddDialog = false })
 }

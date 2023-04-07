ac5e36766bc40f159ffee05e4239970797fc01a4
leebeebeom <boole92@naver.com>
2023-04-08 04:21:29 +0900

    완성

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/mainmenu/DrawerMainMenus.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/mainmenu/DrawerMainMenus.kt
@@ -6,10 +6,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.domain.model.Folder
+import com.leebeebeom.clothinghelper.ui.drawer.component.folder.DrawerFolders
+import com.leebeebeom.clothinghelper.ui.drawer.component.submenu.SubMenuType
+import com.leebeebeom.clothinghelper.ui.drawer.component.submenu.SubMenus
+import com.leebeebeom.clothinghelper.ui.drawer.component.submenu.closet.ClothesCategoryType
 import com.leebeebeom.clothinghelper.ui.drawer.rememberDrawerItemDropdownMenuState
 import com.leebeebeom.clothinghelper.ui.drawer.rememberDrawerItemState
+import com.leebeebeom.clothinghelper.ui.theme.Black11
+import com.leebeebeom.clothinghelper.ui.theme.Black18
 import com.leebeebeom.clothinghelper.ui.util.AddFolder
+import com.leebeebeom.clothinghelper.ui.util.EditFolder
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.ImmutableSet
 import kotlinx.collections.immutable.persistentListOf
@@ -18,13 +27,31 @@ import kotlinx.collections.immutable.persistentListOf
 fun DrawerMainMenus(
     mainMenus: ImmutableList<MainMenu>,
     onMainMenuClick: (MainMenuType) -> Unit,
+    onSubMenuClick: (SubMenuType) -> Unit,
+    onClothesCategoryClick: (ClothesCategoryType) -> Unit,
+    onFolderClick: (Folder) -> Unit,
+    folders: (parentKey: String) -> ImmutableList<Folder>,
     folderNames: (parentKey: String) -> ImmutableSet<String>,
     foldersSize: (parentKey: String) -> Int,
     itemsSize: (parentKey: String) -> Int,
     addFolder: AddFolder,
-    subMenus: @Composable (MainMenuType) -> Unit,
-    archiveFolders: @Composable () -> Unit
+    editFolder: EditFolder
 ) {
+    @Composable
+    fun Folders(parentKey: String, backgroundColor: Color) {
+        DrawerFolders(
+            parentKey = parentKey,
+            folders = folders,
+            folderNames = folderNames,
+            backgroundColor = backgroundColor,
+            foldersSize = foldersSize,
+            itemsSize = { 0 },
+            onFolderClick = onFolderClick,
+            addFolder = addFolder,
+            editFolder = editFolder
+        )
+    }
+
     Column {
         mainMenus.forEach { mainMenu ->
             key(mainMenu.type) {
@@ -34,8 +61,19 @@ fun DrawerMainMenus(
                     DrawerMainMenu(
                         mainMenu = mainMenu,
                         onMainMenuClick = onMainMenuClick,
-                        subMenus = subMenus,
-                        state = state
+                        state = state,
+                        subMenus = { mainMenuType ->
+                            SubMenus(
+                                mainMenuType = mainMenuType,
+                                onSubMenuClick = onSubMenuClick,
+                                onClothesCategoryClick = onClothesCategoryClick,
+                                foldersSize = foldersSize,
+                                folderNames = folderNames,
+                                itemsSize = { 0 },
+                                addFolder = addFolder,
+                                folders = { Folders(parentKey = it, backgroundColor = Black18) }
+                            )
+                        }
                     )
                 } else {
                     val state = rememberDrawerItemDropdownMenuState()
@@ -47,7 +85,12 @@ fun DrawerMainMenus(
                         foldersSize = foldersSize,
                         itemsSize = itemsSize,
                         addFolder = addFolder,
-                        archiveFolders = archiveFolders,
+                        folders = {
+                            Folders(
+                                parentKey = MainMenuType.Archive.name,
+                                backgroundColor = Black11
+                            )
+                        },
                     )
                 }
             }

86aa73deb1ac15e48324770af6ab297d60714d76
leebeebeom <boole92@naver.com>
2023-04-07 09:40:14 +0900

    완성

---
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/submenu/brand/DrawerBrandSubMenu.kt
@@ -0,0 +1,60 @@
+package com.leebeebeom.clothinghelper.ui.drawer.component.submenu
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.ui.component.IconWrapper
+import com.leebeebeom.clothinghelper.ui.drawer.DrawerItemDropdownMenuState
+import com.leebeebeom.clothinghelper.ui.drawer.component.*
+import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerBrandDropdownMenu
+import com.leebeebeom.clothinghelper.ui.drawer.rememberDrawerItemDropdownMenuState
+import com.leebeebeom.clothinghelper.ui.util.AddFolder
+import kotlinx.collections.immutable.ImmutableSet
+
+@Composable
+fun DrawerBrandSubMenu(
+    subMenu: SubMenu,
+    onClick: () -> Unit,
+    folderNames: (parentKey: String) -> ImmutableSet<String>,
+    foldersSize: (parentKey: String) -> Int,
+    itemSize: (parentKey: String) -> Int,
+    addFolder: AddFolder,
+    state: DrawerItemDropdownMenuState = rememberDrawerItemDropdownMenuState(),
+    folders: @Composable (parentKey: String) -> Unit
+) {
+    DrawerRow(
+        onClick = onClick,
+        onLongClick = state::onLongClick,
+        onSizeChange = state::onSizeChanged
+    ) {
+        DrawerInsideRow {
+            IconWrapper(modifier = Modifier.padding(end = 8.dp), drawable = R.drawable.ic_dot)
+            DrawerTextWithDoubleCount(
+                modifier = Modifier.padding(vertical = 4.dp),
+                text = subMenu.name,
+                style = MaterialTheme.typography.subtitle1,
+                folderSize = { foldersSize(subMenu.type.name) },
+                itemSize = { itemSize(subMenu.type.name) }
+            )
+
+            DrawerExpandIcon(
+                expanded = { state.expanded },
+                toggleExpand = state::toggleExpand,
+                dataSize = { foldersSize(subMenu.type.name) }
+            )
+        }
+
+        DrawerBrandDropdownMenu(
+            state = state,
+            subMenu = subMenu,
+            onDismiss = state::onDismissDropDownMenu,
+            folderNames = { folderNames(subMenu.type.name) },
+            addFolder = addFolder,
+            expand = state::expand
+        )
+    }
+    DrawerItemsWrapperWithExpandAnimation(expand = { state.expanded }) { folders(subMenu.type.name) }
+}

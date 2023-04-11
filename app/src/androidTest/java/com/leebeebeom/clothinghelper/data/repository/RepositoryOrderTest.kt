1c0286856bfe590e392ab7c03d4ea9c431fd91b1
leebeebeom <boole92@naver.com>
2023-04-12 02:07:05 +0900

    테스트 로직 변경

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/data/repository/RepositoryOrderTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/repository/RepositoryOrderTest.kt
@@ -2,38 +2,30 @@ package com.leebeebeom.clothinghelper.data.repository
 
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import com.leebeebeom.clothinghelper.data.SignInEmail
-import com.leebeebeom.clothinghelper.data.SignInPassword
 import com.leebeebeom.clothinghelper.data.waitTime
 import com.leebeebeom.clothinghelper.domain.model.BaseModel
 import com.leebeebeom.clothinghelper.domain.repository.BaseDataRepository
 import com.leebeebeom.clothinghelper.domain.repository.UserRepository
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
 
 @OptIn(ExperimentalCoroutinesApi::class)
 suspend inline fun <T : BaseModel> TestScope.repositoryOrderTest(
     repository: BaseDataRepository<T>,
     userRepository: UserRepository,
     initDataList: List<T>,
-    assertOrder: (origin: List<T>, new: List<T>) -> Unit,
-    refPath: DataBasePath
+    assertOrder: (origin: List<T>, new: List<T>) -> Unit
 ) {
-    userRepository.signIn(email = SignInEmail, password = SignInPassword)
     backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) { repository.allDataFlow.collect() }
     waitTime()
 
-    initDataList.shuffled().forEach { repository.add(it) }
-    advanceUntilIdle()
+    initDataList.shuffled().map { coroutineScope { async { repository.add(it) } } }.awaitAll()
     waitTime()
     assertOrder(initDataList, repository.allDataFlow.first().data)
 
-    Firebase.database.reference.child(userRepository.userFlow.first()!!.uid).child(refPath.path)
-        .removeValue().await()
+    Firebase.database.reference.child(userRepository.userFlow.first()!!.uid).removeValue().await()
 }

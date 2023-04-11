11bc517b85d9312440be3c2ecdc501e4cd43b16a
leebeebeom <boole92@naver.com>
2023-04-12 04:28:39 +0900

    테스트 완

---
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/data/repository/FolderRepositoryTest.kt
@@ -0,0 +1,114 @@
+package com.leebeebeom.clothinghelper.data.repository
+
+import androidx.test.core.app.ApplicationProvider
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.leebeebeom.clothinghelper.data.SignInPassword
+import com.leebeebeom.clothinghelper.data.repository.preference.FolderPreferencesRepositoryImpl
+import com.leebeebeom.clothinghelper.data.waitTime
+import com.leebeebeom.clothinghelper.domain.model.Folder
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.persistentSetOf
+import kotlinx.collections.immutable.toImmutableSet
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class FolderRepositoryTest {
+    private val dispatcher = StandardTestDispatcher()
+    private val scope = TestScope(dispatcher)
+    private val userRepository = UserRepositoryImpl(appScope = scope, dispatcherIO = dispatcher)
+    private val folderRepository = FolderRepositoryImpl(
+        folderPreferencesRepository = FolderPreferencesRepositoryImpl(
+            context = ApplicationProvider.getApplicationContext(),
+            appScope = scope
+        ),
+        appScope = scope,
+        dispatcherIO = dispatcher,
+        dispatcherDefault = dispatcher,
+        userRepository = userRepository
+    )
+
+    @Before
+    fun init() = runTest(dispatcher) {
+        userRepository.signIn(RepositoryTestEmail, SignInPassword)
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) { folderRepository.allDataFlow.collect() }
+        waitTime()
+
+        List(10) { parent ->
+            List(parent + 1) { index ->
+                Folder(
+                    name = "parent $parent folder $index",
+                    parentKey = "$parent"
+                )
+            }.map { async { folderRepository.add(it) } }
+        }.flatten().awaitAll()
+    }
+
+    @After
+    fun removeData() = runTest(dispatcher) {
+        userRepository.signIn(RepositoryTestEmail, SignInPassword)
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) { userRepository.userFlow.collect() }
+        waitTime()
+
+        Firebase.database.reference.child(userRepository.userFlow.first()!!.uid).removeValue()
+    }
+
+    @Test
+    fun folderMapTest() = runTest(dispatcher) {
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) { folderRepository.allDataFlow.collect() }
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) { folderRepository.allFoldersMapFlow.collect() }
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) { folderRepository.folderNamesMapFlow.collect() }
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) { folderRepository.foldersSizeMapFlow.collect() }
+        waitTime()
+
+        val foldersMap = folderRepository.allFoldersMapFlow.first()
+        val folderNamesMap = folderRepository.folderNamesMapFlow.first()
+        val foldersSizeMap = folderRepository.foldersSizeMapFlow.first()
+
+        foldersMap.data.forEach { (key, value) -> assert(value.size == key.toInt() + 1) }
+
+        folderNamesMap.forEach { (key, value) -> assert(List(key.toInt() + 1) { "parent $key folder $it" }.toImmutableSet() == value) }
+
+        foldersSizeMap.forEach { (key, value) -> assert((key.toInt() + 1) == value) }
+
+        val key = 5.toString()
+        val newFolder = Folder(name = "parent $key zAdded Folder", parentKey = key)
+        folderRepository.add(newFolder)
+        waitTime()
+
+        val addedFolder =
+            folderRepository.allFoldersMapFlow.first().data.getOrDefault(key, persistentListOf())
+                .last()
+        assert(addedFolder.name == newFolder.name)
+        assert(addedFolder.parentKey == newFolder.parentKey)
+
+        assert(
+            folderRepository.folderNamesMapFlow.first().getOrDefault(key, persistentSetOf())
+                .last() == newFolder.name
+        )
+        assert(folderRepository.foldersSizeMapFlow.first().getOrDefault(key, 0) == key.toInt() + 2)
+
+        Firebase.database.reference.child(userRepository.userFlow.first()!!.uid)
+            .child(DataBasePath.Folder.path)
+            .child(addedFolder.key).removeValue().await()
+
+        foldersMap.data.forEach { (key, value) -> assert(value.size == key.toInt() + 1) }
+
+        folderNamesMap.forEach { (key, value) -> assert(List(key.toInt() + 1) { "parent $key folder $it" }.toImmutableSet() == value) }
+
+        foldersSizeMap.forEach { (key, value) -> assert((key.toInt() + 1) == value) }
+    }
+}

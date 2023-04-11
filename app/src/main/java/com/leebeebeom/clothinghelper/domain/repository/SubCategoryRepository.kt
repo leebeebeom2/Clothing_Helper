164b6eb5ba7f46ed9a74996de50dc8502c5ce565
leebeebeom <boole92@naver.com>
2023-04-11 23:47:00 +0900

    SubCategory 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/SubCategoryRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.leebeebeom.clothinghelper.domain.repository
-
-import com.leebeebeom.clothinghelper.domain.model.SubCategory
-import com.leebeebeom.clothinghelper.ui.drawer.content.MainCategoryType
-
-interface SubCategoryRepository : BaseContainerRepository<SubCategory, MainCategoryType>
--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/preference/FolderPreferenceRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/preference/FolderPreferenceRepository.kt
@@ -4,18 +4,9 @@ import com.leebeebeom.clothinghelper.data.repository.preference.Order
 import com.leebeebeom.clothinghelper.data.repository.preference.Sort
 import com.leebeebeom.clothinghelper.data.repository.preference.SortPreferences
 import kotlinx.coroutines.flow.SharedFlow
-import javax.inject.Qualifier
 
-interface SortPreferenceRepository {
+interface FolderPreferenceRepository {
     val sortFlow: SharedFlow<SortPreferences>
     suspend fun changeSort(sort: Sort)
     suspend fun changeOrder(order: Order)
 }
-
-@Qualifier
-@Retention(AnnotationRetention.RUNTIME)
-annotation class SubCategoryPreferencesRepository
-
-@Qualifier
-@Retention(AnnotationRetention.RUNTIME)
-annotation class FolderPreferencesRepository

6adb5ab0cebfeb10274b0fe9db347d96825b90bd
leebeebeom <boole92@naver.com>
2023-04-11 16:07:42 +0900

    SubCategory 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/FolderRepository.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/repository/FolderRepository.kt
@@ -1,5 +1,19 @@
 package com.leebeebeom.clothinghelper.domain.repository
 
 import com.leebeebeom.clothinghelper.domain.model.Folder
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.coroutines.flow.SharedFlow
 
-interface FolderRepository : BaseContainerRepository<Folder, String>
+interface FolderRepository : BaseDataRepository<Folder> {
+    val allFoldersMapFlow: SharedFlow<FolderResultMap>
+    val folderNamesMapFlow: SharedFlow<ImmutableMap<String, ImmutableSet<String>>>
+    val foldersSizeMapFlow: SharedFlow<ImmutableMap<String, Int>>
+}
+
+sealed class FolderResultMap(val data: ImmutableMap<String, ImmutableList<Folder>>) {
+    class Success(data: ImmutableMap<String, ImmutableList<Folder>>) : FolderResultMap(data)
+    class Fail(data: ImmutableMap<String, ImmutableList<Folder>>, val exception: Throwable) :
+        FolderResultMap(data)
+}

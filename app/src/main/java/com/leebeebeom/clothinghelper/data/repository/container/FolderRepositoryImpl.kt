854f707fb7272e5a070414c4b99805ca325b0013
leebeebeom <boole92@naver.com>
2023-04-11 16:13:43 +0900

    SubCategory 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/container/FolderRepositoryImpl.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/container/FolderRepositoryImpl.kt
@@ -1,80 +1,103 @@
 package com.leebeebeom.clothinghelper.data.repository.container
 
+import com.leebeebeom.clothinghelper.data.repository.BaseDataRepositoryImpl
 import com.leebeebeom.clothinghelper.data.repository.DataBasePath
+import com.leebeebeom.clothinghelper.data.repository.preference.Order
+import com.leebeebeom.clothinghelper.data.repository.preference.Sort
+import com.leebeebeom.clothinghelper.data.repository.preference.SortPreferences
 import com.leebeebeom.clothinghelper.di.AppScope
 import com.leebeebeom.clothinghelper.di.DispatcherDefault
 import com.leebeebeom.clothinghelper.di.DispatcherIO
 import com.leebeebeom.clothinghelper.domain.model.Folder
-import com.leebeebeom.clothinghelper.domain.model.SubCategory
-import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
-import com.leebeebeom.clothinghelper.domain.repository.UserRepository
+import com.leebeebeom.clothinghelper.domain.repository.*
 import com.leebeebeom.clothinghelper.domain.repository.preference.FolderPreferencesRepository
 import com.leebeebeom.clothinghelper.domain.repository.preference.SortPreferenceRepository
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.collections.immutable.toImmutableMap
+import kotlinx.collections.immutable.toImmutableSet
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @Singleton
 class FolderRepositoryImpl @Inject constructor(
-    @FolderPreferencesRepository folderPreferencesRepository: SortPreferenceRepository,
+    @FolderPreferencesRepository private val folderPreferencesRepository: SortPreferenceRepository,
     @AppScope appScope: CoroutineScope,
     @DispatcherIO dispatcherIO: CoroutineDispatcher,
     @DispatcherDefault dispatcherDefault: CoroutineDispatcher,
     userRepository: UserRepository,
-) : BaseContainerRepositoryImpl<Folder, String>(
-    sortFlow = folderPreferencesRepository.sortFlow,
+) : BaseDataRepositoryImpl<Folder>(
     refPath = DataBasePath.Folder,
     appScope = appScope,
     type = Folder::class.java,
     dispatcherIO = dispatcherIO,
-    dispatcherDefault = dispatcherDefault,
-    userRepository = userRepository,
+    userRepository = userRepository
 ), FolderRepository {
 
-    override fun groupByKeySelector(element: Folder) = element.parentKey
+    override val allDataFlow =
+        super.allDataFlow.combine(
+            flow = folderPreferencesRepository.sortFlow,
+            transform = ::getSortedFolder
+        )
+            .flowOn(dispatcherDefault)
+            .distinctUntilChanged()
+            .shareIn(scope = appScope, started = SharingStarted.WhileSubscribed(5000), replay = 1)
 
-    suspend fun init(dispatcher: CoroutineDispatcher, subCategories: List<SubCategory>) =
-        withContext(dispatcher) {
-            subCategories.map { subCategory ->
-                List(subCategory.name.last().digitToInt()) {
-                    Folder(
-                        name = "folder $it",
-                        parentKey = subCategory.key,
-                        subCategoryKey = subCategory.key,
-                        mainCategoryType = subCategory.mainCategoryType
-                    )
-                }.map { async { add(it) } }
-            }.flatten()
-        }
+    override suspend fun add(data: Folder) {
+        // push override 때문에 editDate 변경 됨
+        val dataWithKey = data.addKey(key = getKey())
 
-    suspend fun init2(dispatcher: CoroutineDispatcher, folders: List<Folder>) =
-        withContext(dispatcher) {
-            folders.map { folder ->
-                List(folder.name.last().digitToInt()) {
-                    Folder(
-                        name = "folder 1-$it",
-                        parentKey = folder.key,
-                        subCategoryKey = folder.subCategoryKey,
-                        mainCategoryType = folder.mainCategoryType
-                    )
-                }.map { async { add(it) } }
-            }.flatten()
-        }
+        super.push(dataWithKey)
+    }
 
-    suspend fun init3(dispatcher: CoroutineDispatcher, folders: List<Folder>) =
-        withContext(dispatcher) {
-            folders.filter { it.subCategoryKey != it.parentKey }.map { folder ->
-                List(folder.name.last().digitToInt()) {
-                    Folder(
-                        name = "folder 2-$it",
-                        parentKey = folder.key,
-                        subCategoryKey = folder.subCategoryKey,
-                        mainCategoryType = folder.mainCategoryType
-                    )
-                }.map { async { add(it) } }
-            }.flatten()
+    override suspend fun push(data: Folder) = super.push(data = data.changeEditDate())
+
+    override val allFoldersMapFlow: SharedFlow<FolderResultMap> =
+        allDataFlow.mapLatest { dataResult ->
+            dataResult.toMap { it.parentKey }
+        }.flowOn(dispatcherDefault)
+            .distinctUntilChanged()
+            .shareIn(scope = appScope, started = SharingStarted.WhileSubscribed(5000), replay = 1)
+
+    override val folderNamesMapFlow = allFoldersMapFlow.mapLatest { allDataMap ->
+        allDataMap.data.mapValues { mapElement ->
+            mapElement.value.map { element -> element.name }.toImmutableSet()
+        }.toImmutableMap()
+    }.flowOn(dispatcherDefault)
+        .distinctUntilChanged()
+        .shareIn(scope = appScope, started = SharingStarted.WhileSubscribed(5000), replay = 1)
+
+    override val foldersSizeMapFlow = allFoldersMapFlow.mapLatest { allDataMap ->
+        allDataMap.data.mapValues { mapElement -> mapElement.value.size }.toImmutableMap()
+    }.flowOn(dispatcherDefault)
+        .distinctUntilChanged()
+        .shareIn(scope = appScope, started = SharingStarted.WhileSubscribed(5000), replay = 1)
+
+    private fun getSortedFolder(
+        dataResult: DataResult<Folder>,
+        sortPreferences: SortPreferences,
+    ) = when (dataResult) {
+        is DataResult.Fail -> dataResult
+        is DataResult.Success -> {
+            val allData = dataResult.data
+            val sort = sortPreferences.sort
+            val order = sortPreferences.order
+
+            val sortedList = when {
+                sort == Sort.Name && order == Order.Ascending -> allData.sortedBy { it.name }
+                sort == Sort.Name && order == Order.Descending -> allData.sortedByDescending { it.name }
+                sort == Sort.Create && order == Order.Ascending -> allData.sortedBy { it.createDate }
+                sort == Sort.Create && order == Order.Descending -> allData.sortedByDescending { it.createDate }
+                sort == Sort.Edit && order == Order.Ascending -> allData.sortedBy { it.editDate }
+                sort == Sort.Edit && order == Order.Descending -> allData.sortedByDescending { it.editDate }
+                else -> allData
+            }
+
+            DataResult.Success(sortedList.toImmutableList())
         }
+    }
 }

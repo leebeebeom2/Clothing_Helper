fd18b866dd8e5645e73621c127339a74bab7dd2c
leebeebeom <boole92@naver.com>
2023-04-12 00:06:41 +0900

    SubCategory 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/data/repository/DatabasePath.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/data/repository/DatabasePath.kt
@@ -1,5 +1,5 @@
 package com.leebeebeom.clothinghelper.data.repository
 
 enum class DataBasePath(val path: String) {
-    SubCategory("sub categories"), Folder("folders"), Todo("todos")
+    Folder("folders"), Todo("todos")
 }

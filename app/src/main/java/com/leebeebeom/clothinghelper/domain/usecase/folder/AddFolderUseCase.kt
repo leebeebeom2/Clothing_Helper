9a5b37953bbde0330cbbf09a1880b79318a02c12
leebeebeom <boole92@naver.com>
2023-04-13 14:34:34 +0900

    MenuType 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/AddFolderUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/AddFolderUseCase.kt
@@ -1,22 +1,11 @@
 package com.leebeebeom.clothinghelper.domain.usecase.folder
 
 import com.leebeebeom.clothinghelper.domain.model.Folder
+import com.leebeebeom.clothinghelper.domain.model.MenuType
 import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
-import com.leebeebeom.clothinghelper.ui.drawer.content.MainCategoryType
 import javax.inject.Inject
 
 class AddFolderUseCase @Inject constructor(private val folderRepository: FolderRepository) {
-    suspend fun add(
-        parentKey: String,
-        subCategoryKey: String,
-        name: String,
-        mainCategoryType: MainCategoryType,
-    ) = folderRepository.add(
-        data = Folder(
-            parentKey = parentKey,
-            subCategoryKey = subCategoryKey,
-            name = name,
-            mainCategoryType = mainCategoryType
-        )
-    )
+    suspend fun add(parentKey: String, name: String, menuType: MenuType) =
+        folderRepository.add(data = Folder(parentKey = parentKey, name = name, menuType = menuType))
 }

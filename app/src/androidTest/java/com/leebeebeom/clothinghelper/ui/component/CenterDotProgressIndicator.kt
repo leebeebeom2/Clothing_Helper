4bb81ee4482b579228ea8cbdc9da686a2db49bf8
leebeebeom <boole92@naver.com>
2023-04-14 03:01:25 +0900

    테스트 로직 변경

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/component/CenterDotProgressIndicator.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/component/CenterDotProgressIndicator.kt
@@ -1,83 +1,73 @@
 package com.leebeebeom.clothinghelper.ui.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
-import com.leebeebeom.clothinghelper.data.repository.UserRepositoryImpl
-import com.leebeebeom.clothinghelper.domain.usecase.user.GoogleSignInUseCase
-import com.leebeebeom.clothinghelper.domain.usecase.user.SignInUseCase
 import com.leebeebeom.clothinghelper.ui.HiltTestActivity
-import com.leebeebeom.clothinghelper.ui.signin.ui.resetpassword.ResetPasswordScreen
-import com.leebeebeom.clothinghelper.ui.signin.ui.resetpassword.ResetPasswordScreenTag
-import com.leebeebeom.clothinghelper.ui.signin.ui.signin.SignInScreen
-import com.leebeebeom.clothinghelper.ui.signin.ui.signin.SignInScreenTag
-import com.leebeebeom.clothinghelper.ui.signin.ui.signin.SignInViewModel
 import com.leebeebeom.clothinghelper.ui.theme.ClothingHelperTheme
 import com.leebeebeom.clothinghelper.ui.waitTagExist
 import com.leebeebeom.clothinghelper.ui.waitTagNotExist
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.test.StandardTestDispatcher
+import com.leebeebeom.clothinghelper.ui.waitTextExist
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class CenterDotProgressIndicatorTest {
     @get:Rule
     val rule = createAndroidComposeRule<HiltTestActivity>()
     private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
-    private val userRepository = UserRepositoryImpl(
-        CoroutineScope(SupervisorJob() + Dispatchers.Default),
-        StandardTestDispatcher()
-    )
-    private val viewModel = SignInViewModel(
-        googleSignInUseCase = GoogleSignInUseCase(userRepository),
-        signInUseCase = SignInUseCase(userRepository),
-        savedStateHandle = SavedStateHandle()
-    )
+    private val screen1Route = "screen 1"
+    private val screen2Route = "screen 2"
+    private var isLoading by mutableStateOf(false)
 
     @Before
     fun init() {
         rule.setContent {
             ClothingHelperTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "resetPassword") {
-                    composable("resetPassword") {
-                        ResetPasswordScreen(popBackStack = { })
+                NavHost(navController = navController, startDestination = screen1Route) {
+                    composable(screen1Route) {
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            Text(text = screen1Route)
+                        }
                     }
-                    composable("signIn") {
-                        SignInScreen(
-                            navigateToResetPassword = { },
-                            navigateToSignUp = { },
-                            viewModel = viewModel
-                        )
+                    composable(screen2Route) {
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            Text(text = screen2Route)
+
+                            CenterDotProgressIndicator(show = { isLoading })
+                        }
                     }
                 }
-                navController.navigate("signIn")
+                navController.navigate(screen2Route)
             }
         }
     }
 
     @Test
     fun blockBackPressTest() {
-        rule.waitTagExist(SignInScreenTag)
+        rule.waitTextExist(screen2Route)
 
-        viewModel.setLoading(true)
+        isLoading = true
 
         rule.waitTagExist(CenterDotProgressIndicatorTag)
         repeat(5) { device.pressBack() }
 
-        viewModel.setLoading(false)
+        isLoading = false
+
         rule.waitTagNotExist(CenterDotProgressIndicatorTag)
         device.pressBack()
-        rule.waitTagExist(ResetPasswordScreenTag)
+        rule.waitTextExist(screen1Route)
     }
 }

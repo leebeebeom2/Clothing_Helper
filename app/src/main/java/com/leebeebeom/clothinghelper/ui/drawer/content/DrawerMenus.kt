b539db10bd5b81b2080e6fc6367a45694b9f7492
leebeebeom <boole92@naver.com>
2023-04-15 03:45:28 +0900

    testTag 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/DrawerMenus.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/DrawerMenus.kt
@@ -7,13 +7,17 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+
+const val DrawerTag = "drawer"
 
 @Composable // skippable
 fun DrawerMenus(content: @Composable () -> Unit) {
     val scrollState = rememberScrollState()
     Column(
         modifier = Modifier
+            .testTag(DrawerTag)
             .verticalScroll(scrollState)
             .fillMaxSize()
             .padding(bottom = 40.dp)

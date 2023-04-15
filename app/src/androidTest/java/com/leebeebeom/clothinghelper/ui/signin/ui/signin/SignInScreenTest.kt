7f7523eb8d39aeccf4c84d50b80985250583dd1f
leebeebeom <boole92@naver.com>
2023-04-16 01:30:47 +0900

    테스트 로직 변경

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreenTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/ui/signin/SignInScreenTest.kt
@@ -8,17 +8,21 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.leebeebeom.clothinghelper.*
+import com.leebeebeom.clothinghelper.R.string.*
 import com.leebeebeom.clothinghelper.data.InvalidEmail
 import com.leebeebeom.clothinghelper.data.NotFoundEmail
 import com.leebeebeom.clothinghelper.data.SignInEmail
 import com.leebeebeom.clothinghelper.data.SignInPassword
+import com.leebeebeom.clothinghelper.getInvisibleText
+import com.leebeebeom.clothinghelper.onNodeWithStringRes
 import com.leebeebeom.clothinghelper.ui.HiltTestActivity
 import com.leebeebeom.clothinghelper.ui.MainNavHost
 import com.leebeebeom.clothinghelper.ui.component.CenterDotProgressIndicatorTag
 import com.leebeebeom.clothinghelper.ui.main.essentialmenu.main.MainScreenTag
 import com.leebeebeom.clothinghelper.ui.signin.ui.resetpassword.ResetPasswordScreenTag
 import com.leebeebeom.clothinghelper.ui.signin.ui.signup.SignUpScreenTag
+import com.leebeebeom.clothinghelper.waitStringResExist
+import com.leebeebeom.clothinghelper.waitTagExist
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.After
@@ -31,9 +35,9 @@ class SignInScreenTest {
     val rule = createAndroidComposeRule<HiltTestActivity>()
     private val restorationTester = StateRestorationTester(rule)
     private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-    private val emailTextField by lazy { rule.onNodeWithStringRes(R.string.email) }
-    private val passwordTextField by lazy { rule.onNodeWithStringRes(R.string.password) }
-    private val signInButton by lazy { rule.onNodeWithStringRes(R.string.sign_in) }
+    private val emailTextField = rule.onNodeWithStringRes(email)
+    private val passwordTextField = rule.onNodeWithStringRes(password)
+    private val signInButton = rule.onNodeWithStringRes(sign_in)
 
     @Before
     fun init() {
@@ -75,15 +79,15 @@ class SignInScreenTest {
         signInButton.assertIsNotEnabled()
 
         localSignInRestoreTest(
-            email = InvalidEmail, password = SignInPassword, error = R.string.error_invalid_email
+            email = InvalidEmail, password = SignInPassword, error = error_invalid_email
         )
 
         localSignInRestoreTest(
-            email = NotFoundEmail, password = SignInPassword, error = R.string.error_user_not_found
+            email = NotFoundEmail, password = SignInPassword, error = error_user_not_found
         )
 
         localSignInRestoreTest(
-            email = SignInEmail, password = "123456", error = R.string.error_wrong_password
+            email = SignInEmail, password = "123456", error = error_wrong_password
         )
 
         emailTextField.performTextInput(SignInEmail)
@@ -100,10 +104,10 @@ class SignInScreenTest {
 
     @Test
     fun navigateTest() {
-        rule.onNodeWithStringRes(R.string.forgot_password).performClick()
+        rule.onNodeWithStringRes(forgot_password).performClick()
         rule.onNodeWithTag(ResetPasswordScreenTag).assertExists()
         repeat(2) { device.pressBack() }
-        rule.onNodeWithStringRes(R.string.sign_up_with_email).performClick()
+        rule.onNodeWithStringRes(sign_up_with_email).performClick()
         rule.onNodeWithTag(SignUpScreenTag).assertExists()
         repeat(2) { device.pressBack() }
         rule.onNodeWithTag(SignInScreenTag).assertExists()
@@ -136,7 +140,7 @@ class SignInScreenTest {
 
         signInButton.performClick()
 
-        rule.waitStringResExist(R.string.error_invalid_email)
+        rule.waitStringResExist(error_invalid_email)
         signInButton.assertIsNotEnabled()
 
         emailTextField.performTextInput("0")

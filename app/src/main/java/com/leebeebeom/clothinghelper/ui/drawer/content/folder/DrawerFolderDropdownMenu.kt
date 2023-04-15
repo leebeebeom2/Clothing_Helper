e88c4addb4c57de91b683cf3554adab11cbbf80d
leebeebeom <boole92@naver.com>
2023-04-15 23:56:23 +0900

    skippable 체크

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/folder/DrawerFolderDropdownMenu.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/folder/DrawerFolderDropdownMenu.kt
@@ -14,7 +14,7 @@ import com.leebeebeom.clothinghelper.ui.util.EditFolder
 import kotlinx.collections.immutable.ImmutableSet
 
 @Composable
-fun DrawerDownMenuEditAndAddFolder(
+fun DrawerDownMenuEditAndAddFolder( // skippable
     state: DrawerDropdownMenuState,
     onDismiss: () -> Unit,
     expand: () -> Unit,

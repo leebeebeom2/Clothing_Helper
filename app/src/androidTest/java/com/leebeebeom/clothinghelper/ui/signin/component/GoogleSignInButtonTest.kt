919ad34caea7eb38828d212f8541ed8fe20b699a
leebeebeom <boole92@naver.com>
2023-04-16 01:26:56 +0900

    테스트 로직 변경

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/component/GoogleSignInButtonTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/signin/component/GoogleSignInButtonTest.kt
@@ -9,9 +9,9 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.performClick
 import com.google.firebase.auth.FirebaseAuth
 import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.onNodeWithStringRes
 import com.leebeebeom.clothinghelper.ui.HiltTestActivity
-import com.leebeebeom.clothinghelper.ui.onNodeWithStringRes
-import com.leebeebeom.clothinghelper.ui.waitStringResExist
+import com.leebeebeom.clothinghelper.waitStringResExist
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test

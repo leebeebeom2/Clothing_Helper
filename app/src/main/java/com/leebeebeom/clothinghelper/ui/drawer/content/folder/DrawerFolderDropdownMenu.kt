db8688560f1f9b59ea110979a357ca945f85a7df
leebeebeom <boole92@naver.com>
2023-04-17 03:10:14 +0900

    deleteFolder 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/folder/DrawerFolderDropdownMenu.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/folder/DrawerFolderDropdownMenu.kt
@@ -1,10 +1,16 @@
 package com.leebeebeom.clothinghelper.ui.drawer.content.folder
 
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.domain.model.Folder
 import com.leebeebeom.clothinghelper.ui.component.dialog.AddFolderDialog
+import com.leebeebeom.clothinghelper.ui.component.dialog.DeleteFolderDialog
 import com.leebeebeom.clothinghelper.ui.component.dialog.EditFolderDialog
 import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenu
 import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenuItem
@@ -22,10 +28,12 @@ fun DrawerDownMenuEditAndAddFolder( // skippable
     childFolderNames: () -> ImmutableSet<String>,
     addFolder: AddFolder,
     editFolder: EditFolder,
+    deleteFolder: (Folder) -> Unit,
     selectedFolder: () -> Folder
 ) {
     var showEditDialog by rememberSaveable { mutableStateOf(false) }
     var showAddDialog by rememberSaveable { mutableStateOf(false) }
+    var showDeleteDialog by rememberSaveable { mutableStateOf(false) }
 
     val localSelectedFolder by remember(selectedFolder) { derivedStateOf(selectedFolder) }
 
@@ -37,6 +45,11 @@ fun DrawerDownMenuEditAndAddFolder( // skippable
 
         DrawerDropdownMenuItem(text = R.string.dropdown_menu_add_folder) {
             showAddDialog = true
+            onDismiss()
+        }
+
+        DrawerDropdownMenuItem(text = R.string.dropdown_menu_delete) {
+            showDeleteDialog = true
             onDismiss()
         }
     }
@@ -55,4 +68,13 @@ fun DrawerDownMenuEditAndAddFolder( // skippable
             addFolder(localSelectedFolder.key, name, localSelectedFolder.menuType)
             expand()
         }, show = { showAddDialog }, onDismiss = { showAddDialog = false })
+
+    DeleteFolderDialog(
+        onDismiss = { showDeleteDialog = false },
+        toBeDeletedFolders = listOf(selectedFolder()),
+        show = { showDeleteDialog },
+        onPositiveButtonClick = {
+            deleteFolder(selectedFolder())
+        }
+    )
 }

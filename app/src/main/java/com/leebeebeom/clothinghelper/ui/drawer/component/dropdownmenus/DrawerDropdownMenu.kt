4bd6701343853b48ab4900fc6f21dcc8a779da3b
leebeebeom <boole92@naver.com>
2023-04-16 23:40:00 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/dropdownmenus/DrawerDropdownMenu.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/dropdownmenus/DrawerDropdownMenu.kt
@@ -3,11 +3,14 @@ package com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.ui.theme.BlackA3
@@ -16,9 +19,9 @@ import com.leebeebeom.clothinghelper.ui.theme.BlackA3
 fun DrawerDropdownMenu( // skippable
     state: DrawerDropdownMenuState,
     onDismiss: () -> Unit,
-    density: Density = LocalDensity.current,
     dropdownMenuItems: @Composable () -> Unit
 ) {
+    val density = LocalDensity.current
     val offset by remember {
         derivedStateOf {
             with(density) {

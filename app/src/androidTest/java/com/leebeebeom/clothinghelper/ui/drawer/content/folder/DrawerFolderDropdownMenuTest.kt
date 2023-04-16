6daf2e4a0d225671124250fe1d5b0a516fc39939
leebeebeom <boole92@naver.com>
2023-04-16 23:54:06 +0900

    addFolderExpandTest 추가

--- app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/drawer/content/folder/DrawerFolderDropdownMenuTest.kt
+++ app/src/androidTest/java/com/leebeebeom/clothinghelper/ui/drawer/content/folder/DrawerFolderDropdownMenuTest.kt
@@ -4,16 +4,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import com.leebeebeom.clothinghelper.R
+import com.leebeebeom.clothinghelper.R.string.add_folder
+import com.leebeebeom.clothinghelper.R.string.dropdown_menu_add_folder
+import com.leebeebeom.clothinghelper.R.string.dropdown_menu_edit
+import com.leebeebeom.clothinghelper.R.string.edit_folder
 import com.leebeebeom.clothinghelper.domain.model.Folder
+import com.leebeebeom.clothinghelper.onNodeWithStringRes
 import com.leebeebeom.clothinghelper.ui.HiltTestActivity
 import com.leebeebeom.clothinghelper.ui.drawer.DrawerItemDropdownMenuState
 import com.leebeebeom.clothinghelper.ui.drawer.rememberDrawerItemDropdownMenuState
-import com.leebeebeom.clothinghelper.ui.onNodeWithStringRes
 import com.leebeebeom.clothinghelper.ui.theme.ClothingHelperTheme
 import kotlinx.collections.immutable.toImmutableSet
 import org.junit.Before
@@ -27,9 +33,16 @@ class DrawerFolderDropdownMenuTest {
     private lateinit var state: DrawerItemDropdownMenuState
     private val folderNames = List(5) { "$it" }.toImmutableSet()
     private val childFolderNames = List(5) { "${it + 5}" }.toImmutableSet()
-    private var addFolder = ""
-    private var editFolder = ""
-    private val selectedFolder = Folder(name = "이름")
+    private val initialName = "초기 폴더명"
+    private val selectedFolder = Folder(name = initialName)
+
+    private val dropdownEdit = rule.onNodeWithStringRes(dropdown_menu_edit)
+    private val dropdownAddFolder = rule.onNodeWithStringRes(dropdown_menu_add_folder)
+    private val editTitle = rule.onNodeWithStringRes(edit_folder)
+    private val addTitle = rule.onNodeWithStringRes(add_folder)
+    private val textField = rule.onNodeWithStringRes(R.string.folder)
+    private val checkButton = rule.onNodeWithStringRes(R.string.check)
+    private val cancelButton = rule.onNodeWithStringRes(R.string.cancel)
 
     @Before
     fun init() {
@@ -40,15 +53,11 @@ class DrawerFolderDropdownMenuTest {
 
                     DrawerDownMenuEditAndAddFolder(state = state,
                         onDismiss = state::onDismissDropDownMenu,
-                        expand = { state.showDropdownMenu },
+                        expand = state::expand,
                         folderNames = { folderNames },
                         childFolderNames = { childFolderNames },
-                        addFolder = { _, _, _ ->
-                            addFolder = "clicked"
-                        },
-                        editFolder = { _, _ ->
-                            editFolder = "clicked"
-                        },
+                        addFolder = { _, _, _ -> },
+                        editFolder = { _, _ -> },
                         selectedFolder = { selectedFolder })
                 }
             }
@@ -60,30 +69,43 @@ class DrawerFolderDropdownMenuTest {
         state.onLongClick(Offset.Zero)
 
         repeat(2) {
-            rule.onNodeWithStringRes(R.string.dropdown_menu_edit).assertExists()
-            rule.onNodeWithStringRes(R.string.dropdown_menu_add_folder).assertExists()
+            dropdownEdit.assertExists()
+            dropdownAddFolder.assertExists()
 
             restorationTester.emulateSavedInstanceStateRestore()
         }
 
-        rule.onNodeWithStringRes(R.string.dropdown_menu_edit).performClick()
+        dropdownEdit.performClick()
 
         repeat(2) {
-            rule.onNodeWithStringRes(R.string.edit_folder).assertExists()
-            rule.onNodeWithText("이름").assertExists()
+            editTitle.assertExists()
+            textField.assert(hasText(initialName))
 
             restorationTester.emulateSavedInstanceStateRestore()
         }
 
-        rule.onNodeWithStringRes(R.string.cancel).performClick()
+        cancelButton.performClick()
 
         state.onLongClick(Offset.Zero)
-        rule.onNodeWithStringRes(R.string.dropdown_menu_add_folder).performClick()
+        dropdownAddFolder.performClick()
 
         repeat(2) {
-            rule.onNodeWithStringRes(R.string.add_folder).assertExists()
+            addTitle.assertExists()
+            textField.assert(!hasText(initialName))
 
             restorationTester.emulateSavedInstanceStateRestore()
         }
+    }
+
+    @Test
+    fun addFolderExpandTest() {
+        state.onLongClick(Offset.Zero)
+        assert(!state.expanded)
+
+        dropdownAddFolder.performClick()
+        textField.performTextInput("이름")
+        checkButton.performClick()
+
+        assert(state.expanded)
     }
 }

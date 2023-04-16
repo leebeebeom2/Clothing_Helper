77c91383e9395c55f7b23e5641f0d429157be549
leebeebeom <boole92@naver.com>
2023-04-17 03:08:14 +0900

    패딩 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/component/dialog/component/CustomDialog.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/component/dialog/component/CustomDialog.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.leebeebeom.clothinghelper.ui.component.HeightSpacer
 import com.leebeebeom.clothinghelper.ui.component.SingleLineText
 import com.leebeebeom.clothinghelper.ui.theme.Black
 import com.leebeebeom.clothinghelper.ui.theme.WhiteSmoke
@@ -29,7 +30,7 @@ fun CustomDialog(
                 style = MaterialTheme.typography.h6,
                 modifier = Modifier.padding(start = 4.dp, bottom = 12.dp)
             )
-
+            HeightSpacer(dp = 12)
             content()
 
             DialogTextButtons(

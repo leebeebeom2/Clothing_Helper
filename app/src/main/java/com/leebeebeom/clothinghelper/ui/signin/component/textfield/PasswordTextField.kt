fec1cb63fe09b9663b3934b767e986eb03a3910e
leebeebeom <boole92@naver.com>
2023-04-16 23:23:59 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/component/textfield/PasswordTextField.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/component/textfield/PasswordTextField.kt
@@ -3,8 +3,13 @@ package com.leebeebeom.clothinghelper.ui.signin.component.textfield
 import androidx.annotation.StringRes
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.LocalContentColor
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.testTag
@@ -22,8 +27,8 @@ fun PasswordTextField(
     error: () -> Int?,
     imeAction: ImeAction,
     onInputChange: (String) -> Unit,
-    focusRequester: FocusRequester = remember { FocusRequester() }
 ) {
+    val focusRequester = remember { FocusRequester() }
     var isVisible by rememberSaveable { mutableStateOf(false) }
 
     StatefulMaxWidthTextField(

701fe1c14455a33ad667358ac32998ee1a277100
leebeebeom <boole92@naver.com>
2023-04-16 23:18:25 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/component/SignInBaseColumn.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/component/SignInBaseColumn.kt
@@ -1,13 +1,16 @@
 package com.leebeebeom.clothinghelper.ui.signin.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.ui.util.noRippleClickable
@@ -15,9 +18,10 @@ import com.leebeebeom.clothinghelper.ui.util.noRippleClickable
 @Composable // skippable
 fun SignInBaseColumn(
     modifier: Modifier = Modifier,
-    focusManager: FocusManager = LocalFocusManager.current,
     content: @Composable ColumnScope.() -> Unit,
 ) {
+    val focusManager = LocalFocusManager.current
+
     Column(
         modifier = modifier
             .fillMaxSize()

9132b5fdc0b861b771529d50bd84f3a9af4c57a9
leebeebeom <boole92@naver.com>
2023-04-17 03:16:29 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/DeleteFolderUseCase.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/domain/usecase/folder/DeleteFolderUseCase.kt
@@ -5,7 +5,7 @@ import com.leebeebeom.clothinghelper.domain.repository.FolderRepository
 import javax.inject.Inject
 
 class DeleteFolderUseCase @Inject constructor(private val folderRepository: FolderRepository) {
-    suspend fun deletedFolder(folder: Folder) {
+    suspend fun delete(folder: Folder) {
         val deletedFolder = folder.copy(isDeleted = true)
 
         folderRepository.push(deletedFolder)

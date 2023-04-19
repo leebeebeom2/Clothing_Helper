1917a44a58f2301d1d4a09b654958fc3ec7e395b
leebeebeom <boole92@naver.com>
2023-04-19 23:41:38 +0900

    FoldersState, FolderNamesState, FoldersSizeState 생성

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/state/States.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/state/States.kt
@@ -1,6 +1,11 @@
 package com.leebeebeom.clothinghelper.ui.state
 
+import com.leebeebeom.clothinghelper.domain.model.Folder
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.persistentSetOf
 
 interface ToastUiState {
     val toastTexts: ImmutableList<Int>
@@ -8,4 +13,24 @@ interface ToastUiState {
 
 interface LoadingUiState {
     val isLoading: Boolean
+}
+
+interface FoldersState {
+    val foldersMap: ImmutableMap<String, ImmutableList<Folder>>
+
+    fun getFolders(key: String) =
+        foldersMap.getOrDefault(key = key, defaultValue = persistentListOf())
+}
+
+interface FolderNamesState {
+    val folderNamesMap: ImmutableMap<String, ImmutableSet<String>>
+
+    fun getFolderNames(key: String) =
+        folderNamesMap.getOrDefault(key = key, defaultValue = persistentSetOf())
+}
+
+interface FoldersSizeState {
+    val foldersSizeMap: ImmutableMap<String, Int>
+
+    fun getFoldersSize(key: String) = foldersSizeMap.getOrDefault(key = key, defaultValue = 0)
 }

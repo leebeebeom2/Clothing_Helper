fcc94072a810b7bf4ff9d241f21b10dd36138b59
leebeebeom <boole92@naver.com>
2023-04-23 02:11:14 +0900

    AnimatedIconWrapper 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/component/Components.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/component/Components.kt
@@ -1,6 +1,10 @@
 package com.leebeebeom.clothinghelper.ui.component
 
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -13,6 +17,27 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalAnimationGraphicsApi::class)
+@Composable
+fun AnimatedIconWrapper( // skippable
+    modifier: Modifier = Modifier,
+    @DrawableRes drawable: Int,
+    atEnd: () -> Boolean,
+    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+) {
+    val painter = rememberAnimatedVectorPainter(
+        AnimatedImageVector.animatedVectorResource(id = drawable),
+        atEnd = atEnd()
+    )
+
+    Icon(
+        modifier = modifier,
+        painter = painter,
+        tint = tint,
+        contentDescription = null
+    )
+}
+
 @Composable // skippable
 fun IconWrapper(
     modifier: Modifier = Modifier,
@@ -22,8 +47,8 @@ fun IconWrapper(
     Icon(
         modifier = modifier,
         painter = painterResource(id = drawable),
-        contentDescription = null,
-        tint = tint
+        tint = tint,
+        contentDescription = null
     )
 }
 

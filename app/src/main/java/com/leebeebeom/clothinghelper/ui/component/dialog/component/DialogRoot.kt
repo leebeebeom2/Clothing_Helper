21412635c5098181ad2258d48269a15475de525d
leebeebeom <boole92@naver.com>
2023-04-23 04:27:09 +0900

    clearFocus 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/component/dialog/component/DialogRoot.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/component/dialog/component/DialogRoot.kt
@@ -8,13 +8,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import com.leebeebeom.clothinghelper.ui.util.clearFocus
 
 @Composable // skippable
 fun DialogRoot(onDismiss: () -> Unit, content: @Composable () -> Unit) {
     Dialog(onDismissRequest = onDismiss) {
         Surface(shape = MaterialTheme.shapes.medium) {
             Column(
-                modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 12.dp)
+                modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 12.dp)
+                    .clearFocus()
             ) {
                 content()
             }

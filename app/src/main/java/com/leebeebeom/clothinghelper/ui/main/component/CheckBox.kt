f002cf78a1276f8dcec4a5ca24b1e4c43dda5e44
leebeebeom <boole92@naver.com>
2023-04-23 02:11:31 +0900

    AnimatedIconWrapper 추가

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/main/component/CheckBox.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/main/component/CheckBox.kt
@@ -1,58 +1,30 @@
 package com.leebeebeom.clothinghelper.ui.main.component
 
-import androidx.annotation.DrawableRes
-import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
-import androidx.compose.animation.graphics.res.animatedVectorResource
-import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
-import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import com.leebeebeom.clothinghelper.R
-import com.leebeebeom.clothinghelper.ui.component.CustomIconButton
-import com.leebeebeom.clothinghelper.ui.component.IconWrapper
+import com.leebeebeom.clothinghelper.ui.component.AnimatedIconWrapper
 import com.leebeebeom.clothinghelper.ui.util.noRippleClickable
 
 @Composable
-fun CircleCheckBox(
-    modifier: Modifier = Modifier, isChecked: () -> Boolean, onClick: () -> Unit, size: Dp
+fun SquareCheckBox( // skippable
+    modifier: Modifier = Modifier,
+    isChecked: () -> Boolean,
+    onClick: () -> Unit,
+    size: Dp
 ) {
-    val painter = rememberCheckBoxPainter(res = R.drawable.check_anim_circle, isChecked = isChecked)
-
-    CustomIconButton(
-        modifier = modifier,
-        onClick = onClick,
-        painter = painter,
-        tint = checkBoxIconTint(),
-        size = size
-    )
-}
-
-@Composable
-fun SquareCheckBox(
-    modifier: Modifier = Modifier, isChecked: () -> Boolean, onClick: () -> Unit, size: Dp
-) {
-    val painter =
-        rememberCheckBoxPainter(res = R.drawable.check_anime_square, isChecked = isChecked)
-    IconWrapper(
+    AnimatedIconWrapper(
         modifier =
         modifier
             .size(size)
-            .noRippleClickable { onClick() }, painter = painter,
+            .noRippleClickable { onClick() }, drawable = R.drawable.check_anime_square,
+        atEnd = isChecked,
         tint = checkBoxIconTint()
     )
 }
-
-@OptIn(ExperimentalAnimationGraphicsApi::class)
-@Composable
-fun rememberCheckBoxPainter(@DrawableRes res: Int, isChecked: () -> Boolean) =
-    rememberAnimatedVectorPainter(
-        animatedImageVector = AnimatedImageVector.animatedVectorResource(
-            id = res
-        ), atEnd = isChecked()
-    )
 
 @Composable
 fun checkBoxIconTint() = LocalContentColor.current.copy(0.7f)

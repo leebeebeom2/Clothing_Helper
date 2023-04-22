c9ca34e76dc12d1426517dfa74e72b03d99fb24e
leebeebeom <boole92@naver.com>
2023-04-23 02:12:27 +0900

    painter 인자 삭제

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/component/CustomIconButton.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/component/CustomIconButton.kt
@@ -4,12 +4,15 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 
@@ -23,31 +26,10 @@ fun CustomIconButton(
     @StringRes text: Int? = null,
     contentDescription: String? = null
 ) {
-    CustomIconButton(
-        modifier = modifier,
-        onClick = onClick,
-        painter = painterResource(id = drawable),
-        tint = tint,
-        size = size,
-        text = text,
-        contentDescription = contentDescription
-    )
-}
-
-@Composable // skippable
-fun CustomIconButton(
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit,
-    painter: Painter,
-    size: Dp = Dp.Unspecified,
-    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current),
-    @StringRes text: Int? = null,
-    contentDescription: String? = null
-) {
     IconButton(modifier = modifier, onClick = onClick) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Icon(
-                painter = painter,
+                painter = painterResource(id = drawable),
                 contentDescription = contentDescription,
                 modifier = Modifier.size(size),
                 tint = tint

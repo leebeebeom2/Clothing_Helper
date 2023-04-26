282624fc2b3850ca123ebbf38f1292c8d79c7cee
leebeebeom <boole92@naver.com>
2023-04-27 02:37:33 +0900

    구조 변경

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/DrawerMenus.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/DrawerMenus.kt
@@ -9,19 +9,80 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+import com.leebeebeom.clothinghelper.ui.drawer.content.mainmenu.DrawerMainMenus
+import com.leebeebeom.clothinghelper.ui.util.AddFolder
+import com.leebeebeom.clothinghelper.ui.util.CurrentBackStack
+import com.leebeebeom.clothinghelper.ui.util.DeleteFolder
+import com.leebeebeom.clothinghelper.ui.util.EditFolder
+import com.leebeebeom.clothinghelper.ui.util.FolderNames
+import com.leebeebeom.clothinghelper.ui.util.Folders
+import com.leebeebeom.clothinghelper.ui.util.FoldersSize
+import com.leebeebeom.clothinghelper.ui.util.ItemsSize
+import com.leebeebeom.clothinghelper.ui.util.OnClothesCategoryClick
+import com.leebeebeom.clothinghelper.ui.util.OnEssentialMenuClick
+import com.leebeebeom.clothinghelper.ui.util.OnFolderClick
+import com.leebeebeom.clothinghelper.ui.util.OnMainMenuClick
+import com.leebeebeom.clothinghelper.ui.util.OnSubMenuClick
 
 const val DrawerTag = "drawer"
 
 @Composable // skippable
-fun DrawerMenus(content: @Composable () -> Unit) {
-    val scrollState = rememberScrollState()
+fun DrawerMenus(
+    closeDrawer: () -> Unit,
+    onEssentialMenuClick: OnEssentialMenuClick,
+    onMainMenuClick: OnMainMenuClick,
+    onSubMenuClick: OnSubMenuClick,
+    onClothesCategoryClick: OnClothesCategoryClick,
+    onFolderClick: OnFolderClick,
+    folders: Folders,
+    folderNames: FolderNames,
+    foldersSize: FoldersSize,
+    itemsSize: ItemsSize,
+    addFolder: AddFolder,
+    editFolder: EditFolder,
+    deleteFolder: DeleteFolder,
+    currentBackStack: CurrentBackStack
+) {
     Column(
         modifier = Modifier
             .testTag(DrawerTag)
-            .verticalScroll(scrollState)
+            .verticalScroll(rememberScrollState())
             .fillMaxSize()
             .padding(bottom = 40.dp)
     ) {
-        content()
+        DrawerEssentialMenus(
+            onEssentialMenuClick = {
+                closeDrawer()
+                onEssentialMenuClick(it)
+            },
+            currentBackStack = currentBackStack
+        )
+
+        DrawerMainMenus(
+            onMainMenuClick = {
+                closeDrawer()
+                onMainMenuClick(it)
+            },
+            onSubMenuClick = {
+                closeDrawer()
+                onSubMenuClick(it)
+            },
+            onClothesCategoryClick = {
+                closeDrawer()
+                onClothesCategoryClick(it)
+            },
+            onFolderClick = {
+                closeDrawer()
+                onFolderClick(it)
+            },
+            folders = folders,
+            folderNames = folderNames,
+            foldersSize = foldersSize,
+            itemsSize = itemsSize,
+            addFolder = addFolder,
+            editFolder = editFolder,
+            deleteFolder = deleteFolder,
+            currentBackStack = currentBackStack
+        )
     }
 }

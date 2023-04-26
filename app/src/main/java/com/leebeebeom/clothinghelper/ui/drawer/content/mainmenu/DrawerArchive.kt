f867e988d51fedf7c26d5b700ad06055e87bee0d
leebeebeom <boole92@naver.com>
2023-04-27 02:40:02 +0900

    현재 위치 배경 컬러, 하위 메뉴 이동 시 드로우 오픈, 리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/mainmenu/DrawerArchive.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/mainmenu/DrawerArchive.kt
@@ -1,43 +1,89 @@
 package com.leebeebeom.clothinghelper.ui.drawer.content.mainmenu
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.leebeebeom.clothinghelper.R
 import com.leebeebeom.clothinghelper.domain.model.MenuType
-import com.leebeebeom.clothinghelper.ui.drawer.DrawerItemDropdownMenuState
+import com.leebeebeom.clothinghelper.ui.drawer.OpenWhenNavigateToChild
 import com.leebeebeom.clothinghelper.ui.drawer.component.DrawerContentWithDoubleCount
+import com.leebeebeom.clothinghelper.ui.drawer.component.dropdownmenus.DrawerDropdownMenuState
+import com.leebeebeom.clothinghelper.ui.drawer.rememberDrawerCurrentPositionBackgroundColorWithArg
+import com.leebeebeom.clothinghelper.ui.theme.Black11
 import com.leebeebeom.clothinghelper.ui.util.AddFolder
-import kotlinx.collections.immutable.ImmutableSet
+import com.leebeebeom.clothinghelper.ui.util.CurrentBackStack
+import com.leebeebeom.clothinghelper.ui.util.DeleteFolder
+import com.leebeebeom.clothinghelper.ui.util.EditFolder
+import com.leebeebeom.clothinghelper.ui.util.FolderNames
+import com.leebeebeom.clothinghelper.ui.util.Folders
+import com.leebeebeom.clothinghelper.ui.util.FoldersSize
+import com.leebeebeom.clothinghelper.ui.util.ItemsSize
+import com.leebeebeom.clothinghelper.ui.util.OnFolderClick
 
 @Composable
 fun DrawerArchive(
-    state: DrawerItemDropdownMenuState,
+    state: DrawerDropdownMenuState,
     onClick: (MainMenuType) -> Unit,
-    folderNames: (parentKey: String) -> ImmutableSet<String>,
-    foldersSize: (parentKey: String) -> Int,
-    itemsSize: (parentKey: String) -> Int,
+    onFolderClick: OnFolderClick,
+    folders: Folders,
+    folderNames: FolderNames,
+    foldersSize: FoldersSize,
+    itemsSize: ItemsSize,
     addFolder: AddFolder,
-    folders: @Composable () -> Unit,
-    onSizeChange: (height: Int) -> Unit
+    editFolder: EditFolder,
+    deleteFolder: DeleteFolder,
+    onSizeChanged: (height: Int) -> Unit,
+    currentBackStack: CurrentBackStack,
+    onLongClick: (Offset) -> Unit,
+    onChildSizeChange: (IntSize) -> Unit,
+    toggleExpand: () -> Unit,
+    onDismissDropdownMenu: () -> Unit,
+    expand: () -> Unit
 ) {
+    val backgroundColor by rememberDrawerCurrentPositionBackgroundColorWithArg(
+        currentBackStack = currentBackStack,
+        parentKey = MainMenuType.Archive.name
+    )
+
+    OpenWhenNavigateToChild(
+        currentBackStack = currentBackStack,
+        folders = folders,
+        expand = expand,
+        key = MainMenuType.Archive.name
+    )
+
     DrawerContentWithDoubleCount(
         modifier = Modifier
             .testTag(DrawerMainMenuTag)
-            .onSizeChanged { onSizeChange(it.height) },
-        key = MainMenuType.Archive.name,
+            .onSizeChanged { onSizeChanged(it.height) },
+        currentPositionBackgroundColor = { backgroundColor },
+        subFoldersParentKey = MainMenuType.Archive.name,
         menuType = MenuType.Archive,
         text = R.string.archive_cap,
         textStyle = drawerMainMenuTextStyle(),
         onClick = { onClick(MainMenuType.Archive) },
+        folders = folders,
         foldersSize = foldersSize,
         folderNames = folderNames,
         itemsSize = itemsSize,
         addFolder = addFolder,
-        folders = folders,
+        editFolder = editFolder,
+        deleteFolder = deleteFolder,
         state = state,
-        height = { 0.dp }
+        height = { 0.dp },
+        onLongClick = onLongClick,
+        onSizeChanged = onChildSizeChange,
+        toggleExpand = toggleExpand,
+        onDismissDropdownMenu = onDismissDropdownMenu,
+        expand = expand,
+        folderBaseBackground = Black11,
+        onFolderClick = onFolderClick,
+        currentBackStack = currentBackStack,
+        folderBasePadding = 0.dp,
     )
 }

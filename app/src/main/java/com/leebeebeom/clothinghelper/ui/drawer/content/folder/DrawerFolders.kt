5b0ccb5f8e7cf17edc09f74ebc218a09660e3f93
leebeebeom <boole92@naver.com>
2023-04-27 02:55:29 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/folder/DrawerFolders.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/folder/DrawerFolders.kt
@@ -10,27 +10,32 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
-import com.leebeebeom.clothinghelper.domain.model.Folder
 import com.leebeebeom.clothinghelper.ui.drawer.rememberDrawerItemDropdownMenuState
 import com.leebeebeom.clothinghelper.ui.util.AddFolder
+import com.leebeebeom.clothinghelper.ui.util.CurrentBackStack
+import com.leebeebeom.clothinghelper.ui.util.DeleteFolder
 import com.leebeebeom.clothinghelper.ui.util.EditFolder
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.ImmutableSet
+import com.leebeebeom.clothinghelper.ui.util.FolderNames
+import com.leebeebeom.clothinghelper.ui.util.Folders
+import com.leebeebeom.clothinghelper.ui.util.FoldersSize
+import com.leebeebeom.clothinghelper.ui.util.ItemsSize
+import com.leebeebeom.clothinghelper.ui.util.OnFolderClick
 
 @Composable
 fun DrawerFolders( // skippable
     parentKey: String,
     basePadding: Dp,
     backgroundColor: Color,
-    onFolderClick: (Folder) -> Unit,
-    folders: (parentKey: String) -> ImmutableList<Folder>,
-    folderNames: (parentKey: String) -> ImmutableSet<String>,
-    foldersSize: (parentKey: String) -> Int,
-    itemsSize: (parentKey: String) -> Int,
+    onFolderClick: OnFolderClick,
+    folders: Folders,
+    folderNames: FolderNames,
+    foldersSize: FoldersSize,
+    itemsSize: ItemsSize,
     addFolder: AddFolder,
     editFolder: EditFolder,
-    deleteFolder: (Folder) -> Unit,
-    height: () -> Dp
+    deleteFolder: DeleteFolder,
+    height: () -> Dp,
+    currentBackStack: CurrentBackStack
 ) {
     val localFolders by remember(folders) { derivedStateOf { folders(parentKey) } }
 
@@ -53,7 +58,13 @@ fun DrawerFolders( // skippable
                     editFolder = editFolder,
                     deleteFolder = deleteFolder,
                     state = state,
-                    height = height
+                    height = height,
+                    onLongClick = state::onLongClick,
+                    onSizeChanged = state::onSizeChanged,
+                    toggleExpand = state::toggleExpand,
+                    onDismissDropdownMenu = state::onDismissDropdownMenu,
+                    expand = state::expand,
+                    currentBackStack = currentBackStack
                 )
             }
         }

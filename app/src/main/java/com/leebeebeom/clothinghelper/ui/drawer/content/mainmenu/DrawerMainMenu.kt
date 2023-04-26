b57511a9d928f02030acea1d37667aa8df2e391c
leebeebeom <boole92@naver.com>
2023-04-27 02:39:46 +0900

    현재 위치 배경 컬러, 하위 메뉴 이동 시 드로우 오픈, 리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/mainmenu/DrawerMainMenu.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/content/mainmenu/DrawerMainMenu.kt
@@ -1,34 +1,119 @@
 package com.leebeebeom.clothinghelper.ui.drawer.content.mainmenu
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.Dp
 import com.leebeebeom.clothinghelper.ui.drawer.DrawerItemState
+import com.leebeebeom.clothinghelper.ui.drawer.OpenWhenNavigateToChild
+import com.leebeebeom.clothinghelper.ui.drawer.OpenWhenNavigateToChildNoArg
 import com.leebeebeom.clothinghelper.ui.drawer.component.DrawerExpandIcon
 import com.leebeebeom.clothinghelper.ui.drawer.component.DrawerItemsWrapperWithExpandAnimation
 import com.leebeebeom.clothinghelper.ui.drawer.component.DrawerRow
 import com.leebeebeom.clothinghelper.ui.drawer.component.DrawerText
+import com.leebeebeom.clothinghelper.ui.drawer.content.submenu.SubMenuType
+import com.leebeebeom.clothinghelper.ui.drawer.content.submenu.SubMenus
+import com.leebeebeom.clothinghelper.ui.drawer.rememberDrawerCurrentPositionBackgroundColor
+import com.leebeebeom.clothinghelper.ui.main.mainmenu.brand.BrandGraphRoute
+import com.leebeebeom.clothinghelper.ui.main.mainmenu.clothes.ClothesGraphRoute
+import com.leebeebeom.clothinghelper.ui.main.mainmenu.clothes.closet.ClosetGraphRoute
+import com.leebeebeom.clothinghelper.ui.main.mainmenu.clothes.wish.WishGraphRoute
+import com.leebeebeom.clothinghelper.ui.main.mainmenu.outfit.OutfitGraphRoute
+import com.leebeebeom.clothinghelper.ui.util.AddFolder
+import com.leebeebeom.clothinghelper.ui.util.CurrentBackStack
+import com.leebeebeom.clothinghelper.ui.util.DeleteFolder
+import com.leebeebeom.clothinghelper.ui.util.EditFolder
+import com.leebeebeom.clothinghelper.ui.util.FolderNames
+import com.leebeebeom.clothinghelper.ui.util.Folders
+import com.leebeebeom.clothinghelper.ui.util.FoldersSize
+import com.leebeebeom.clothinghelper.ui.util.ItemsSize
+import com.leebeebeom.clothinghelper.ui.util.OnClothesCategoryClick
+import com.leebeebeom.clothinghelper.ui.util.OnFolderClick
+import com.leebeebeom.clothinghelper.ui.util.OnMainMenuClick
+import com.leebeebeom.clothinghelper.ui.util.OnSubMenuClick
 
 const val DrawerMainMenuTag = "drawer main menu"
 
 @Composable
 fun DrawerMainMenu(
     mainMenu: MainMenu,
-    onMainMenuClick: (MainMenuType) -> Unit,
+    onMainMenuClick: OnMainMenuClick,
+    onSubMenuClick: OnSubMenuClick,
+    onClothesCategoryClick: OnClothesCategoryClick,
+    onFolderClick: OnFolderClick,
+    folders: Folders,
+    folderNames: FolderNames,
+    foldersSize: FoldersSize,
+    itemsSize: ItemsSize,
+    addFolder: AddFolder,
+    editFolder: EditFolder,
+    deleteFolder: DeleteFolder,
     state: DrawerItemState,
     height: () -> Dp,
-    subMenus: @Composable (MainMenuType) -> Unit,
+    currentBackStack: CurrentBackStack,
+    toggleExpand: () -> Unit,
+    expand: () -> Unit,
 ) {
+    val route = when (mainMenu.type) {
+        MainMenuType.Brand -> BrandGraphRoute.BrandMainScreen
+        MainMenuType.Clothes -> ClothesGraphRoute.ClothesScreen
+        MainMenuType.Outfit -> OutfitGraphRoute.OutfitScreen
+        else -> throw IllegalStateException()
+    }
+
+    val currentPositionBackgroundColor by rememberDrawerCurrentPositionBackgroundColor(
+        currentBackStack = currentBackStack,
+        route = route
+    )
+
+    val childRoutes = when (mainMenu.type) {
+        MainMenuType.Brand -> listOf(SubMenuType.Brand.name, SubMenuType.Shop.name)
+        MainMenuType.Clothes -> listOf(ClosetGraphRoute.ClosetScreen, WishGraphRoute.WishScreen)
+        MainMenuType.Outfit -> listOf(SubMenuType.Ootd.name, SubMenuType.Reference.name)
+        else -> throw IllegalStateException()
+    }
+
+    childRoutes.forEach {
+        if (mainMenu.type != MainMenuType.Clothes)
+            OpenWhenNavigateToChild(
+                currentBackStack = currentBackStack,
+                childParentKey = it,
+                expand = expand
+            )
+        else OpenWhenNavigateToChildNoArg(
+            currentBackStack = currentBackStack,
+            childRoute = it,
+            expand = expand
+        )
+    }
+
     DrawerRow(
         modifier = Modifier.testTag(DrawerMainMenuTag),
+        currentPositionBackgroundColor = { currentPositionBackgroundColor },
         onClick = { onMainMenuClick(mainMenu.type) },
         height = height
     ) {
         DrawerText(text = mainMenu.name, style = drawerMainMenuTextStyle())
-        DrawerExpandIcon(expanded = { state.expanded }, toggleExpand = state::toggleExpand)
+        DrawerExpandIcon(expanded = { state.expanded }, toggleExpand = toggleExpand)
     }
 
     DrawerItemsWrapperWithExpandAnimation(expand = { state.expanded },
-        item = { subMenus(mainMenu.type) })
+        item = {
+            SubMenus(
+                mainMenuType = mainMenu.type,
+                onSubMenuClick = onSubMenuClick,
+                onClothesCategoryClick = onClothesCategoryClick,
+                foldersSize = foldersSize,
+                folderNames = folderNames,
+                itemsSize = itemsSize,
+                addFolder = addFolder,
+                folders = folders,
+                height = height,
+                currentBackStack = currentBackStack,
+                onFolderClick = onFolderClick,
+                deleteFolder = deleteFolder,
+                editFolder = editFolder
+            )
+        })
 }

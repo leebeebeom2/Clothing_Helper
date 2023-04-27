02405e5b24817dc655a5a3546267af25806436b8
leebeebeom <boole92@naver.com>
2023-04-28 04:19:06 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/dropdownmenus/DrawerDropdownMenu.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/drawer/component/dropdownmenus/DrawerDropdownMenu.kt
@@ -13,10 +13,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import com.leebeebeom.clothinghelper.ui.drawer.DrawerItemState
 import com.leebeebeom.clothinghelper.ui.theme.BlackA3
 
 @Composable
-fun DrawerDropdownMenu( // skippable
+fun DrawerDropdownMenu(
     state: DrawerDropdownMenuState,
     onDismiss: () -> Unit,
     dropdownMenuItems: @Composable () -> Unit
@@ -51,7 +52,7 @@ fun DrawerDropdownMenu( // skippable
 }
 
 @Stable
-interface DrawerDropdownMenuState {
+interface DrawerDropdownMenuState : DrawerItemState {
     val showDropdownMenu: Boolean
     val longClickOffsetX: Float
     val longClickOffsetY: Float

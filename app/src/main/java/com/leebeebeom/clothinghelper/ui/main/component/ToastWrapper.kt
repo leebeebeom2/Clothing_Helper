b9cdde3a092a99b501e5b63f97d873ab56736fb2
leebeebeom <boole92@naver.com>
2023-04-28 02:05:31 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/main/component/ToastWrapper.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/main/component/ToastWrapper.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import kotlinx.collections.immutable.ImmutableList
 
-@Composable // skippable
+@Composable
 fun ToastWrapper(toastTexts: () -> ImmutableList<Int>, toastShown: () -> Unit) {
     toastTexts().firstOrNull()?.let {
         Toast.makeText(LocalContext.current, stringResource(id = it), Toast.LENGTH_SHORT).show()

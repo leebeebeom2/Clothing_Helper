991a1a4616ca240e454e61b11d70131ed73c3e27
leebeebeom <boole92@naver.com>
2023-04-28 05:36:53 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/state/PasswordState.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/signin/state/PasswordState.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-// stable
 abstract class PasswordState(
     initialEmail: String,
     initialEmailError: Int?,

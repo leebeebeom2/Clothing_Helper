d447396c4350356978d794294a66de5cdacbff9a
leebeebeom <boole92@naver.com>
2023-04-28 05:57:13 +0900

    리팩토링

--- app/src/main/java/com/leebeebeom/clothinghelper/ui/component/BackHandlers.kt
+++ app/src/main/java/com/leebeebeom/clothinghelper/ui/component/BackHandlers.kt
@@ -2,9 +2,6 @@ package com.leebeebeom.clothinghelper.ui.component
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import kotlinx.coroutines.launch
 
@@ -20,7 +17,5 @@ fun SelectModeBackHandler(
 
 @Composable
 fun BackHandlerWrapper(enabled: () -> Boolean, task: () -> Unit) {
-    val localEnabled by remember(enabled) { derivedStateOf(enabled) }
-
-    BackHandler(enabled = localEnabled, onBack = task)
+    BackHandler(enabled = enabled(), onBack = task)
 }
